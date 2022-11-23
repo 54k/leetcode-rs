8bc3037f5e4ec6e0c823b525ef2b5cdd84ebfa0f
Boris Korotaev <54k@bk.ru>
2022-11-23 15:39:15 +0800

    day 11

--- src/day_11.rs
+++ src/day_11.rs
@@ -1,7 +1,7 @@
 #[allow(dead_code)]
 pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
     use std::collections::HashSet;
-    let mut cache = HashSet::with_capacity(board.len() * board[0].len());
+    let mut cache = HashSet::with_capacity(board.len() * board.len());
     for r in 0..board.len() {
         for c in 0..board.len() {
             let ch = board[r][c];
@@ -17,9 +17,23 @@ pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
     true
 }
 
+#[allow(dead_code)]
+pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+    let len = matrix.len();
+    for i in 0..(len + 1) / 2 {
+        for j in 0..len / 2 {
+            let temp = matrix[len - 1 - j][i];
+            matrix[len - 1 - j][i] = matrix[len - 1 - i][len - j - 1];
+            matrix[len - 1 - i][len - j - 1] = matrix[j][len - 1 - i];
+            matrix[j][len - 1 - i] = matrix[i][j];
+            matrix[i][j] = temp;
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::day_11::is_valid_sudoku;
+    use crate::day_11::{is_valid_sudoku, rotate};
 
     #[test]
     fn test64() {
@@ -37,5 +51,20 @@ mod test {
                 vec!['.', '.', '.', '.', '8', '.', '.', '.', '9'],
             ])
         );
+    }
+
+    #[test]
+    fn test65() {
+        let mut matrix1 = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        rotate(&mut matrix1);
+        println!("{:?}", matrix1);
+        let mut matrix2 = vec![
+            vec![5, 1, 9, 11],
+            vec![2, 4, 8, 10],
+            vec![13, 3, 6, 7],
+            vec![15, 14, 12, 16],
+        ];
+        rotate(&mut matrix2);
+        println!("{:?}", matrix2);
     }
 }
