c338424f172e99189c6f30b68fc8fc43872a54ce
Boris Korotaev <54k@bk.ru>
2022-11-24 18:06:47 +0800

    day 12

--- src/day_12.rs
+++ src/day_12.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -110,6 +113,84 @@ pub fn exist(mut board: Vec<Vec<char>>, word: String) -> bool {
     false
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+// public TreeNode sortedListToBST(ListNode head) {
+//     if(null == head){
+//     return null;
+//     }
+//
+//     if(null == head.next){
+//         return new TreeNode(head.val);
+//     }
+//
+//     ListNode prev = null;
+//     ListNode slow = head;
+//     ListNode fast = head;
+//
+//     while(null != fast && null != fast.next){
+//         prev = slow;
+//         slow = slow.next;
+//         fast = fast.next.next;
+//     }
+//
+//     prev.next = null;
+//
+//     TreeNode root = new TreeNode(slow.val);
+//     root.left = sortedListToBST(head);
+//     root.right = sortedListToBST(slow.next);
+//
+//     return root;
+// }
+#[allow(dead_code)]
+pub fn sorted_list_to_bst(mut head: Option<Box<ListNode>>) -> Option<Rc<RefCell<TreeNode>>> {
+    fn inorder(len: i32, list: &mut Option<Box<ListNode>>) -> Option<Rc<RefCell<TreeNode>>> {
+        if len == 0 {
+            return None;
+        }
+
+        let left = inorder(len / 2, list);
+        if let Some(head) = list {
+            let mut node = TreeNode::new(head.val);
+
+            *list = head.next.take();
+            node.left = left;
+
+            let right = inorder(len - len / 2 - 1, list);
+
+            node.right = right;
+            Some(Rc::new(RefCell::new(node)))
+        } else {
+            None
+        }
+    }
+
+    let mut next = &head;
+    let mut len = 0;
+    while let Some(node) = next {
+        len += 1;
+        next = &node.next;
+    }
+
+    inorder(len, &mut head)
+}
+
 #[cfg(test)]
 mod test {
     use crate::day_12::*;
@@ -191,6 +272,36 @@ mod test {
                 ],
                 "SEE".to_owned()
             )
+        );
+    }
+
+    #[test]
+    fn test73() {
+        println!(
+            "{:?}",
+            sorted_list_to_bst(Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode { val: 3, next: None })),
+                })),
+            })))
+        );
+        println!(
+            "{:?}",
+            sorted_list_to_bst(Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 2,
+                    next: Some(Box::new(ListNode {
+                        val: 3,
+                        next: Some(Box::new(ListNode {
+                            val: 4,
+                            next: Some(Box::new(ListNode { val: 5, next: None }))
+                        })),
+                    })),
+                })),
+            })))
         );
     }
 }
