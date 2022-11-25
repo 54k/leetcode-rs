b4cc65488759a265c7b3c5d50e325b59912163f1
Boris Korotaev <54k@bk.ru>
2022-11-25 15:19:41 +0800

    day 13

---
+++ src/day_13.rs
@@ -0,0 +1,65 @@
+#[allow(dead_code)]
+pub fn sum_subarray_mins(arr: Vec<i32>) -> i32 {
+    use std::collections::VecDeque;
+    const MOD: i32 = 1000000007;
+
+    fn sum_subarray_iter(arr: &Vec<i32>) -> i32 {
+        let mut monotonic_stack = VecDeque::new();
+        let mut sum_of_minimums = 0;
+
+        for i in 0..=arr.len() {
+            while !monotonic_stack.is_empty() &&
+                (i as usize == arr.len() || arr[*monotonic_stack.back().unwrap() as usize] >= arr[i]) {
+                let mid = monotonic_stack.pop_back().unwrap();
+                let left_boundary = if monotonic_stack.is_empty() {
+                    -1i32
+                } else {
+                    *monotonic_stack.back().unwrap()
+                };
+                let right_boundary = i as i32;
+
+                let count = (mid - left_boundary) * (right_boundary - mid) % MOD;
+
+                sum_of_minimums += (count * arr[mid as usize]) % MOD;
+                sum_of_minimums %= MOD;
+            }
+            monotonic_stack.push_back(i as i32);
+        }
+
+        sum_of_minimums
+    }
+
+    fn sum_subarray_dp(arr: &Vec<i32>) -> i32 {
+        let mut dp = vec![0; arr.len()];
+        let mut stack = VecDeque::new();
+
+        for i in 0..arr.len() {
+            while !stack.is_empty() && arr[*stack.back().unwrap() as usize] >= arr[i] {
+                stack.pop_back();
+            }
+
+            if stack.is_empty() {
+                dp[i] = (i + 1) as i32 * arr[i];
+            } else {
+                let j = *stack.back().unwrap() as i32;
+                dp[i] = dp[j as usize] + (i as i32 - j) * arr[i];
+            }
+
+            stack.push_back(i as i32);
+        }
+
+        dp.iter().fold(0, |acc, v| (acc + v) % MOD) % MOD
+    }
+    println!("{}", sum_subarray_iter(&arr));
+    sum_subarray_dp(&arr)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::day_13::*;
+
+    #[test]
+    fn test75() {
+        println!("{:?}", sum_subarray_mins(vec![2, 2, 2]));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,4 @@ pub mod day_6;
 pub mod day_7;
 pub mod day_8;
 pub mod day_9;
+pub mod day_13;
