387074aa5f232ea39b0925e9660642a522289720
Boris Korotaev <54k@bk.ru>
2022-12-08 16:16:03 +0800

    day 26

--- src/day_26.rs
+++ src/day_26.rs
@@ -35,3 +35,60 @@ pub fn leaf_similar(
     dfs(root2, &mut leafs2);
     leafs1 == leafs2
 }
+
+#[allow(dead_code)]
+pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+    use std::collections::HashMap;
+
+    let inorder_map = inorder
+        .into_iter()
+        .enumerate()
+        .map(|x| (x.1, x.0 as i32))
+        .collect::<HashMap<i32, i32>>();
+
+    fn arr_to_tree(
+        preorder: &[i32],
+        i: &mut usize,
+        left: i32,
+        right: i32,
+        m: &HashMap<i32, i32>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if left > right {
+            return None;
+        }
+
+        let mut root = TreeNode {
+            val: preorder[*i],
+            left: None,
+            right: None,
+        };
+
+        *i += 1;
+
+        root.left = arr_to_tree(preorder, i, left, m.get(&root.val).unwrap() - 1, m);
+        root.right = arr_to_tree(preorder, i, m.get(&root.val).unwrap() + 1, right, m);
+
+        Some(Rc::new(RefCell::new(root)))
+    }
+
+    arr_to_tree(
+        &preorder[..],
+        &mut 0,
+        0,
+        preorder.len() as i32 - 1,
+        &inorder_map,
+    )
+}
+
+#[cfg(test)]
+mod test {
+    use crate::day_26::build_tree;
+
+    #[test]
+    fn test102() {
+        println!(
+            "{:?}",
+            build_tree(vec![3, 9, 20, 15, 7], vec![9, 3, 15, 20, 7])
+        );
+    }
+}
