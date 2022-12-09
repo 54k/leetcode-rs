3524b4d2e3efbf12443d58521a8794e7d56075b0
Boris Korotaev <54k@bk.ru>
2022-12-09 15:09:03 +0800

    day 27

--- src/day_27.rs
+++ src/day_27.rs
@@ -34,3 +34,38 @@ pub fn max_ancestor_diff(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     let x = x.borrow();
     dfs(root, x.val, x.val)
 }
+
+#[allow(dead_code)]
+pub fn count_distinct(nums: Vec<i32>, k: i32, p: i32) -> i32 {
+    use std::collections::HashSet;
+    let mut prefix = vec![0; nums.len() + 1];
+    for i in 1..=nums.len() {
+        if nums[i - 1] % p == 0 {
+            prefix[i] = prefix[i - 1] + 1;
+        } else {
+            prefix[i] = prefix[i - 1];
+        }
+    }
+
+    let mut set = HashSet::new();
+    for i in 0..nums.len() {
+        for j in i + 1..=nums.len() {
+            if prefix[j] - prefix[i] <= k {
+                set.insert(&nums[i..j]);
+            }
+        }
+    }
+
+    return set.len() as i32;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test104() {
+        println!("{}", count_distinct(vec![2, 3, 3, 2, 2], 2, 2));
+        println!("{}", count_distinct(vec![1, 2, 3, 4], 4, 1));
+        println!("{}", count_distinct(vec![6, 20, 5, 18], 3, 14));
+    }
+}
