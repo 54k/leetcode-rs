6e0034c1456a8598f623d88caf7c4fca51a1e7b9
Boris Korotaev <54k@bk.ru>
2022-12-30 11:26:09 +0800

    day 48

--- src/day_48.rs
+++ src/day_48.rs
@@ -1,35 +1,64 @@
 #[allow(dead_code)]
 pub fn all_paths_source_target(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    fn dfs(
-        e: i32,
-        adj: &Vec<Vec<i32>>,
-        res: &mut Vec<Vec<i32>>,
-        vis: &mut Vec<bool>,
-        path: &mut Vec<i32>,
-    ) {
-        if vis[e as usize] {
-            return;
+    fn dfs(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        fn dfs(
+            e: i32,
+            adj: &Vec<Vec<i32>>,
+            res: &mut Vec<Vec<i32>>,
+            vis: &mut Vec<bool>,
+            path: &mut Vec<i32>,
+        ) {
+            if vis[e as usize] {
+                return;
+            }
+            vis[e as usize] = true;
+            path.push(e);
+            for v in adj[e as usize].iter() {
+                dfs(*v, adj, res, vis, path);
+            }
+            if e as usize == adj.len() - 1 {
+                res.push(path.clone());
+            }
+            path.pop();
+            vis[e as usize] = false;
         }
 
-        vis[e as usize] = true;
-        path.push(e);
-        for v in adj[e as usize].iter() {
-            dfs(*v, adj, res, vis, path);
+        if graph.is_empty() {
+            return vec![];
         }
-        if e as usize == adj.len() - 1 {
-            res.push(path.clone());
-        }
-        path.pop();
-        vis[e as usize] = false;
+        let mut res = vec![];
+        let mut vis = vec![false; graph.len()];
+        dfs(0, &graph, &mut res, &mut vis, &mut vec![]);
+        res
     }
 
-    if graph.is_empty() {
-        return vec![];
+    fn bfs(graph: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        use std::collections::VecDeque;
+        if graph.is_empty() {
+            return vec![];
+        }
+        let mut res = vec![];
+        let mut vis = vec![false; graph.len()];
+        let mut q = VecDeque::new();
+        q.push_back((0, vec![]));
+        while !q.is_empty() {
+            let (e, mut path) = q.pop_front().unwrap();
+            vis[e as usize] = true;
+            path.push(e);
+            if e as usize == graph.len() - 1 {
+                res.push(path.clone());
+            }
+            for v in graph[e as usize].iter() {
+                if !vis[*v as usize] {
+                    q.push_back((*v, path.clone()));
+                }
+            }
+            vis[e as usize] = false;
+        }
+        res
     }
-    let mut res = vec![];
-    let mut vis = vec![false; graph.len()];
-    dfs(0, &graph, &mut res, &mut vis, &mut vec![]);
-    res
+
+    bfs(graph)
 }
 
 #[cfg(test)]
