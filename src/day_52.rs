05aba4b9e003a01894d6f27cb7bbc04354f40bf0
Boris Korotaev <54k@bk.ru>
2023-01-03 16:30:23 +0800

    day 52

--- src/day_52.rs
+++ src/day_52.rs
@@ -16,6 +16,41 @@ pub fn min_deletion_size(strs: Vec<String>) -> i32 {
     ans
 }
 
+// https://leetcode.com/problems/diagonal-traverse/solutions/459889/diagonal-traverse/?orderBy=most_relevant
+#[allow(dead_code)]
+pub fn find_diagonal_order(mat: Vec<Vec<i32>>) -> Vec<i32> {
+    let n = mat.len();
+    let m = mat[0].len();
+    let mut res = vec![];
+
+    // We have to go over all the elements in the first
+    // row and the last column to cover all possible diagonals
+    for d in 0..n + m - 1 {
+        let mut tmp = vec![];
+        // We need to figure out the "head" of this diagonal
+        // The elements in the first row and the last column
+        // are the respective heads.
+        let mut r = if d < m { 0 } else { d - m + 1 } as i32;
+        let mut c = if d < m { d } else { m - 1 } as i32;
+        // Iterate until one of the indices goes out of scope
+        // Take note of the index math to go down the diagonal
+        while r < n as i32 && c > -1 {
+            tmp.push(mat[r as usize][c as usize]);
+            r += 1;
+            c -= 1;
+        }
+        // Reverse even numbered diagonals. The
+        // article says we have to reverse odd
+        // numbered articles but here, the numbering
+        // is starting from 0 :P
+        if d % 2 == 0 {
+            tmp.reverse();
+        }
+        res.extend(tmp.into_iter());
+    }
+    res
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -38,5 +73,13 @@ mod test {
                 "tsr".to_string()
             ])
         ); // 3
+    }
+
+    #[test]
+    fn test143() {
+        println!(
+            "{:?}",
+            find_diagonal_order(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]])
+        ); // [1,2,4,7,5,3,6,8,9]
     }
 }
