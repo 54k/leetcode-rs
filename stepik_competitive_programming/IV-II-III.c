e4867063aa451cd6ffbd8b51262e6975d4dc29cf
Boris Korotaev <54k@bk.ru>
2023-01-05 15:08:47 +0800

    day 54

---
+++ stepik_competitive_programming/IV-II-III.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define N 501
+
+int adj[N][N];
+int seen[N];
+
+int n, m;
+
+void dfs(int v) {
+    seen[v] = 1;
+    for (int i = 1; i <= adj[v][0]; ++i) {
+        int u = adj[v][i];
+        if (!seen[u]) {
+            dfs(u);
+        }
+    }
+    m--;
+}
+
+int main() {
+    int u, v;
+    scanf("%d %d", &n, &m);
+    for (int i = 1; i <= m; ++i) {
+        scanf("%d %d", &u, &v);
+        adj[u][++adj[u][0]] = v;
+        adj[v][++adj[v][0]] = u;
+    }
+    dfs(1);
+    printf("%d\n", m + 1);
+    return 0;
+}
