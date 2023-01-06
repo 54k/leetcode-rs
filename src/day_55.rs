81eb772e42c53761d714b33ce3985c862d8e29e5
Boris Korotaev <54k@bk.ru>
2023-01-06 11:17:43 +0800

    day 55

--- src/day_55.rs
+++ src/day_55.rs
@@ -43,3 +43,21 @@ pub fn find_repeated_dna_sequences(s: String) -> Vec<String> {
     }
     repeated.into_iter().collect()
 }
+
+// https://leetcode.com/problems/maximum-length-of-repeated-subarray/solutions/127407/maximum-length-of-repeated-subarray/?orderBy=most_relevant
+#[allow(dead_code)]
+pub fn find_length(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    let mut ans = 0;
+    let mut dp = vec![vec![0; nums2.len() + 1]; nums1.len() + 1];
+    for i in (0..nums1.len()).rev() {
+        for j in (0..nums2.len()).rev() {
+            if nums1[i] == nums2[j] {
+                dp[i][j] = dp[i + 1][j + 1] + 1;
+                if ans < dp[i][j] {
+                    ans = dp[i][j];
+                }
+            }
+        }
+    }
+    ans
+}
