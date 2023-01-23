4366406fb958a16dc8c2be81b52391dee8df2236
Boris Korotaev <54k@bk.ru>
2023-01-23 11:03:36 +0800

    day 72

---
+++ src/day_72.rs
@@ -0,0 +1,23 @@
+pub fn find_judge(n: i32, trust: Vec<Vec<i32>>) -> i32 {
+    let mut in_degree = vec![0; n as usize + 1];
+    let mut out_degree = vec![0; n as usize + 1];
+    for t in trust {
+        in_degree[t[1] as usize] += 1;
+        out_degree[t[0] as usize] += 1;
+    }
+    for i in 1..=n as usize {
+        if in_degree[i] == n - 1 && out_degree[i] == 0 {
+            return i as i32;
+        }
+    }
+    -1
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test173() {
+        println!("{}", find_judge(3, vec![vec![1, 3], vec![2, 3]])); // 3
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,7 @@ pub mod day_69;
 pub mod day_7;
 pub mod day_70;
 pub mod day_71;
+pub mod day_72;
 pub mod day_8;
 pub mod day_9;
 pub mod design_twitter_day16;
