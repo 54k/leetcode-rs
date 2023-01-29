27d4a46a2ccca21e5224489c13fdefadc335413f
Boris Korotaev <54k@bk.ru>
2023-01-30 03:46:28 +0800

    day 78

--- src/stepik/competitive_programming.rs
+++ src/stepik/competitive_programming.rs
@@ -856,58 +856,6 @@ fn problem_salesman_bit() {
     solve_salesman_bit(n, distances);
 }
 
-fn solve_friends(n: usize, adj: Vec<Vec<bool>>) {
-    let mut ans = 0;
-    let mut ans_set = vec![];
-    'go: for mask in 1..=(1 << n) {
-        let mut s = vec![];
-        for i in 1..=n {
-            if 1 << i & mask > 0 {
-                s.push(i);
-            }
-        }
-        for i in 1..s.len() {
-            for j in 1..i {
-                if !adj[s[i]][s[j]] {
-                    continue 'go;
-                }
-            }
-        }
-        if ans < s.len() {
-            ans = s.len();
-            ans_set = s;
-        }
-    }
-    println!("{}", ans);
-    println!("{:?}", ans_set);
-}
-
-fn problem_friends() {
-    let mut lines = include_str!("friends.in").lines();
-    let mut v = lines
-        .next()
-        .unwrap()
-        .split_whitespace()
-        .map(|n| n.parse().unwrap())
-        .collect::<Vec<_>>();
-    let n = v[0];
-    let m = v[1];
-    let mut adj = vec![vec![false; n + 1]; n + 1];
-
-    for _ in 0..m {
-        let mut v = lines
-            .next()
-            .unwrap()
-            .split_whitespace()
-            .map(|n| n.parse::<i32>().unwrap())
-            .collect::<Vec<_>>();
-        adj[v[0] as usize][v[1] as usize] = true;
-        adj[v[1] as usize][v[0] as usize] = true;
-    }
-
-    solve_friends(n, adj);
-}
-
 fn solve_friends2(n: usize, adj: Vec<i32>) {
     let mut ans = 0;
     let mut ans_mask = 0;
