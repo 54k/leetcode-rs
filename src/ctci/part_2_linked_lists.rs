16a10793110a27d50770be86d914df5b1fc5e707
Boris Korotaev <54k@bk.ru>
2023-02-14 05:02:46 +0400

    day 94

--- src/ctci/part_2_linked_lists.rs
+++ src/ctci/part_2_linked_lists.rs
@@ -1,94 +1,187 @@
-// #[derive(Debug)]
-// struct ListNode {
-//     val: i32,
-//     next: *mut ListNode,
-// }
-//
-// // Напишите код для удаления дубликатов из несортированного связного списка.
-// // Дополнительно:
-// // Как вы будете решать задачу, если использовать временный буфер запрещено?
-// fn task_2_1(head: *mut ListNode) -> Box<ListNode> {
-//     unsafe fn with_memory(mut head: *mut ListNode) {
-//         use std::collections::*;
-//         let mut set = HashSet::new();
-//         let mut prev: *mut ListNode = std::ptr::null_mut();
-//         while !head.is_null() {
-//             if set.contains(&(*head).val) {
-//                 (*prev).next = (*head).next;
-//             } else {
-//                 set.insert((*head).val);
-//                 prev = head;
-//             }
-//             head = (*head).next;
-//         }
-//     }
-//     unsafe {
-//         with_memory(head);
-//         Box::from_raw(head)
-//     }
-// }
+// Lasciate ogne speranza, voi ch’entrate
+// Оставь надежду, всяк сюда входящий
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
     pub next: Option<Box<ListNode>>,
 }
-
 impl ListNode {
     #[inline]
     fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
 }
-type N = Option<Box<ListNode>>;
+type TNode = Option<Box<ListNode>>;
 
-fn task_2_11(mut n: N) -> N {
-    use std::collections::HashSet;
-    let mut set = HashSet::new();
-    let mut current = n.as_mut().unwrap();
+// Напишите код для удаления дубликатов из несортированного связного списка.
+// Дополнительно:
+// Как вы будете решать задачу, если использовать временный буфер запрещено?
+fn task_2_1(n: TNode) -> TNode {
+    fn linear(n: TNode) -> TNode {
+        use std::collections::HashSet;
+        let mut set = HashSet::new();
 
-    while let Some(i) = current.next.as_mut() {
-        if set.contains(&i.val) {
-            current.next = i.next.take();
-        } else {
-            set.insert(i.val);
+        let mut head = None;
+        let mut tail = &mut head;
+
+        let mut next = n;
+        while let Some(mut t) = next.take() {
+            next = t.next.take();
+            if !set.contains(&t.val) {
+                set.insert(t.val);
+                if tail.is_none() {
+                    head = Some(t);
+                    tail = &mut head;
+                } else {
+                    tail.as_mut().unwrap().next = Some(t);
+                    tail = &mut tail.as_mut().unwrap().next;
+                }
+            }
         }
-        current = current.next.as_mut().unwrap();
+
+        head
     }
-    n
+
+    fn quadratique(n: TNode) -> TNode {
+        let mut head = None;
+        let mut tail = &mut head;
+
+        let mut next = n;
+        while let Some(mut t) = next.take() {
+            let mut runner_head = None;
+            let mut runner_tail = &mut runner_head;
+
+            let mut runner_next = t.next.take();
+            while let Some(mut n) = runner_next.take() {
+                runner_next = n.next.take();
+                if n.val != t.val {
+                    if runner_tail.is_none() {
+                        runner_head = Some(n);
+                        runner_tail = &mut runner_head;
+                    } else {
+                        runner_tail.as_mut().unwrap().next = Some(n);
+                        runner_tail = &mut runner_tail.as_mut().unwrap().next;
+                    }
+                }
+            }
+
+            if tail.is_none() {
+                head = Some(t);
+                tail = &mut head;
+            } else {
+                tail.as_mut().unwrap().next = Some(t);
+                tail = &mut tail.as_mut().unwrap().next;
+            }
+
+            next = runner_head
+        }
+        head
+    }
+    quadratique(n)
+}
+
+// Реализуйте алгоритм для нахождения в односвязном списке k-го элемента с конца
+fn task_2_2(n: &TNode, k: i32) -> &TNode {
+    fn non_req(n: &TNode, k: i32) -> &TNode {
+        let mut p1 = n;
+        let mut p2 = n;
+        for _ in 0..k {
+            p1 = &p1.as_ref().unwrap().next;
+        }
+        while let Some(p) = p1 {
+            p1 = &p.next;
+            p2 = &p2.as_ref().unwrap().next;
+        }
+        p2
+    }
+    non_req(n, k)
+}
+
+// Реализуйте алгоритм, удаляющий узел из середины односвязного списка
+// (то есть узла, не являющегося ни начальным, ни конечным - не обязательно находящимся точно в середине).
+// Доступ предоставляется только к этому узлу.
+//
+// Пример:
+// Ввод: узел с из списка a->b->c->d->e->f
+// Вывод: ничего не возвращается, но новый список имеет вид a->b->c->d->e->f
+fn task_2_3(n: &mut TNode) {
+    if n.is_none() {
+        return;
+    }
+    let mut unwrapped = n;
+    while let Some(u) = unwrapped {
+        if let Some(mut n) = u.next.take() {
+            u.val = n.val;
+            u.next = n.next.take();
+        }
+        unwrapped = &mut u.next;
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    // #[test]
-    // fn test_2_1() {
-    //     println!(
-    //         "{:?}",
-    //         task_2_1(Box::into_raw(Box::new(ListNode {
-    //             val: 1,
-    //             next: Box::into_raw(Box::new(ListNode {
-    //                 val: 1,
-    //                 next: Box::into_raw(Box::new(ListNode {
-    //                     val: 2,
-    //                     next: std::ptr::null_mut(),
-    //                 })),
-    //             })),
-    //         })))
-    //     );
-    // }
-
     #[test]
-    fn test_2_11() {
+    fn test_2_1() {
         let head = Some(Box::new(ListNode {
             val: 1,
             next: Some(Box::new(ListNode {
                 val: 2,
-                next: Some(Box::new(ListNode { val: 1, next: None })),
+                next: Some(Box::new(ListNode {
+                    val: 1,
+                    next: Some(Box::new(ListNode {
+                        val: 2,
+                        next: Some(Box::new(ListNode {
+                            val: 3,
+                            next: Some(Box::new(ListNode { val: 3, next: None })),
+                        })),
+                    })),
+                })),
             })),
         }));
-        println!("{:?}", task_2_11(head.clone()));
+        println!("{:?}", task_2_1(head));
+    }
+
+    #[test]
+    fn test_2_2() {
+        let head = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode { val: 5, next: None })),
+                    })),
+                })),
+            })),
+        }));
+        println!("{:?}", task_2_2(&head, 3));
+    }
+
+    #[test]
+    fn test_2_3() {
+        let mut head = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode { val: 5, next: None })),
+                    })),
+                })),
+            })),
+        }));
+        let mut mid = &mut head;
+        for _ in 0..2 {
+            mid = &mut mid.as_mut().unwrap().next;
+        }
+        task_2_3(mid);
         println!("{:?}", head);
     }
 }
---
+++ src/day_94.rs
@@ -0,0 +1,51 @@
+pub fn add_binary(a: String, b: String) -> String {
+    let a = a.chars().rev().collect::<Vec<_>>();
+    let b = b.chars().rev().collect::<Vec<_>>();
+
+    let mut ans = String::new();
+    let mut carry = 0;
+    let mut i = 0;
+    let mut j = 0;
+
+    loop {
+        let a_i = if i < a.len() {
+            let val = a[i] as u32 - '0' as u32;
+            i += 1;
+            val
+        } else {
+            0
+        };
+
+        let b_j = if j < b.len() {
+            let val = b[j] as u32 - '0' as u32;
+            j += 1;
+            val
+        } else {
+            0
+        };
+
+        let mut sum = a_i + b_j + carry;
+        carry = if sum >= 2 { 1 } else { 0 };
+        sum %= 2;
+        ans.push(char::from_digit(sum, 2).unwrap());
+
+        if i == a.len() && j == b.len() {
+            if carry > 0 {
+                ans.push(char::from_digit(carry, 2).unwrap());
+            }
+            break;
+        }
+    }
+    ans.chars().rev().collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test223() {
+        println!("{}", add_binary("11".to_string(), "1".to_string()));
+        println!("{}", add_binary("1010".to_string(), "1011".to_string()));
+    }
+}
