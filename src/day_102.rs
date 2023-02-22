522fd7c6f6ccf80c471060739daffab6d676869a
54k <54k@bk.ru>
2023-02-22 13:10:26 +0400

    day 102

--- src/day_102.rs
+++ src/day_102.rs
@@ -35,6 +35,38 @@ pub fn ship_within_days(weights: Vec<i32>, days: i32) -> i32 {
     lo
 }
 
+// https://leetcode.com/problems/koko-eating-bananas/
+pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
+    let max = piles.iter().max().unwrap();
+    fn can_eat(piles: &[i32], eat_per_hour: i32, hours: i32) -> bool {
+        let mut hours_taken = 0;
+        for &pile in piles.iter() {
+            if pile % eat_per_hour == 0 {
+                hours_taken += pile / eat_per_hour;
+            } else {
+                hours_taken += pile / eat_per_hour + 1;
+            }
+        }
+        hours_taken <= hours
+    }
+    let mut lo = 1;
+    let mut hi = *max;
+    while lo < hi {
+        let mid = lo + (hi - lo) / 2;
+        if can_eat(&piles, mid, h) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    lo
+}
+
+// https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
+pub fn min_days(bloom_day: Vec<i32>, m: i32, k: i32) -> i32 {
+    0
+}
+
 // https://leetcode.com/problems/count-integers-in-intervals/solutions/2039706/merge-intervals/
 // https://leetcode.com/problems/count-integers-in-intervals/description/
 struct CountIntervals {}
@@ -64,5 +96,31 @@ mod tests {
 
         println!("{}", ship_within_days(vec![3, 2, 2, 4, 1, 4], 3)); // 6
         println!("{}", ship_within_days(vec![1, 2, 3, 1, 1], 4)); // 3
+    }
+
+    #[test]
+    fn test274() {
+        println!("{}", min_eating_speed(vec![3, 6, 7, 11], 8)); // 4
+        println!("{}", min_eating_speed(vec![30, 11, 23, 4, 20], 5)); // 30
+        println!("{}", min_eating_speed(vec![30, 11, 23, 4, 20], 6)); // 23
+        println!("{}", min_eating_speed(vec![312884470], 312884469)); // 2
+        println!(
+            "{}",
+            min_eating_speed(
+                vec![
+                    332484035, 524908576, 855865114, 632922376, 222257295, 690155293, 112677673,
+                    679580077, 337406589, 290818316, 877337160, 901728858, 679284947, 688210097,
+                    692137887, 718203285, 629455728, 941802184,
+                ],
+                823855818,
+            )
+        ); // 112677673
+    }
+
+    #[test]
+    fn test275() {
+        println!("{}", min_days(vec![1, 10, 3, 10, 2], 3, 1)); // 3
+        println!("{}", min_days(vec![1, 10, 3, 10, 2], 3, 2)); // -1
+        println!("{}", min_days(vec![7, 7, 7, 7, 12, 7, 7], 2, 3)); // 12
     }
 }
