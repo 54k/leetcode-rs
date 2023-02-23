71bce84ae79f2bb7404885d93172a6c3fc58fbe3
54k <54k@bk.ru>
2023-02-24 02:48:20 +0400

    day 103

--- src/ctci/part_3_stacks_and_queues.rs
+++ src/ctci/part_3_stacks_and_queues.rs
@@ -1,51 +1,94 @@
 // Опишите как можно использовать один одномерный массив для реализации трех стеков
-fn task_2_3() {
-    trait MultiStack {
-        fn push(&mut self, stack_num: i32, val: i32);
-        fn pop(&mut self, stack_num: i32) -> i32;
-        fn peek(&self, stack_num: i32) -> i32;
-        fn is_empty(&self, stack_num: i32) -> bool;
-        fn is_full(&self, stack_num: i32) -> bool;
-    }
+pub trait MultiStack {
+    fn push(&mut self, stack_num: usize, val: i32);
+    fn pop(&mut self, stack_num: usize) -> i32;
+    fn peek(&self, stack_num: usize) -> i32;
+    fn is_empty(&self, stack_num: usize) -> bool;
+    fn is_full(&self, stack_num: usize) -> bool;
+}
 
-    const NUM_OF_STACKS: usize = 3;
+#[derive(Debug)]
+pub struct FixedMultiStack {
+    stack_size: usize,
+    num_of_stacks: usize,
+    values: Vec<i32>,
+    sizes: Vec<i32>,
+}
 
-    #[derive(Debug)]
-    struct FixedMultiStack {
-        values: Vec<i32>,
-        stack_size: usize,
-        sizes: Vec<usize>,
-    }
-
-    impl FixedMultiStack {
-        fn new(stack_size: usize) -> Self {
-            Self {
-                values: vec![0; stack_size * NUM_OF_STACKS],
-                sizes: vec![0; NUM_OF_STACKS],
-                stack_size,
-            }
+impl FixedMultiStack {
+    pub fn new(stack_size: usize, num_of_stacks: usize) -> Self {
+        Self {
+            values: vec![0; stack_size * num_of_stacks],
+            sizes: vec![0; num_of_stacks],
+            stack_size,
+            num_of_stacks,
         }
     }
+}
 
-    impl MultiStack for FixedMultiStack {
-        fn push(&mut self, stack_num: i32, val: i32) {
-            todo!()
-        }
+impl FixedMultiStack {
+    fn index_of_top(&self, stack_num: usize) -> i32 {
+        let offset = self.stack_size * stack_num;
+        self.sizes[stack_num] - 1 + offset as i32
+    }
+}
 
-        fn pop(&mut self, stack_num: i32) -> i32 {
-            todo!()
-        }
+impl MultiStack for FixedMultiStack {
+    fn push(&mut self, stack_num: usize, val: i32) {
+        let i = self.index_of_top(stack_num);
+        self.values[(i + 1) as usize] = val;
+        self.sizes[stack_num] += 1;
+    }
 
-        fn peek(&self, stack_num: i32) -> i32 {
-            todo!()
-        }
+    fn pop(&mut self, stack_num: usize) -> i32 {
+        let i = self.index_of_top(stack_num);
+        self.sizes[stack_num] -= 1;
+        self.values[i as usize]
+    }
 
-        fn is_empty(&self, stack_num: i32) -> bool {
-            todo!()
-        }
+    fn peek(&self, stack_num: usize) -> i32 {
+        self.values[self.index_of_top(stack_num) as usize]
+    }
 
-        fn is_full(&self, stack_num: i32) -> bool {
-            todo!()
-        }
+    fn is_empty(&self, stack_num: usize) -> bool {
+        self.sizes[stack_num] == 0
+    }
+
+    fn is_full(&self, stack_num: usize) -> bool {
+        self.sizes[stack_num] as usize == self.stack_size
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_fixed_multistack() {
+        let mut stack = FixedMultiStack::new(2, 3);
+        stack.push(0, 1);
+        stack.push(1, 1);
+        stack.push(2, 1);
+
+        stack.push(0, 2);
+        stack.push(1, 2);
+        stack.push(2, 2);
+
+        assert!(stack.is_full(0));
+        assert!(stack.is_full(1));
+        assert!(stack.is_full(2));
+
+        assert_eq!(stack.pop(0), 2);
+        assert_eq!(stack.pop(0), 1);
+
+        assert_eq!(stack.pop(1), 2);
+        assert_eq!(stack.pop(1), 1);
+
+        assert_eq!(stack.pop(2), 2);
+        assert_eq!(stack.pop(2), 1);
+
+        assert!(stack.is_empty(0));
+        assert!(stack.is_empty(1));
+        assert!(stack.is_empty(2));
     }
 }
