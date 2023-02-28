616175a75040e16e4e8ef3f570be6de6a723c5bf
54k <54k@bk.ru>
2023-02-28 05:19:38 +0400

    day 108

--- src/day_108.rs
+++ src/day_108.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 // https://leetcode.com/problems/create-maximum-number/description/
 // https://leetcode.com/problems/create-maximum-number/solutions/77286/short-python-ruby-c/
 // https://leetcode.com/problems/create-maximum-number/solutions/77283/share-my-21ms-java-solution-with-comments/?orderBy=most_relevant
@@ -44,6 +47,43 @@ pub fn maximum_swap(num: i32) -> i32 {
     0
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+// https://leetcode.com/problems/find-duplicate-subtrees/
+pub fn find_duplicate_subtrees(
+    root: Option<Rc<RefCell<TreeNode>>>,
+) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+    use std::collections::HashMap;
+    let mut map = HashMap::new();
+    type Node = Option<Rc<RefCell<TreeNode>>>;
+    fn dfs(root: Option<Rc<RefCell<TreeNode>>>, map: &mut HashMap<String, (Node, i32)>) -> String {
+        if let Some(node) = root.clone() {
+            let node = node.borrow();
+            let left = dfs(node.left.clone(), map);
+            let right = dfs(node.right.clone(), map);
+            let key = format!("({}){}({})", left, node.val, right);
+            map.entry(key.clone()).or_insert((root, 0)).1 += 1;
+            key
+        } else {
+            "".to_string()
+        }
+    }
+    dfs(root, &mut map);
+    map.into_iter()
+        .filter_map(|(_, (node, cnt))| if cnt > 1 { Some(node) } else { None })
+        .collect()
+}
+
+// https://leetcode.com/problems/construct-string-from-binary-tree/description/
+pub fn tree2str(root: Option<Rc<RefCell<TreeNode>>>) -> String {
+    "".to_string()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -61,4 +101,33 @@ mod test {
         println!("{}", maximum_swap(2736)); // 7236
         println!("{}", maximum_swap(9973)); // 9973
     }
+
+    #[test]
+    fn test307() {
+        let root = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None,
+                }))),
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 3,
+                    left: None,
+                    right: None,
+                }))),
+                right: None,
+            }))),
+        })));
+        println!("{:?}", find_duplicate_subtrees(root));
+    }
+
+    #[test]
+    fn test308() {}
 }
