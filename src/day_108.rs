a377c50ab22474ba98cd9bc6dd513b726ab3a1c8
Boris Korotaev <54k@bk.ru>
2023-02-28 14:36:24 +0400

    day 108

--- src/day_108.rs
+++ src/day_108.rs
@@ -15,8 +15,9 @@ pub fn max_number(mut nums1: Vec<i32>, mut nums2: Vec<i32>, k: i32) -> Vec<i32> 
             }
             stack.push(*n);
         }
-        nums[0..k].iter().copied().take(k).collect()
+        stack[0..k].iter().copied().take(k).collect()
     }
+
     fn merge(a: Vec<i32>, b: Vec<i32>) -> Vec<i32> {
         use std::collections::VecDeque;
         let mut a = VecDeque::from(a);
@@ -28,13 +29,13 @@ pub fn max_number(mut nums1: Vec<i32>, mut nums2: Vec<i32>, k: i32) -> Vec<i32> 
         }
         merged
     }
+
     let k = k as usize;
     let mut ans = vec![];
     let mut k1 = (k as i32 - nums2.len() as i32).max(0) as usize;
     while k1 <= k.min(nums1.len()) {
         let p1 = prepare(&mut nums1, k1);
         let p2 = prepare(&mut nums2, k - k1);
-        println!("{:?} \n{:?} \n ***", p1, p2);
         let merged = merge(p1, p2);
         ans = ans.max(merged);
         k1 += 1;
@@ -43,8 +44,24 @@ pub fn max_number(mut nums1: Vec<i32>, mut nums2: Vec<i32>, k: i32) -> Vec<i32> 
 }
 
 // https://leetcode.com/problems/maximum-swap/description/
+// https://leetcode.com/problems/maximum-swap/solutions/107068/java-simple-solution-o-n-time/
 pub fn maximum_swap(num: i32) -> i32 {
-    0
+    let mut num = num.to_string().chars().collect::<Vec<_>>();
+    let mut bucket = vec![0; 10];
+    for i in 0..num.len() {
+        bucket[num[i] as usize - '0' as usize] = i;
+    }
+
+    'outer: for i in 0..num.len() {
+        for j in ((num[i] as usize - '0' as usize) + 1..=9).rev() {
+            if bucket[j] > i {
+                num.swap(i, bucket[j]);
+                break 'outer;
+            }
+        }
+    }
+
+    num.into_iter().collect::<String>().parse::<i32>().unwrap()
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -79,9 +96,38 @@ pub fn find_duplicate_subtrees(
         .collect()
 }
 
+// https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/description/
+pub fn find_second_minimum_value(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    type Node = Option<Rc<RefCell<TreeNode>>>;
+    fn inorder(root: Node, nodes: &mut Vec<i32>) {
+        if let Some(n) = root {
+            let n = n.borrow();
+            nodes.push(n.val);
+            inorder(n.left.clone(), nodes);
+            inorder(n.right.clone(), nodes);
+        }
+    }
+    let mut nodes = vec![];
+    inorder(root, &mut nodes);
+    nodes.sort();
+    for i in 1..nodes.len() {
+        if nodes[i] > nodes[i - 1] {
+            return nodes[i];
+        }
+    }
+    -1
+}
+
 // https://leetcode.com/problems/construct-string-from-binary-tree/description/
+// https://leetcode.com/problems/construct-string-from-binary-tree/editorial/
 pub fn tree2str(root: Option<Rc<RefCell<TreeNode>>>) -> String {
     "".to_string()
+}
+
+// https://leetcode.com/problems/coin-change/description/
+// https://leetcode.com/problems/coin-change/editorial/
+pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+    todo!()
 }
 
 #[cfg(test)]
@@ -100,6 +146,7 @@ mod test {
     fn test306() {
         println!("{}", maximum_swap(2736)); // 7236
         println!("{}", maximum_swap(9973)); // 9973
+        println!("{}", maximum_swap(98368)); // 98863
     }
 
     #[test]
@@ -129,5 +176,34 @@ mod test {
     }
 
     #[test]
-    fn test308() {}
+    fn test308() {
+        let root = Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5,
+                    left: None,
+                    right: None,
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 7,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
+        })));
+        println!("{:?}", find_second_minimum_value(root));
+    }
+
+    #[test]
+    fn test309() {}
+
+    #[test]
+    fn test310() {}
 }
