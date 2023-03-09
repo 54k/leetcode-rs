be515dc2f111ccecb2ac2173eee50c9e30d25b16
Boris Korotaev <54k@bk.ru>
2023-03-09 10:40:50 +0400

    day 117

---
+++ src/day_117.rs
@@ -0,0 +1,14 @@
+// todo https://leetcode.com/problems/find-and-replace-in-string/
+// todo https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/description/
+// todo https://leetcode.com/problems/split-with-minimum-sum/description/
+// todo https://leetcode.com/problems/task-scheduler/description/
+// todo https://leetcode.com/problems/strong-password-checker/description/
+// todo https://leetcode.com/problems/replace-non-coprime-numbers-in-array/description/
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test337() {}
+}
