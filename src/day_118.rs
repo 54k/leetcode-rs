c6760e555cd7f3361fdb1cfa7dffe1e8dd317a58
Boris Korotaev <54k@bk.ru>
2023-03-10 12:34:48 +0400

    day 118

---
+++ src/day_118.rs
@@ -0,0 +1,168 @@
+// todo https://leetcode.com/problems/task-scheduler-ii/
+// todo https://leetcode.com/problems/strong-password-checker/description/
+// todo https://leetcode.com/problems/replace-non-coprime-numbers-in-array/description/
+
+// https://leetcode.com/problems/linked-list-random-node/description/
+// https://leetcode.com/problems/linked-list-random-node/editorial/
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+struct Solution {
+    head: Option<Box<ListNode>>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl Solution {
+    fn new(head: Option<Box<ListNode>>) -> Self {
+        Self { head }
+    }
+
+    fn get_random(&self) -> i32 {
+        use rand::{thread_rng, Rng};
+        let mut scope = 1.0;
+        let mut chosen_value = 0;
+        let mut head = self.head.as_ref();
+        while let Some(h) = head {
+            let r = thread_rng().gen::<f64>();
+            if r < 1.0 / scope {
+                chosen_value = h.val;
+            }
+            scope += 1.0;
+            head = h.next.as_ref();
+        }
+        chosen_value
+    }
+}
+
+// https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/description/
+pub fn find_k_distant_indices(nums: Vec<i32>, key: i32, k: i32) -> Vec<i32> {
+    use std::collections::*;
+    let mut set = HashSet::new();
+    let mut key_idx = vec![];
+    for i in 0..nums.len() {
+        if nums[i] == key {
+            key_idx.push(i as i32);
+        }
+    }
+    for i in 0..nums.len() {
+        for &j in &key_idx {
+            if (i as i32 - j).abs() <= k {
+                set.insert(i as i32);
+            }
+        }
+    }
+
+    let mut ans = set.into_iter().collect::<Vec<_>>();
+    ans.sort();
+    ans
+}
+
+// https://leetcode.com/problems/split-with-minimum-sum/description/
+pub fn split_num(num: i32) -> i32 {
+    // Assign digits to num1 and num2 alternatively.
+    let mut num = num.to_string().chars().collect::<Vec<_>>();
+    num.sort();
+    let mut n1 = vec![];
+    let mut n2 = vec![];
+    for (i, &char) in num.iter().enumerate() {
+        if i % 2 == 0 {
+            n1.push(char);
+        } else {
+            n2.push(char);
+        }
+    }
+    n1.into_iter().collect::<String>().parse::<i32>().unwrap()
+        + n2.into_iter().collect::<String>().parse::<i32>().unwrap()
+}
+
+// https://leetcode.com/problems/task-scheduler/description/
+// https://leetcode.com/problems/task-scheduler/solutions/104495/java-o-n-solution-beats-99-76-use-only-array-easy-understanding/
+pub fn least_interval(tasks: Vec<char>, n: i32) -> i32 {
+    // There is one crucial point for you:
+    // the ONLY thing you need to care is the max number of one task!
+    // We set apart each max task with interval n, and we hope to put all other tasks into those intervals. If the number of those tasks exceeds the interval space, then we don't need any idle interval at all. If not, the interval space plus the max tasks will be the least interval. Be care for the existent of multiple max tasks.
+    let mut storage = vec![0; 26];
+    for i in 0..tasks.len() {
+        storage[tasks[i] as usize - 'A' as usize] += 1;
+    }
+    let mut max = 0;
+    let mut count = 1;
+    for num in storage {
+        if num == 0 {
+            continue;
+        }
+        if max < num {
+            max = num;
+            count = 1;
+        } else if max == num {
+            count += 1;
+        }
+    }
+    let space = (n + 1) * (max - 1) + count;
+    if space < tasks.len() as i32 {
+        tasks.len() as i32
+    } else {
+        space
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test338() {
+        let mut sol = Solution::new(Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode { val: 3, next: None })),
+            })),
+        })));
+
+        println!("{}", sol.get_random());
+        println!("{}", sol.get_random());
+        println!("{}", sol.get_random());
+    }
+
+    #[test]
+    fn test339() {
+        println!(
+            "{:?}",
+            find_k_distant_indices(vec![3, 4, 9, 1, 3, 9, 5], 9, 1)
+        ); // [1,2,3,4,5,6]
+        println!("{:?}", find_k_distant_indices(vec![2, 2, 2, 2, 2], 2, 2)); // [0,1,2,3,4]
+    }
+
+    #[test]
+    fn test340() {
+        println!("{:?}", split_num(4325)); // 59
+        println!("{:?}", split_num(687)); // 75
+    }
+
+    #[test]
+    fn test341() {
+        println!(
+            "{:?}",
+            least_interval(vec!['A', 'A', 'A', 'B', 'B', 'B'], 2)
+        ); // 8
+        println!(
+            "{:?}",
+            least_interval(vec!['A', 'A', 'A', 'B', 'B', 'B'], 0)
+        ); // 6
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,7 @@ pub mod day_114;
 pub mod day_115;
 pub mod day_116;
 pub mod day_117;
+pub mod day_118;
 pub mod day_12;
 pub mod day_13;
 pub mod day_14;
