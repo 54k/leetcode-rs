6b7d6d1599d3e31d00541d38399afee104778145
54k <54k@bk.ru>
2023-03-18 18:30:59 +0400

    day 126

--- src/leetcode_grind/day_126.rs
+++ src/leetcode_grind/day_126.rs
@@ -76,6 +76,49 @@ mod two_stacks {
     }
 }
 
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+pub fn find_disappeared_numbers(mut nums: Vec<i32>) -> Vec<i32> {
+    fn negation_approach(mut nums: Vec<i32>) -> Vec<i32> {
+        // let mut nums = nums;
+        // let mut result = vec![];
+        // for i in 0..nums.len() {
+        //     let new_index = (nums[i].abs() - 1) as usize;
+        //     if 0 < nums[new_index] {
+        //         nums[new_index] = -nums[new_index];
+        //     }
+        // }
+        //
+        // for (i, val) in nums.iter().enumerate() {
+        //     if 0 < nums[i] {
+        //         result.push(i as i32 + 1);
+        //     }
+        // }
+        //
+        // result
+        todo!()
+    }
+    fn cyclic_sort_approach(mut nums: Vec<i32>) -> Vec<i32> {
+        let mut ans = vec![];
+        let mut i = 0;
+        while i < nums.len() {
+            let correct = nums[i] - 1;
+            if nums[i] != nums[correct as usize] {
+                nums.swap(correct as usize, i);
+            } else {
+                i += 1;
+            }
+        }
+
+        for i in 0..nums.len() {
+            if nums[i] as usize != i + 1 {
+                ans.push(i as i32 + 1);
+            }
+        }
+        ans
+    }
+    cyclic_sort_approach(nums)
+}
+
 // https://leetcode.com/problems/find-all-duplicates-in-an-array/
 pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {
     todo!()
@@ -89,5 +132,14 @@ mod test {
         println!("{:?}", find_duplicates(vec![4, 3, 2, 7, 8, 2, 3, 1])); // [2,3]
         println!("{:?}", find_duplicates(vec![1, 1, 2])); // [1]
         println!("{:?}", find_duplicates(vec![1])); // []
+    }
+
+    #[test]
+    fn test355() {
+        println!(
+            "{:?}",
+            find_disappeared_numbers(vec![4, 3, 2, 7, 8, 2, 3, 1])
+        ); // [5,6]
+        println!("{:?}", find_disappeared_numbers(vec![1, 1])); // [2]
     }
 }
