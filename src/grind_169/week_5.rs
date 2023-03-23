61948a86fb66ea6f4e85c588403209b6d4d62ea1
Boris Korotaev <54k@bk.ru>
2023-03-23 23:20:21 +0400

    day 131

--- src/grind_169/week_5.rs
+++ src/grind_169/week_5.rs
@@ -719,7 +719,32 @@ mod rpw {
 }
 
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
-pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {}
+pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
+    fn using_sorting(mut nums: Vec<i32>, k: i32) -> i32 {
+        nums.sort();
+        nums[nums.len() - k as usize]
+    }
+    fn using_count_sort(nums: Vec<i32>, mut k: i32) -> i32 {
+        const PAD: usize = 10000;
+        let mut arr = vec![0; 2 * PAD + 1];
+        for num in nums {
+            arr[num as usize + PAD] += 1;
+        }
+        let mut i = arr.len() - 1;
+        while k > 0 {
+            if arr[i] == 0 {
+                i -= 1;
+                continue;
+            } else {
+                let to_delete = arr[i].min(k);
+                arr[i] -= to_delete;
+                k -= to_delete;
+            }
+        }
+        i as i32 - PAD as i32
+    }
+    using_count_sort(nums, k)
+}
 
 #[cfg(test)]
 mod test {
