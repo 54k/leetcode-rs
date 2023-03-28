a7617d58e4d0af49d128fdb40b762df42ac8d619
54k <54k@bk.ru>
2023-03-28 20:22:49 +0400

    day 136

--- src/grind_169/week_6.rs
+++ src/grind_169/week_6.rs
@@ -584,6 +584,65 @@ pub fn three_sum_closest(mut nums: Vec<i32>, target: i32) -> i32 {
     ans
 }
 
+// https://leetcode.com/problems/rotate-list/description/
+pub fn rotate_right(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+    if head.is_none() || k == 0 {
+        return head;
+    }
+    fn reverse(mut head: Option<Box<ListNode>>) -> (Option<Box<ListNode>>, i32) {
+        let mut len = 0;
+        let mut prev = None;
+        while let Some(mut h) = head {
+            let next = h.next.take();
+            h.next = prev;
+            prev = Some(h);
+            head = next;
+            len += 1;
+        }
+        (prev, len)
+    }
+    fn split(
+        mut head: Option<Box<ListNode>>,
+        mut at: i32,
+    ) -> (Option<Box<ListNode>>, Option<Box<ListNode>>) {
+        let mut pre_tail = &mut head;
+        while at > 1 {
+            if let Some(pt) = pre_tail {
+                pre_tail = &mut pt.next;
+            }
+            at -= 1;
+        }
+        let tail = pre_tail.as_mut().unwrap().next.take();
+        (head, tail)
+    }
+
+    let (head, len) = reverse(head);
+
+    let at = k % len;
+    if at == 0 {
+        return reverse(head).0;
+    }
+
+    let (head, tail) = split(head, at);
+    let (mut head, _) = reverse(head);
+    let (tail, _) = reverse(tail);
+
+    let mut head_tail = &mut head;
+    while let Some(ht) = head_tail {
+        head_tail = &mut ht.next;
+    }
+    if let Some(tail) = tail {
+        let _ = head_tail.insert(tail);
+    }
+    head
+}
+
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/editorial/
+pub fn find_min(nums: Vec<i32>) -> i32 {
+    0
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -811,5 +870,35 @@ mod test {
     fn test_three_sum_closest() {
         println!("{}", three_sum_closest(vec![-1, 2, 1, -4], 1)); // 2
         println!("{}", three_sum_closest(vec![0, 0, 0], 1)); // 0
+    }
+
+    #[test]
+    fn tst_rotate_right() {
+        let head = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode {
+                    val: 3,
+                    next: Some(Box::new(ListNode {
+                        val: 4,
+                        next: Some(Box::new(ListNode { val: 5, next: None })),
+                    })),
+                })),
+            })),
+        }));
+        println!("{:?}", rotate_right(head, 2));
+        println!("{:?}", rotate_right(None, 0));
+
+        let head = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode { val: 2, next: None })),
+        }));
+        println!("{:?}", rotate_right(head, 2));
+    }
+
+    #[test]
+    fn test_find_min() {
+        println!("{}", find_min(vec![]));
     }
 }
