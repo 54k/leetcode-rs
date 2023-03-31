0a5b3f009316ab3b09500f88bd557f26036a3d9f
Boris Korotaev <54k@bk.ru>
2023-03-31 22:02:06 +0400

    day 139

--- src/leetcode_grind/day_139.rs
+++ src/leetcode_grind/day_139.rs
@@ -69,6 +69,23 @@ impl NumMatrix {
     }
 }
 
+// https://leetcode.com/problems/range-sum-query-immutable/
+struct NumArray {
+    prefix: Vec<i32>,
+}
+impl NumArray {
+    fn new(nums: Vec<i32>) -> Self {
+        let mut prefix = vec![0; nums.len() + 1];
+        for i in 0..nums.len() {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        Self { prefix }
+    }
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        self.prefix[right as usize + 1] - self.prefix[left as usize]
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
