2516122ca20f8b477d51093776fe97e61bb4e235
54k <54k@bk.ru>
2023-04-03 20:38:24 +0300

    day 142

--- src/leetcode_grind/day_142.rs
+++ src/leetcode_grind/day_142.rs
@@ -15,6 +15,23 @@ pub fn num_rescue_boats(mut people: Vec<i32>, limit: i32) -> i32 {
     ans
 }
 
+pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
+    let mut i = 0;
+    let mut j = nums.len() - 1;
+    let mut ans = vec![0; nums.len()];
+    for k in (0..nums.len()).rev() {
+        let square = if nums[i].abs() < nums[j].abs() {
+            j -= 1;
+            nums[j + 1]
+        } else {
+            i += 1;
+            nums[i - 1]
+        };
+        ans[k] = square * square;
+    }
+    ans
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -22,5 +39,10 @@ mod test {
     #[test]
     fn test391() {
         println!("{}", num_rescue_boats(vec![3, 5, 3, 4], 5)); // 4
+    }
+
+    #[test]
+    fn test392() {
+        println!("{:?}", sorted_squares(vec![-4, -1, 0, 3, 10]));
     }
 }
