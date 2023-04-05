2dd6912251b6a2e77b8b304119292aa24d9c5901
54k <54k@bk.ru>
2023-04-05 13:52:40 +0300

    day 144

--- src/leetcode_grind/day_144.rs
+++ src/leetcode_grind/day_144.rs
@@ -10,6 +10,29 @@ pub fn minimize_array_value(nums: Vec<i32>) -> i32 {
     ans
 }
 
+// https://leetcode.com/problems/first-letter-to-appear-twice/description/
+pub fn repeated_character(s: String) -> char {
+    use std::collections::HashSet;
+    let mut set = HashSet::new();
+    for ch in s.chars() {
+        if set.contains(&ch) {
+            return ch;
+        }
+        set.insert(ch);
+    }
+    unreachable!()
+}
+
+// https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+// https://leetcode.com/problems/check-if-the-sentence-is-pangram/editorial/
+pub fn check_if_pangram(sentence: String) -> bool {
+    let mut set = 0;
+    for ch in sentence.chars() {
+        set |= 1 << (ch as i32 - 'a' as i32);
+    }
+    set == (1 << 26) - 1
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -17,5 +40,18 @@ mod test {
     #[test]
     fn test400() {
         println!("{}", minimize_array_value(vec![3, 7, 1, 6])); // 5
+    }
+
+    #[test]
+    fn test401() {
+        println!("{}", repeated_character("abccbaacz".to_string())); // c
+    }
+
+    #[test]
+    fn test402() {
+        println!(
+            "{}",
+            check_if_pangram("thequickbrownfoxjumpsoverthelazydog".to_string())
+        ); // true
     }
 }
