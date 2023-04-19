1b342d1b861cc332249ad353d1838f5be3b4ccbf
54k <54k@bk.ru>
2023-04-19 15:31:33 +0300

    day 158

--- src/leetcode_grind/day_158.rs
+++ src/leetcode_grind/day_158.rs
@@ -26,3 +26,47 @@ pub fn longest_zig_zag(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     dfs(root, false, 0, &mut longest_path);
     longest_path
 }
+
+// https://leetcode.com/problems/reduce-array-size-to-the-half/description/
+pub fn min_set_size(arr: Vec<i32>) -> i32 {
+    use std::collections::HashMap;
+    let n = arr.len() as i32;
+    let mut map = HashMap::new();
+    for num in arr {
+        *map.entry(num).or_insert(0) += 1;
+    }
+    let mut ans = 0;
+    let mut freqs = map.values().copied().collect::<Vec<i32>>();
+    freqs.sort();
+    let mut removed = 0;
+    for i in (0..freqs.len()).rev() {
+        removed += freqs[i];
+        ans += 1;
+        if removed >= n / 2 {
+            break;
+        }
+    }
+    ans
+}
+
+// https://leetcode.com/problems/how-many-apples-can-you-put-into-the-basket/description/
+pub fn max_number_of_apples(weight: Vec<i32>) -> i32 {
+    let max = weight.iter().copied().max().unwrap();
+    let mut buckets = vec![0; max as usize + 1];
+    for w in weight {
+        buckets[w as usize] += 1;
+    }
+    let mut w = 0;
+    let mut count = 0;
+    for i in 0..max as usize + 1 {
+        if buckets[i] > 0 {
+            let c = buckets[i].min((5000 - w) / i);
+            if c == 0 {
+                break;
+            }
+            w += c * i;
+            count += c;
+        }
+    }
+    count as i32
+}
