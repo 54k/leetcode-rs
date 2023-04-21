38938cc18855749fdb9a3f47ef614f18d9dfb79e
54k <54k@bk.ru>
2023-04-21 14:53:59 +0300

    day 160

--- src/leetcode_grind/day_160.rs
+++ src/leetcode_grind/day_160.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 // https://leetcode.com/problems/number-of-flowers-in-full-bloom/
 pub fn full_bloom_flowers(mut flowers: Vec<Vec<i32>>, people: Vec<i32>) -> Vec<i32> {
     // fn find_in_full_bloom(flowers: &Vec<Vec<i32>>, time: i32) -> i32 {
@@ -80,6 +82,56 @@ pub fn profitable_schemes(n: i32, min_profit: i32, group: Vec<i32>, profits: Vec
     dp[0][0][0] as i32
 }
 
+// https://leetcode.com/problems/n-queens/
+pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
+    use std::collections::HashSet;
+    fn backtrack(
+        board: &mut Vec<Vec<String>>,
+        row: i32,
+        cols: &mut HashSet<i32>,
+        diag: &mut HashSet<i32>,
+        anti_diag: &mut HashSet<i32>,
+        ans: &mut Vec<Vec<String>>,
+    ) {
+        if row == board.len() as i32 {
+            let board = board
+                .clone()
+                .into_iter()
+                .map(|x| x.into_iter().collect::<String>())
+                .collect::<Vec<_>>();
+            ans.push(board);
+            return;
+        }
+        for col in 0..board[0].len() as i32 {
+            let i = row - col;
+            let j = row + col;
+            if cols.contains(&col) || diag.contains(&i) || anti_diag.contains(&j) {
+                continue;
+            }
+            cols.insert(col);
+            diag.insert(i);
+            anti_diag.insert(j);
+            board[row as usize][col as usize] = "Q".to_string();
+            backtrack(board, row + 1, cols, diag, anti_diag, ans);
+            board[row as usize][col as usize] = ".".to_string();
+            cols.remove(&col);
+            diag.remove(&i);
+            anti_diag.remove(&j);
+        }
+    }
+    let mut board = vec![vec![".".to_string(); n as usize]; n as usize];
+    let mut ans = vec![];
+    backtrack(
+        &mut board,
+        0,
+        &mut HashSet::new(),
+        &mut HashSet::new(),
+        &mut HashSet::new(),
+        &mut ans,
+    );
+    ans
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -102,5 +154,10 @@ mod test {
     #[test]
     fn test_450() {
         println!("{}", profitable_schemes(5, 3, vec![2, 3], vec![2, 2]));
+    }
+
+    #[test]
+    fn test_451() {
+        println!("{:?}", solve_n_queens(4)); // [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
     }
 }
