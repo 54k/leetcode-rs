b9d8abfdd9f39494d454d6a07cb3bfbc25cbebef
54k <54k@bk.ru>
2023-04-22 15:00:41 +0300

    day 161

--- src/leetcode_grind/day_161.rs
+++ src/leetcode_grind/day_161.rs
@@ -17,3 +17,60 @@ pub fn min_insertions(s: String) -> i32 {
     s2.reverse();
     s1.len() as i32 - lcs(&s1, &s2)
 }
+
+// https://leetcode.com/problems/maximum-length-of-pair-chain/description/
+pub fn find_longest_chain(mut pairs: Vec<Vec<i32>>) -> i32 {
+    fn dp(mut pairs: Vec<Vec<i32>>) -> i32 {
+        pairs.sort_by_key(|x| x[0]);
+        let mut dp = vec![1; pairs.len()];
+        let mut ans = 0;
+        for i in 0..pairs.len() {
+            for j in 0..i {
+                if pairs[j][1] < pairs[i][0] {
+                    dp[i] = dp[i].max(dp[j] + 1);
+                    ans = ans.max(dp[j] + 1);
+                }
+            }
+        }
+        ans
+    }
+    fn greedy(mut pairs: Vec<Vec<i32>>) -> i32 {
+        pairs.sort_by_key(|x| x[1]);
+        let mut ans = 0;
+        let mut cur = i32::MIN;
+        for i in 0..pairs.len() {
+            if cur < pairs[i][0] {
+                cur = pairs[i][1];
+                ans += 1;
+            }
+        }
+        ans
+    }
+    greedy(pairs)
+}
+
+// https://leetcode.com/problems/solving-questions-with-brainpower/description/
+pub fn most_points(questions: Vec<Vec<i32>>) -> i64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test454() {
+        println!(
+            "{}",
+            find_longest_chain(vec![vec![1, 2], vec![7, 8], vec![4, 5]])
+        ); // 3
+    }
+
+    #[test]
+    fn test_455() {
+        println!(
+            "{}",
+            most_points(vec![vec![3, 2], vec![4, 3], vec![4, 4], vec![2, 5]])
+        ); // 5
+    }
+}
