29456b05754ba78a184bf8e0bec4007387366ac8
54k <54k@bk.ru>
2023-04-30 18:04:38 +0300

    day 169

--- src/leetcode_grind/day_169.rs
+++ src/leetcode_grind/day_169.rs
@@ -32,6 +32,45 @@ pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
     ans as i32
 }
 
+// https://leetcode.com/problems/third-maximum-number/description/
+pub fn third_max(nums: Vec<i32>) -> i32 {
+    let k = 3;
+    let mut max = vec![i64::MIN; k];
+    for num in nums {
+        let mut t = num as i64;
+        for i in 0..k {
+            if max[i] < t {
+                let t1 = max[i];
+                max[i] = t;
+                t = t1;
+            } else if max[i] == t {
+                break;
+            }
+        }
+    }
+    if max[k - 1] > i64::MIN {
+        max[k - 1] as i32
+    } else {
+        max[0] as i32
+    }
+}
+
+// https://leetcode.com/problems/largest-number-at-least-twice-of-others/description/
+pub fn dominant_index(nums: Vec<i32>) -> i32 {
+    let mut max = nums.iter().copied().max().unwrap();
+    let mut ans = 0;
+    for (i, &num) in nums.iter().enumerate() {
+        if num != max {
+            if num * 2 > max {
+                return -1;
+            }
+        } else {
+            ans = i;
+        }
+    }
+    ans as i32
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -45,5 +84,17 @@ mod test {
     fn test466() {
         println!("{}", find_max_consecutive_ones(vec![1, 0, 1, 1, 0, 1])); // 4
         println!("{}", find_max_consecutive_ones(vec![1, 0, 1, 1, 0])); // 4
+    }
+
+    #[test]
+    fn test467() {
+        println!("{}", third_max(vec![3, 2, 1])); // 1
+        println!("{}", third_max(vec![1, 2])); // 2
+        println!("{}", third_max(vec![2, 2, 3, 1])); // 1
+    }
+
+    #[test]
+    fn test468() {
+        println!("{}", dominant_index(vec![3, 6, 1, 0])); // 1
     }
 }
