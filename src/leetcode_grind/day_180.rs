53774a60b6e0855628b93b40ca44f67c33545552
54k <54k@bk.ru>
2023-05-10 19:29:36 +0300

    day 179

---
+++ src/leetcode_grind/day_180.rs
@@ -0,0 +1,5 @@
+
+// https://leetcode.com/problems/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/description/
+pub fn num_triplets(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    todo!()
+}
