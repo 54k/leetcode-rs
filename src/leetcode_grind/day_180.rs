2985390a60726c6af328055bab4152916cf364b3
54k <54k@bk.ru>
2023-05-11 03:33:52 +0300

    day 180

--- src/leetcode_grind/day_180.rs
+++ src/leetcode_grind/day_180.rs
@@ -1,5 +1,51 @@
-
 // https://leetcode.com/problems/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/description/
 pub fn num_triplets(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-    todo!()
+    use std::collections::HashMap;
+    let mut freq1 = nums1.iter().copied().fold(HashMap::new(), |mut m, v| {
+        *m.entry(v as i64 * v as i64).or_insert(0) += 1;
+        m
+    });
+    let mut freq2 = nums2.iter().copied().fold(HashMap::new(), |mut m, v| {
+        *m.entry(v as i64 * v as i64).or_insert(0) += 1;
+        m
+    });
+
+    let mut ans = 0;
+    for j in 0..nums2.len() {
+        for k in j + 1..nums2.len() {
+            let f = &(nums2[j] as i64 * nums2[k] as i64);
+            if freq1.contains_key(f) {
+                // println!("t1 f {} j {} k {} freq {}", f, j, k, freq1[f]);
+                ans += freq1[f]
+            }
+        }
+    }
+
+    for j in 0..nums1.len() {
+        for k in j + 1..nums1.len() {
+            let f = &(nums1[j] as i64 * nums1[k] as i64);
+            if freq2.contains_key(f) {
+                // println!("t1 f {} j {} k {} freq {}", f, j, k, freq2[f]);
+                ans += freq2[f]
+            }
+        }
+    }
+    ans
+}
+
+// https://leetcode.com/problems/uncrossed-lines/description/
+pub fn max_uncrossed_lines(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    let (n1, n2) = (nums1.len(), nums2.len());
+    let mut dp = vec![vec![0; n2 + 1]; n1 + 1];
+
+    for i in 1..=n1 {
+        for j in 1..=n2 {
+            if nums1[i - 1] == nums2[j - 1] {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = dp[i][j - 1].max(dp[i - 1][j]);
+            }
+        }
+    }
+    dp[n1][n2]
 }
