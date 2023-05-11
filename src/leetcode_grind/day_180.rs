6c85a053b30382308284484139baba2928e8813b
54k <54k@bk.ru>
2023-05-11 23:25:50 +0300

    day 180

--- src/leetcode_grind/day_180.rs
+++ src/leetcode_grind/day_180.rs
@@ -1,3 +1,6 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 // https://leetcode.com/problems/number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/description/
 pub fn num_triplets(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
     use std::collections::HashMap;
@@ -62,4 +65,70 @@ pub fn find_peak_element(nums: Vec<i32>) -> i32 {
         }
     }
     lo as i32
+}
+
+pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    fn search(nums: &Vec<i32>, target: i32, first: bool) -> i32 {
+        let mut ans = -1;
+        let (mut lo, mut hi) = (0, nums.len() - 1);
+        while lo <= hi {
+            let mid = lo + (hi - lo) / 2;
+            if first {
+                if nums[mid] >= target {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                if nums[mid] <= target {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+            if nums[mid] == target {
+                ans = mid as i32;
+            }
+        }
+        ans
+    }
+
+    vec![search(&nums, target, true), search(&nums, target, false)]
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+pub fn closest_value(root: Option<Rc<RefCell<TreeNode>>>, target: f64) -> i32 {
+    let mut node = root.clone();
+    let mut closest = root.as_ref().unwrap().borrow().val;
+    while let Some(n) = node {
+        if (n.borrow().val as f64 - target).abs() == (closest as f64 - target).abs()
+            && n.borrow().val < closest
+        {
+            closest = n.borrow().val;
+        } else if (n.borrow().val as f64 - target).abs() < (closest as f64 - target).abs() {
+            closest = n.borrow().val;
+        }
+        if target < n.borrow().val as f64 {
+            node = n.borrow().left.clone();
+        } else {
+            node = n.borrow().right.clone();
+        }
+    }
+    closest
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test497() {
+        println!("{:?}", search_range(vec![5, 7, 7, 8, 8, 10], 8));
+    }
 }
