56cfe9b9437eb1be8e2b231604100b5e8fe910bd
54k <54k@bk.ru>
2023-05-16 00:58:01 +0300

    day 185

---
+++ src/leetcode_grind/day_185.rs
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-k-beauty-of-a-number/description/
+pub fn divisor_substrings(num: i32, k: i32) -> i32 {
+    todo!()
+}
+
+// https://leetcode.com/problems/recover-binary-search-tree/description/
+pub fn recover_tree(root: &mut Option<Rc<RefCell<TreeNode>>>) {
+    todo!()
+}
