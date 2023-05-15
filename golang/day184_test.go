ff09c4a443b68cf839a98f792fe95dd9e32b06af
54k <54k@bk.ru>
2023-05-16 00:00:28 +0300

    day 184

--- golang/day184_test.go
+++ golang/day184_test.go
@@ -113,3 +113,18 @@ func isValidBST(root *TreeNode) bool {
 	}
 	return approach1(root) == approach2(root)
 }
+
+func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
+	var succ *TreeNode
+
+	for root != nil {
+		if p.Val >= root.Val {
+			root = root.Right
+		} else {
+			succ = root
+			root = root.Left
+		}
+	}
+
+	return succ
+}
