f67d9039bf2b2e2e9788610a1f136b9bde3bf37d
54k <54k@bk.ru>
2023-05-16 03:48:33 +0300

    day 185

---
+++ golang/day185_test.go
@@ -0,0 +1,41 @@
+package main
+
+// https://leetcode.com/problems/swap-nodes-in-pairs/
+func swapRec(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	firstNode := head
+	secondNode := head.Next
+
+	firstNode.Next = swapPairs(secondNode.Next)
+	secondNode.Next = firstNode
+
+	return secondNode
+}
+
+func swapIter(head *ListNode) *ListNode {
+	dummy := &ListNode{
+		Val:  0,
+		Next: head,
+	}
+	prev := dummy
+
+	for head != nil && head.Next != nil {
+		first := head
+		second := head.Next
+
+		prev.Next = second
+		first.Next = second.Next
+		second.Next = first
+
+		prev = first
+		head = first.Next
+	}
+	return dummy.Next
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	return swapIter(head)
+}
