e6dba6c9873adb3ffa6e863d198fbd26ad888b76
54k <54k@bk.ru>
2023-05-24 12:35:45 -0400

    day_193

--- src/leetcode_grind/day_193.rs
+++ src/leetcode_grind/day_193.rs
@@ -1,7 +1,43 @@
 // https://leetcode.com/problems/falling-squares/
 // https://leetcode.com/problems/falling-squares/editorial/
 pub fn falling_squares(positions: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    struct H(Vec<i32>);
+    impl H {
+        fn update(&mut self, l: usize, r: usize, x: i32) {
+            for i in l..=r {
+                self.0[i] = x;
+            }
+        }
+        fn query(&self, l: usize, r: usize) -> i32 {
+            let mut max = -1;
+            for i in l..=r {
+                max = max.max(self.0[i]);
+            }
+            max
+        }
+    }
+    use std::collections::{BTreeSet, HashMap};
+    let mut set = BTreeSet::new();
+    for p in &positions {
+        set.insert(p[0]);
+        set.insert(p[0] + p[1] - 1);
+    }
+    let idx = set
+        .into_iter()
+        .enumerate()
+        .map(|(a, b)| (b, a))
+        .collect::<HashMap<i32, usize>>();
+    let mut heights = H(vec![0; idx.len()]);
+    let mut best = 0;
+    let mut ans = vec![];
+    for pos in positions {
+        let (left, right) = (idx[&pos[0]], idx[&(pos[0] + pos[1] - 1)]);
+        let h = heights.query(left, right) + pos[1];
+        heights.update(left, right, h);
+        best = best.max(h);
+        ans.push(best);
+    }
+    ans
 }
 
 // https://leetcode.com/problems/maximum-subsequence-score/description/
