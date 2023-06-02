5afb20c2f6380aa505c3c1ef241583670856e38b
54k <54k@bk.ru>
2023-06-02 15:13:14 +0300

    day 202

--- golang/day202_test.go
+++ golang/day202_test.go
@@ -29,3 +29,37 @@ func maxProfit1(prices []int) int {
 	}
 	return best
 }
+
+// https://leetcode.com/problems/maximum-sum-circular-subarray/description/
+func maxSubarraySumCircular(nums []int) int {
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	n := len(nums)
+	rightMax := make([]int, n)
+	rightMax[n-1] = nums[n-1]
+
+	for suffixSum, i := nums[n-1], n-2; i >= 0; i-- {
+		suffixSum += nums[i]
+		rightMax[i] = suffixSum
+		if rightMax[i+1] > rightMax[i] {
+			rightMax[i] = rightMax[i+1]
+		}
+	}
+
+	maxSum := nums[0]
+	specialSum := nums[0]
+
+	for i, prefixSum, curMax := 0, 0, 0; i < n; i++ {
+		curMax = max(curMax, 0) + nums[i]
+		maxSum = max(maxSum, curMax)
+		prefixSum += nums[i]
+		if i+1 < n {
+			specialSum = max(specialSum, prefixSum+rightMax[i+1])
+		}
+	}
+	return max(maxSum, specialSum)
+}
