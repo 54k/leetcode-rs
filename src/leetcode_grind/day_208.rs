094fb832f0650511fda18c9e22005cc83e85a202
54k <54k@bk.ru>
2023-06-08 08:18:15 -0400

    day 208

--- src/leetcode_grind/day_208.rs
+++ src/leetcode_grind/day_208.rs
@@ -13,3 +13,28 @@ pub fn count_negatives(grid: Vec<Vec<i32>>) -> i32 {
 
     count
 }
+
+// https://leetcode.com/discuss/interview-question/algorithms/125398/given-a-stack-sort-it-in-non-decreasing-order
+pub fn sort_stack(mut s: Vec<i32>) -> Vec<i32> {
+    let mut t = vec![];
+    while let Some(top) = s.pop() {
+        while !t.is_empty() && *t.last().unwrap() < top {
+            s.push(t.pop().unwrap());
+        }
+        t.push(top);
+    }
+    while let Some(top) = t.pop() {
+        s.push(top);
+    }
+    s
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test505() {
+        println!("{:?}", sort_stack(vec![4, 1, 3, 2, 5, 0, 3]));
+    }
+}
