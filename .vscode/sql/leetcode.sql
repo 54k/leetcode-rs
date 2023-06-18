947e9010882cb45b7b158d84a6030fbb563a5e02
54k <54k@bk.ru>
2023-06-18 16:38:06 +0300

    day 218

--- .vscode/sql/leetcode.sql
+++ .vscode/sql/leetcode.sql
@@ -1,2 +1,4 @@
 -- https://leetcode.com/problems/second-highest-salary/
 select (select distinct salary from employee order by salary desc limit 1 offset 1) as SecondHighestSalary;
+-- https://leetcode.com/problems/product-sales-analysis-i/description/
+select p.product_name, s.year, s.price from sales s left join product p on p.product_id = s.product_id order by p.product_name;
