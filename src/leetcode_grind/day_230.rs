22ca914f9250971f43515c4fa72f98cfa02e0c7f
54k <54k@bk.ru>
2023-06-30 22:24:44 +0300

    day 230

--- src/leetcode_grind/day_230.rs
+++ src/leetcode_grind/day_230.rs
@@ -110,6 +110,81 @@ pub fn latest_day_to_cross_dfs(row: i32, col: i32, cells: Vec<Vec<i32>>) -> i32 
     left
 }
 
+pub fn latest_day_to_cross(row: i32, col: i32, cells: Vec<Vec<i32>>) -> i32 {
+    struct DSU {
+        root: Vec<usize>,
+        size: Vec<usize>,
+    }
+
+    impl DSU {
+        fn new(size: usize) -> Self {
+            let mut root = vec![0; size];
+            for i in 0..size {
+                root[i] = i;
+            }
+            Self {
+                root,
+                size: vec![1; size],
+            }
+        }
+
+        fn find(&mut self, x: usize) -> usize {
+            if self.root[x] != x {
+                self.root[x] = self.find(self.root[x]);
+            }
+            self.root[x]
+        }
+
+        fn union(&mut self, x: usize, y: usize) {
+            let (mut x, mut y) = (self.find(x), self.find(y));
+            if x == y {
+                return;
+            }
+
+            if self.size[x] < self.size[y] {
+                std::mem::swap(&mut x, &mut y);
+            }
+
+            self.root[x] = y;
+            self.size[y] += self.size[x];
+        }
+    }
+
+    let mut dsu = DSU::new(row as usize * col as usize + 2);
+    let mut grid = vec![vec![0; col as usize]; row as usize];
+    const DIR: [(i32, i32); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+    for i in (0..cells.len()).rev() {
+        let (r, c) = (cells[i][0] as usize - 1, cells[i][1] as usize - 1);
+        grid[r][c] = 1;
+        let index_1 = r as i32 * col + c as i32 + 1;
+        for d in &DIR {
+            let (new_r, new_c) = (r as i32 + d.0, c as i32 + d.1);
+            let index_2 = new_r * col + new_c + 1;
+
+            if new_r >= 0
+                && new_r < row
+                && new_c >= 0
+                && new_c < col
+                && grid[new_r as usize][new_c as usize] == 1
+            {
+                dsu.union(index_1 as usize, index_2 as usize);
+            }
+        }
+
+        if r == 0 {
+            dsu.union(0, index_1 as usize);
+        }
+        if r as usize == row as usize - 1 {
+            dsu.union(row as usize * col as usize + 1, index_1 as usize);
+        }
+        if dsu.find(0) == dsu.find(row as usize * col as usize + 1) {
+            return i as i32;
+        }
+    }
+    -1
+}
+
 // https://leetcode.com/problems/bricks-falling-when-hit/description/
 pub fn hit_bricks(grid: Vec<Vec<i32>>, hits: Vec<Vec<i32>>) -> Vec<i32> {
     todo!()
@@ -119,6 +194,7 @@ pub fn hit_bricks(grid: Vec<Vec<i32>>, hits: Vec<Vec<i32>>) -> Vec<i32> {
 pub fn maximum_minutes(grid: Vec<Vec<i32>>) -> i32 {
     todo!()
 }
+
 // https://leetcode.com/problems/cracking-the-safe/description/
 pub fn crack_safe(n: i32, k: i32) -> String {
     todo!()
