e5cef8c2c24ad3f95b4e2f491bcb78abaa33b44e
54k <54k@bk.ru>
2023-07-11 14:43:48 +0200

    day 241

--- golang/day_241_test.go
+++ golang/day_241_test.go
@@ -57,3 +57,47 @@ func distanceK(root *TreeNode, target *TreeNode, k int) []int {
 
 	return []int{}
 }
+
+// https://leetcode.com/problems/find-if-path-exists-in-graph/description/
+func validPath(n int, edges [][]int, source int, destination int) bool {
+	graph := map[int][]int{}
+
+	for _, e := range edges {
+		if graph[e[0]] == nil {
+			graph[e[0]] = []int{}
+		}
+		if graph[e[1]] == nil {
+			graph[e[1]] = []int{}
+		}
+
+		graph[e[0]] = append(graph[e[0]], e[1])
+		graph[e[1]] = append(graph[e[1]], e[0])
+	}
+
+	lvl := []int{source}
+	seen := map[int]bool{}
+	seen[source] = true
+
+	for len(lvl) > 0 {
+		nextLvl := []int{}
+
+		for _, cur := range lvl {
+
+			if cur == destination {
+				return true
+			}
+
+			for _, next := range graph[cur] {
+				if !seen[next] {
+					seen[next] = true
+					nextLvl = append(nextLvl, next)
+				}
+			}
+		}
+
+		lvl = nextLvl
+
+	}
+
+	return false
+}
