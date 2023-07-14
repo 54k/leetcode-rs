d389cb0bd7b6dc080362307d5a861c37bafab790
54k <54k@bk.ru>
2023-07-14 12:02:30 +0200

    day 244

--- golang/day_244_test.go
+++ golang/day_244_test.go
@@ -15,3 +15,24 @@ func longestSubsequence(arr []int, difference int) int {
 
 	return answer
 }
+
+// https://leetcode.com/problems/destroy-sequential-targets/description/
+func destroyTargets(nums []int, space int) int {
+	m := map[int]int{}
+	max := -(1 << 31)
+	for _, n := range nums {
+		m[n%space]++
+		if max < m[n%space] {
+			max = m[n%space]
+		}
+	}
+
+	ans := 1 << 31
+	for _, n := range nums {
+		if m[n%space] == max && n < ans {
+			ans = n
+		}
+	}
+
+	return ans
+}
