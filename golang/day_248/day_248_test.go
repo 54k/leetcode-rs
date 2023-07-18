efd50ad168e7f78bc4425dc6ff5c0f0d221574b7
54k <54k@bk.ru>
2023-07-18 22:58:44 +0200

    day 248

--- golang/day_248/day_248_test.go
+++ golang/day_248/day_248_test.go
@@ -209,6 +209,33 @@ func merge(intervals [][]int) [][]int {
 	return ans
 }
 
+// https://leetcode.com/problems/candy/submissions/
+func candy(ratings []int) int {
+	candies := make([]int, len(ratings))
+	for i := 0; i < len(candies); i++ {
+		candies[i] = 1
+	}
+
+	for i := 1; i < len(ratings); i++ {
+		if ratings[i] > ratings[i-1] {
+			candies[i] = candies[i-1] + 1
+		}
+	}
+
+	sum := candies[len(candies)-1]
+	for i := len(ratings) - 2; i >= 0; i-- {
+		if ratings[i] > ratings[i+1] {
+			if candies[i+1]+1 > candies[i] {
+				candies[i] = candies[i+1] + 1
+			}
+		}
+
+		sum += candies[i]
+	}
+
+	return sum
+}
+
 func TestLRUCache(t *testing.T) {
 	lru := Constructor(2)
 	lru.Put(1, 1)
