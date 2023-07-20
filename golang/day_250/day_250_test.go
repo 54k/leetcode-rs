59d2b4cff0298b70ad58518a200034acd64a1455
54k <54k@bk.ru>
2023-07-20 17:28:04 +0200

    day 250

--- golang/day_250/day_250_test.go
+++ golang/day_250/day_250_test.go
@@ -25,3 +25,79 @@ label:
 	}
 	return stack
 }
+
+// https://leetcode.com/problems/palindrome-linked-list/description/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindromeRec(head *ListNode) bool {
+	frontPointer := head
+	var checkRec func(*ListNode) bool
+	checkRec = func(h *ListNode) bool {
+		if h != nil {
+			if !checkRec(h.Next) {
+				return false
+			}
+			if h.Val != frontPointer.Val {
+				return false
+			}
+			frontPointer = frontPointer.Next
+		}
+		return true
+	}
+
+	return checkRec(head)
+}
+
+func isPalindromeRevNoMem(head *ListNode) bool {
+	mid := func(h *ListNode) *ListNode {
+		slow, fast := h, h
+		for fast.Next != nil && fast.Next.Next != nil {
+			fast = fast.Next.Next
+			slow = slow.Next
+		}
+		return slow
+	}
+
+	rev := func(h *ListNode) *ListNode {
+		var prev *ListNode
+		for h != nil {
+			next := h.Next
+			h.Next = prev
+			prev = h
+			h = next
+		}
+		return prev
+	}
+
+	eq := func(a, b *ListNode) bool {
+		result := true
+		for result && b != nil {
+			if a.Val != b.Val {
+				result = false
+			}
+			a = a.Next
+			b = b.Next
+		}
+		return result
+	}
+
+	firstHalfEnd := mid(head)
+	secHalfStart := rev(firstHalfEnd.Next)
+
+	res := eq(head, secHalfStart)
+	rev(firstHalfEnd.Next)
+	return res
+}
+
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
