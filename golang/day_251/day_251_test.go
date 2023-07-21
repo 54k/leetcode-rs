adc00cbaab2462706bbb54628bce82bff8eb374e
54k <54k@bk.ru>
2023-07-21 15:18:15 +0200

    day 251

--- golang/day_251/day_251_test.go
+++ golang/day_251/day_251_test.go
@@ -41,3 +41,53 @@ func findNumberOfLIS(nums []int) int {
 
 	return result
 }
+
+// https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
+func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
+	type pair struct {
+		to     int
+		weight int
+	}
+	adj := map[int][]pair{}
+	for _, f := range flights {
+		if _, ok := adj[f[0]]; !ok {
+			adj[f[0]] = []pair{}
+		}
+		adj[f[0]] = append(adj[f[0]], pair{f[1], f[2]})
+	}
+	dist := make([]int, n)
+	for i := 0; i < len(dist); i++ {
+		dist[i] = 1 << 30
+	}
+
+	lvl := []pair{pair{src, 0}}
+	stops := 0
+
+	for stops <= k && len(lvl) > 0 {
+		newLvl := []pair{}
+
+		for _, el := range lvl {
+			node, d := el.to, el.weight
+			if _, ok := adj[node]; !ok {
+				continue
+			}
+
+			for _, next := range adj[node] {
+				nnode, nprice := next.to, next.weight
+				if nprice+d >= dist[nnode] {
+					continue
+				}
+				dist[nnode] = nprice + d
+				newLvl = append(newLvl, pair{nnode, dist[nnode]})
+			}
+		}
+
+		lvl = newLvl
+		stops++
+	}
+
+	if dist[dst] == 1<<30 {
+		return -1
+	}
+	return dist[dst]
+}
