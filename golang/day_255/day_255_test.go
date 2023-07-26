e68052f54c4ce8010ce83eb738f1782ef81a6d0b
54k <54k@bk.ru>
2023-07-26 02:02:24 +0200

    day 255

--- golang/day_255/day_255_test.go
+++ golang/day_255/day_255_test.go
@@ -1,5 +1,10 @@
 package day255
 
+import (
+	"fmt"
+	"testing"
+)
+
 type Node struct {
 	Val      int
 	Children []*Node
@@ -92,4 +97,140 @@ func peakIndexInMountainArray(arr []int) int {
 		}
 	}
 	return lo
+}
+
+// https://leetcode.com/problems/parallel-courses/description/
+func minimumSemesters(n int, relations [][]int) int {
+	adj := make([][]int, n+1)
+
+	for i := 0; i <= n; i++ {
+		adj[i] = []int{}
+	}
+
+	for _, r := range relations {
+		adj[r[0]] = append(adj[r[0]], r[1])
+	}
+
+	maxLength := 1
+	vis := make([]int, n+1)
+
+	var dfs func(int) int
+	dfs = func(node int) int {
+		if vis[node] != 0 {
+			return vis[node]
+		} else {
+			vis[node] = -1
+		}
+
+		maxLength := 1
+		for _, endNode := range adj[node] {
+			len := dfs(endNode)
+			if len == -1 {
+				return -1
+			}
+			if len+1 > maxLength {
+				maxLength = len + 1
+			}
+		}
+		vis[node] = maxLength
+		return maxLength
+	}
+
+	for node := 1; node <= n; node++ {
+		len := dfs(node)
+		if len == -1 {
+			return -1
+		}
+		if len > maxLength {
+			maxLength = len
+		}
+	}
+
+	return maxLength
+}
+
+// https://leetcode.com/problems/parallel-courses-ii/description/
+func minNumberOfSemesters(n int, relations [][]int, k int) int {
+	adj := make([][]int, n)
+
+	for i := 0; i < len(adj); i++ {
+		adj[i] = []int{}
+	}
+	for _, r := range relations {
+		f, t := r[0]-1, r[1]-1
+		adj[f] = append(adj[f], t)
+	}
+
+	memo := make([]int, 1<<n)
+	for i := 0; i < len(memo); i++ {
+		memo[i] = -1
+	}
+
+	popCount := func(n int) int {
+		ans := 0
+		for n != 0 {
+			if n&1 == 1 {
+				ans++
+			}
+			n >>= 1
+		}
+		return ans
+	}
+
+	var dp func(int) int
+	dp = func(mask int) int {
+		if mask == (1<<n)-1 {
+			return 0
+		}
+		if memo[mask] != -1 {
+			return memo[mask]
+		}
+
+		degree := make([]int, n)
+
+		for i := 0; i < n; i++ {
+			if mask&(1<<i) == 0 {
+				for _, nxt := range adj[i] {
+					degree[nxt]++
+				}
+			}
+		}
+
+		cm := 0 // candidates mask
+		for i := 0; i < n; i++ {
+			if degree[i] == 0 && mask&(1<<i) == 0 {
+				cm |= 1 << i
+			}
+		}
+		cmCnt := popCount(cm) // num of candidates
+
+		ans := n + 1
+		if cmCnt <= k {
+			sem := 1 + dp(mask|cm)
+			if ans > sem {
+				ans = sem
+			}
+		} else {
+			cur := cm // current count of candidates
+			for ; cur > 0; cur = (cur - 1) & cm {
+				curCnt := popCount(cur)
+				if curCnt == k {
+					sem := 1 + dp(mask|cur)
+					if ans > sem {
+						ans = sem
+					}
+				}
+			}
+		}
+
+		memo[mask] = ans
+		return ans
+	}
+
+	return dp(0)
+}
+
+func TestMinNumOfSemesters(t *testing.T) {
+	ans := minNumberOfSemesters(4, [][]int{{2, 1}, {3, 1}, {1, 4}}, 2)
+	fmt.Println(ans)
 }
--- src/leetcode_grind/day_255.rs
+++ src/leetcode_grind/day_255.rs
@@ -55,3 +55,173 @@ pub fn minimum_semesters(n: i32, relations: Vec<Vec<i32>>) -> i32 {
 
     return lvl;
 }
+
+// https://leetcode.com/problems/parallel-courses-ii/description/
+pub fn min_number_of_semesters_tle(n: i32, relations: Vec<Vec<i32>>, k: i32) -> i32 {
+    let mut adj = vec![vec![]; n as usize];
+    let mut in_degree = vec![0; n as usize];
+
+    for r in relations {
+        adj[r[0] as usize - 1].push(r[1] as usize - 1);
+        in_degree[r[1] as usize] += 1;
+    }
+
+    fn get_candidates(in_degree: &Vec<i32>, taken: &Vec<usize>, n: usize) -> Vec<usize> {
+        let mut candidates = Vec::new();
+        for i in 0..n {
+            if in_degree[i] == 0 && !taken.contains(&i) {
+                candidates.push(i);
+            }
+        }
+        candidates
+    }
+
+    fn backtrack(
+        taken: Vec<usize>,
+        in_degree: Vec<i32>,
+        adj: &Vec<Vec<usize>>,
+        k: usize,
+        n: usize,
+        memo: &mut HashMap<Vec<usize>, i32>,
+    ) -> i32 {
+        // println!("taken {:?} degrees {:?}", taken, in_degree);
+        if taken.len() == n {
+            return 0;
+        }
+        let key = taken.clone();
+
+        if memo.contains_key(&key) {
+            return memo[&key];
+        }
+
+        let mut ans = i32::MAX;
+        let candidates = get_candidates(&in_degree, &taken, n);
+
+        if candidates.len() == 0 {
+            panic!("oops");
+        }
+
+        // println!("candidates {:?}", candidates);
+
+        for subset in 1..1 << candidates.len() {
+            let ones = (subset as u32).count_ones();
+            if ones != (k as u32).min(candidates.len() as u32) {
+                continue;
+            }
+
+            let mut new_taken = vec![];
+            let mut new_in_degree = in_degree.clone();
+
+            for i in 0..candidates.len() {
+                if ((subset >> i) & 1) == 1 {
+                    new_taken.push(candidates[i]);
+                }
+            }
+
+            for &i in &new_taken {
+                for &neighbor in &adj[i] {
+                    new_in_degree[neighbor] -= 1;
+                }
+            }
+
+            new_taken.extend(taken.clone());
+
+            let next_ans = backtrack(new_taken, new_in_degree, adj, k, n, memo) + 1;
+            ans = ans.min(next_ans);
+        }
+
+        memo.insert(key, ans);
+
+        ans
+    }
+
+    use std::collections::HashMap;
+    backtrack(
+        vec![],
+        in_degree,
+        &adj,
+        k as usize,
+        n as usize,
+        &mut HashMap::new(),
+    )
+}
+
+pub fn min_number_of_semesters(n: i32, relations: Vec<Vec<i32>>, k: i32) -> i32 {
+    let mut adj = vec![vec![]; n as usize];
+    for r in relations {
+        let (from, to) = (r[0] as usize - 1, r[1] as usize - 1);
+        adj[from].push(to);
+    }
+    let mut dp = vec![-1; 1 << n as usize];
+    return solve(0, n as usize, k as usize, &adj, &mut dp);
+
+    fn solve(mask: usize, n: usize, k: usize, adj: &Vec<Vec<usize>>, dp: &mut Vec<i32>) -> i32 {
+        if mask == (1 << n) - 1 {
+            // All courses are taken
+            return 0;
+        }
+        if dp[mask] != -1 {
+            // memoization
+            return dp[mask];
+        }
+
+        let mut in_degree = vec![0; n];
+
+        for i in 0..n {
+            if (mask & (1 << i)) != 0 {
+                continue;
+            }
+            for &it in &adj[i] {
+                in_degree[it] += 1;
+            }
+        }
+
+        let mut temp = 0; // For a mask of all nodes with 0-indegree
+        for i in 0..n {
+            if in_degree[i] == 0 && (mask & (1 << i)) == 0 {
+                temp |= 1 << i;
+            }
+        }
+
+        let mut j = temp;
+        let mut cnt = (j as u32).count_ones() as usize; // count of nodes with 0-indegree
+
+        let mut ans = n as i32 + 1; // ans will be 'n' in the worst case, so take (n+1) as infinity
+
+        if cnt > k {
+            while j != 0 {
+                // iterate through all submasks of temp
+                cnt = (j as u32).count_ones() as usize;
+                if cnt != k {
+                    j = (j - 1) & temp;
+                    continue;
+                }
+                ans = ans.min(1 + solve(mask | j, n, k, adj, dp));
+                j = (j - 1) & temp;
+            }
+        } else {
+            ans = ans.min(1 + solve(mask | j, n, k, adj, dp));
+        }
+        dp[mask] = ans;
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_semesters() {
+        println!("{:?}", min_number_of_semesters(11, vec![], 2));
+        println!("{:?}", min_number_of_semesters(14, vec![vec![11, 7]], 2));
+        println!(
+            "{:?}",
+            min_number_of_semesters(5, vec![vec![2, 1], vec![3, 1], vec![4, 1], vec![1, 5]], 2)
+        );
+        println!(
+            "{:?}",
+            min_number_of_semesters(4, vec![vec![2, 1], vec![3, 1], vec![1, 4]], 2)
+        );
+    }
+}
