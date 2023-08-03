9a9891fcf482eeaf1056448b033f5d29ad42d7d7
54k <54k@bk.ru>
2023-08-03 11:30:50 +0200

    day 264

--- golang/day_264/day_264_test.go
+++ golang/day_264/day_264_test.go
@@ -41,3 +41,81 @@ func letterCombinations(digits string) []string {
 
 	return ans
 }
+
+// https://leetcode.com/problems/design-linked-list/description/
+type Node struct {
+	val  int
+	next *Node
+}
+
+type MyLinkedList struct {
+	head *Node
+}
+
+func Constructor() MyLinkedList {
+	return MyLinkedList{nil}
+}
+
+func (this *MyLinkedList) getNode(index int) *Node {
+	cur := this.head
+	for cur != nil && index > 0 {
+		cur = cur.next
+		index--
+	}
+	return cur
+}
+
+func (this *MyLinkedList) getTail() *Node {
+	cur := this.head
+	for cur != nil && cur.next != nil {
+		cur = cur.next
+	}
+	return cur
+}
+
+func (this *MyLinkedList) Get(index int) int {
+	cur := this.getNode(index)
+	if cur == nil {
+		return -1
+	}
+	return cur.val
+}
+
+func (this *MyLinkedList) AddAtHead(val int) {
+	this.head = &Node{val, this.head}
+}
+
+func (this *MyLinkedList) AddAtTail(val int) {
+	if this.head == nil {
+		this.AddAtHead(val)
+		return
+	}
+	tail := this.getTail()
+	tail.next = &Node{val, nil}
+}
+
+func (this *MyLinkedList) AddAtIndex(index int, val int) {
+	if index == 0 {
+		this.AddAtHead(val)
+		return
+	}
+	prev := this.getNode(index - 1)
+	if prev == nil {
+		return
+	}
+	cur := &Node{val, prev.next}
+	prev.next = cur
+}
+
+func (this *MyLinkedList) DeleteAtIndex(index int) {
+	cur := this.getNode(index)
+	if cur == nil {
+		return
+	}
+	if index == 0 {
+		this.head = cur.next
+	} else {
+		prev := this.getNode(index - 1)
+		prev.next = cur.next
+	}
+}
