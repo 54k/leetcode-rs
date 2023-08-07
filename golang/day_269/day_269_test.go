7bf210e78e1009c9c53497a0ea05894ab84ad0f5
54k <54k@bk.ru>
2023-08-08 01:43:45 +0200

    day 269

--- golang/day_269/day_269_test.go
+++ golang/day_269/day_269_test.go
@@ -29,7 +29,6 @@ func trapBrute(height []int) int {
 	return ans
 }
 
-// https://leetcode.com/problems/trapping-rain-water/description/
 func trapPrefix(height []int) int {
 	max := func(a, b int) int {
 		if a > b {
@@ -60,6 +59,30 @@ func trapPrefix(height []int) int {
 	}
 	for i := 1; i < size-1; i++ {
 		ans += min(leftMax[i], rightMax[i]) - height[i]
+	}
+	return ans
+}
+
+func trap2pointers(height []int) int {
+	left, right := 0, len(height)-1
+	leftMax, rightMax := 0, 0
+	ans := 0
+	for left < right {
+		if height[left] < height[right] {
+			if height[left] >= leftMax {
+				leftMax = height[left]
+			} else {
+				ans += leftMax - height[left]
+			}
+			left++
+		} else {
+			if height[right] >= rightMax {
+				rightMax = height[right]
+			} else {
+				ans += rightMax - height[right]
+			}
+			right--
+		}
 	}
 	return ans
 }
