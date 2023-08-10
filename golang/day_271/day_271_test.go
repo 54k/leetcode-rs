1f3e24ff1157824829bde65b35181a7958bab3ce
54k <54k@bk.ru>
2023-08-10 13:50:02 +0200

    day 271

--- golang/day_271/day_271_test.go
+++ golang/day_271/day_271_test.go
@@ -1,5 +1,6 @@
 package day271
 
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 func search(nums []int, target int) bool {
 	existsInFirst := func(start int, el int) bool {
 		return nums[start] <= el
@@ -35,4 +36,29 @@ func search(nums []int, target int) bool {
 		}
 	}
 	return false
+}
+
+// https://leetcode.com/problems/sort-colors/description/
+func sortColors(nums []int) {
+	p0 := 0
+	curr := 0
+	p2 := len(nums) - 1
+
+	var tmp int
+	for curr <= p2 {
+		if nums[curr] == 0 {
+			tmp = nums[p0]
+			nums[p0] = nums[curr]
+			nums[curr] = tmp
+			p0++
+			curr++
+		} else if nums[curr] == 2 {
+			tmp = nums[curr]
+			nums[curr] = nums[p2]
+			nums[p2] = tmp
+			p2--
+		} else {
+			curr++
+		}
+	}
 }
