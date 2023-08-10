78b83afb0886d39071e0d9f3cb32e8cc73d5f2f1
54k <54k@bk.ru>
2023-08-10 17:37:05 +0200

    day 271

--- golang/day_271/day_271_test.go
+++ golang/day_271/day_271_test.go
@@ -1,5 +1,7 @@
 package day271
 
+import "fmt"
+
 // https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
 func search(nums []int, target int) bool {
 	existsInFirst := func(start int, el int) bool {
@@ -61,4 +63,54 @@ func sortColors(nums []int) {
 			curr++
 		}
 	}
+}
+
+// https://leetcode.com/problems/subtree-of-another-tree/description/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	var isSame func(*TreeNode, *TreeNode) bool
+	isSame = func(l *TreeNode, r *TreeNode) bool {
+		if l == nil || r == nil {
+			return l == nil && r == nil
+		}
+		return l.Val == r.Val && isSame(l.Left, r.Left) && isSame(l.Right, r.Right)
+	}
+	var dfs func(*TreeNode) bool
+	dfs = func(root *TreeNode) bool {
+		if root == nil {
+			return false
+		}
+
+		if root.Val == subRoot.Val && isSame(root, subRoot) {
+			return true
+		}
+		return dfs(root.Left) || dfs(root.Right)
+	}
+
+	return dfs(root)
+}
+
+// https://leetcode.com/problems/find-duplicate-subtrees/description/
+func findDuplicateSubtrees(root *TreeNode) []*TreeNode {
+	res := []*TreeNode{}
+	cnt := map[string]int{}
+	var dfs func(*TreeNode) string
+	dfs = func(root *TreeNode) string {
+		if root == nil {
+			return ""
+		}
+		key := fmt.Sprintf("(%s)%s(%s)", dfs(root.Left), root.Val, dfs(root.Right))
+		cnt[key] += 1
+		if cnt[key] == 2 {
+			res = append(res, root)
+		}
+		return key
+	}
+	dfs(root)
+	return res
 }
