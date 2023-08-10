8c9f5afbc25a24388b3e7f2b8e6115e1c3e795ff
54k <54k@bk.ru>
2023-08-10 23:58:53 +0200

    day 271

--- src/leetcode_grind/day_271.rs
+++ src/leetcode_grind/day_271.rs
@@ -38,3 +38,37 @@ pub fn count_unival_subtrees(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     dfs(root, &mut ans);
     ans
 }
+
+// https://leetcode.com/problems/maximize-distance-to-closest-person/description/
+pub fn max_dist_to_closest(seats: Vec<i32>) -> i32 {
+    pub fn max_dist_to_closest(seats: Vec<i32>) -> i32 {
+        let n = seats.len();
+        let mut left = vec![n; n];
+        let mut right = vec![n; n];
+
+        for i in 0..n {
+            if seats[i] == 1 {
+                left[i] = 0;
+            } else if i > 0 {
+                left[i] = left[i - 1] + 1;
+            }
+        }
+
+        for i in (0..n).rev() {
+            if seats[i] == 1 {
+                right[i] = 0;
+            } else if (i < n - 1) {
+                right[i] = right[i + 1] + 1;
+            }
+        }
+
+        let mut ans = 0;
+        for i in 0..n {
+            if seats[i] == 0 {
+                ans = ans.max(left[i].min(right[i]));
+            }
+        }
+        ans as i32
+    }
+    max_dist_to_closest(seats)
+}
