563009456fe2c2e002366bb4182f6e21bd55fd12
54k <54k@bk.ru>
2023-08-12 23:42:38 +0200

    day 273

--- src/leetcode_grind/day_273.rs
+++ src/leetcode_grind/day_273.rs
@@ -45,3 +45,124 @@ pub fn split_painting(segments: Vec<Vec<i32>>) -> Vec<Vec<i64>> {
     }
     res
 }
+
+// https://leetcode.com/problems/data-stream-as-disjoint-intervals/description/
+mod sum_ranges_sorted_set {
+    use std::collections::BTreeSet;
+    struct SummaryRanges {
+        set: BTreeSet<i32>,
+    }
+
+    impl SummaryRanges {
+        fn new() -> Self {
+            Self {
+                set: BTreeSet::new(),
+            }
+        }
+
+        fn add_num(&mut self, value: i32) {
+            self.set.insert(value);
+        }
+
+        fn get_intervals(&self) -> Vec<Vec<i32>> {
+            if self.set.is_empty() {
+                return vec![];
+            }
+            let mut ans = vec![];
+            let (mut left, mut right) = (-1, -1);
+            for &val in &self.set {
+                if left < 0 {
+                    left = val;
+                    right = val;
+                } else if (val == right + 1) {
+                    right = val;
+                } else {
+                    ans.push(vec![left, right]);
+                    left = val;
+                    right = val;
+                }
+            }
+            ans.push(vec![left, right]);
+            ans
+        }
+    }
+}
+
+// https://leetcode.com/problems/the-skyline-problem/
+pub fn get_skyline(buildings: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    use std::collections::BTreeSet;
+    use std::collections::HashMap;
+
+    let mut set = BTreeSet::new();
+    for b in &buildings {
+        set.insert(b[0]);
+        set.insert(b[1]);
+    }
+
+    let edges = set.into_iter().collect::<Vec<_>>();
+    let edge_idx_map = edges
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(|(i, x)| (x, i))
+        .collect::<HashMap<i32, usize>>();
+
+    let mut heights = vec![0; edges.len()];
+    for building in buildings {
+        let left = edge_idx_map[&building[0]];
+        let right = edge_idx_map[&building[1]];
+        let height = building[2];
+
+        for idx in left..right {
+            heights[idx] = heights[idx].max(height);
+        }
+    }
+
+    let mut ans: Vec<Vec<i32>> = vec![];
+    for i in 0..heights.len() {
+        let curr_height = heights[i];
+        let curr_pos = edges[i];
+
+        if ans.is_empty() || (ans[ans.len() - 1][1] != curr_height) {
+            ans.push(vec![curr_pos, curr_height]);
+        }
+    }
+    ans
+}
+
+// https://leetcode.com/problems/amount-of-new-area-painted-each-day/description/
+pub fn amount_painted(paint: Vec<Vec<i32>>) -> Vec<i32> {
+    use std::collections::BTreeMap;
+    let mut records = vec![];
+    let mut max_pos = 0;
+    for i in 0..paint.len() {
+        let (start, end) = (paint[i][0], paint[i][1]);
+        records.push((start as usize, i, 1)); // use 1 and -1 to record the type
+        records.push((end as usize, i, -1));
+        max_pos = max_pos.max(end as usize);
+    }
+    records.sort();
+
+    let mut ans = vec![0; paint.len()];
+    let mut indexes = BTreeMap::new();
+    let mut i = 0;
+
+    for pos in 0..=max_pos {
+        while i < records.len() && records[i].0 == pos {
+            let (index, t) = (records[i].1, records[i].2);
+            if t == 1 {
+                *indexes.entry(index).or_insert(0) += 1;
+            } else if t == -1 {
+                *indexes.entry(index).or_insert(0) -= 1;
+                if indexes[&index] <= 0 {
+                    indexes.remove(&index);
+                }
+            }
+            i += 1;
+        }
+        if indexes.len() > 0 {
+            ans[*indexes.keys().take(1).find(|_| true).unwrap()] += 1;
+        }
+    }
+    ans
+}
