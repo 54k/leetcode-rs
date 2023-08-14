906e9091298e59a6fcabc1acdf483e8fb292af86
54k <54k@bk.ru>
2023-08-14 21:26:47 +0200

    day 275

--- golang/day_275/day_275_test.go
+++ golang/day_275/day_275_test.go
@@ -32,3 +32,87 @@ func findKthLargest(nums []int, k int) int {
 	}
 	return quickselect(nums, k)
 }
+
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	var dfs func(int, int) *TreeNode
+	dfs = func(lo int, hi int) *TreeNode {
+		if lo > hi {
+			return nil
+		}
+
+		m := (lo + hi) / 2
+		mid := &TreeNode{nums[m], nil, nil}
+		mid.Left = dfs(lo, m-1)
+		mid.Right = dfs(m+1, hi)
+		return mid
+	}
+
+	return dfs(0, len(nums)-1)
+}
+
+// https://leetcode.com/problems/maximum-gap/description/
+func maximumGap(nums []int) int {
+	type Bucket struct {
+		used   bool
+		minVal int
+		maxVal int
+	}
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+	if len(nums) == 0 || len(nums) < 2 {
+		return 0
+	}
+	mini, maxi := (1 << 30), -(1 << 30)
+	for _, v := range nums {
+		if v < mini {
+			mini = v
+		}
+		if v > maxi {
+			maxi = v
+		}
+	}
+
+	bucketSize := max(1, (maxi-mini)/len(nums)-1) // bucket size or capacity
+	bucketNum := (maxi-mini)/bucketSize + 1       // number of buckets
+	buckets := make([]*Bucket, bucketNum)
+	for i := 0; i < len(buckets); i++ {
+		buckets[i] = &Bucket{false, (1 << 30), -(1 << 30)}
+	}
+
+	for _, num := range nums {
+		bucketIdx := (num - mini) / bucketSize
+		buckets[bucketIdx].used = true
+		buckets[bucketIdx].minVal = min(num, buckets[bucketIdx].minVal)
+		buckets[bucketIdx].maxVal = max(num, buckets[bucketIdx].maxVal)
+	}
+
+	prevBucketMax := mini
+	maxGap := 0
+
+	for _, bucket := range buckets {
+		if !bucket.used {
+			continue
+		}
+		maxGap = max(maxGap, bucket.minVal-prevBucketMax)
+		prevBucketMax = bucket.maxVal
+	}
+	return maxGap
+}
