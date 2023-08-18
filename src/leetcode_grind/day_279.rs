64c4db81e65de21a0a3ead20218a3f484bda2432
54k <54k@bk.ru>
2023-08-18 15:28:58 +0200

    day 279

--- src/leetcode_grind/day_279.rs
+++ src/leetcode_grind/day_279.rs
@@ -29,4 +29,91 @@ pub fn remove_outer_parentheses(s: String) -> String {
     }
     ans.into_iter().collect()
 }
-    
+
+// https://leetcode.com/problems/baseball-game/description/
+pub fn cal_points(operations: Vec<String>) -> i32 {
+    let mut stack = vec![];
+    for op in operations {
+        let op = op.as_str();
+        match op {
+            "+" => {
+                let f = stack.pop().unwrap();
+                let s = *stack.last().unwrap();
+                stack.push(f);
+                stack.push(f + s);
+            }
+            "D" => {
+                let f = stack.last().unwrap();
+                stack.push(f * 2);
+            }
+            "C" => {
+                stack.pop();
+            }
+            _ => {
+                stack.push(i32::from_str_radix(op, 10).unwrap());
+            }
+        }
+        println!("{:?}", stack);
+    }
+    stack.into_iter().sum::<i32>()
+}
+
+// https://leetcode.com/problems/crawler-log-folder/
+pub fn min_operations(logs: Vec<String>) -> i32 {
+    let mut count = 0;
+    for log in logs {
+        let log = &log[..log.len() - 1];
+
+        match log {
+            ".." => count = 0.max(count - 1),
+            "." => {}
+            _ => {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+// https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
+pub fn min_length(s: String) -> i32 {
+    let mut s = s.chars().collect::<Vec<_>>();
+    loop {
+        let mut changed = false;
+        let mut stack = vec![];
+        for &ch in &s {
+            match ch {
+                'B' => {
+                    if stack.len() > 0 && *stack.last().unwrap() == 'A' {
+                        changed = true;
+                        stack.pop();
+                    } else {
+                        stack.push(ch);
+                    }
+                }
+                'D' => {
+                    if stack.len() > 0 && *stack.last().unwrap() == 'C' {
+                        changed = true;
+                        stack.pop();
+                    } else {
+                        stack.push(ch);
+                    }
+                }
+                _ => {
+                    stack.push(ch);
+                }
+            }
+        }
+        s = stack;
+        if !changed {
+            break;
+        }
+    }
+    s.len() as i32
+}
+
+#[test]
+fn test_min_length() {
+    let res = min_length("ABFCACDB".to_string());
+    println!("{res}");
+}
