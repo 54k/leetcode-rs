2152ea88074055901e96501c10f338b253180edc
54k <54k@bk.ru>
2023-08-20 14:52:49 +0200

    day 281

--- src/leetcode_grind/day_281.rs
+++ src/leetcode_grind/day_281.rs
@@ -219,3 +219,13 @@ pub fn max_events(mut events: Vec<Vec<i32>>) -> i32 {
     res
 }
 
+// https://leetcode.com/problems/meeting-rooms/description/
+pub fn can_attend_meetings(mut intervals: Vec<Vec<i32>>) -> bool {
+    intervals.sort();
+    for i in 1..intervals.len() {
+        if intervals[i - 1][1] > intervals[i][0] {
+            return false;
+        }
+    }
+    true
+}
