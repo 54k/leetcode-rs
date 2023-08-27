d57f1ee57f387a06bfbf2269ec9d8b7060ee422b
54k <54k@bk.ru>
2023-08-27 11:11:44 +0200

    day 288

--- src/leetcode_grind/day_288.rs
+++ src/leetcode_grind/day_288.rs
@@ -78,3 +78,45 @@ pub fn can_cross_ii(stones: Vec<i32>) -> bool {
     }
     false
 }
+
+// https://leetcode.com/problems/design-a-stack-with-increment-operation/description/
+struct CustomStack {
+    stack: Vec<(i32, i32)>,
+    max_size: usize,
+}
+
+impl CustomStack {
+    fn new(maxSize: i32) -> Self {
+        Self {
+            stack: Vec::new(),
+            max_size: maxSize as usize,
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        if self.stack.len() < self.max_size {
+            self.stack.push((x, 0));
+        }
+    }
+
+    fn pop(&mut self) -> i32 {
+        if self.stack.is_empty() {
+            return -1;
+        }
+        let top = self.stack.pop().unwrap();
+        self.increment(self.stack.len() as i32, top.1);
+        top.0 + top.1
+    }
+
+    fn increment(&mut self, k: i32, val: i32) {
+        if self.stack.is_empty() {
+            return;
+        }
+        let n = self.stack.len();
+        if n < k as usize {
+            self.stack[n - 1].1 += val;
+        } else {
+            self.stack[k as usize - 1].1 += val;
+        }
+    }
+}
