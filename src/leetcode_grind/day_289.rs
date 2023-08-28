d6a4633863af1e35d5fca7a2fcc7ab2b03311199
54k <54k@bk.ru>
2023-08-28 11:52:00 +0200

    day 289

--- src/leetcode_grind/day_289.rs
+++ src/leetcode_grind/day_289.rs
@@ -1,41 +1,80 @@
 // https://leetcode.com/problems/implement-stack-using-queues/description/
-use std::collections::VecDeque;
+mod stack_2_queues {
+    use std::collections::VecDeque;
 
-struct MyStack {
-    q1: VecDeque<i32>,
-    q2: VecDeque<i32>,
-    top: i32,
+    struct MyStack {
+        q1: VecDeque<i32>,
+        q2: VecDeque<i32>,
+        top: i32,
+    }
+
+    impl MyStack {
+        fn new() -> Self {
+            Self {
+                q1: VecDeque::new(),
+                q2: VecDeque::new(),
+                top: -1,
+            }
+        }
+
+        fn push(&mut self, x: i32) {
+            self.q1.push_back(x);
+            self.top = x;
+        }
+
+        fn pop(&mut self) -> i32 {
+            while self.q1.len() > 1 {
+                self.top = self.q1.pop_front().unwrap();
+                self.q2.push_back(self.top);
+            }
+            let res = self.q1.pop_front().unwrap();
+            std::mem::swap(&mut self.q1, &mut self.q2);
+            res
+        }
+
+        fn top(&mut self) -> i32 {
+            self.top
+        }
+
+        fn empty(&mut self) -> bool {
+            self.q1.is_empty()
+        }
+    }
 }
 
-impl MyStack {
-    fn new() -> Self {
-        Self {
-            q1: VecDeque::new(),
-            q2: VecDeque::new(),
-            top: -1,
+mod stack_1_queue {
+    use std::collections::VecDeque;
+    struct MyStack {
+        queue: VecDeque<i32>,
+    }
+
+    impl MyStack {
+        fn new() -> Self {
+            Self {
+                queue: VecDeque::new(),
+            }
         }
-    }
 
-    fn push(&mut self, x: i32) {
-        self.q1.push_back(x);
-        self.top = x;
-    }
-
-    fn pop(&mut self) -> i32 {
-        while self.q1.len() > 1 {
-            self.top = self.q1.pop_front().unwrap();
-            self.q2.push_back(self.top);
+        fn push(&mut self, x: i32) {
+            self.queue.push_back(x);
+            let mut sz = self.queue.len();
+            while sz > 1 {
+                let f = self.queue.pop_front().unwrap();
+                self.queue.push_back(f);
+                sz -= 1;
+            }
         }
-        let res = self.q1.pop_front().unwrap();
-        std::mem::swap(&mut self.q1, &mut self.q2);
-        res
-    }
 
-    fn top(&mut self) -> i32 {
-        self.top
-    }
+        fn pop(&mut self) -> i32 {
+            self.queue.pop_front().unwrap()
+        }
 
-    fn empty(&mut self) -> bool {
-        self.q1.is_empty()
+        fn top(&self) -> i32 {
+            *self.queue.front().unwrap()
+        }
+
+        fn empty(&self) -> bool {
+            self.queue.is_empty()
+        }
     }
 }
