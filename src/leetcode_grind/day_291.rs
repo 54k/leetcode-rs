219c421f27442b885a605ccc70bafa53fb39314b
54k <54k@bk.ru>
2023-08-30 10:58:34 +0200

    day 291

--- src/leetcode_grind/day_291.rs
+++ src/leetcode_grind/day_291.rs
@@ -28,3 +28,47 @@ pub fn maximum_length_of_ranges(nums: Vec<i32>) -> Vec<i32> {
 
     ans
 }
+
+// https://leetcode.com/problems/minimum-replacements-to-sort-the-array/description/
+pub fn minimum_replacement(nums: Vec<i32>) -> i64 {
+    let mut ans = 0;
+    let mut nums = nums.into_iter().map(|x| x as i64).collect::<Vec<_>>();
+    for i in (0..nums.len() - 1).rev() {
+        if nums[i] <= nums[i + 1] {
+            continue;
+        }
+
+        let num_elements = (nums[i] + nums[i + 1] - 1) / nums[i + 1];
+
+        ans += num_elements - 1;
+
+        nums[i] = nums[i] / num_elements;
+    }
+    ans
+}
+
+// https://leetcode.com/problems/find-permutation/description/
+pub fn find_permutation(s: String) -> Vec<i32> {
+    fn rev(res: &mut [i32], start: usize, end: usize) {
+        for i in 0..(end - start) / 2 {
+            res.swap(i + start, end - i - 1);
+        }
+    }
+
+    let s = s.chars().collect::<Vec<_>>();
+    let mut res = vec![0; s.len() + 1];
+    for i in 0..res.len() as i32 {
+        res[i as usize] = i + 1;
+    }
+
+    let mut i = 1;
+    while i <= s.len() {
+        let j = i;
+        while i <= s.len() && s[i - 1] == 'D' {
+            i += 1;
+        }
+        rev(&mut res, j - 1, i);
+        i += 1;
+    }
+    res
+}
