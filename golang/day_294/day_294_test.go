ba52b6938deea439bcd1292a03a49cb2fea9734d
54k <54k@bk.ru>
2023-09-02 14:09:49 +0200

    day 294

---
+++ golang/day_294/day_294_test.go
@@ -0,0 +1,70 @@
+package day294
+
+type TrieNode struct {
+	children map[byte]*TrieNode
+	isWord   bool
+}
+
+func newTrie() *TrieNode {
+	return &TrieNode{map[byte]*TrieNode{}, false}
+}
+
+func buildTrie(dictionary []string) *TrieNode {
+	root := newTrie()
+	for _, word := range dictionary {
+		node := root
+		for _, ch := range word {
+			ch := byte(ch)
+			if _, ok := node.children[ch]; !ok {
+				node.children[ch] = newTrie()
+			}
+			node = node.children[ch]
+		}
+		node.isWord = true
+	}
+	return root
+}
+
+func minExtraChar(s string, dictionary []string) int {
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+
+	memo := make([]int, len(s)+1)
+	for i := 0; i < len(memo); i++ {
+		memo[i] = -1
+	}
+
+	root := buildTrie(dictionary)
+
+	var dp func(int) int
+	dp = func(start int) int {
+		if start == len(s) {
+			return 0
+		}
+		if memo[start] != -1 {
+			return memo[start]
+		}
+
+		node := root
+
+		ans := dp(start+1) + 1
+		for end := start; end < len(s); end++ {
+			ch := s[end]
+			if _, ok := node.children[ch]; !ok {
+				break
+			}
+			node = node.children[ch]
+			if node.isWord {
+				ans = min(ans, dp(end+1))
+			}
+		}
+		memo[start] = ans
+		return ans
+	}
+
+	return dp(0)
+}
