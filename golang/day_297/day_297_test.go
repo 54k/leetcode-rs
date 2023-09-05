b88bcb0442d2251f77d8d27f08d3e79f617e2466
54k <54k@bk.ru>
2023-09-05 13:55:53 +0200

    day 297

--- golang/day_297/day_297_test.go
+++ golang/day_297/day_297_test.go
@@ -7,7 +7,7 @@ type Node struct {
 	Random *Node
 }
 
-func copyRandomList(head *Node) *Node {
+func copyRandomListGraph(head *Node) *Node {
 	cache := map[*Node]*Node{}
 	var copy func(*Node) *Node
 	copy = func(root *Node) *Node {
@@ -28,4 +28,77 @@ func copyRandomList(head *Node) *Node {
 	}
 
 	return copy(head)
+}
+
+func copyRandomListLinkedList(head *Node) *Node {
+	visited := map[*Node]*Node{}
+	if head == nil {
+		return head
+	}
+
+	getClonedNode := func(n *Node) *Node {
+		if n == nil {
+			return n
+		}
+		if _, ok := visited[n]; ok {
+			return visited[n]
+		}
+		visited[n] = &Node{n.Val, nil, nil}
+		return visited[n]
+	}
+
+	oldNode := head
+	newNode := &Node{oldNode.Val, nil, nil}
+	visited[oldNode] = newNode
+
+	for oldNode != nil {
+		newNode.Random = getClonedNode(oldNode.Random)
+		newNode.Next = getClonedNode(oldNode.Next)
+
+		oldNode = oldNode.Next
+		newNode = newNode.Next
+	}
+
+	return visited[head]
+}
+
+func copyRandomListConstantSpace(head *Node) *Node {
+	if head == nil {
+		return head
+	}
+
+	ptr := head
+	for ptr != nil {
+		newNode := &Node{ptr.Val, nil, nil}
+		newNode.Next = ptr.Next
+		ptr.Next = newNode
+		ptr = newNode.Next
+	}
+
+	ptr = head
+	for ptr != nil {
+		if ptr.Random != nil {
+			ptr.Next.Random = ptr.Random.Next
+		} else {
+			ptr.Next.Random = nil
+		}
+		ptr = ptr.Next.Next
+	}
+
+	ptrOldList := head
+	ptrNewList := head.Next
+	headOld := head.Next
+
+	for ptrOldList != nil {
+		ptrOldList.Next = ptrOldList.Next.Next
+		if ptrNewList.Next != nil {
+			ptrNewList.Next = ptrNewList.Next.Next
+		} else {
+			ptrNewList.Next = nil
+		}
+		ptrOldList = ptrOldList.Next
+		ptrNewList = ptrNewList.Next
+	}
+
+	return headOld
 }
