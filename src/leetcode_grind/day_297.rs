5a0586338b8a1b39d9d0bba3f20ffc45014c7304
54k <54k@bk.ru>
2023-09-05 23:43:59 +0200

    day 297

--- src/leetcode_grind/day_297.rs
+++ src/leetcode_grind/day_297.rs
@@ -29,3 +29,41 @@ pub fn max_chunks_to_sorted_ii(arr: Vec<i32>) -> i32 {
     }
     chunks
 }
+
+pub fn max_chunks_to_sorted_iii(arr: Vec<i32>) -> i32 {
+    let n = arr.len();
+    let mut stack = vec![];
+
+    for i in 0..n {
+        let mut maxi = arr[i];
+        while stack.len() > 0 && stack[stack.len() - 1] > arr[i] {
+            maxi = maxi.max(stack.pop().unwrap());
+        }
+        stack.push(maxi);
+    }
+    stack.len() as i32
+}
+
+// https://leetcode.com/problems/sliding-window-maximum/description/
+pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    use std::collections::VecDeque;
+    let mut queue = VecDeque::new();
+    let mut ans = vec![];
+
+    for i in 0..nums.len() {
+        while queue.len() > 0 && nums[queue[queue.len() - 1]] < nums[i] {
+            queue.pop_back();
+        }
+        queue.push_back(i);
+
+        while queue.len() > 0 && (queue[0] as i32) < i as i32 - k as i32 + 1 {
+            queue.pop_front();
+        }
+
+        if i + 1 >= k as usize {
+            ans.push(nums[queue[0]]);
+        }
+    }
+
+    ans
+}
