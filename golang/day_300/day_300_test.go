96fd344248a457fb0207a1b1cbd879ef9e59a42a
54k <54k@bk.ru>
2023-09-08 12:50:04 +0200

    day 300

--- golang/day_300/day_300_test.go
+++ golang/day_300/day_300_test.go
@@ -12,3 +12,20 @@ func minSwaps(s string) int {
 	}
 	return (bal + 1) / 2
 }
+
+// https://leetcode.com/problems/max-chunks-to-make-sorted/description/
+func maxChunksToSorted(arr []int) int {
+	stack := []int{}
+	for _, n := range arr {
+		maxi := n
+		for len(stack) > 0 && stack[len(stack)-1] > n {
+			top := stack[len(stack)-1]
+			if top > maxi {
+				maxi = top
+			}
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, maxi)
+	}
+	return len(stack)
+}
