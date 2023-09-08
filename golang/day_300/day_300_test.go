4cd9e1345e9431a3fbbe69d9ba436568e7f28692
54k <54k@bk.ru>
2023-09-08 19:08:24 +0200

    day 300

--- golang/day_300/day_300_test.go
+++ golang/day_300/day_300_test.go
@@ -29,3 +29,25 @@ func maxChunksToSorted(arr []int) int {
 	}
 	return len(stack)
 }
+
+// https://leetcode.com/problems/ternary-expression-parser/description/
+func parseTernary(expression string) string {
+	for len(expression) > 1 {
+		qMarkIdx := len(expression) - 1
+		for expression[qMarkIdx] != '?' {
+			qMarkIdx--
+		}
+		var ch byte
+		if expression[qMarkIdx-1] == 'T' {
+			ch = expression[qMarkIdx+1]
+		} else {
+			ch = expression[qMarkIdx+3]
+		}
+
+		first := expression[:qMarkIdx-1]
+		second := string(ch)
+		third := expression[qMarkIdx+4:]
+		expression = first + second + third
+	}
+	return expression
+}
--- src/leetcode_grind/day_300.rs
+++ src/leetcode_grind/day_300.rs
@@ -11,3 +11,35 @@ pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
     }
     ans
 }
+
+// https://leetcode.com/problems/ternary-expression-parser/description/
+pub fn parse_ternary(expression: String) -> String {
+    let mut stack = vec![];
+    let expression = expression.chars().collect::<Vec<_>>();
+    let mut i = expression.len() as i32 - 1;
+
+    while i >= 0 {
+        let curr = expression[i as usize];
+        if curr >= '0' && curr <= '9' || curr == 'T' || curr == 'F' {
+            stack.push(curr);
+        } else if curr == '?' {
+            let on_true = stack.pop().unwrap();
+            let on_false = stack.pop().unwrap();
+            stack.push(if expression[i as usize - 1] == 'T' {
+                on_true
+            } else {
+                on_false
+            });
+            i -= 1;
+        }
+
+        i -= 1;
+    }
+    stack.pop().unwrap().to_string()
+}
+
+#[test]
+fn test_parse_ternary() {
+    let res = parse_ternary("T?2:3".to_string());
+    println!("{res}");
+}
