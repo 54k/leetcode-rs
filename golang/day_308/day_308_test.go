9704b702ca334fab83de56aa34cd5f78906a9267
54k <54k@bk.ru>
2023-09-16 13:27:48 +0200

    day 308

--- golang/day_308/day_308_test.go
+++ golang/day_308/day_308_test.go
@@ -79,6 +79,75 @@ func minimumEffortPathBfs(heights [][]int) int {
 	return minEffort
 }
 
+func minimumEffortPathDfs(heights [][]int) int {
+	type pair struct {
+		x, y int
+	}
+
+	abs := func(a int) int {
+		if a < 0 {
+			return -a
+		}
+		return a
+	}
+
+	canReach := func(effort int) bool {
+		visited := [][]bool{}
+		for i := 0; i < len(heights); i++ {
+			visited = append(visited, make([]bool, len(heights[0])))
+		}
+		visited[0][0] = true
+
+		validMoves := func(cur pair) []pair {
+			dirs := []pair{{-1, 0}, {1, 0}, {0, 1}, {0, -1}}
+			moves := []pair{}
+			for _, dir := range dirs {
+				nx, ny := cur.x+dir.x, cur.y+dir.y
+				if nx < 0 || nx >= len(heights) || ny < 0 || ny >= len(heights[0]) {
+					continue
+				}
+				if visited[nx][ny] {
+					continue
+				}
+				diff := abs(heights[cur.x][cur.y] - heights[nx][ny])
+				if diff <= effort {
+					moves = append(moves, pair{nx, ny})
+				}
+			}
+			return moves
+		}
+
+		var dfs func(pair) bool
+		dfs = func(v pair) bool {
+			if v.x == len(heights)-1 && v.y == len(heights[0])-1 {
+				return true
+			}
+
+			for _, u := range validMoves(v) {
+				visited[u.x][u.y] = true
+				if dfs(u) {
+					return true
+				}
+			}
+			return false
+		}
+
+		return dfs(pair{0, 0})
+	}
+
+	left, right := 0, 1<<31
+	for left < right {
+		mid := left + (right-left)/2
+		if canReach(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
+
 func TestMinEffortPath(t *testing.T) {
 	res := minimumEffortPathBfs([][]int{{1, 2, 2}, {3, 8, 2}, {5, 3, 5}})
 	fmt.Println(res) // 2
