71d701eb1f58fccc336d92a5aec8562ede44a3cb
54k <54k@bk.ru>
2023-09-22 18:35:20 +0200

    day 314

--- src/leetcode_grind/day_314.rs
+++ src/leetcode_grind/day_314.rs
@@ -64,3 +64,41 @@ pub fn is_subsequence_dp_lcs_ii(s: String, t: String) -> bool {
     }
     false
 }
+
+// https://leetcode.com/problems/shortest-way-to-form-string/description/
+pub fn shortest_way(source: String, target: String) -> i32 {
+    fn is_subsequece(s: &Vec<char>, t: &Vec<char>) -> bool {
+        let mut i = 0;
+        for &ch in t {
+            if ch == s[i] {
+                i += 1;
+            }
+            if i == s.len() {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    let (s, t) = (
+        source.chars().collect::<Vec<_>>(),
+        target.chars().collect::<Vec<_>>(),
+    );
+
+    let mut left = 0;
+    let mut right = target.len() as i32 + 1;
+
+    while left < right {
+        let mid = (left + right) / 2;
+        if is_subsequece(&t, &s.repeat(mid as usize)) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    if left > t.len() as i32 {
+        return -1;
+    }
+    left
+}
