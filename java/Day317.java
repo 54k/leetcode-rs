98ef5706f76c94922e59057b88fb7a12839a517d
54k <54k@bk.ru>
2023-09-25 02:42:14 +0200

    day 317

--- java/Day317.java
+++ java/Day317.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day317 {
     // https://leetcode.com/problems/find-the-difference/description
     static class Solution {
@@ -10,6 +13,52 @@ public class Day317 {
                 ch ^= c;
             }
             return ch;
+        }
+    }
+
+    static class Trie {
+        static class Node {
+            Map<Character, Node> children = new HashMap<Character, Node>();
+            boolean isEnd;
+        }
+
+        Node root;
+
+        public Trie() {
+            root = new Node();
+        }
+
+        public void insert(String word) {
+            var root = this.root;
+            for (var ch : word.toCharArray()) {
+                if (!root.children.containsKey(ch)) {
+                    root.children.put(ch, new Node());
+                }
+                root = root.children.get(ch);
+            }
+            root.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            var root = this.root;
+            for (var ch : word.toCharArray()) {
+                if (!root.children.containsKey(ch)) {
+                    return false;
+                }
+                root = root.children.get(ch);
+            }
+            return root.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            var root = this.root;
+            for (var ch : prefix.toCharArray()) {
+                if (!root.children.containsKey(ch)) {
+                    return false;
+                }
+                root = root.children.get(ch);
+            }
+            return true;
         }
     }
 }
