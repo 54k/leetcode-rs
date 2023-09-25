e31f60545a0702c0b80f135ea85e3c63b4340853
54k <54k@bk.ru>
2023-09-25 20:34:13 +0200

    day 317

--- golang/day_317/day_317_test.go
+++ golang/day_317/day_317_test.go
@@ -1,5 +1,7 @@
 package day317
 
+import "strings"
+
 // https://leetcode.com/problems/find-the-difference/description
 func findTheDifference(s string, t string) byte {
 	tMap := map[rune]int{}
@@ -16,4 +18,52 @@ func findTheDifference(s string, t string) byte {
 		return byte(k)
 	}
 	panic("oops")
+}
+
+// https://leetcode.com/problems/replace-words/description/
+func replaceWords(dictionary []string, sentence string) string {
+	type TrieNode struct {
+		children map[rune]*TrieNode
+		word     string
+	}
+
+	new := func() *TrieNode {
+		return &TrieNode{
+			children: make(map[rune]*TrieNode, 26),
+		}
+	}
+
+	trie := new()
+	for _, root := range dictionary {
+		cur := trie
+		for _, ch := range root {
+			if cur.children[ch-'a'] == nil {
+				cur.children[ch-'a'] = new()
+			}
+			cur = cur.children[ch-'a']
+		}
+		cur.word = root
+	}
+
+	ans := ""
+	for _, w := range strings.Split(sentence, " ") {
+		if len(ans) > 0 {
+			ans += " "
+		}
+
+		cur := trie
+		for _, ch := range w {
+			if cur.children[ch-'a'] == nil || cur.word != "" {
+				break
+			}
+			cur = cur.children[ch-'a']
+		}
+		if cur.word != "" {
+			ans += cur.word
+		} else {
+			ans += w
+		}
+	}
+
+	return ans
 }
