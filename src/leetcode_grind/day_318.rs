217f0a618f16d3e2c72b4138c0f573a33cb9a2a3
54k <54k@bk.ru>
2023-09-26 14:56:32 +0200

    day 318

--- src/leetcode_grind/day_318.rs
+++ src/leetcode_grind/day_318.rs
@@ -26,3 +26,74 @@ pub fn remove_duplicate_letters(s: String) -> String {
 
     stack.into_iter().collect::<_>()
 }
+
+// https://leetcode.com/problems/3sum-smaller/description/
+pub fn three_sum_smaller_bin_search(nums: Vec<i32>, target: i32) -> i32 {
+    if nums.len() < 3 {
+        return 0;
+    }
+
+    fn two_sum_smaller(nums: &Vec<i32>, start: usize, target: i32) -> i32 {
+        fn binary_search(nums: &Vec<i32>, start: usize, target: i32) -> usize {
+            let mut lo = start;
+            let mut hi = nums.len() - 1;
+
+            while lo < hi {
+                let mid = (lo + hi + 1) / 2;
+                if nums[mid] < target {
+                    lo = mid;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+            lo
+        }
+
+        let mut sum = 0;
+        for i in start..nums.len() - 1 {
+            let j = binary_search(nums, i, target - nums[i]);
+            sum += j as i32 - i as i32;
+        }
+        sum
+    }
+
+    let mut nums = nums;
+    nums.sort();
+    let mut sum = 0;
+    for i in 0..nums.len() - 2 {
+        sum += two_sum_smaller(&nums, i + 1, target - nums[i]);
+    }
+    sum
+}
+
+pub fn three_sum_smaller_two_pointers(nums: Vec<i32>, target: i32) -> i32 {
+    if nums.len() < 3 {
+        return 0;
+    }
+
+    fn two_sum_smaller(nums: &Vec<i32>, start: usize, target: i32) -> i32 {
+        let mut sum = 0;
+        let mut left = start;
+        let mut right = nums.len() - 1;
+
+        while left < right {
+            if nums[left] + nums[right] < target {
+                sum += right as i32 - left as i32;
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        sum
+    }
+
+    let mut nums = nums;
+    nums.sort();
+    let mut count = 0;
+
+    for i in 0..nums.len() - 2 {
+        count += two_sum_smaller(&nums, i + 1, target - nums[i]);
+    }
+
+    count
+}
