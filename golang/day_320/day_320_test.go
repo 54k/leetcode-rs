6c51ce5109c13599a3298e6fa66210cf86a089e5
54k <54k@bk.ru>
2023-09-28 22:32:46 +0200

    day 320

--- golang/day_320/day_320_test.go
+++ golang/day_320/day_320_test.go
@@ -1,13 +1,27 @@
 package day320
 
 // https://leetcode.com/problems/sort-array-by-parity/description
-func sortArrayByParity(nums []int) []int {
+func sortArrayByParityLomuto(nums []int) []int {
 	left := 0
 	for right := 0; right < len(nums); right++ {
 		if nums[right]&1 == 0 {
 			nums[left], nums[right] = nums[right], nums[left]
 			left++
 		}
+	}
+	return nums
+}
+
+func sortArrayByParityHoare(nums []int) []int {
+	left, right := 0, len(nums)-1
+	for left != right {
+		for left != right && nums[left]&1 == 0 {
+			left++
+		}
+		for left != right && nums[right]&1 == 1 {
+			right--
+		}
+		nums[left], nums[right] = nums[right], nums[left]
 	}
 	return nums
 }
