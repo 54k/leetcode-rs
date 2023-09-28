cbdc15d55ea0727c26d9c8f9da4e223c21239fff
54k <54k@bk.ru>
2023-09-28 02:39:12 +0200

    day 320

---
+++ golang/day_320/day_320_test.go
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/sort-array-by-parity/description
+func sortArrayByParity(nums []int) []int {
+	left := 0
+	for right := 0; right < len(nums); right++ {
+		if nums[right]&1 == 0 {
+			nums[left], nums[right] = nums[right], nums[left]
+			left++
+		}
+	}
+	return nums
+}
