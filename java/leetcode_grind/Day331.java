5d6f880522a7c4336234ce7052c081d685779519
54k <54k@bk.ru>
2023-10-09 21:08:29 +0200

    day 331

--- java/leetcode_grind/Day331.java
+++ java/leetcode_grind/Day331.java
@@ -2,6 +2,7 @@ package leetcode_grind;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Day331 {
@@ -146,6 +147,249 @@ public class Day331 {
                 }
 
                 return nodesAndParents.get(data.charAt(0) - '0').second.second;
+            }
+        }
+    }
+
+    static class Solution3 {
+        class Node {
+            public int val;
+            public List<Node> children;
+
+            public Node() {
+            }
+
+            public Node(int _val) {
+                val = _val;
+            }
+
+            public Node(int _val, List<Node> _children) {
+                val = _val;
+                children = _children;
+            }
+        }
+
+        class Codec {
+            static class WrappableInt {
+                int value;
+
+                WrappableInt(int v) {
+                    value = v;
+                }
+
+                void increment() {
+                    value++;
+                }
+            }
+
+            // Encodes a tree to a single string.
+            public String serialize(Node root) {
+                var sb = new StringBuilder();
+                _serialize(root, sb);
+                return sb.toString();
+            }
+
+            void _serialize(Node root, StringBuilder sb) {
+                if (root == null) {
+                    return;
+                }
+
+                sb.append((char) (root.val + '0'));
+                sb.append((char) (root.children.size() + '0'));
+
+                for (var child : root.children) {
+                    _serialize(child, sb);
+                }
+            }
+
+            public Node deserialize(String data) {
+                if (data.isEmpty()) {
+                    return null;
+                }
+                return _desearialize(data, new WrappableInt(0));
+            }
+
+            Node _desearialize(String data, WrappableInt index) {
+                if (index.value == data.length()) {
+                    return null;
+                }
+                var node = new Node(data.charAt(index.value) - '0', new ArrayList<>());
+                index.increment();
+                var numChildren = data.charAt(index.value) - '0';
+                for (int i = 0; i < numChildren; i++) {
+                    index.increment();
+                    node.children.add(_desearialize(data, index));
+                }
+                return node;
+            }
+        }
+    }
+
+    static class Solution4 {
+        class Node {
+            public int val;
+            public List<Node> children;
+
+            public Node() {
+            }
+
+            public Node(int _val) {
+                val = _val;
+            }
+
+            public Node(int _val, List<Node> _children) {
+                val = _val;
+                children = _children;
+            }
+        }
+
+        class Codec {
+            static class WrappableInt {
+                int value;
+
+                WrappableInt(int v) {
+                    value = v;
+                }
+
+                void increment() {
+                    value++;
+                }
+            }
+
+            public String serialize(Node root) {
+                var sb = new StringBuilder();
+                _serialize(root, sb);
+                return sb.toString();
+            }
+
+            void _serialize(Node root, StringBuilder sb) {
+                if (root == null) {
+                    return;
+                }
+
+                sb.append((char) (root.val + '0'));
+
+                for (var child : root.children) {
+                    _serialize(child, sb);
+                }
+
+                sb.append('#');
+            }
+
+            public Node deserialize(String data) {
+                if (data.isEmpty()) {
+                    return null;
+                }
+                return _desearialize(data, new WrappableInt(0));
+            }
+
+            Node _desearialize(String data, WrappableInt index) {
+                if (index.value == data.length()) {
+                    return null;
+                }
+
+                var node = new Node(data.charAt(index.value) - '0', new ArrayList<>());
+                index.increment();
+
+                while (data.charAt(index.value) != '#') {
+                    node.children.add(_desearialize(data, index));
+                }
+
+                index.increment();
+
+                return node;
+            }
+        }
+    }
+
+    static class Solution5 {
+        class Node {
+            public int val;
+            public List<Node> children;
+
+            public Node() {
+            }
+
+            public Node(int _val) {
+                val = _val;
+            }
+
+            public Node(int _val, List<Node> _children) {
+                val = _val;
+                children = _children;
+            }
+        }
+
+        class Codec {
+            public String serialize(Node root) {
+                if (root == null)
+                    return "";
+                var sb = new StringBuilder();
+                _serialize(root, sb);
+                return sb.toString();
+            }
+
+            void _serialize(Node root, StringBuilder sb) {
+                var q = new LinkedList<Node>();
+
+                var endNode = new Node();
+                var childNode = new Node();
+                q.add(root);
+                q.add(endNode);
+
+                while (!q.isEmpty()) {
+                    var node = q.poll();
+                    if (node == endNode) {
+                        sb.append('#');
+                        if (!q.isEmpty()) {
+                            q.add(endNode);
+                        }
+                    } else if (node == childNode) {
+                        sb.append('$');
+                    } else {
+                        sb.append((char) (node.val + '0'));
+                        for (var child : node.children) {
+                            q.add(child);
+                        }
+
+                        if (q.peek() != endNode) {
+                            q.add(childNode);
+                        }
+                    }
+                }
+            }
+
+            public Node deserialize(String data) {
+                if (data.isEmpty()) {
+                    return null;
+                }
+                var rootNode = new Node(data.charAt(0) - '0', new ArrayList<>());
+                _deserialize(data, rootNode);
+                return rootNode;
+            }
+
+            void _deserialize(String data, Node rootNode) {
+                var currentLevel = new LinkedList<Node>();
+                var prevLevel = new LinkedList<Node>();
+                currentLevel.add(rootNode);
+                Node parentNode = rootNode;
+
+                for (int i = 1; i < data.length(); i++) {
+                    char d = data.charAt(i);
+                    if (d == '#') {
+                        prevLevel = currentLevel;
+                        currentLevel = new LinkedList<>();
+                        parentNode = prevLevel.poll();
+                    } else {
+                        if (d == '$') {
+                            parentNode = prevLevel.poll();
+                        } else {
+                            Node childNode = new Node(d - '0', new ArrayList<>());
+                            currentLevel.add(childNode);
+                            parentNode.children.add(childNode);
+                        }
+                    }
+                }
             }
         }
     }
