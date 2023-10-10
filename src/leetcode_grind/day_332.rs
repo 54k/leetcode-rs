9df038a2178d47f37d6c6ab5940203dcfb21619e
54k <54k@bk.ru>
2023-10-10 20:00:44 +0200

    day 332

--- src/leetcode_grind/day_332.rs
+++ src/leetcode_grind/day_332.rs
@@ -75,3 +75,52 @@ pub fn min_operations(nums: Vec<i32>) -> i32 {
 
     ans
 }
+
+// https://leetcode.com/problems/valid-number/description/
+pub fn is_number(s: String) -> bool {
+    use std::collections::{HashMap, HashSet};
+    let dfa = vec![
+        HashMap::from([("num", 1), ("sign", 2), ("dot", 3)]),
+        HashMap::from([("num", 1), ("dot", 4), ("exponent", 5)]),
+        HashMap::from([("num", 1), ("dot", 3)]),
+        HashMap::from([("num", 4)]),
+        HashMap::from([("num", 4), ("exponent", 5)]),
+        HashMap::from([("sign", 6), ("num", 7)]),
+        HashMap::from([("num", 7)]),
+        HashMap::from([("num", 7)]),
+    ];
+
+    let finite_state = HashSet::from([1, 4, 7]);
+
+    let mut curr_state = 0;
+    let mut curr_group = "";
+
+    let s = s.chars().collect::<Vec<_>>();
+    for i in 0..s.len() {
+        if char::is_digit(s[i], 10) {
+            curr_group = "num";
+        } else if s[i] == '-' || s[i] == '+' {
+            curr_group = "sign";
+        } else if s[i] == 'e' || s[i] == 'E' {
+            curr_group = "exponent";
+        } else if s[i] == '.' {
+            curr_group = "dot";
+        } else {
+            return false;
+        }
+
+        if dfa[curr_state].contains_key(curr_group) {
+            curr_state = dfa[curr_state][curr_group];
+        } else {
+            return false;
+        }
+    }
+
+    finite_state.contains(&curr_state)
+}
+
+#[test]
+fn test_is_number() {
+    let res = is_number("2e0".to_string());
+    println!("{res}");
+}
