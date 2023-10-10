23298ee6e2727fb701a6bd8f96aa24d624ed6eb6
54k <54k@bk.ru>
2023-10-10 22:49:17 +0200

    day 332

--- golang/day_332/day_332_test.go
+++ golang/day_332/day_332_test.go
@@ -1,6 +1,30 @@
 package day332
 
+import "sort"
+
 // https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous/description/
-func minOperationsBinarySearch(nums []int) int {
-	return 0
+func minOperations(nums []int) int {
+	n := len(nums)
+	uniq := map[int]bool{}
+	for _, num := range nums {
+		uniq[num] = true
+	}
+	newNum := []int{}
+	for num, _ := range uniq {
+		newNum = append(newNum, num)
+	}
+
+	sort.Ints(newNum)
+	ans := 1 << 30
+	j := 0
+	for i := 0; i < len(newNum); i++ {
+		for j < len(newNum) && newNum[j] < newNum[i]+n {
+			j++
+		}
+		count := j - i
+		if ans > n-count {
+			ans = n - count
+		}
+	}
+	return ans
 }
