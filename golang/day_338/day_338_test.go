bcc967139e03770a8497266574f197a11cef04e6
54k <54k@bk.ru>
2023-10-16 18:12:02 +0200

    day 338

--- golang/day_338/day_338_test.go
+++ golang/day_338/day_338_test.go
@@ -15,3 +15,28 @@ func generate(numRows int) [][]int {
 	}
 	return triangle
 }
+
+// https://leetcode.com/problems/next-greater-node-in-linked-list/description/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func nextLargerNodes(head *ListNode) []int {
+	type pair struct {
+		idx, val int
+	}
+	ans := []int{}
+	stack := []pair{}
+	for head != nil {
+		for len(stack) > 0 && stack[len(stack)-1].val < head.Val {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			ans[top.idx] = head.Val
+		}
+		ans = append(ans, 0)
+		stack = append(stack, pair{len(ans) - 1, head.Val})
+		head = head.Next
+	}
+	return ans
+}
