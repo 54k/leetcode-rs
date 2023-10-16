fcbbb20f90e41c57b57a7e068b249df7fabb9391
54k <54k@bk.ru>
2023-10-16 13:36:26 +0200

    day 338

--- src/leetcode_grind/day_338.rs
+++ src/leetcode_grind/day_338.rs
@@ -25,3 +25,55 @@ pub fn get_row_mem_optimized_dp(row_index: i32) -> Vec<i32> {
     }
     row
 }
+
+// https://leetcode.com/problems/painting-the-walls/
+pub fn paint_walls(cost: Vec<i32>, time: Vec<i32>) -> i32 {
+    let n = cost.len();
+    const INF: i32 = 1000_000_000;
+
+    let mut dp = vec![INF; n + 1];
+    dp[0] = 0;
+
+    for i in (0..n).rev() {
+        let mut next = vec![0; n + 1];
+        for remain in 1..=n {
+            let paint = cost[i] + dp[0.max(remain as i32 - 1 - time[i]) as usize];
+            let not_paint = dp[remain];
+            next[remain] = paint.min(not_paint);
+        }
+        dp = next;
+    }
+    dp[n]
+}
+
+// https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/description/
+pub fn num_ways(steps: i32, arr_len: i32) -> i32 {
+    const MOD: i32 = 1000_000_007;
+    let arr_len = arr_len.min(steps) as usize;
+    let steps = steps as usize;
+    let mut prev = vec![0; arr_len];
+    prev[0] = 1;
+
+    for _ in 1..=steps {
+        let mut next = vec![0; arr_len];
+        for pos in 0..arr_len {
+            let mut ans = prev[pos];
+
+            let mut left = 0;
+            if pos > 0 {
+                left = prev[pos - 1];
+            }
+
+            let mut right = 0;
+            if pos < arr_len - 1 {
+                right = prev[pos + 1];
+            }
+
+            ans = ((ans + left) % MOD + right) % MOD;
+            next[pos] = ans;
+        }
+        prev = next;
+    }
+
+    prev[0]
+}
