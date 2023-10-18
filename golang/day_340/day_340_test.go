e406e3b0d8a0cc4846c623e697492adc20e4ddc4
54k <54k@bk.ru>
2023-10-18 22:21:57 +0200

    day 340

--- golang/day_340/day_340_test.go
+++ golang/day_340/day_340_test.go
@@ -1,5 +1,11 @@
 package day340
 
+import (
+	"fmt"
+	"testing"
+	"unicode"
+)
+
 // https://leetcode.com/problems/parallel-courses-iii/description/
 func minimumTime(n int, relations [][]int, time []int) int {
 	max := func(a, b int) int {
@@ -42,4 +48,44 @@ func minimumTime(n int, relations [][]int, time []int) int {
 		ans = max(ans, dfs(node))
 	}
 	return ans
+}
+
+// 1 - 2 * 3 / 2 == -2
+// https://leetcode.com/problems/basic-calculator-ii/description/
+func calculate(s string) int {
+	result := 0
+
+	currentNum := 0
+	operator := '+'
+	lastNum := 0
+
+	for i, ch := range s {
+		if unicode.IsDigit(ch) {
+			currentNum = currentNum*10 + int(ch-'0')
+		}
+
+		if !unicode.IsDigit(ch) && ch != ' ' || i == len(s)-1 {
+			if operator == '+' {
+				result += lastNum
+				lastNum = currentNum
+			} else if operator == '-' {
+				result += lastNum
+				lastNum = -currentNum
+			} else if operator == '*' {
+				lastNum *= currentNum
+			} else if operator == '/' {
+				lastNum /= currentNum
+			}
+
+			operator = ch
+			currentNum = 0
+		}
+	}
+
+	return result + lastNum
+}
+
+func TestCalclulate(t *testing.T) {
+	res := calculate("1 - 2 * 3 / 2")
+	fmt.Println(res) // -2
 }
