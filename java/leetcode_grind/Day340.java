e90d14a1039b480e0ee65927cf87d8c1f50d6e52
54k <54k@bk.ru>
2023-10-18 21:35:57 +0200

    day 340

--- java/leetcode_grind/Day340.java
+++ java/leetcode_grind/Day340.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 public class Day340 {
     // https://leetcode.com/problems/parallel-courses-iii/description/
@@ -164,6 +165,43 @@ public class Day340 {
                 }
             }
             return validList.size();
+        }
+    }
+
+    // https://leetcode.com/problems/basic-calculator-ii/description/
+    static class Solution3 {
+        public int calculate(String s) {
+            if (s == null || s.isEmpty())
+                return 0;
+            int len = s.length();
+            Stack<Integer> stack = new Stack<>();
+            int currentNumber = 0;
+            char operation = '+';
+            for (int i = 0; i < len; i++) {
+                char currentChar = s.charAt(i);
+                if (Character.isDigit(currentChar)) {
+                    currentNumber = (currentNumber * 10) + (currentChar - '0');
+                }
+                if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == len - 1) {
+                    if (operation == '-') {
+                        stack.push(-currentNumber);
+                    } else if (operation == '+') {
+                        stack.push(currentNumber);
+                    } else if (operation == '*') {
+                        stack.push(stack.pop() * currentNumber);
+                    } else if (operation == '/') {
+                        stack.push(stack.pop() / currentNumber);
+                    }
+                    operation = currentChar;
+                    currentNumber = 0;
+                }
+            }
+
+            int result = 0;
+            while (!stack.isEmpty()) {
+                result += stack.pop();
+            }
+            return result;
         }
     }
 }
