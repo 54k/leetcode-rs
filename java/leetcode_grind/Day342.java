c07fd0b9d583f80a498c4ac48585355edeccd26f
54k <54k@bk.ru>
2023-10-20 20:26:51 +0200

    day 342

--- java/leetcode_grind/Day342.java
+++ java/leetcode_grind/Day342.java
@@ -5,59 +5,146 @@ import java.util.List;
 import java.util.Stack;
 
 public class Day342 {
-    public interface NestedInteger {
-        // @return true if this NestedInteger holds a single integer, rather than a
-        // nested list.
-        public boolean isInteger();
+    // https://leetcode.com/problems/mini-parser/description/
+    static class NestedIteratorProblem {
+        public interface NestedInteger {
+            // @return true if this NestedInteger holds a single integer, rather than a
+            // nested list.
+            public boolean isInteger();
 
-        // @return the single integer that this NestedInteger holds, if it holds a
-        // single integer
-        // Return null if this NestedInteger holds a nested list
-        public Integer getInteger();
+            // @return the single integer that this NestedInteger holds, if it holds a
+            // single integer
+            // Return null if this NestedInteger holds a nested list
+            public Integer getInteger();
 
-        // @return the nested list that this NestedInteger holds, if it holds a nested
-        // list
-        // Return empty list if this NestedInteger holds a single integer
-        public List<NestedInteger> getList();
-    }
-
-    // https://leetcode.com/problems/flatten-nested-list-iterator/
-    public class NestedIterator implements Iterator<Integer> {
-        Integer ret;
-        Stack<Iterator<NestedInteger>> list = new Stack<>();
-
-        public NestedIterator(List<NestedInteger> nestedList) {
-            list.push(nestedList.iterator());
+            // @return the nested list that this NestedInteger holds, if it holds a nested
+            // list
+            // Return empty list if this NestedInteger holds a single integer
+            public List<NestedInteger> getList();
         }
 
-        @Override
-        public Integer next() {
-            hasNext();
-            var val = ret;
-            ret = null;
-            return val;
-        }
+        public class NestedIterator implements Iterator<Integer> {
+            Integer ret;
+            Stack<Iterator<NestedInteger>> list = new Stack<>();
 
-        @Override
-        public boolean hasNext() {
-            if (ret == null) {
-                if (list.isEmpty()) {
-                    return false;
-                }
-                if (list.peek().hasNext()) {
-                    var next = list.peek().next();
-                    if (next.isInteger()) {
-                        ret = next.getInteger();
+            public NestedIterator(List<NestedInteger> nestedList) {
+                list.push(nestedList.iterator());
+            }
+
+            @Override
+            public Integer next() {
+                hasNext();
+                var val = ret;
+                ret = null;
+                return val;
+            }
+
+            @Override
+            public boolean hasNext() {
+                if (ret == null) {
+                    if (list.isEmpty()) {
+                        return false;
+                    }
+                    if (list.peek().hasNext()) {
+                        var next = list.peek().next();
+                        if (next.isInteger()) {
+                            ret = next.getInteger();
+                        } else {
+                            list.push(next.getList().iterator());
+                            hasNext();
+                        }
                     } else {
-                        list.push(next.getList().iterator());
+                        list.pop();
                         hasNext();
                     }
-                } else {
-                    list.pop();
-                    hasNext();
                 }
+                return ret != null;
             }
-            return ret != null;
+        }
+    }
+    
+    // https://leetcode.com/problems/mini-parser/description/
+    static class NestedListParserProblem {
+        public static class NestedInteger {
+            // Constructor initializes an empty nested list.
+            public NestedInteger() {
+            }
+
+            // Constructor initializes a single integer.
+            public NestedInteger(int value) {
+            }
+
+            // @return true if this NestedInteger holds a single integer, rather than a
+            // nested list.
+            public boolean isInteger() {
+                return false;
+            }
+
+            // @return the single integer that this NestedInteger holds, if it holds a
+            // single integer
+            // Return null if this NestedInteger holds a nested list
+            public Integer getInteger() {
+                return 0;
+            }
+
+            // Set this NestedInteger to hold a single integer.
+            public void setInteger(int value) {
+            }
+
+            // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+            public void add(NestedInteger ni) {
+            }
+
+            // @return the nested list that this NestedInteger holds, if it holds a nested
+            // list
+            // Return empty list if this NestedInteger holds a single integer
+            public List<NestedInteger> getList() {
+                return List.of();
+            }
+        }
+
+        static class SolutionRecursive {
+            public NestedInteger deserialize(String s) {
+                var i = new int[1];
+                return solve(s, i).getList().get(0);
+            }
+
+            NestedInteger solve(String s, int[] i) {
+                NestedInteger result = new NestedInteger();
+                Integer currentNum = null;
+                Integer sign = 1;
+
+                while (i[0] < s.length()) {
+                    var ch = s.charAt(i[0]);
+                    if (ch == '-') {
+                        sign = -1;
+                    } else if (Character.isDigit(ch)) {
+                        if (currentNum == null) {
+                            currentNum = 0;
+                        }
+                        currentNum = currentNum * 10 + (ch - '0');
+                    } else if (ch == '[') {
+                        i[0]++;
+                        result.add(solve(s, i));
+                    } else if (ch == ',') {
+                        if (currentNum != null) {
+                            result.add(new NestedInteger(currentNum * sign));
+                        }
+                        currentNum = null;
+                        sign = 1;
+                    } else if (ch == ']') {
+                        break;
+                    }
+
+                    i[0]++;
+                }
+
+                if (currentNum != null) {
+                    result.add(new NestedInteger(currentNum * sign));
+                }
+
+                return result;
+            }
         }
     }
 }
