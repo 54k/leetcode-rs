cd6b5e37a74da8b1e658724a1754bb5bbe2f17f0
54k <54k@bk.ru>
2023-10-20 19:34:49 +0200

    day 342

--- golang/day_342/day_342_test.go
+++ golang/day_342/day_342_test.go
@@ -62,3 +62,65 @@ func calculate(s string) int {
 	i := 0
 	return solve(s+"@", &i)
 }
+
+// https://leetcode.com/problems/flatten-nested-list-iterator/description/
+
+// This is the interface that allows for creating nested lists.
+// You should not implement it, or speculate about its implementation
+type NestedInteger struct {
+}
+
+// Return true if this NestedInteger holds a single integer, rather than a nested list.
+func (this NestedInteger) IsInteger() bool { return false }
+
+// Return the single integer that this NestedInteger holds, if it holds a single integer
+// The result is undefined if this NestedInteger holds a nested list
+// So before calling this method, you should have a check
+func (this NestedInteger) GetInteger() int { return 0 }
+
+// Set this NestedInteger to hold a single integer.
+func (n *NestedInteger) SetInteger(value int) {}
+
+// Set this NestedInteger to hold a nested list and adds a nested integer to it.
+func (this *NestedInteger) Add(elem NestedInteger) {}
+
+// Return the nested list that this NestedInteger holds, if it holds a nested list
+// The list length is zero if this NestedInteger holds a single integer
+// You can access NestedInteger's List element directly if you want to modify it
+func (this NestedInteger) GetList() []*NestedInteger { return []*NestedInteger{} }
+
+type NestedIterator struct {
+	integers []int
+	position int
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+	integers := []int{}
+
+	var flatten func(list []*NestedInteger)
+	flatten = func(list []*NestedInteger) {
+		for _, li := range list {
+			if li.IsInteger() {
+				integers = append(integers, li.GetInteger())
+			} else {
+				flatten(li.GetList())
+			}
+		}
+	}
+
+	flatten(nestedList)
+	return &NestedIterator{integers, 0}
+}
+
+func (this *NestedIterator) Next() int {
+	if this.position >= len(this.integers) {
+		return -1
+	}
+	res := this.integers[this.position]
+	this.position++
+	return res
+}
+
+func (this *NestedIterator) HasNext() bool {
+	return this.position < len(this.integers)
+}
