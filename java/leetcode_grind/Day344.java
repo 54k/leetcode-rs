dc2f0a72feb40386f76695fffce166c51a176d27
54k <54k@bk.ru>
2023-10-23 00:52:16 +0200

    day 344

--- java/leetcode_grind/Day344.java
+++ java/leetcode_grind/Day344.java
@@ -102,4 +102,111 @@ public class Day344 {
             return ans;
         }
     }
+
+    // https://leetcode.com/problems/maximum-score-of-a-good-subarray/description/
+    static class Solution3 {
+        public int maximumScoreBinarySearch(int[] nums, int k) {
+            var binSearch = new Object() {
+                int apply(int[] nums, int target) {
+                    var lo = 0;
+                    var hi = nums.length;
+                    while (lo < hi) {
+                        var mid = (lo + hi) / 2;
+                        if (nums[mid] < target) {
+                            lo = mid + 1;
+                        } else {
+                            hi = mid;
+                        }
+                    }
+                    return lo;
+                }
+            };
+
+            var solve = new Object() {
+                int apply(int[] nums, int k) {
+                    var left = new int[k];
+                    var currMin = Integer.MAX_VALUE;
+                    for (int i = k - 1; i >= 0; i--) {
+                        currMin = Math.min(currMin, nums[i]);
+                        left[i] = currMin;
+                    }
+
+                    var right = new int[nums.length - k];
+                    currMin = Integer.MAX_VALUE;
+                    for (int i = 0; i < right.length; i++) {
+                        currMin = Math.min(currMin, nums[i + k]);
+                        right[i] = currMin;
+                    }
+
+                    var ans = 0;
+                    for (int j = 0; j < right.length; j++) {
+                        currMin = right[j];
+                        var i = binSearch.apply(left, currMin);
+                        ans = Math.max(ans, currMin * (k + j - i + 1));
+                    }
+                    return ans;
+                }
+            };
+
+            var ans = solve.apply(nums, k);
+            for (int i = 0; i < nums.length / 2; i++) {
+                var temp = nums[i];
+                nums[i] = nums[nums.length - 1 - i];
+                nums[nums.length - 1 - i] = temp;
+            }
+            return Math.max(ans, solve.apply(nums, nums.length - k - 1));
+        }
+
+        public int maximumScoreMonotonicStack(int[] nums, int k) {
+            var n = nums.length;
+            var left = new int[n];
+            Arrays.fill(left, -1);
+            var stack = new Stack<Integer>();
+
+            for (int i = n - 1; i >= 0; i--) {
+                while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                    left[stack.pop()] = i;
+                }
+                stack.push(i);
+            }
+
+            stack.clear();
+            var right = new int[n];
+            Arrays.fill(right, n);
+
+            for (int i = 0; i < n; i++) {
+                while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                    right[stack.pop()] = i;
+                }
+                stack.push(i);
+            }
+
+            var ans = 0;
+            for (int i = 0; i < n; i++) {
+                if (left[i] < k && right[i] > k) {
+                    ans = Math.max(nums[i] * (right[i] - left[i] - 1), ans);
+                }
+            }
+            return ans;
+        }
+
+        public int maximumScoreGreedy(int[] nums, int k) {
+            var n = nums.length;
+            var ans = nums[k];
+            var currMin = nums[k];
+            var left = k;
+            var right = k;
+            while (left > 0 || right < n - 1) {
+                if ((left > 0 ? nums[left - 1] : 0) < (right < n - 1 ? nums[right + 1] : 0)) {
+                    right++;
+                    currMin = Math.min(currMin, nums[right]);
+                } else {
+                    left--;
+                    currMin = Math.min(currMin, nums[left]);
+                }
+                ans = Math.max(ans, currMin * (right - left + 1));
+            }
+            return ans;
+        }
+    }
 }
