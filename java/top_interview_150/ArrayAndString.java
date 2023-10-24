406347c8a8ef606fc866bf4afbff983916782aa5
54k <54k@bk.ru>
2023-10-24 19:05:13 +0200

    day 346

--- java/top_interview_150/ArrayAndString.java
+++ java/top_interview_150/ArrayAndString.java
@@ -1,7 +1,11 @@
 package top_interview_150;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 // https://leetcode.com/studyplan/top-interview-150/
 public class ArrayAndString {
+
     // https://leetcode.com/problems/merge-sorted-array/description
     static class Solution1 {
         public void merge3PointersBackward(int[] nums1, int m, int[] nums2, int n) {
@@ -49,7 +53,7 @@ public class ArrayAndString {
     }
 
     // https://leetcode.com/problems/remove-duplicates-from-sorted-array
-    static class Solution {
+    static class Solution3 {
         public int removeDuplicates1(int[] nums) {
             int left = 0;
             for (int right = 0; right < nums.length; right++) {
@@ -68,6 +72,148 @@ public class ArrayAndString {
                 }
             }
             return insertIndex;
+        }
+    }
+
+    // https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description
+    static class Solution4 {
+        public int removeDuplicates1(int[] nums) {
+            Consumer<Integer> removeFromArray = (index) -> {
+                for (int i = index; i < nums.length - 1; i++) {
+                    nums[i] = nums[i + 1];
+                }
+            };
+
+            int length = nums.length;
+            int i = 1;
+            int count = 1;
+
+            while (i < length) {
+                if (nums[i] == nums[i - 1]) {
+                    count++;
+                    if (count > 2) {
+                        removeFromArray.accept(i);
+                        i--;
+                        length--;
+                    }
+                }
+                i++;
+            }
+            return length;
+        }
+
+        public int removeDuplicates2(int[] nums) {
+            int j = 1, count = 1;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i - 1] == nums[i]) {
+                    count++;
+                } else {
+                    count = 1;
+                }
+                if (count <= 2) {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+            return j;
+        }
+
+        public int removeDuplicates3(int[] nums) {
+            var k = 2;
+            var left = 0;
+            var dups = 1;
+
+            for (var right = 1; right < nums.length; right++) {
+                if (nums[left] != nums[right]) {
+                    nums[++left] = nums[right];
+                    dups = 1;
+                } else if (dups < k) {
+                    nums[++left] = nums[right];
+                    dups++;
+                }
+            }
+
+            return ++left;
+        }
+    }
+
+    // https://leetcode.com/problems/majority-element
+    static class Solution5 {
+        public int majorityElement3(int[] nums) {
+            Arrays.sort(nums);
+            return nums[nums.length / 2];
+        }
+
+        public int majorityElement4(int[] nums) {
+            int n = nums.length;
+            int majority_element = 0;
+
+            for (int i = 0; i < 32; i++) {
+                int bit = 1 << i;
+
+                int bit_count = 0;
+                for (int num : nums) {
+                    if ((num & bit) != 0) {
+                        bit_count++;
+                    }
+                }
+
+                if (bit_count > n / 2) {
+                    majority_element |= bit;
+                }
+            }
+
+            return majority_element;
+        }
+
+        public int majorityElement6(int[] nums) {
+            var countFreq = new Object() {
+                int apply(int target) {
+                    var freq = 0;
+                    for (var num : nums) {
+                        if (num == target) {
+                            freq++;
+                        }
+                    }
+                    return freq;
+                }
+            };
+
+            var majorityElementRec = new Object() {
+                int apply(int lo, int hi) {
+                    if (lo == hi) {
+                        return nums[lo];
+                    }
+
+                    int mid = (hi - lo) / 2 + lo;
+                    int left = apply(lo, mid);
+                    int right = apply(mid + 1, hi);
+
+                    if (left == right) {
+                        return left;
+                    }
+
+                    if (countFreq.apply(left) > countFreq.apply(right)) {
+                        return left;
+                    } else {
+                        return right;
+                    }
+                }
+            };
+
+            return majorityElementRec.apply(0, nums.length - 1);
+        }
+
+        public int majorityElement7(int[] nums) {
+            int count = 0;
+            Integer candidate = null;
+            for (var num : nums) {
+                if (count == 0) {
+                    candidate = num;
+                }
+                count += candidate == num ? 1 : -1;
+            }
+            return candidate;
         }
     }
 }
