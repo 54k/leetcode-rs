30ba0c995402b21912ae2415140da882cadc3ccf
54k <54k@bk.ru>
2023-10-27 15:03:09 +0200

    day 349

---
+++ golang/day_349/day_349_test.go
@@ -0,0 +1,83 @@
+package day349
+
+// https://leetcode.com/problems/burst-balloons/description/
+func maxCoinsTopDown(nums []int) int {
+	arr := make([]int, len(nums)+2)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = 1
+	}
+	for i := 0; i < len(nums); i++ {
+		arr[i+1] = nums[i]
+	}
+
+	nums = arr
+	n := len(nums)
+
+	memo := make([][]int, n)
+	for i := 0; i < n; i++ {
+		memo[i] = make([]int, n)
+	}
+
+	var dp func(int, int) int
+	dp = func(left, right int) int {
+		if right-left < 0 {
+			return 0
+		}
+		if memo[left][right] > 0 {
+			return memo[left][right]
+		}
+		ans := 0
+		for i := left; i <= right; i++ {
+			rest := dp(left, i-1) + dp(i+1, right)
+			gain := nums[i] * nums[left-1] * nums[right+1]
+			if ans < rest+gain {
+				ans = rest + gain
+			}
+		}
+		memo[left][right] = ans
+		return ans
+	}
+
+	return dp(1, n-2)
+}
+
+func maxCoinsBottomUp(nums []int) int {
+	arr := make([]int, len(nums)+2)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = 1
+	}
+	for i := 0; i < len(nums); i++ {
+		arr[i+1] = nums[i]
+	}
+
+	nums = arr
+	n := len(nums)
+
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	for diff := 1; diff < n; diff++ {
+		for left := 1; left < n-diff; left++ {
+			right := left + diff - 1
+
+			ans := 0
+			for k := left; k <= right; k++ {
+
+				rest := dp[left][k-1] + dp[k+1][right]
+				gain := nums[k] * nums[left-1] * nums[right+1]
+
+				if ans < rest+gain {
+					ans = rest + gain
+				}
+			}
+
+			if dp[left][right] < ans {
+				dp[left][right] = ans
+			}
+		}
+	}
+
+	return dp[1][n-2]
+}
