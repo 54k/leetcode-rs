7a5c27ba4ecd6d5f06a00a7bc56f6b66b0327b05
54k <54k@bk.ru>
2023-10-28 18:15:12 +0200

    day 350

---
+++ java/leetcode_grind/Day350.java
@@ -0,0 +1,73 @@
+package leetcode_grind;
+
+import java.util.ArrayList;
+
+public class Day350 {
+    // https://leetcode.com/problems/diagonal-traverse/description/
+    static class Solution {
+        public int[] findDiagonalOrderTraverseAndReverse(int[][] mat) {
+            var n = mat.length;
+            var m = mat[0].length;
+
+            var result = new int[n * m];
+            for (int d = 0, k = 0; d < n + m - 1; d++) {
+                var diag = new ArrayList<Integer>();
+
+                var r = d < m ? 0 : d - m + 1;
+                var c = d < m ? d : m - 1;
+
+                while (c > -1 && r < n) {
+                    diag.add(mat[r][c]);
+                    r++;
+                    c--;
+                }
+
+                for (int i = 0; i < diag.size() / 2 && d % 2 == 0; i++) {
+                    var tmp = diag.get(i);
+                    diag.set(i, diag.get(diag.size() - i - 1));
+                    diag.set(diag.size() - i - 1, tmp);
+                }
+
+                for (var num : diag)
+                    result[k++] = num;
+            }
+
+            return result;
+        }
+
+        public int[] findDiagonalOrderSimulation(int[][] mat) {
+            var n = mat.length;
+            var m = mat[0].length;
+
+            var dir = 1;
+            var result = new int[n * m];
+            var r = 0;
+
+            var row = 0;
+            var col = 0;
+
+            while (row < n && col < m) {
+                result[r++] = mat[row][col];
+
+                var newRow = row + (dir == 1 ? -1 : 1);
+                var newCol = col + (dir == 1 ? 1 : -1);
+
+                if (newRow < 0 || newRow == n || newCol < 0 || newCol == m) {
+                    if (dir == 1) {
+                        row += col == m - 1 ? 1 : 0;
+                        col += col < m - 1 ? 1 : 0;
+                    } else {
+                        col += row == n - 1 ? 1 : 0;
+                        row += row < n - 1 ? 1 : 0;
+                    }
+                    dir = 1 - dir;
+                } else {
+                    row = newRow;
+                    col = newCol;
+                }
+            }
+
+            return result;
+        }
+    }
+}
