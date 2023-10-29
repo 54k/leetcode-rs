47760ab15be311f4c43da91fa1301f82899b7b8d
54k <54k@bk.ru>
2023-10-29 13:34:04 +0100

    day 351

--- java/leetcode_grind/Day351.java
+++ java/leetcode_grind/Day351.java
@@ -4,7 +4,7 @@ import java.util.PriorityQueue;
 
 public class Day351 {
     // https://leetcode.com/problems/eliminate-maximum-number-of-monsters/description/
-    static class Solution {
+    static class Solution1 {
         public int eliminateMaximum(int[] dist, int[] speed) {
             var heap = new PriorityQueue<Double>();
             for (int i = 0; i < speed.length; i++) {
@@ -18,6 +18,20 @@ public class Day351 {
                 ans++;
             }
             return ans;
+        }
+    }
+
+    // https://leetcode.com/problems/minimum-health-to-beat-game/
+    static class Solution2 {
+        public long minimumHealth(int[] damage, int armor) {
+            long healthLost = 0;
+            var maxDamage = 0;
+            for (var d : damage) {
+                maxDamage = Math.max(maxDamage, d);
+                healthLost += d;
+            }
+
+            return healthLost + 1 - Math.min(armor, maxDamage);
         }
     }
 }
