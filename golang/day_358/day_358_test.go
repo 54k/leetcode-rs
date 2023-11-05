64c16cdddc0204d5153433d47632f9a0819737f4
54k <54k@bk.ru>
2023-11-05 14:45:44 +0100

    day 358

--- golang/day_358/day_358_test.go
+++ golang/day_358/day_358_test.go
@@ -51,3 +51,95 @@ func fib(n int) int {
 	}
 	return b
 }
+
+// https://leetcode.com/problems/n-th-tribonacci-number/description
+func tribonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	if n == 2 {
+		return 1
+	}
+	a, b, c := 0, 1, 1
+	for i := 3; i <= n; i++ {
+		d := a + b + c
+		a, b, c = b, c, d
+	}
+	return c
+}
+
+// https://leetcode.com/problems/min-cost-climbing-stairs/description
+func minCostClimbingStairs(cost []int) int {
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+
+	a, b := cost[0], cost[1]
+	for i := 2; i < len(cost); i++ {
+		c := min(a, b) + cost[i]
+		a, b = b, c
+	}
+	return min(a, b)
+}
+
+// https://leetcode.com/problems/house-robber/description
+func rob(nums []int) int {
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	a, b := nums[0], max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		c := max(b, a+nums[i])
+		a, b = b, c
+	}
+	return max(a, b)
+}
+
+// https://leetcode.com/problems/delete-and-earn/description
+func deleteAndEarn(nums []int) int {
+	max := func(a, b int) int {
+		if a < b {
+			return b
+		}
+		return a
+	}
+
+	points := map[int]int{}
+	memo := map[int]int{}
+	maxNum := 0
+
+	for _, num := range nums {
+		points[num] += num
+		maxNum = max(maxNum, num)
+	}
+
+	var dp func(int) int
+	dp = func(curr int) int {
+		if curr == 0 {
+			return 0
+		}
+
+		if curr == 1 {
+			return points[curr]
+		}
+
+		if v, ok := memo[curr]; ok {
+			return v
+		}
+
+		gain := points[curr]
+		memo[curr] = max(dp(curr-1), dp(curr-2)+gain)
+		return memo[curr]
+	}
+
+	return dp(maxNum)
+}
