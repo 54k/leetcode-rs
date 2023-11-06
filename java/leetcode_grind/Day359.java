46f9f382ac0f984f60b824f53f799aba6ed7b5cc
54k <54k@bk.ru>
2023-11-06 12:34:40 +0100

    day 359

--- java/leetcode_grind/Day359.java
+++ java/leetcode_grind/Day359.java
@@ -47,4 +47,29 @@ public class Day359 {
             availableSeats.add(seatNumber);
         }
     }
+
+    // https://leetcode.com/problems/last-stone-weight-ii/description/
+    static class Solution1 {
+        public int lastStoneWeightII(int[] stones) {
+            var dp = new boolean[1501];
+            dp[0] = true;
+
+            var sum = 0;
+
+            for (var stone : stones) {
+                sum += stone;
+                for (int i = Math.min(1500, sum); i >= stone; i--) {
+                    dp[i] |= dp[i - stone];
+                }
+            }
+
+            for (int i = sum / 2; i >= 0; i--) {
+                if (dp[i]) {
+                    return sum - i - i;
+                }
+            }
+
+            return 0;
+        }
+    }
 }
