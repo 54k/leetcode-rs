99d011600e31cc5effcd30f1d6798f5436e4c375
54k <54k@bk.ru>
2023-11-06 20:31:49 +0100

    day 359

--- src/leetcode_grind/day_359.rs
+++ src/leetcode_grind/day_359.rs
@@ -21,3 +21,49 @@ pub fn count_primes(n: i32) -> i32 {
     }
     primes_count
 }
+
+// https://leetcode.com/problems/prime-pairs-with-target-sum/
+pub fn find_prime_pairs(n: i32) -> Vec<Vec<i32>> {
+    if n <= 3 {
+        return vec![];
+    }
+
+    let n = n as usize;
+    let mut primes = vec![false; n];
+    for p in 2..=((n as f64).sqrt() as usize) {
+        if !primes[p] {
+            for j in (p * p..n).step_by(p) {
+                primes[j] = true;
+            }
+        }
+    }
+
+    let mut ans = vec![];
+    for i in ((n + 1) / 2..=n - 2).rev() {
+        if !primes[i] && !primes[n - i] {
+            let (f, s) = ((n - i) as i32, i as i32);
+            ans.push(vec![f, s]);
+        }
+    }
+    ans
+}
+
+// https://leetcode.com/problems/check-if-all-the-integers-in-a-range-are-covered/description/
+pub fn is_covered(ranges: Vec<Vec<i32>>, left: i32, right: i32) -> bool {
+    let mut r = vec![0; 52];
+    for range in ranges {
+        r[range[0] as usize] += 1;
+        r[range[1] as usize + 1] -= 1;
+    }
+
+    for i in 1..r.len() {
+        r[i] += r[i - 1];
+    }
+
+    for i in left as usize..=right as usize {
+        if r[i] == 0 {
+            return false;
+        }
+    }
+    true
+}
