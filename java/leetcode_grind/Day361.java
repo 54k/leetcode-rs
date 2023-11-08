99259ebb95e3a14e8f1e90fc9a7b7ab4b56cb681
54k <54k@bk.ru>
2023-11-08 18:58:11 +0100

    day 361

--- java/leetcode_grind/Day361.java
+++ java/leetcode_grind/Day361.java
@@ -139,4 +139,53 @@ public class Day361 {
             return dp[0][s.length() - 1][26];
         }
     }
+
+    // https://leetcode.com/problems/unique-paths-ii/description
+    static class Solution3 {
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            var dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+
+            for (var i = 0; i < obstacleGrid.length; i++) {
+                for (var j = 0; j < obstacleGrid[0].length; j++) {
+                    if (obstacleGrid[i][j] == 1) {
+                        continue;
+                    }
+
+                    if (i * j == 0) {
+                        if (i == 0 && j == 0) {
+                            dp[i][j] = 1;
+                        } else if (i == 0) {
+                            dp[i][j] += dp[i][j - 1];
+                        } else {
+                            dp[i][j] += dp[i - 1][j];
+                        }
+                    } else {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    }
+                }
+            }
+
+            return dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+        }
+    }
+
+    // https://leetcode.com/problems/minimum-falling-path-sum/description
+    static class Solution4 {
+        public int minFallingPathSum(int[][] matrix) {
+            var n = matrix[0].length;
+            for (int i = matrix.length - 2; i >= 0; i--) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] += Math.min(
+                            matrix[i + 1][j],
+                            Math.min(matrix[i + 1][Math.max(j - 1, 0)], matrix[i + 1][Math.min(j + 1, n - 1)]));
+                }
+            }
+
+            var ans = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                ans = Math.min(ans, matrix[0][i]);
+            }
+            return ans;
+        }
+    }
 }
