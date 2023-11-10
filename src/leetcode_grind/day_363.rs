5c440a66cba393df069d9e9b33e3f8956df7bcca
54k <54k@bk.ru>
2023-11-10 14:52:48 +0100

    day 363

--- src/leetcode_grind/day_363.rs
+++ src/leetcode_grind/day_363.rs
@@ -25,3 +25,70 @@ pub fn number_of_ways(s: String) -> i64 {
 
     ans
 }
+
+// https://leetcode.com/problems/restore-the-array-from-adjacent-pairs/description
+pub fn restore_array_dfs(adjacent_pairs: Vec<Vec<i32>>) -> Vec<i32> {
+    use std::collections::HashMap;
+    let mut adj = HashMap::new();
+    for edge in adjacent_pairs {
+        adj.entry(edge[0]).or_insert(vec![]).push(edge[1]);
+        adj.entry(edge[1]).or_insert(vec![]).push(edge[0]);
+    }
+
+    let mut root = 0;
+    for (k, v) in &adj {
+        if v.len() == 1 {
+            root = *k;
+            break;
+        }
+    }
+    let mut ans = vec![];
+    fn dfs(node: i32, prev: i32, ans: &mut Vec<i32>, adj: &HashMap<i32, Vec<i32>>) {
+        ans.push(node);
+        for &next in &adj[&node] {
+            if next != prev {
+                dfs(next, node, ans, adj);
+            }
+        }
+    }
+
+    dfs(root, -1, &mut ans, &adj);
+    ans
+}
+
+pub fn restore_array_iterative(adjacent_pairs: Vec<Vec<i32>>) -> Vec<i32> {
+    use std::collections::HashMap;
+    let mut adj = HashMap::new();
+    for edge in adjacent_pairs {
+        adj.entry(edge[0]).or_insert(vec![]).push(edge[1]);
+        adj.entry(edge[1]).or_insert(vec![]).push(edge[0]);
+    }
+
+    let mut root = 0;
+    for (k, v) in &adj {
+        if v.len() == 1 {
+            root = *k;
+            break;
+        }
+    }
+
+    let mut ans = vec![0; adj.len()];
+    let mut curr = root;
+    ans[0] = root;
+    let mut i = 1;
+    let mut prev = i32::MAX;
+
+    while i < adj.len() {
+        for &neighbor in &adj[&curr] {
+            if neighbor != prev {
+                ans[i] = neighbor;
+                i += 1;
+                prev = curr;
+                curr = neighbor;
+                break;
+            }
+        }
+    }
+
+    ans
+}
