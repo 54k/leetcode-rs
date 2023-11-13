444b073979557414572e0fe8adb02a2c6740bc7a
54k <54k@bk.ru>
2023-11-14 00:01:53 +0100

    day 366

--- java/leetcode_grind/Day366.java
+++ java/leetcode_grind/Day366.java
@@ -1,10 +1,16 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 
 public class Day366 {
     // https://leetcode.com/problems/sort-vowels-in-a-string/description
-    static class Solution {
+    static class Solution1 {
         public String sortVowels(String s) {
             Function<Character, Boolean> isVowel = (vow) -> {
                 return vow == 'a' || vow == 'e' || vow == 'i' || vow == 'o' || vow == 'u' || vow == 'i' ||
@@ -35,4 +41,52 @@ public class Day366 {
         }
     }
 
+    // https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/description/
+    static class Solution2 {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> visited = new HashSet<>();
+        int k;
+
+        void toGraph(TreeNode root, TreeNode parent) {
+            if (root == null) {
+                return;
+            }
+
+            if (!graph.containsKey(root.val)) {
+                graph.put(root.val, new ArrayList<>());
+            }
+
+            if (parent != null) {
+                if (!graph.containsKey(parent.val)) {
+                    graph.put(parent.val, new ArrayList<>());
+                }
+                graph.get(parent.val).add(root.val);
+                graph.get(root.val).add(parent.val);
+            }
+
+            toGraph(root.left, root);
+            toGraph(root.right, root);
+        }
+
+        void dfs(int v, int d) {
+            if (d == k) {
+                result.add(v);
+                return;
+            }
+            visited.add(v);
+            for (var u : graph.get(v)) {
+                if (!visited.contains(u)) {
+                    dfs(u, d + 1);
+                }
+            }
+        }
+
+        public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+            this.k = k;
+            toGraph(root, null);
+            dfs(target.val, 0);
+            return result;
+        }
+    }
 }
