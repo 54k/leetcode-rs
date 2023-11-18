f30701dd13eede29b517b98e2db63f1e5f158b30
54k <54k@bk.ru>
2023-11-18 18:01:52 +0100

    day 371

--- java/leetcode_grind/Day371.java
+++ java/leetcode_grind/Day371.java
@@ -1,6 +1,10 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Day371 {
     // https://leetcode.com/problems/frequency-of-the-most-frequent-element/description
@@ -74,4 +78,68 @@ public class Day371 {
         }
     }
 
+    // https://leetcode.com/problems/range-frequency-queries/description/
+    class RangeFreqQuery {
+        Map<Integer, List<Integer>> idx = new HashMap<>();
+
+        public RangeFreqQuery(int[] arr) {
+            var i = 0;
+            for (var num : arr) {
+                idx.putIfAbsent(num, new ArrayList<>());
+                idx.get(num).add(i);
+                i++;
+            }
+        }
+
+        int lower(List<Integer> list, int target) {
+            int lo = -1;
+            int hi = list.size();
+
+            while (hi - lo > 1) {
+                int mid = (lo + hi) / 2;
+
+                if (list.get(mid) < target) {
+                    lo = mid;
+                } else {
+                    hi = mid;
+                }
+            }
+
+            return hi == list.size() || list.get(hi) < target ? -1 : hi;
+        }
+
+        int upper(List<Integer> list, int target) {
+            int lo = -1;
+            int hi = list.size();
+
+            while (hi - lo > 1) {
+                int mid = (lo + hi) / 2;
+
+                if (list.get(mid) <= target) {
+                    lo = mid;
+                } else {
+                    hi = mid;
+                }
+            }
+            return lo > -1 && list.get(lo) <= target ? hi : -1;
+        }
+
+        public int query(int left, int right, int value) {
+            var list = idx.get(value);
+            if (list == null) {
+                return 0;
+            }
+            var l = lower(list, left);
+            if (l == -1) {
+                return 0;
+            }
+
+            var r = upper(list, right);
+            if (r == -1) {
+                return 0;
+            }
+
+            return r - l;
+        }
+    }
 }
