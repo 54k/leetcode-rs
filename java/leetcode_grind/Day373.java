c09449adc6ed6ce6606c20d2ac2e81ecea50d40d
54k <54k@bk.ru>
2023-11-20 11:58:29 +0100

    day 373

--- java/leetcode_grind/Day373.java
+++ java/leetcode_grind/Day373.java
@@ -1,5 +1,6 @@
 package leetcode_grind;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -78,7 +79,73 @@ public class Day373 {
     // https://leetcode.com/problems/count-palindromic-subsequences/solutions/2850466/c-java-python3-counting-prefixes-and-suffixes/
     class Solution2 {
         public int countPalindromes(String s) {
-            throw new Error("not implemented");
+            int n = s.length();
+            int[] cnt = new int[10];
+            int[][][] prefix = new int[n][10][10];
+
+            for (int i = 0; i < n; i++) {
+                int c = s.charAt(i) - '0';
+                if (i > 0) {
+                    for (int j = 0; j < 10; j++) {
+                        for (int k = 0; k < 10; k++) {
+                            prefix[i][j][k] = prefix[i - 1][j][k];
+                            if (c == k) {
+                                prefix[i][j][k] += cnt[j];
+                            }
+                        }
+                    }
+                }
+                cnt[c]++;
+            }
+
+            Arrays.fill(cnt, 0);
+
+            int[][][] suffix = new int[n][10][10];
+            for (int i = n - 1; i >= 0; i--) {
+                int c = s.charAt(i) - '0';
+                if (i < n - 1) {
+                    for (int j = 0; j < 10; j++) {
+                        for (int k = 0; k < 10; k++) {
+                            suffix[i][j][k] = suffix[i + 1][j][k];
+                            if (c == k) {
+                                suffix[i][j][k] += cnt[j];
+                            }
+                        }
+                    }
+                }
+                cnt[c]++;
+            }
+
+            int ans = 0;
+            int MOD = 1_000_000_007;
+
+            for (int i = 2; i < n - 2; i++) {
+                for (int j = 0; j < 10; j++) {
+                    for (int k = 0; k < 10; k++) {
+                        ans = (int) ((ans + 1L * prefix[i - 1][j][k] * suffix[i + 1][j][k]) % MOD);
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+    // https://leetcode.com/problems/maximum-xor-product/description/
+    // https://leetcode.com/problems/maximum-xor-product/solutions/4304377/no-multiplication/
+    static class Solution3 {
+        public int maximumXorProduct(long a, long b, int n) {
+            int mod = 1_000_000_007;
+
+            if (n > 0) {
+                for (long bt = 1L << (n - 1); bt > 0; bt >>= 1) {
+                    if ((Math.min(a, b) & bt) == 0) {
+                        a ^= bt;
+                        b ^= bt;
+                    }
+                }
+            }
+
+            return (int) (a % mod * (b % mod) % mod);
         }
     }
 }
