d29da1160bd5c8aea9ed8ad28ac93c753112e19d
54k <54k@bk.ru>
2023-11-24 20:53:10 +0100

    day 377

--- src/leetcode_grind/day_377.rs
+++ src/leetcode_grind/day_377.rs
@@ -7,7 +7,6 @@ pub fn max_coins_1(piles: Vec<i32>) -> i32 {
     let mut queue = piles.into_iter().collect::<VecDeque<_>>();
 
     while !queue.is_empty() {
-        let n = queue.len();
         a += queue.pop_back().unwrap();
         b += queue.pop_back().unwrap();
         c += queue.pop_front().unwrap();
@@ -40,5 +39,45 @@ pub fn max_coins_3(piles: Vec<i32>) -> i32 {
         res += piles[i];
         i += 2;
     }
+    res
+}
+
+pub fn max_coins_4(piles: Vec<i32>) -> i32 {
+    fn sort(piles: &mut Vec<i32>) {
+        let mut max = *piles.iter().max().unwrap();
+        let mut cnt = 0;
+        while max > 0 {
+            cnt += 1;
+            max /= 10;
+        }
+
+        for i in 0..cnt {
+            let mut buckets = vec![vec![]; 10];
+            for num in piles.iter() {
+                let k = (*num / 10_i32.pow(i) % 10) as usize;
+                buckets[k].push(*num);
+            }
+
+            let mut z = 0;
+            for j in 0..=9 {
+                for &num in &buckets[j] {
+                    piles[z] = num;
+                    z += 1;
+                }
+            }
+        }
+    }
+
+    let mut res = 0;
+    let mut piles = piles;
+    sort(&mut piles);
+
+    let n = piles.len();
+    let mut i = n / 3;
+    while i < n {
+        res += piles[i];
+        i += 2;
+    }
+
     res
 }
