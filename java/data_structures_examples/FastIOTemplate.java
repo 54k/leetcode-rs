64eb72811eb7939e1434a32e21d423746b246caa
54k <54k@bk.ru>
2023-11-26 21:08:06 +0100

    day 379

---
+++ java/data_structures_examples/FastIOTemplate.java
@@ -0,0 +1,98 @@
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+
+class FastIO extends PrintWriter {
+    private InputStream stream;
+    private byte[] buf = new byte[1 << 16];
+    private int curChar;
+    private int numChars;
+
+    // standard input
+    public FastIO() {
+        this(System.in, System.out);
+    }
+
+    public FastIO(InputStream i, OutputStream o) {
+        super(o);
+        stream = i;
+    }
+
+    // file input
+    public FastIO(String i, String o) throws IOException {
+        super(new FileWriter(o));
+        stream = new FileInputStream(i);
+    }
+
+    // throws InputMismatchException() if previously detected end of file
+    private int nextByte() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
+        }
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars == -1) {
+                return -1; // end of file
+            }
+        }
+        return buf[curChar++];
+    }
+
+    // to read in entire lines, replace c <= ' '
+    // with a function that checks whether c is a line break
+    public String next() {
+        int c;
+        do {
+            c = nextByte();
+        } while (c <= ' ');
+
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = nextByte();
+        } while (c > ' ');
+        return res.toString();
+    }
+
+    public int nextInt() { // nextLong() would be implemented similarly
+        int c;
+        do {
+            c = nextByte();
+        } while (c <= ' ');
+
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = nextByte();
+        }
+
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res = 10 * res + c - '0';
+            c = nextByte();
+        } while (c > ' ');
+        return res * sgn;
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        FastIO io = new FastIO();
+    }
+}
