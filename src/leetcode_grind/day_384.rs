d4553e0f5c37950d10f69bd5a4d3d43cbca95b13
54k <54k@bk.ru>
2023-12-01 21:04:24 +0100

    day 384

--- src/leetcode_grind/day_384.rs
+++ src/leetcode_grind/day_384.rs
@@ -35,3 +35,45 @@ pub fn array_strings_are_equal(word1: Vec<String>, word2: Vec<String>) -> bool {
 
     s1 == word1.len() && s2 == word2.len()
 }
+
+// https://leetcode.com/problems/queries-on-number-of-points-inside-a-circle/description/
+pub fn count_points(points: Vec<Vec<i32>>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+    fn lower_bound(points: &Vec<Vec<i32>>, target: i32) -> i32 {
+        let mut lo = -1;
+        let mut hi = points.len() as i32;
+        while lo + 1 < hi {
+            let mid = (lo + hi) / 2;
+            if points[mid as usize][0] < target {
+                lo = mid;
+            } else {
+                hi = mid;
+            }
+        }
+        hi
+    }
+
+    let mut points = points;
+    points.sort();
+
+    let mut ans = vec![0; queries.len()];
+
+    for (i, circle) in queries.into_iter().enumerate() {
+        let from = lower_bound(&points, circle[0] - circle[2]);
+
+        // println!("{:?}, {}, {}", points, from, circle[0]);
+
+        if from == -1 || from == points.len() as i32 {
+            continue;
+        }
+
+        for j in from as usize..points.len() {
+            let point = &points[j];
+            let dist = (circle[0] - point[0]).pow(2) + (circle[1] - point[1]).pow(2);
+            if (dist <= circle[2].pow(2)) {
+                ans[i] += 1;
+            }
+        }
+    }
+
+    ans
+}
