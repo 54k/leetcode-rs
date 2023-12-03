eb8b586979bfac504e1d95d5c7067321e89fbbed
54k <54k@bk.ru>
2023-12-03 22:44:37 +0100

    day 386

--- src/leetcode_grind/day_386.rs
+++ src/leetcode_grind/day_386.rs
@@ -8,3 +8,27 @@ pub fn min_time_to_visit_all_points(points: Vec<Vec<i32>>) -> i32 {
     }
     ans
 }
+
+// https://leetcode.com/problems/check-if-it-is-a-straight-line/
+pub fn check_straight_line(coordinates: Vec<Vec<i32>>) -> bool {
+    fn get_diff_x(a: &Vec<i32>, b: &Vec<i32>) -> i64 {
+        a[0] as i64 - b[0] as i64
+    }
+
+    fn get_diff_y(a: &Vec<i32>, b: &Vec<i32>) -> i64 {
+        a[1] as i64 - b[1] as i64
+    }
+
+    let dx = get_diff_x(&coordinates[1], &coordinates[0]);
+    let dy = get_diff_y(&coordinates[1], &coordinates[0]);
+
+    for i in 2..coordinates.len() {
+        if dy * get_diff_x(&coordinates[i], &coordinates[0])
+            != dx * get_diff_y(&coordinates[i], &coordinates[0])
+        {
+            return false;
+        }
+    }
+
+    true
+}
