089155b6637dd09c8bd4bcaec35fa39357002fcc
54k <54k@bk.ru>
2023-12-04 18:05:10 +0100

    day 387

--- java/leetcode_grind/Day387.java
+++ java/leetcode_grind/Day387.java
@@ -3,6 +3,7 @@ package leetcode_grind;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
@@ -152,6 +153,14 @@ public class Day387 {
             Pair(F k, S v) {
                 key = k;
                 value = v;
+            }
+
+            F getKey() {
+                return key;
+            }
+
+            S getValue() {
+                return value;
             }
         }
 
