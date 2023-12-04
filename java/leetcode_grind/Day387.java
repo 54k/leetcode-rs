8be1f656781746a31b80a5cd9dd573527882117c
54k <54k@bk.ru>
2023-12-04 18:08:54 +0100

    day 387

--- java/leetcode_grind/Day387.java
+++ java/leetcode_grind/Day387.java
@@ -205,4 +205,43 @@ public class Day387 {
             return 0;
         }
     }
+
+    // https://leetcode.com/problems/buddy-strings/description/
+    static class Solution {
+        public boolean buddyStrings(String s, String goal) {
+            if (s.length() != goal.length()) {
+                return false;
+            }
+
+            if (s.equals(goal)) {
+                int[] freq = new int[26];
+                for (int i = 0; i < s.length(); i++) {
+                    char cur = s.charAt(i);
+                    freq[cur - 'a']++;
+                    if (freq[cur - 'a'] == 2) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            int firstIndex = -1;
+            int secondIndex = -1;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != goal.charAt(i)) {
+                    if (firstIndex == -1) {
+                        firstIndex = i;
+                    } else if (secondIndex == -1) {
+                        secondIndex = i;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            if (secondIndex == -1) {
+                return false;
+            }
+            return s.charAt(firstIndex) == goal.charAt(secondIndex) && s.charAt(secondIndex) == goal.charAt(firstIndex);
+        }
+    }
 }
