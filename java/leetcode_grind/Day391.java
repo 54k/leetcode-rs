7e56886dc43c02fcc03237b321ecb7bbe33119d0
54k <54k@bk.ru>
2023-12-08 16:23:58 +0100

    day 391

--- java/leetcode_grind/Day391.java
+++ java/leetcode_grind/Day391.java
@@ -1,5 +1,6 @@
 package leetcode_grind;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
@@ -25,7 +26,7 @@ public class Day391 {
     }
 
     // https://leetcode.com/problems/construct-string-from-binary-tree/descriptions
-    static class Solution {
+    static class Solution1 {
         public String tree2strRecursive(TreeNode root) {
             if (root == null) {
                 return "";
@@ -84,4 +85,42 @@ public class Day391 {
         }
     }
 
+    static class Solution2 {
+        public int reversePairs(int[] nums) {
+            if (nums.length <= 1) {
+                return 0;
+            }
+
+            int[] leftArray = Arrays.copyOfRange(nums, 0, nums.length / 2);
+            int[] rightArray = Arrays.copyOfRange(nums, nums.length / 2, nums.length);
+
+            int left = reversePairs(leftArray);
+            int right = reversePairs(rightArray);
+
+            Arrays.sort(leftArray);
+            Arrays.sort(rightArray);
+
+            int cross = countCrossInversions(leftArray, rightArray);
+
+            return left + right + cross;
+        }
+
+        int countCrossInversions(int[] num1, int[] num2) {
+            int count = 0;
+            int i = 0, j = 0;
+            while (i < num1.length && j < num2.length) {
+                if (isReverse(num1[i], num2[j])) {
+                    count += (num1.length - i);
+                    j++;
+                } else {
+                    i++;
+                }
+            }
+            return count;
+        }
+
+        boolean isReverse(int a, int b) {
+            return (a % 2 == 0) ? a / 2 > b : (a - 1) / 2 >= b;
+        }
+    }
 }
