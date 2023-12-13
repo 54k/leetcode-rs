14b3a0eba8d30018778de0cd49b13da9893177ac
54k <54k@bk.ru>
2023-12-13 14:35:20 +0100

    day 396

--- src/leetcode_grind/day_396.rs
+++ src/leetcode_grind/day_396.rs
@@ -26,3 +26,27 @@ pub fn num_special(mat: Vec<Vec<i32>>) -> i32 {
 
     ans
 }
+
+// https://leetcode.com/problems/difference-between-ones-and-zeros-in-row-and-column/description/
+pub fn ones_minus_zeros(grid: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let (m, n) = (grid.len(), grid[0].len());
+    let mut diff_grid = vec![vec![0; n]; m];
+
+    let mut onesr = vec![0; m];
+    let mut onesc = vec![0; n];
+
+    for r in 0..m {
+        for c in 0..n {
+            onesr[r] += grid[r][c];
+            onesc[c] += grid[r][c];
+        }
+    }
+
+    for r in 0..m {
+        for c in 0..n {
+            diff_grid[r][c] = 2 * (onesr[r] + onesc[c]) - (m as i32 + n as i32);
+        }
+    }
+
+    diff_grid
+}
