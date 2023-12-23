f9f4cde1e14083f1afd957b786612ffcd8c4011a
54k <54k@bk.ru>
2023-12-23 16:34:24 +0100

    day 406

--- java/leetcode_grind/Day406.java
+++ java/leetcode_grind/Day406.java
@@ -207,4 +207,41 @@ public class Day406 {
             return depth.get(p) + depth.get(q) - 2 * depth.get(v.val);
         }
     }
+
+    // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-ii/description/
+    static class Solution7 {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            TreeNode ans = LCA(root, p, q);
+            if (ans == p) {
+                return dfs(p, q) ? p : null;
+            } else if (ans == q) {
+                return dfs(q, p) ? q : null;
+            }
+            return ans;
+        }
+
+        TreeNode LCA(TreeNode node, TreeNode p, TreeNode q) {
+            if (node == null || node == p || node == q) {
+                return node;
+            }
+            TreeNode left = LCA(node.left, p, q);
+            TreeNode right = LCA(node.right, p, q);
+            if (left != null && right != null) {
+                return node;
+            } else if (left != null) {
+                return left;
+            }
+            return right;
+        }
+
+        boolean dfs(TreeNode node, TreeNode target) {
+            if (node == target) {
+                return true;
+            }
+            if (node == null) {
+                return false;
+            }
+            return dfs(node.left, target) || dfs(node.right, target);
+        }
+    }
 }
