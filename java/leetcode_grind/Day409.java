f0aa0ef19f69fdad2f2f552993efc7c8b894e77a
54k <54k@bk.ru>
2023-12-26 21:16:16 +0100

    day 409

--- java/leetcode_grind/Day409.java
+++ java/leetcode_grind/Day409.java
@@ -171,4 +171,77 @@ public class Day409 {
             return sub.size();
         }
     }
+
+    // https://leetcode.com/problems/longest-non-decreasing-subarray-from-two-arrays/description/
+    static class Solution6 {
+        public int maxNonDecreasingLength(int[] nums1, int[] nums2) {
+            int[][] dp = new int[nums1.length][2];
+            dp[0][0] = 1;
+            dp[0][1] = 1;
+
+            int ans = 1;
+
+            for (int i = 1; i < nums1.length; i++) {
+                dp[i][0] = 1;
+                dp[i][1] = 1;
+
+                if (nums1[i] >= nums1[i - 1]) {
+                    dp[i][0] = Math.max(dp[i][0], dp[i - 1][0] + 1);
+                }
+
+                if (nums1[i] >= nums2[i - 1]) {
+                    dp[i][0] = Math.max(dp[i][0], dp[i - 1][1] + 1);
+                }
+
+                if (nums2[i] >= nums2[i - 1]) {
+                    dp[i][1] = Math.max(dp[i][1], dp[i - 1][1] + 1);
+                }
+
+                if (nums2[i] >= nums1[i - 1]) {
+                    dp[i][1] = Math.max(dp[i][1], dp[i - 1][0] + 1);
+                }
+
+                ans = Math.max(ans, Math.max(dp[i][0], dp[i][1]));
+            }
+
+            return ans;
+        }
+    }
+
+    // https://leetcode.com/problems/russian-doll-envelopes/description
+    static class Solution7 {
+        int lengthOfLIS(int[] nums) {
+            int[] dp = new int[nums.length];
+            int len = 0;
+            for (int num : nums) {
+                int i = Arrays.binarySearch(dp, 0, len, num);
+                if (i < 0) {
+                    i = -(i + 1);
+                }
+                dp[i] = num;
+                if (i == len) {
+                    len++;
+                }
+            }
+            return len;
+        }
+
+        public int maxEnvelopes(int[][] envelopes) {
+            Arrays.sort(envelopes, new Comparator<int[]>() {
+                public int compare(int[] arr1, int[] arr2) {
+                    if (arr1[0] == arr2[0]) {
+                        return arr2[1] - arr1[1];
+                    } else {
+                        return arr1[0] - arr2[0];
+                    }
+                }
+            });
+
+            int[] secondDim = new int[envelopes.length];
+            for (int i = 0; i < envelopes.length; i++) {
+                secondDim[i] = envelopes[i][1];
+            }
+            return lengthOfLIS(secondDim);
+        }
+    }
 }
