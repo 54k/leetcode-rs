78c0188edf017a3c05486a986fd24bbda0237b1b
54k <54k@bk.ru>
2023-12-26 21:50:51 +0100

    day 409

--- java/leetcode_grind/Day409.java
+++ java/leetcode_grind/Day409.java
@@ -245,4 +245,45 @@ public class Day409 {
             return lengthOfLIS(secondDim);
         }
     }
+
+    // https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/description
+    static class Solution8 {
+        int bisectRight(int[] A, int target, int right) {
+            if (right == 0) {
+                return 0;
+            }
+
+            int left = 0;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (A[mid] <= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+        public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
+            int n = obstacles.length;
+            int lisLength = 0;
+
+            int[] answer = new int[n];
+            int[] lis = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                int height = obstacles[i];
+
+                int idx = bisectRight(lis, height, lisLength);
+                if (idx == lisLength) {
+                    lisLength++;
+                }
+                lis[idx] = height;
+                answer[i] = idx + 1;
+            }
+
+            return answer;
+        }
+    }
 }
