cffeeea01c91a93f57e15e7ae42e55f5f20096dd
54k <54k@bk.ru>
2023-12-26 22:40:38 +0100

    day 409

--- src/leetcode_grind/day_409.rs
+++ src/leetcode_grind/day_409.rs
@@ -12,3 +12,30 @@ pub fn find_longest_chain(pairs: Vec<Vec<i32>>) -> i32 {
     }
     ans
 }
+
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+pub fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+    let mut ans = vec![];
+
+    let mut nums1 = nums1;
+    nums1.sort();
+    let mut nums2 = nums2;
+    nums2.sort();
+
+    let mut i = 0;
+    let mut j = 0;
+
+    while i < nums1.len() && j < nums2.len() {
+        if nums1[i] < nums2[j] {
+            i += 1;
+        } else if nums1[i] > nums2[j] {
+            j += 1;
+        } else {
+            ans.push(nums1[i]);
+            i += 1;
+            j += 1;
+        }
+    }
+
+    ans
+}
