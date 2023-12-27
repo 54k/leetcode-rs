d85cd8e9fcdc2ee0ebfd909ad92b05382a5dbd9b
54k <54k@bk.ru>
2023-12-27 16:19:54 +0100

    day 410

--- java/leetcode_grind/Day410.java
+++ java/leetcode_grind/Day410.java
@@ -23,4 +23,36 @@ public class Day410 {
             return totalTime;
         }
     }
+
+    // https://leetcode.com/problems/house-robber-ii/description/
+    static class Solution2 {
+        public int rob(int[] nums) {
+            if (nums.length == 0) {
+                return 0;
+            }
+
+            if (nums.length == 1) {
+                return nums[0];
+            }
+
+            int max1 = robSimple(nums, 0, nums.length - 2);
+            int max2 = robSimple(nums, 1, nums.length - 1);
+
+            return Math.max(max1, max2);
+        }
+
+        int robSimple(int[] nums, int start, int end) {
+            int t1 = 0;
+            int t2 = 0;
+
+            for (int i = start; i <= end; i++) {
+                int temp = t1;
+                int current = nums[i];
+                t1 = Math.max(current + t2, t1);
+                t2 = temp;
+            }
+
+            return t1;
+        }
+    }
 }
