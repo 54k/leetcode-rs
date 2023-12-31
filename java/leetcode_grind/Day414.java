9c91407df24296d8e5f5ac1331909a19d8b01caa
54k <54k@bk.ru>
2023-12-31 15:34:17 +0100

    day 414

--- java/leetcode_grind/Day414.java
+++ java/leetcode_grind/Day414.java
@@ -50,4 +50,21 @@ public class Day414 {
             return maxprofit;
         }
     }
+
+    // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/description
+    static class Solution3 {
+        public int maxProfit(int[] prices) {
+            int sold = Integer.MIN_VALUE, held = Integer.MIN_VALUE, reset = 0;
+
+            for (int price : prices) {
+                int preSold = sold;
+
+                sold = held + price;
+                held = Math.max(held, reset - price);
+                reset = Math.max(reset, preSold);
+            }
+
+            return Math.max(sold, reset);
+        }
+    }
 }
