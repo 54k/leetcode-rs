fe80690010f87c2cacc7edd0ce1a150c3649d9b0
54k <54k@bk.ru>
2024-01-06 12:39:02 +0100

    day 420

---
+++ java/leetcode_grind/Day420.java
@@ -0,0 +1,70 @@
+package leetcode_grind;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Day420 {
+    // https://leetcode.com/problems/maximum-profit-in-job-scheduling/description
+    static class Solution1 {
+        int[] memo = new int[50001];
+
+        public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+            List<List<Integer>> jobs = new ArrayList<>();
+            Arrays.fill(memo, -1);
+
+            int length = profit.length;
+            for (int i = 0; i < length; i++) {
+                ArrayList<Integer> currJob = new ArrayList<>();
+                currJob.add(startTime[i]);
+                currJob.add(endTime[i]);
+                currJob.add(profit[i]);
+                jobs.add(currJob);
+            }
+
+            jobs.sort(Comparator.comparingInt(a -> a.get(0)));
+
+            for (int i = 0; i < length; i++) {
+                startTime[i] = jobs.get(i).get(0);
+            }
+
+            return findMaxProfit(jobs, startTime, length, 0);
+        }
+
+        int findMaxProfit(List<List<Integer>> jobs, int[] startTime, int n, int position) {
+            if (position == n) {
+                return 0;
+            }
+
+            if (memo[position] != -1) {
+                return memo[position];
+            }
+
+            int nextIndex = findNextJob(startTime, jobs.get(position).get(1));
+
+            int maxProfit = Math.max(
+                    findMaxProfit(jobs, startTime, n, position + 1),
+                    jobs.get(position).get(2) + findMaxProfit(jobs, startTime, n, nextIndex));
+
+            return memo[position] = maxProfit;
+        }
+
+        int findNextJob(int[] startTime, int lastEndingTime) {
+            int start = 0;
+            int end = startTime.length - 1;
+            int nextIndex = startTime.length;
+
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (startTime[mid] >= lastEndingTime) {
+                    nextIndex = mid;
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            return nextIndex;
+        }
+    }
+}
