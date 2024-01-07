c9cee57db0ae16741a55ea52eb5edd9042a05f05
54k <54k@bk.ru>
2024-01-07 21:39:13 +0100

    day 421

--- src/leetcode_grind/day_421.rs
+++ src/leetcode_grind/day_421.rs
@@ -14,3 +14,23 @@ pub fn maximum_difference(nums: Vec<i32>) -> i32 {
     }
     diff
 }
+
+// https://leetcode.com/problems/two-furthest-houses-with-different-colors/description/
+pub fn max_distance(colors: Vec<i32>) -> i32 {
+    let cur = (colors[0], 0);
+    let mut diff = 0;
+    for (i, &color) in colors.iter().enumerate().skip(1) {
+        if color != cur.0 {
+            diff = diff.max(i as i32 - cur.1);
+        }
+    }
+
+    let cur = (*colors.last().unwrap(), colors.len() as i32 - 1);
+    for (i, &color) in colors.iter().enumerate().rev().skip(1) {
+        if color != cur.0 {
+            diff = diff.max(cur.1 - i as i32);
+        }
+    }
+
+    diff
+}
