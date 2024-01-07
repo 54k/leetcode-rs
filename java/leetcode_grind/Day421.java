a3ad3ad9621f873b3918030445e360f2e4c5bbf7
54k <54k@bk.ru>
2024-01-08 00:03:13 +0100

    day 421

--- java/leetcode_grind/Day421.java
+++ java/leetcode_grind/Day421.java
@@ -367,4 +367,38 @@ public class Day421 {
             return dp[budget];
         }
     }
+
+    // https://leetcode.com/problems/beautiful-array/description/
+    static class Solution8 {
+        Map<Integer, int[]> memo;
+
+        public int[] beautifulArray(int n) {
+            memo = new HashMap<>();
+            return f(n);
+        }
+
+        int[] f(int N) {
+            if (memo.containsKey(N)) {
+                return memo.get(N);
+            }
+
+            int[] ans = new int[N];
+            if (N == 1) {
+                ans[0] = 1;
+            } else {
+                int t = 0;
+
+                for (int x : f((N + 1) / 2)) {
+                    ans[t++] = 2 * x - 1;
+                }
+
+                for (int x : f(N / 2)) {
+                    ans[t++] = 2 * x;
+                }
+            }
+
+            memo.put(N, ans);
+            return ans;
+        }
+    }
 }
