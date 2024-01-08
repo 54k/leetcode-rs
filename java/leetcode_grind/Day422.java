f3e5f513687a2a428e6d0551bb5e1589cf32d53c
54k <54k@bk.ru>
2024-01-08 20:25:10 +0100

    day 422

--- java/leetcode_grind/Day422.java
+++ java/leetcode_grind/Day422.java
@@ -47,4 +47,30 @@ public class Day422 {
             return ans;
         }
     }
+
+    // https://leetcode.com/problems/subtree-of-another-tree/description/
+    static class Solution2 {
+        boolean isSame(TreeNode root, TreeNode subRoot) {
+            if (root == null && subRoot == null) {
+                return true;
+            } else if (root == null && subRoot != null) {
+                return false;
+            } else if (root != null && subRoot == null) {
+                return false;
+            }
+            return root.val == subRoot.val && isSame(root.left, subRoot.left) && isSame(root.right, subRoot.right);
+        }
+
+        public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+            if (root == null) {
+                return subRoot == null;
+            }
+
+            if (root.val == subRoot.val) {
+                return isSame(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+            }
+
+            return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        }
+    }
 }
