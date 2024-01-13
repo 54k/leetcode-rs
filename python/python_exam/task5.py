422c252a583a502848f1a0deca8435560f471d17
54k <54k@bk.ru>
2024-01-13 18:32:32 +0100

    day 427

--- python/python_exam/task5.py
+++ python/python_exam/task5.py
@@ -1,6 +1,6 @@
 # Author: Boris Korotaev
 # tast 5
-def double_values(m: dict[int, int]) -> dict[int, int]:
+def double_values_i(m: dict[int, int]) -> dict[int, int]:
     ret = {}
     for k, v in m.items():
         if type(k) is int or type(k) is float:
@@ -9,8 +9,24 @@ def double_values(m: dict[int, int]) -> dict[int, int]:
             ret[k] = v
     return ret
 
+# tast 5
+def double_values_ii(m: dict[int, int]) -> dict[int, int]:
+    ret = {}
+    for k, v in m.items():
+        if type(v) is int or type(v) is float:
+            ret[k] = 2 * v
+        else:
+            ret[k] = v
+    return ret
+
 if __name__ == "__main__":
     # 5
-    print(double_values({1: "1", 2: "2", 3: "3", "4": "4"}))
-    print(double_values({"4": "4"}))
-    print(double_values({1.02: "1.02"}))
+    print("Double map keys:")
+    print(double_values_i({1: 1, 2: 2, 3: "3", "4": "4"}))
+    print(double_values_i({"4": "4"}))
+    print(double_values_i({1.02: 1.02}))
+
+    print("Double map values:")
+    print(double_values_ii({1: 1, 2: "2", 3: 3, "4": "4"}))
+    print(double_values_ii({"4": "4"}))
+    print(double_values_ii({1.02: 1.02}))
