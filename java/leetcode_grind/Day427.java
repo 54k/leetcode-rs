7951ebd87ad137ab40435664798c9216e2148dcb
54k <54k@bk.ru>
2024-01-13 23:03:57 +0100

    day 427

--- java/leetcode_grind/Day427.java
+++ java/leetcode_grind/Day427.java
@@ -43,4 +43,27 @@ public class Day427 {
             return dummy.next;
         }
     }
+
+    // https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/description/
+    static class Solution2 {
+        public int areaOfMaxDiagonal(int[][] dimensions) {
+            int ans = 0;
+            double mx = 0.0;
+
+            for (var d : dimensions) {
+                double a = (double) d[0] * (double) d[0];
+                double b = (double) d[1] * (double) d[1];
+                double diag = Math.sqrt((double) a + (double) b);
+
+                if (diag > mx) {
+                    ans = d[0] * d[1];
+                    mx = diag;
+                } else if (diag == mx) {
+                    ans = Math.max(ans, d[0] * d[1]);
+                }
+            }
+
+            return ans;
+        }
+    }
 }
