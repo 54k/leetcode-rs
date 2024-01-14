dbfb1a1b14ccdc6c8d8c4b58143d27248e9f78c7
54k <54k@bk.ru>
2024-01-14 18:20:22 +0100

    day 428

--- java/leetcode_grind/Day428.java
+++ java/leetcode_grind/Day428.java
@@ -60,4 +60,65 @@ public class Day428 {
             return res.stream().mapToInt(Integer::valueOf).toArray();
         }
     }
+
+    // https://www.geeksforgeeks.org/problems/find-duplicate-rows-in-a-binary-matrix/1
+    static class Solution4 {
+        static class Trie {
+            Trie[] children;
+            boolean leaf;
+
+            Trie() {
+                children = new Trie[2];
+                leaf = false;
+            }
+        }
+
+        static Trie head;
+
+        static boolean insert(int[] row) {
+            Trie cur = head;
+
+            for (int r : row) {
+                if (cur.children[r] == null) {
+                    cur.children[r] = new Trie();
+                }
+                cur = cur.children[r];
+            }
+
+            if (cur.leaf) {
+                return false;
+            }
+            return cur.leaf = true;
+        }
+
+        public static ArrayList<Integer> repeatedRows(int matrix[][], int m, int n) {
+            head = new Trie();
+            ArrayList<Integer> res = new ArrayList<>();
+            int i = 0;
+            // code here
+            for (int[] r : matrix) {
+                if (!insert(r)) {
+                    res.add(i);
+                }
+                i++;
+            }
+            return res;
+        }
+    }
+
+    // https://leetcode.com/problems/unique-binary-search-trees/description
+    static class Solution5 {
+        public int numTrees(int n) {
+            int[] G = new int[n + 1];
+            G[0] = 1;
+            G[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    G[i] += G[j - 1] * G[i - j];
+                }
+            }
+            return G[n];
+        }
+    }
 }
