469cd956c0588ada0e2492296aeeb051b0198781
54k <54k@bk.ru>
2024-02-07 00:26:11 +0100

    day 451

--- java/leetcode_grind/Day451.java
+++ java/leetcode_grind/Day451.java
@@ -16,4 +16,20 @@ public class Day451 {
             return groups.values().stream().toList();
         }
     }
+
+    // https://leetcode.com/problems/coin-change-ii/description
+    static class Solution1 {
+        public int change(int amount, int[] coins) {
+            int[] dp = new int[amount + 1];
+            dp[0] = 1;
+            for (int c : coins) {
+                for (int amt = 1; amt <= amount; amt++) {
+                    if (amt >= c) {
+                        dp[amt] += dp[amt - c];
+                    }
+                }
+            }
+            return dp[amount];
+        }
+    }
 }
