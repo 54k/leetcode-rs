0a6d9c1444341fcbec22214756b3ea7c4adf2032
54k <54k@bk.ru>
2024-02-07 00:20:55 +0100

    day 451

--- src/leetcode_grind/day_451.rs
+++ src/leetcode_grind/day_451.rs
@@ -16,3 +16,31 @@ pub fn count_arrangement(n: i32) -> i32 {
     perm(1, n, &mut ans, 0);
     ans
 }
+
+// https://leetcode.com/problems/beautiful-arrangement-ii/description/
+pub fn construct_array(n: i32, k: i32) -> Vec<i32> {
+    let mut ans = vec![0; n as usize];
+    let mut c = 0;
+    for v in 1..n - k {
+        ans[c] = v;
+        c += 1;
+    }
+    for i in 0..=k {
+        ans[c] = if i % 2 == 0 { n - k + i / 2 } else { n - i / 2 };
+        c += 1;
+    }
+    ans
+}
+
+// https://leetcode.com/problems/determine-color-of-a-chessboard-square/description/
+pub fn square_is_white(coordinates: String) -> bool {
+    let c = coordinates.as_bytes();
+    let f = c[0] as usize - 'a' as usize;
+    let s = c[1] as usize - '1' as usize;
+
+    let mut color = f % 2 == 1;
+    if s % 2 == 1 {
+        color = !color;
+    }
+    color
+}
