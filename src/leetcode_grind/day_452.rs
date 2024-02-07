290547ac1a681c6241c3375eeb5e643c4c512c57
54k <54k@bk.ru>
2024-02-07 23:00:41 +0100

    day 452

--- src/leetcode_grind/day_452.rs
+++ src/leetcode_grind/day_452.rs
@@ -23,3 +23,29 @@ pub fn frequency_sort(s: String) -> String {
     }
     ans
 }
+
+pub fn frequency_sort_buckets(s: String) -> String {
+    use std::collections::HashMap;
+    let mut freq = HashMap::new();
+    let mut buckets = HashMap::new();
+    for ch in s.chars() {
+        *freq.entry(ch).or_insert(0) += 1;
+    }
+
+    for (k, v) in freq {
+        for _ in 0..v {
+            buckets.entry(v).or_insert(vec![]).push(k);
+        }
+    }
+
+    let mut res = "".to_string();
+
+    for i in (1..=s.len()).rev() {
+        if buckets.contains_key(&i) {
+            for ch in buckets.remove(&i).unwrap() {
+                res.push(ch);
+            }
+        }
+    }
+    res
+}
