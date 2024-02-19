41abbf9cd11618ee21adae9ef3a6bb9a1c59a7c0
54k <54k@bk.ru>
2024-02-19 12:33:22 +0100

    day 464

--- src/leetcode_grind/day_464.rs
+++ src/leetcode_grind/day_464.rs
@@ -7,3 +7,21 @@ pub fn is_power_of_two(n: i32) -> bool {
     (n & (n - 1)) == 0
 }
 
+// https://leetcode.com/problems/string-matching-in-an-array/
+pub fn string_matching(words: Vec<String>) -> Vec<String> {
+    let mut words = words;
+    words.sort_by_key(|x| x.len());
+    let n = words.len();
+    let mut ans = vec![];
+    for i in 0..n {
+        let w1 = &words[i];
+        for j in i + 1..n {
+            let w2 = &words[j];
+            if w2.contains(w1) {
+                ans.push(w1.clone());
+                break;
+            }
+        }
+    }
+    ans
+}
