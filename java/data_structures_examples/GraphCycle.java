551748d4ae6c06eeecccb6d0f84da8ac309d7906
54k <54k@bk.ru>
2024-02-26 11:48:32 +0100

    day 470

--- java/data_structures_examples/GraphCycle.java
+++ java/data_structures_examples/GraphCycle.java
@@ -1,4 +1,4 @@
-package leetcode_grind;
+package data_structures_examples;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,75 +6,8 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.Stack;
 
-public class Day470 {
-    // https://leetcode.com/problems/same-tree/description
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    static class Solution1 {
-        public boolean isSameTree(TreeNode p, TreeNode q) {
-            var stack = new Stack<TreeNode>();
-            stack.push(p);
-            stack.push(q);
-            while (stack.size() >= 2) {
-                var a = stack.pop();
-                var b = stack.pop();
-
-                if ((a == null && b != null) || (a != null && b == null)
-                        || (a != null && b != null && a.val != b.val)) {
-                    return false;
-                }
-
-                if (a == null && b == null) {
-                    continue;
-                }
-
-                if (a != null) {
-                    stack.push(a.left);
-                } else {
-                    stack.push(null);
-                }
-
-                if (b != null) {
-                    stack.push(b.left);
-                } else {
-                    stack.push(null);
-                }
-
-                if (a != null) {
-                    stack.push(a.right);
-                } else {
-                    stack.push(null);
-                }
-
-                if (b != null) {
-                    stack.push(b.right);
-                } else {
-                    stack.push(null);
-                }
-            }
-
-            return stack.isEmpty();
-        }
-    }
+public class GraphCycle {
 
     // https://leetcode.com/problems/distance-to-a-cycle-in-undirected-graph/description/
     class Solution {
