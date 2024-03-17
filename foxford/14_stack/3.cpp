05e2d23351ab0214488e5f2b37aec67b3e552383
54k <54k@bk.ru>
2024-03-17 19:47:05 +0100

    day 490

--- foxford/14_stack/3.cpp
+++ foxford/14_stack/3.cpp
@@ -6,11 +6,11 @@ int main() {
     vector<char> s;
 
     for (auto ch : str) {
+        s.push_back(ch);
         while (s.size() >= 2 and s[s.size() - 1] == s[s.size() - 2]) {
             s.pop_back();
             s.pop_back();
         }
-        s.push_back(ch);
     }
 
     for (auto ch : s) {
