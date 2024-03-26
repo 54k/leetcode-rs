705b6ef943eab36934e93e84d2dffbf0e498a896
54k <54k@bk.ru>
2024-03-26 19:46:56 +0100

    day 499

---
+++ java/leetcode_grind/Day499.java
@@ -0,0 +1,30 @@
+package leetcode_grind;
+
+public class Day499 {
+    // https://leetcode.com/problems/first-missing-positive/description
+    static class Solution1 {
+        public int firstMissingPositive(int[] nums) {
+            int n = nums.length;
+
+            int i = 0;
+            while (i < n) {
+                int correctIdx = nums[i] - 1;
+                if (nums[i] > 0 && nums[i] <= n && nums[i] != nums[correctIdx]) {
+                    int t = nums[i];
+                    nums[i] = nums[correctIdx];
+                    nums[correctIdx] = t;
+                } else {
+                    i++;
+                }
+            }
+
+            for (i = 0; i < n; i++) {
+                if (nums[i] != i + 1) {
+                    return i + 1;
+                }
+            }
+
+            return n + 1;
+        }
+    }
+}
