8c05b86dc6a3366edb1350834a200b675ce413bc
54k <54k@bk.ru>
2024-04-02 00:20:43 +0200

    day 505

--- foxford/20_special_sorting/3.cpp
+++ foxford/20_special_sorting/3.cpp
@@ -1,6 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long _merge(vector<int> &arr, vector<int> &tmp, int left, int mid, int right)
+{
+    int i = left;
+    int k = left;
+    int j = mid;
+
+    long long inv_count = 0;
+
+    while (i < mid and j <= right)
+    {
+        if (arr[i] <= arr[j])
+        {
+            tmp[k++] = arr[i++];
+        }
+        else
+        {
+            tmp[k++] = arr[j++];
+            inv_count += (mid * 1ll - i);
+        }
+    }
+
+    while (i < mid)
+    {
+        tmp[k++] = arr[i++];
+    }
+    while (j <= right)
+    {
+        tmp[k++] = arr[j++];
+    }
+    for (i = left; i <= right; i++)
+    {
+        arr[i] = tmp[i];
+    }
+    return inv_count;
+}
+
+long long _mergeSort(vector<int> &arr, vector<int> &tmp, int left, int right)
+{
+    long long inv_count = 0;
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+        inv_count += _mergeSort(arr, tmp, left, mid);
+        inv_count += _mergeSort(arr, tmp, mid + 1, right);
+        inv_count += _merge(arr, tmp, left, mid + 1, right);
+    }
+    return inv_count;
+}
+
+long long mergeSort(vector<int> &arr)
+{
+    vector<int> tmp(arr.size());
+    return _mergeSort(arr, tmp, 0, arr.size() - 1);
+}
+
 int main()
 {
     int n;
@@ -10,13 +65,8 @@ int main()
     {
         cin >> e;
     }
-    long long ans = 0;
-    for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n; j++)
-            if (v[i] > v[j])
-            {
-                ans++;
-            }
+
+    long long ans = mergeSort(v);
     cout << ans << endl;
     return 0;
 }
