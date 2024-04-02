5f72b3f4d7d02fa0288b2846f1a6ff7445ac504d
54k <54k@bk.ru>
2024-04-02 02:00:32 +0200

    day 505

---
+++ foxford/20_special_sorting/4.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> a;
+vector<int> b;
+
+int main()
+{
+    cin >> n;
+    a.resize(n);
+    for (auto &i : a)
+    {
+        cin >> i;
+    }
+
+    cin >> m;
+    b.resize(m);
+    for (auto &i : b)
+    {
+        cin >> i;
+    }
+
+    return 0;
+}
