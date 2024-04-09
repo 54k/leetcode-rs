5c273c092c4b49e3853d8ef545eda5192d6a3db1
54k <54k@bk.ru>
2024-04-09 03:38:21 +0200

    day 513

---
+++ java/leetcode_grind/Day513.java
@@ -0,0 +1,37 @@
+package leetcode_grind;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Day513 {
+
+    // https://leetcode.com/problems/time-needed-to-buy-tickets/
+    static class Solution1 {
+        public int timeRequiredToBuy(int[] tickets, int k) {
+            Queue<Integer> queue = new LinkedList<>();
+
+            for (int i = 0; i < tickets.length; i++) {
+                queue.add(i);
+            }
+
+            int time = 0;
+
+            while (!queue.isEmpty()) {
+                time++;
+
+                int front = queue.poll();
+                tickets[front]--;
+
+                if (k == front && tickets[front] == 0) {
+                    return time;
+                }
+
+                if (tickets[front] != 0) {
+                    queue.add(front);
+                }
+            }
+
+            return time;
+        }
+    }
+
