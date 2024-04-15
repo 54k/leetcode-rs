92c0b96a843a6083e04f80f23ea9124f1d299ef1
54k <54k@bk.ru>
2024-04-15 23:17:46 +0200

    day 520

--- cpp/code_forces/edu/itmo/seg_tree1/part3/2.cpp
+++ cpp/code_forces/edu/itmo/seg_tree1/part3/2.cpp
@@ -10,7 +10,7 @@ struct segtree
     {
         size = 1;
         while (size < a.size())
-            size += 2;
+            size *= 2;
         tree.assign(size * 2 - 1, 0);
         build(a, 0, 0, size);
     }
@@ -40,7 +40,7 @@ struct segtree
 
     void set(int i, int v, int x, int lx, int rx)
     {
-        if (lx == rx - 1)
+        if (rx - lx == 1)
         {
             tree[x] = v;
             return;
@@ -57,10 +57,60 @@ struct segtree
         }
         tree[x] = tree[x * 2 + 1] + tree[x * 2 + 2];
     }
+
+    int find(int v)
+    {
+        return find(v, 0, 0, size);
+    }
+
+    int find(int v, int x, int lx, int rx)
+    {
+        if (rx - lx == 1)
+        {
+            return lx;
+        }
+        int m = (lx + rx) / 2;
+        if (v < tree[x * 2 + 2])
+        {
+            return find(v, x * 2 + 2, m, rx);
+        }
+        else
+        {
+            return find(v - tree[x * 2 + 2], x * 2 + 1, lx, m);
+        }
+    }
 };
 
 int main()
 {
     ios::sync_with_stdio(false);
+    int n, e;
+    cin >> n;
+    int i = n;
+
+    vector<int> a(n, 1);
+    segtree t(a);
+
+    vector<int> p(n);
+    for (auto &x : p)
+    {
+        cin >> x;
+    }
+
+    deque<int> ans;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int e = p[i];
+        int f = t.find(e);
+        t.set(f, 0);
+        ans.push_front(f + 1);
+    }
+
+    for (auto &x : ans)
+    {
+        cout << x << " ";
+    }
+
+    cout << endl;
     return 0;
 }
