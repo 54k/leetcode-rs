6e6fa60dc6b9ac8ffd0ebd1714177a045a8a5a4d
54k <54k@bk.ru>
2024-05-04 21:59:15 +0300

    day 538

--- src/leetcode_grind/day_538.rs
+++ src/leetcode_grind/day_538.rs
@@ -31,3 +31,44 @@ pub fn wiggle_max_length(nums: Vec<i32>) -> i32 {
     }
     count
 }
+
+// https://leetcode.com/problems/longest-valid-parentheses/description
+pub fn longest_valid_parentheses(s: String) -> i32 {
+    let s = s.as_bytes();
+    let mut left = 0;
+    let mut right = 0;
+    let mut max_len = 0;
+
+    for i in 0..s.len() {
+        if s[i] == b'(' {
+            left += 1;
+        } else {
+            right += 1;
+        }
+        if left == right {
+            max_len = max_len.max(2 * right);
+        } else if right > left {
+            left = 0;
+            right = 0;
+        }
+    }
+
+    left = 0;
+    right = 0;
+
+    for i in (0..s.len()).rev() {
+        if s[i] == b'(' {
+            left += 1;
+        } else {
+            right += 1;
+        }
+        if left == right {
+            max_len = max_len.max(2 * left);
+        } else if left > right {
+            left = 0;
+            right = 0;
+        }
+    }
+
+    max_len
+}
