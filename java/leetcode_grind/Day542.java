2e305104d8940e062cdc7c9dd7e97934b6e1619f
54k <54k@bk.ru>
2024-05-09 15:47:34 +0300

    day 542

--- java/leetcode_grind/Day542.java
+++ java/leetcode_grind/Day542.java
@@ -77,4 +77,44 @@ public class Day542 {
             return minCost;
         }
     }
+
+    static class Solution2 {
+        public long[] minCost(int n, int[][] roads, int[] appleCost, int k) {
+            List<List<Pair<Integer, Integer>>> graph = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                graph.add(new ArrayList<>());
+            }
+            for (int[] road : roads) {
+                int cityA = road[0] - 1, cityB = road[1] - 1, cost = road[2];
+                graph.get(cityA).add(new Pair<Integer, Integer>(cityB, cost));
+                graph.get(cityB).add(new Pair<Integer, Integer>(cityA, cost));
+            }
+            long[] result = new long[n];
+            for (int startCity = 0; startCity < n; startCity++) {
+                result[startCity] = appleCost[startCity];
+            }
+            Queue<Pair<Long, Integer>> heap = new PriorityQueue<>((a, b) -> Long.compare(a.getKey(), b.getKey()));
+            for (int startCity = 0; startCity < n; startCity++) {
+                heap.add(new Pair<>((long) appleCost[startCity], startCity));
+            }
+            while (!heap.isEmpty()) {
+                Pair<Long, Integer> current = heap.poll();
+                long totalCost = current.getKey();
+                int currCity = current.getValue();
+
+                if (result[currCity] < totalCost) {
+                    continue;
+                }
+                for (Pair<Integer, Integer> next : graph.get(currCity)) {
+                    int neighbor = next.getKey();
+                    int cost = next.getValue();
+                    if (result[neighbor] > result[currCity] + (k + 1) * cost) {
+                        result[neighbor] = result[currCity] + (k + 1) * cost;
+                        heap.add(new Pair<Long, Integer>(result[neighbor], neighbor));
+                    }
+                }
+            }
+            return result;
+        }
+    }
 }
