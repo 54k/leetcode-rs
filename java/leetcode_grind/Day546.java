210ddb399bec1f308120e6c0eaffe5fcdfa546bf
54k <54k@bk.ru>
2024-05-13 08:44:51 +0200

    day 546

--- java/leetcode_grind/Day546.java
+++ java/leetcode_grind/Day546.java
@@ -36,4 +36,28 @@ public class Day546 {
             return score;
         }
     }
+
+    // https://leetcode.com/problems/handshakes-that-dont-cross/description
+    static class Solution2 {
+        static int m = 1000000007;
+
+        int mul(int a, int b) {
+            return (int) ((long) a * b % m);
+        }
+
+        public int numberOfWays(int numPeople) {
+            int n = numPeople / 2;
+            int[] inv = new int[numPeople / 2 + 2];
+            inv[1] = 1;
+            for (int i = 2; i < n + 2; i++) {
+                int k = m / i, r = m % i;
+                inv[i] = m - mul(k, inv[r]);
+            }
+            int C = 1;
+            for (int i = 0; i < n; i++) {
+                C = mul(mul(2 * (2 * i + 1), inv[i + 2]), C);
+            }
+            return C;
+        }
+    }
 }
