548cf40e2a4bcb92b05a5787a2aa5cafd1963b12
54k <54k@bk.ru>
2024-05-14 21:34:34 +0200

    day 547

--- java/leetcode_grind/Day547.java
+++ java/leetcode_grind/Day547.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Arrays;
+
 public class Day547 {
     static class Solution1 {
         // https://leetcode.com/problems/path-with-maximum-gold/description
@@ -37,6 +39,84 @@ public class Day547 {
             }
 
             return mx;
+        }
+    }
+
+    // https://leetcode.com/problems/find-the-shortest-superstring/description/
+    static class Solution2 {
+        public String shortestSuperstring(String[] A) {
+            int N = A.length;
+            int[][] overlaps = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    int m = Math.min(A[i].length(), A[j].length());
+                    for (int k = m; k >= 0; --k) {
+                        if (A[i].endsWith(A[j].substring(0, k))) {
+                            overlaps[i][j] = k;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            int[][] dp = new int[1 << N][N];
+            int[][] parent = new int[1 << N][N];
+            for (int mask = 0; mask < (1 << N); ++mask) {
+                Arrays.fill(parent[mask], -1);
+
+                for (int bit = 0; bit < N; ++bit)
+                    if (((mask >> bit) & 1) > 0) {
+                        int pmask = mask ^ (1 << bit);
+                        if (pmask == 0)
+                            continue;
+
+                        for (int i = 0; i < N; i++)
+                            if (((pmask >> i) & 1) > 0) {
+                                int val = dp[pmask][i] + overlaps[i][bit];
+                                if (val > dp[mask][bit]) {
+                                    dp[mask][bit] = val;
+                                    parent[mask][bit] = i;
+                                }
+                            }
+                    }
+            }
+
+            int[] perm = new int[N];
+            boolean[] seen = new boolean[N];
+            int t = 0;
+            int mask = (1 << N) - 1;
+
+            int p = 0;
+            for (int j = 0; j < N; ++j) {
+                if (dp[(1 << N) - 1][j] > dp[(1 << N) - 1][p])
+                    p = j;
+            }
+
+            while (p != -1) {
+                perm[t++] = p;
+                seen[p] = true;
+                int p2 = parent[mask][p];
+                mask ^= 1 << p;
+                p = p2;
+            }
+
+            for (int i = 0; i < t / 2; ++i) {
+                int v = perm[i];
+                perm[i] = perm[t - 1 - i];
+                perm[t - 1 - i] = v;
+            }
+
+            for (int i = 0; i < N; ++i)
+                if (!seen[i])
+                    perm[t++] = i;
+
+            StringBuilder ans = new StringBuilder(A[perm[0]]);
+            for (int i = 1; i < N; ++i) {
+                int overlap = overlaps[perm[i - 1]][perm[i]];
+                ans.append(A[perm[i]].substring(overlap));
+            }
+            return ans.toString();
         }
     }
 }
--- python/leetcode_grind/day_547.py
+++ python/leetcode_grind/day_547.py
@@ -49,3 +49,27 @@ class Solution2:
                 res = max(res, A[i][j] - pre)
                 A[i][j] = min(A[i][j], pre)
         return res
+    
+# https://leetcode.com/problems/super-egg-drop/description/
+class Solution3:
+    def superEggDrop(self, k: int, n: int) -> int:
+        @lru_cache(None)
+        def dp(k, n):
+            if n == 0:
+                return 0
+            if k == 1:
+                return n
+            lo, hi = 1, n
+            while lo + 1 < hi:
+                x = (lo + hi) // 2
+                t1 = dp(k-1, x-1)
+                t2 = dp(k, n-x)
+
+                if t1 < t2:
+                    lo = x
+                elif t1 > t2:
+                    hi = x
+                else:
+                    lo = hi = x
+            return 1 + min(max(dp(k-1, x-1), dp(k, n-x)) for x in (lo, hi))
+        return dp(k, n)
