5add36081c7a6855b0ede81b1c1c228ee731771e
54k <54k@bk.ru>
2024-05-15 11:24:08 +0200

    day 548

--- java/data_structures_examples/LongestCycleInGraph.java
+++ java/data_structures_examples/LongestCycleInGraph.java
@@ -1,4 +1,4 @@
-package leetcode_grind;
+package data_structures_examples;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-public class day548 {
+public class LongestCycleInGraph {
     // https://leetcode.com/problems/find-the-safest-path-in-a-grid/description
     static class Solution1 {
         public int maximumSafenessFactor(List<List<Integer>> grid) {
