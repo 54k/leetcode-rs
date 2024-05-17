3669b02bb850761ad55babc6164bea359b53e408
54k <54k@bk.ru>
2024-05-17 20:18:23 +0200

    day 550

--- java/leetcode_grind/Day550.java
+++ java/leetcode_grind/Day550.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Stack;
+
 public class Day550 {
 
     public class TreeNode {
@@ -34,6 +36,49 @@ public class Day550 {
             root.right = removeLeafNodes(root.right, target);
             if (root.left == null && root.right == null) {
                 return root.val == target ? null : root;
+            }
+            return root;
+        }
+    }
+
+    static class Solution2 {
+        public TreeNode removeLeafNodes(TreeNode root, int target) {
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode currentNode = root, lastRightNode = null;
+
+            while (!stack.isEmpty() || currentNode != null) {
+                while (currentNode != null) {
+                    stack.push(currentNode);
+                    currentNode = currentNode.left;
+                }
+
+                currentNode = stack.peek();
+
+                if (currentNode.right != lastRightNode && currentNode.right != null) {
+                    currentNode = currentNode.right;
+                    continue;
+                }
+
+                stack.pop();
+
+                if (currentNode.right == null && currentNode.left == null && currentNode.val == target) {
+                    if (stack.isEmpty()) {
+                        return null;
+                    }
+
+                    TreeNode parent = stack.isEmpty() ? null : stack.peek();
+
+                    if (parent != null) {
+                        if (parent.left == currentNode) {
+                            parent.left = null;
+                        } else {
+                            parent.right = null;
+                        }
+                    }
+                }
+
+                lastRightNode = currentNode;
+                currentNode = null;
             }
             return root;
         }
