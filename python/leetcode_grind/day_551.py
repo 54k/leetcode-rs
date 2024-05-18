dfa57c4382fa276b3469cc3b9341205c6180272e
54k <54k@bk.ru>
2024-05-18 22:27:08 +0200

    day 551

--- python/leetcode_grind/day_551.py
+++ python/leetcode_grind/day_551.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/sum-of-distances-in-tree/description/
-class Solution:
+class Solution1:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
         graph = collections.defaultdict(set)
         for u, v in edges:
@@ -24,4 +24,30 @@ class Solution:
         
         dfs()
         dfs2()
+        return ans
+
+#  https://leetcode.com/problems/binary-tree-cameras/description/
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution2:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        covered = {None}
+
+        def dfs(node, par=None):
+            nonlocal ans
+            if node:
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+                if par is None and node not in covered or \
+                        node.left not in covered or node.right not in covered:
+                    ans += 1
+                    covered.update({node, par, node.left, node.right})
+
+        dfs(root)
         return ans
