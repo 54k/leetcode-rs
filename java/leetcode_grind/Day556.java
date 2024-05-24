3d569fb765522ea7ffb2fa428a7b3a42c801d2f1
54k <54k@bk.ru>
2024-05-24 18:43:47 +0200

    day 556

--- java/leetcode_grind/Day556.java
+++ java/leetcode_grind/Day556.java
@@ -72,4 +72,18 @@ public class Day556 {
             nums[j] = temp;
         }
     }
+
+    // https://leetcode.com/problems/longest-continuous-increasing-subsequence/description/
+    static class Solution3 {
+        public int findLengthOfLCIS(int[] nums) {
+            int ans = 0;
+            int anchor = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (i > 0 && nums[i - 1] >= nums[i])
+                    anchor = i;
+                ans = Math.max(ans, i - anchor + 1);
+            }
+            return ans;
+        }
+    }
 }
