04d8cf696c7e4dea1c69c9d8639832e74815e61f
54k <54k@bk.ru>
2024-05-30 11:53:16 +0200

    day 562

---
+++ python/leetcode_grind/day_562.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/description
+class Solution1:
+    def countTriplets(self, arr: List[int]) -> int:
+        count = 0
+        for start in range(len(arr) - 1):
+            xor_A = 0
+
+            for mid in range(start + 1, len(arr)):
+                xor_A ^= arr[mid-1]
+                xor_B = 0
+
+                for end in range(mid, len(arr)):
+                    xor_B ^= arr[end]
+                    if xor_A == xor_B:
+                        count += 1
+        return count
