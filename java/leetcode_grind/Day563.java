4a72e677cb3612ba5479257b1c28f00161692e36
54k <54k@bk.ru>
2024-05-31 10:32:52 +0200

    day 563

---
+++ java/leetcode_grind/Day563.java
@@ -0,0 +1,20 @@
+package leetcode_grind;
+
+public class Day563 {
+    // https://leetcode.com/problems/single-number-iii/
+    static class Solution1 {
+        public int[] singleNumber(int[] nums) {
+            int bitmask = 0;
+            for (int num : nums)
+                bitmask ^= num;
+
+            int diff = bitmask & (-bitmask);
+            int x = 0;
+            for (int num : nums) {
+                if ((num & diff) != 0)
+                    x ^= num;
+            }
+            return new int[] { x, bitmask ^ x };
+        }
+    }
+}
