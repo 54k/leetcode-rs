7e0bb23e2671341da7cd79f177e237843c5412dd
54k <54k@bk.ru>
2024-06-06 21:21:02 +0200

    day 569

--- python/leetcode_grind/day_569.py
+++ python/leetcode_grind/day_569.py
@@ -19,3 +19,24 @@ class Solution1:
                     if current_card + i != heapq.heappop(min_heap):
                         return False
         return True
+
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+class Solution2:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left, right = 0, n-1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[left]:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if target <= nums[right] and target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
