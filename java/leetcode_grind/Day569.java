6f2e9f0a3b531c32a0f0c24da89daf6fd9b23945
54k <54k@bk.ru>
2024-06-06 16:28:24 +0200

    day 569

--- java/leetcode_grind/Day569.java
+++ java/leetcode_grind/Day569.java
@@ -64,4 +64,41 @@ public class Day569 {
             return true;
         }
     }
+
+    // https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+    static class Solution3 {
+        public int search(int[] nums, int target) {
+            int n = nums.length;
+            int left = 0, right = n - 1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] > nums[n - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            int answer = binarySearch(nums, 0, left - 1, target);
+            if (answer != -1) {
+                return answer;
+            }
+            return binarySearch(nums, left, n - 1, target);
+        }
+
+        int binarySearch(int[] nums, int leftBoundary, int rightBoundary, int target) {
+            int left = leftBoundary, right = rightBoundary;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
 }
