f3a545375fddcb1e6a01f8fc9e79ec1ae2b1547c
54k <54k@bk.ru>
2024-06-10 14:07:06 +0200

    day 573

--- java/leetcode_grind/Day573.java
+++ java/leetcode_grind/Day573.java
@@ -1,5 +1,9 @@
 package leetcode_grind;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day573 {
 
     // https://leetcode.com/problems/height-checker/description/
@@ -103,6 +107,41 @@ public class Day573 {
         public int heightChecker(int[] heights) {
             int[] sortedHeights = heights.clone();
             heapSort(sortedHeights);
+
+            int count = 0;
+            for (int i = 0; i < sortedHeights.length; i++) {
+                if (heights[i] != sortedHeights[i]) {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+
+    static class Solution3 {
+        void countingSort(int[] arr) {
+            Map<Integer, Integer> counts = new HashMap<>();
+            int minVal = Arrays.stream(arr).min().getAsInt();
+            int maxVal = Arrays.stream(arr).max().getAsInt();
+
+            for (int val : arr) {
+                counts.put(val, counts.getOrDefault(val, 0) + 1);
+            }
+
+            int index = 0;
+
+            for (int val = minVal; val <= maxVal; ++val) {
+                while (counts.getOrDefault(val, 0) > 0) {
+                    arr[index] = val;
+                    index += 1;
+                    counts.put(val, counts.get(val) - 1);
+                }
+            }
+        }
+
+        public int heightChecker(int[] heights) {
+            int[] sortedHeights = heights.clone();
+            countingSort(sortedHeights);
 
             int count = 0;
             for (int i = 0; i < sortedHeights.length; i++) {
