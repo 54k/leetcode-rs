ab4ad117fdf3342a81c4906f11b753551288b0fa
54k <54k@bk.ru>
2024-06-14 20:17:34 +0200

    day 577

--- java/leetcode_grind/Day577.java
+++ java/leetcode_grind/Day577.java
@@ -35,4 +35,30 @@ public class Day577 {
             return answer;
         }
     }
+
+    // https://leetcode.com/problems/minimum-increment-to-make-array-unique/description
+    static class Solution3 {
+        public int minIncrementForUnique(int[] nums) {
+            int n = nums.length;
+            int max = 0;
+            int minIncrements = 0;
+            for (int val : nums) {
+                max = Math.max(max, val);
+            }
+            int[] frequencyCount = new int[n + max];
+            for (int val : nums) {
+                frequencyCount[val]++;
+            }
+            for (int i = 0; i < frequencyCount.length; i++) {
+                if (frequencyCount[i] <= 1)
+                    continue;
+
+                int duplicates = frequencyCount[i] - 1;
+                frequencyCount[i + 1] += duplicates;
+                frequencyCount[i] = 1;
+                minIncrements += duplicates;
+            }
+            return minIncrements;
+        }
+    }
 }
--- python/leetcode_grind/day_577.py
+++ python/leetcode_grind/day_577.py
@@ -32,3 +32,15 @@ class Solution2:
         for c in cnt:
             ans += c * (c + 1) // 2
         return ans
+        
+# https://leetcode.com/problems/minimum-increment-to-make-array-unique/description
+class Solution3:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        min_increments = 0
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                increment = nums[i - 1] + 1 - nums[i]
+                min_increments += increment
+                nums[i] = nums[i - 1] + 1
+        return min_increments
