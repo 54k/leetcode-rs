5fa45e6cee3a29cfd8a6aae1d3e0700c8676cbc9
54k <54k@bk.ru>
2024-06-15 14:14:40 +0200

    day 578

--- java/leetcode_grind/Day578.java
+++ java/leetcode_grind/Day578.java
@@ -50,4 +50,39 @@ public class Day578 {
             return count;
         }
     }
+
+    // https://leetcode.com/problems/put-boxes-into-the-warehouse-ii/description
+    static class Solution3 {
+        public int maxBoxesInWarehouse(int[] boxes, int[] warehouse) {
+            int n = warehouse.length;
+            int minHeight = Integer.MAX_VALUE;
+            int[] effectiveHeights = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                minHeight = Math.min(minHeight, warehouse[i]);
+                effectiveHeights[i] = minHeight;
+            }
+
+            minHeight = Integer.MAX_VALUE;
+            for (int i = n - 1; i >= 0; i--) {
+                minHeight = Math.min(minHeight, warehouse[i]);
+                effectiveHeights[i] = Math.max(effectiveHeights[i], minHeight);
+            }
+
+            Arrays.sort(effectiveHeights);
+            Arrays.sort(boxes);
+
+            int count = 0;
+            int boxIndex = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (boxIndex < boxes.length && boxes[boxIndex] <= effectiveHeights[i]) {
+                    ++count;
+                    ++boxIndex;
+                }
+            }
+
+            return count;
+        }
+    }
 }
--- python/leetcode_grind/day_578.py
+++ python/leetcode_grind/day_578.py
@@ -14,3 +14,18 @@ class Solution1:
                 break
             w += -heappop(q)
         return w
+
+# https://leetcode.com/problems/put-boxes-into-the-warehouse-i/description/
+class Solution2:
+    def maxBoxesInWarehouse(self, boxes: List[int], warehouse: List[int]) -> int:
+        i = 0
+        count = 0
+        boxes.sort(reverse = True)
+        for room in warehouse:
+            while i < len(boxes) and boxes[i] > room:
+                i += 1
+            if i == len(boxes):
+                return count
+            count += 1
+            i += 1
+        return count
