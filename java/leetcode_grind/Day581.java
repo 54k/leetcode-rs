a254af7642e9411e228148f032c67b34f45a0996
54k <54k@bk.ru>
2024-06-18 16:37:07 +0200

    day 581

--- java/leetcode_grind/Day581.java
+++ java/leetcode_grind/Day581.java
@@ -37,4 +37,37 @@ public class Day581 {
             return netProfit;
         }
     }
+
+    // https://leetcode.com/problems/most-profit-assigning-work/description
+    static class Solution2 {
+        public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+            List<int[]> jobProfile = new ArrayList<>();
+            jobProfile.add(new int[] { 0, 0 });
+            for (int i = 0; i < difficulty.length; i++) {
+                jobProfile.add(new int[] { profit[i], difficulty[i] });
+            }
+
+            jobProfile.sort((a, b) -> Integer.compare(b[0], a[0]));
+            for (int i = 0; i < jobProfile.size() - 1; i++) {
+                jobProfile.get(i + 1)[1] = Math.min(jobProfile.get(i)[1], jobProfile.get(i + 1)[1]);
+            }
+
+            int netProfit = 0;
+            for (int i = 0; i < worker.length; i++) {
+                int ability = worker[i];
+                int l = 0, r = jobProfile.size() - 1, jobProfit = 0;
+                while (l <= r) {
+                    int mid = (l + r) / 2;
+                    if (jobProfile.get(mid)[1] <= ability) {
+                        jobProfit = Math.max(jobProfit, jobProfile.get(mid)[0]);
+                        r = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                }
+                netProfit += jobProfit;
+            }
+            return netProfit;
+        }
+    }
 }
