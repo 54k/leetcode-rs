df899fb7023dcff1902f2ce256324baf4af7a3ad
54k <54k@bk.ru>
2024-06-21 01:48:35 +0200

    day 583

--- python/leetcode_grind/day_583.py
+++ python/leetcode_grind/day_583.py
@@ -32,3 +32,32 @@ class Allocator:
 # obj = Allocator(n)
 # param_1 = obj.allocate(size,mID)
 # param_2 = obj.free(mID)
+
+# https://leetcode.com/problems/magnetic-force-between-two-balls/description/
+class Solution1:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        def can_place_balls(x, position, m):
+            prev_ball_pos = position[0]
+            balls_placed = 1
+            for i in range(1, len(position)):
+                curr_pos = position[i]
+                if curr_pos - prev_ball_pos >= x:
+                    balls_placed += 1
+                    prev_ball_pos = curr_pos
+                if balls_placed == m:
+                    return True
+            return False
+        
+        answer = 0
+        position.sort()
+
+        low = 1
+        high = int(position[-1] / (m - 1.0)) + 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if can_place_balls(mid, position, m):
+                answer = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        return answer
