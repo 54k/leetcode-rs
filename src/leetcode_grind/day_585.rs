a8b8a55cf6423beb9208905f0ce3dc5ad1c1872b
54k <54k@bk.ru>
2024-06-23 00:45:25 +0400

    day 585

--- src/leetcode_grind/day_585.rs
+++ src/leetcode_grind/day_585.rs
@@ -14,3 +14,24 @@ pub fn number_of_special_substrings(s: String) -> i32 {
     }
     ans as i32
 }
+
+// https://leetcode.com/problems/count-number-of-nice-subarrays/description
+pub fn number_of_subarrays(nums: Vec<i32>, k: i32) -> i32 {
+    fn at_most(nums: &Vec<i32>, k: i32) -> i32 {
+        let mut window_size = 0;
+        let mut subarrays = 0;
+        let mut start = 0;
+
+        for end in 0..nums.len() {
+            window_size += nums[end] % 2;
+            while window_size > k {
+                window_size -= nums[start] % 2;
+                start += 1;
+            }
+            subarrays += end - start + 1;
+        }
+        subarrays as i32
+    }
+
+    at_most(&nums, k) - at_most(&nums, k - 1)
+}
