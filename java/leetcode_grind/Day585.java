e614a4350ab62cd7319b721dfbfa40bf5c95b197
54k <54k@bk.ru>
2024-06-22 15:23:24 +0400

    day 585

---
+++ java/leetcode_grind/Day585.java
@@ -0,0 +1,24 @@
+package leetcode_grind;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day585 {
+    // https://leetcode.com/problems/count-number-of-nice-subarrays/description
+    static class Solution1 {
+        public int numberOfSubarrays(int[] nums, int k) {
+            int currSum = 0, subarrays = 0;
+            Map<Integer, Integer> prefixSum = new HashMap<>();
+            prefixSum.put(currSum, 1);
+
+            for (int i = 0; i < nums.length; i++) {
+                currSum += nums[i] % 2;
+                if (prefixSum.containsKey(currSum - k)) {
+                    subarrays += prefixSum.get(currSum - k);
+                }
+                prefixSum.put(currSum, prefixSum.getOrDefault(currSum, 0) + 1);
+            }
+            return subarrays;
+        }
+    }
+}
