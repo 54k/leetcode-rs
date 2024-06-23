c00b444b6d97779b44addfd0ca467bdfe02995f9
54k <54k@bk.ru>
2024-06-24 01:58:15 +0400

    day 586

--- java/leetcode_grind/Day586.java
+++ java/leetcode_grind/Day586.java
@@ -137,4 +137,46 @@ public class Day586 {
             return new ArrayList<String>(output);
         }
     }
+
+    // https://leetcode.com/problems/repeated-dna-sequences/description/
+    static class Solution5 {
+        public List<String> findRepeatedDnaSequences(String s) {
+            int L = 10, n = s.length();
+            if (n <= L)
+                return new ArrayList<>();
+            Map<Character, Integer> toInt = new HashMap<>() {
+                {
+                    put('A', 0);
+                    put('C', 1);
+                    put('G', 2);
+                    put('T', 3);
+                }
+            };
+
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++)
+                nums[i] = toInt.get(s.charAt(i));
+
+            int bitmask = 0;
+            Set<Integer> seen = new HashSet<>();
+            Set<String> output = new HashSet<>();
+            for (int start = 0; start < n - L + 1; ++start) {
+                if (start != 0) {
+                    bitmask <<= 2;
+                    bitmask |= nums[start + L - 1];
+                    bitmask &= ~(3 << (2 * L));
+                } else {
+                    for (int i = 0; i < L; ++i) {
+                        bitmask <<= 2;
+                        bitmask |= nums[i];
+                    }
+                }
+
+                if (seen.contains(bitmask))
+                    output.add(s.substring(start, start + L));
+                seen.add(bitmask);
+            }
+            return new ArrayList<String>(output);
+        }
+    }
 }
