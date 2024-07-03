4244d150b4fe0c2e6caf3e5e0777a0b10c9dc8e1
54k <54k@bk.ru>
2024-07-03 08:27:50 +0400

    day 595

--- java/leetcode_grind/Day595.java
+++ java/leetcode_grind/Day595.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 public class Day595 {
     // https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/description/?envType=daily-question&envId=2024-07-03
@@ -54,6 +55,44 @@ public class Day595 {
                 minDiff = Math.min(minDiff, largestFour.get(i) - smallestFour.get(i));
             }
             return minDiff;
+        }
+    }
+
+    // https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+    static class Solution3 {
+        public int findKthLargest(int[] nums, int k) {
+            List<Integer> list = new ArrayList<>();
+            for (int num : nums) {
+                list.add(num);
+            }
+            return quickSelect(list, k);
+        }
+
+        int quickSelect(List<Integer> nums, int k) {
+            int pivotIndex = new Random().nextInt(nums.size());
+            int pivot = nums.get(pivotIndex);
+
+            List<Integer> left = new ArrayList<>();
+            List<Integer> mid = new ArrayList<>();
+            List<Integer> right = new ArrayList<>();
+
+            for (int num : nums) {
+                if (num > pivot) {
+                    left.add(num);
+                } else if (num < pivot) {
+                    right.add(num);
+                } else {
+                    mid.add(num);
+                }
+            }
+
+            if (k <= left.size()) {
+                return quickSelect(left, k);
+            }
+            if (left.size() + mid.size() < k) {
+                return quickSelect(right, k - left.size() - mid.size());
+            }
+            return pivot;
         }
     }
 }
