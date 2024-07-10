f4936d4f2d5c0cd88b3d5cdba88bc78abf095245
54k <54k@bk.ru>
2024-07-10 12:56:07 +0400

    day 602

--- src/leetcode_grind/day_602.rs
+++ src/leetcode_grind/day_602.rs
@@ -10,3 +10,25 @@ pub fn min_operations_1(logs: Vec<String>) -> i32 {
     }
     folder_depth
 }
+
+// https://leetcode.com/problems/max-increase-to-keep-city-skyline/description/
+pub fn max_increase_keeping_skyline(grid: Vec<Vec<i32>>) -> i32 {
+    let n = grid.len();
+    let mut row_maxes = vec![0; n];
+    let mut col_maxes = vec![0; n];
+
+    for r in 0..n {
+        for c in 0..n {
+            row_maxes[r] = row_maxes[r].max(grid[r][c]);
+            col_maxes[c] = col_maxes[c].max(grid[r][c]);
+        }
+    }
+
+    let mut ans = 0;
+    for r in 0..n {
+        for c in 0..n {
+            ans += row_maxes[r].min(col_maxes[c]) - grid[r][c];
+        }
+    }
+    ans
+}
