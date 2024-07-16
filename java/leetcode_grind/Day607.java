7dfc904a8b23aa38cc7132416f97a2d26f239161
54k <54k@bk.ru>
2024-07-16 14:32:26 +0200

    day 607

--- java/leetcode_grind/Day607.java
+++ java/leetcode_grind/Day607.java
@@ -1,5 +1,12 @@
 package leetcode_grind;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
 public class Day607 {
     public static class TreeNode {
         int val;
@@ -71,4 +78,99 @@ public class Day607 {
         }
     }
 
+    static class Pair<F, S> {
+        F key;
+        S value;
+
+        Pair(F k, S v) {
+            key = k;
+            value = v;
+        }
+
+        F getKey() {
+            return key;
+        }
+
+        S getValue() {
+            return value;
+        }
+    }
+
+    static class Solution2 {
+        public String getDirections(TreeNode root, int startValue, int destValue) {
+            Map<Integer, TreeNode> parentMap = new HashMap<>();
+            TreeNode startNode = findStartNode(root, startValue);
+            populateParentMap(root, parentMap);
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(startNode);
+            Set<TreeNode> visitedNodes = new HashSet<>();
+            Map<TreeNode, Pair<TreeNode, String>> pathTracker = new HashMap<>();
+            visitedNodes.add(startNode);
+
+            while (!queue.isEmpty()) {
+                TreeNode currentNode = queue.poll();
+                if (currentNode.val == destValue) {
+                    return backtrackPath(currentNode, pathTracker);
+                }
+
+                if (parentMap.containsKey(currentNode.val)) {
+                    TreeNode parentNode = parentMap.get(currentNode.val);
+                    if (!visitedNodes.contains(parentNode)) {
+                        queue.add(parentNode);
+                        pathTracker.put(parentNode, new Pair(currentNode, "U"));
+                        visitedNodes.add(parentNode);
+                    }
+                }
+
+                if (currentNode.left != null && !visitedNodes.contains(currentNode.left)) {
+                    queue.add(currentNode.left);
+                    pathTracker.put(currentNode.left, new Pair(currentNode, "L"));
+                    visitedNodes.add(currentNode.left);
+                }
+
+                if (currentNode.right != null && !visitedNodes.contains(currentNode.right)) {
+                    queue.add(currentNode.right);
+                    pathTracker.put(currentNode.right, new Pair(currentNode, "R"));
+                    visitedNodes.add(currentNode.right);
+                }
+            }
+
+            return "";
+        }
+
+        String backtrackPath(TreeNode node, Map<TreeNode, Pair<TreeNode, String>> pathTracker) {
+            StringBuilder path = new StringBuilder();
+            while (pathTracker.containsKey(node)) {
+                path.append(pathTracker.get(node).getValue());
+                node = pathTracker.get(node).getKey();
+            }
+            path.reverse();
+            return path.toString();
+        }
+
+        void populateParentMap(TreeNode node, Map<Integer, TreeNode> parentMap) {
+            if (node == null)
+                return;
+            if (node.left != null) {
+                parentMap.put(node.left.val, node);
+                populateParentMap(node.left, parentMap);
+            }
+            if (node.right != null) {
+                parentMap.put(node.right.val, node);
+                populateParentMap(node.right, parentMap);
+            }
+        }
+
+        TreeNode findStartNode(TreeNode node, int startValue) {
+            if (node == null)
+                return null;
+            if (node.val == startValue)
+                return node;
+            TreeNode leftResult = findStartNode(node.left, startValue);
+            if (leftResult != null)
+                return leftResult;
+            return findStartNode(node.right, startValue);
+        }
+    }
 }
