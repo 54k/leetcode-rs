2a869b02a639b2ba75c34f8d6187d0f39c966828
54k <54k@bk.ru>
2024-07-16 15:16:13 +0200

    day 607

--- java/leetcode_grind/Day607.java
+++ java/leetcode_grind/Day607.java
@@ -173,4 +173,48 @@ public class Day607 {
             return findStartNode(node.right, startValue);
         }
     }
+
+    static class Solution3 {
+        public String getDirections(TreeNode root, int startValue, int destValue) {
+            StringBuilder startPath = new StringBuilder();
+            StringBuilder destPath = new StringBuilder();
+            findPath(root, startValue, startPath);
+            findPath(root, destValue, destPath);
+
+            StringBuilder directions = new StringBuilder();
+            int commonPathLength = 0;
+
+            while (commonPathLength < startPath.length() && commonPathLength < destPath.length()
+                    && startPath.charAt(commonPathLength) == destPath.charAt(commonPathLength)) {
+                commonPathLength++;
+            }
+
+            for (int i = 0; i < startPath.length() - commonPathLength; i++) {
+                directions.append("U");
+            }
+            for (int i = commonPathLength; i < destPath.length(); i++) {
+                directions.append(destPath.charAt(i));
+            }
+            return directions.toString();
+        }
+
+        boolean findPath(TreeNode node, int target, StringBuilder path) {
+            if (node == null)
+                return false;
+            if (node.val == target)
+                return true;
+
+            path.append("L");
+            if (findPath(node.left, target, path)) {
+                return true;
+            }
+            path.deleteCharAt(path.length() - 1);
+            path.append("R");
+            if (findPath(node.right, target, path)) {
+                return true;
+            }
+            path.deleteCharAt(path.length() - 1);
+            return false;
+        }
+    }
 }
