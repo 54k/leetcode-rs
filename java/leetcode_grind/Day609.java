bfbb09cbeaf2a71635f508b86ad36863962a9fde
54k <54k@bk.ru>
2024-07-18 15:14:26 +0200

    day 609

--- java/leetcode_grind/Day609.java
+++ java/leetcode_grind/Day609.java
@@ -84,4 +84,40 @@ public class Day609 {
             traverseTree(currNode.right, currNode, graph, leafNodes);
         }
     }
+
+    static class Solution2 {
+        public int countPairs(TreeNode root, int distance) {
+            return postOrder(root, distance)[11];
+        }
+
+        int[] postOrder(TreeNode currentNode, int distance) {
+            if (currentNode == null)
+                return new int[12];
+            else if (currentNode.left == null && currentNode.right == null) {
+                int[] current = new int[12];
+                current[0] = 1;
+                return current;
+            }
+
+            int[] left = postOrder(currentNode.left, distance);
+            int[] right = postOrder(currentNode.right, distance);
+            int[] current = new int[12];
+
+            for (int i = 0; i < 10; i++) {
+                current[i + 1] += left[i] + right[i];
+            }
+
+            current[11] += left[11] + right[11];
+
+            for (int d1 = 0; d1 <= distance; d1++) {
+                for (int d2 = 0; d2 <= distance; d2++) {
+                    if (2 + d1 + d2 <= distance) {
+                        current[11] += left[d1] * right[d2];
+                    }
+                }
+            }
+
+            return current;
+        }
+    }
 }
