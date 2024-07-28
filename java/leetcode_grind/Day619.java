344848d5c30ac1ea1dabef447054ce6b602b57a4
54k <54k@bk.ru>
2024-07-28 15:52:33 +0200

    day 619

--- java/leetcode_grind/Day619.java
+++ java/leetcode_grind/Day619.java
@@ -185,4 +185,73 @@ public class Day619 {
             return answer.toString();
         }
     }
+
+    static class Solution4 {
+        ArrayList<Integer> addStrings(ArrayList<Integer> num1, ArrayList<Integer> num2) {
+            ArrayList<Integer> ans = new ArrayList<>();
+            int carry = 0;
+
+            for (int i = 0; i < num1.size() || i < num2.size(); ++i) {
+                int digit1 = i < num1.size() ? num1.get(i) : 0;
+                int digit2 = i < num2.size() ? num2.get(i) : 0;
+
+                int sum = digit1 + digit2 + carry;
+                carry = sum / 10;
+                ans.add(sum % 10);
+            }
+
+            if (carry != 0) {
+                ans.add(carry);
+            }
+            return ans;
+        }
+
+        ArrayList<Integer> multiplyOneDigit(StringBuilder firstNumber, char secondNumberDigit, int numZeros) {
+            ArrayList<Integer> currentResult = new ArrayList<>();
+            for (int i = 0; i < numZeros; i++) {
+                currentResult.add(0);
+            }
+            int carry = 0;
+            for (int i = 0; i < firstNumber.length(); i++) {
+                char firstNumberDigit = firstNumber.charAt(i);
+                int multiplication = (secondNumberDigit - '0') * (firstNumberDigit - '0') + carry;
+                carry = multiplication / 10;
+                currentResult.add(multiplication % 10);
+            }
+            if (carry != 0) {
+                currentResult.add(carry);
+            }
+            return currentResult;
+        }
+
+        public String multiply(String num1, String num2) {
+            if (num1.equals("0") || num2.equals("0")) {
+                return "0";
+            }
+            StringBuilder firstNumber = new StringBuilder(num1);
+            StringBuilder secondNumber = new StringBuilder(num2);
+
+            firstNumber.reverse();
+            secondNumber.reverse();
+
+            int N = firstNumber.length() + secondNumber.length();
+            ArrayList<Integer> ans = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                ans.add(0);
+            }
+
+            for (int i = 0; i < secondNumber.length(); i++) {
+                ans = addStrings(multiplyOneDigit(firstNumber, secondNumber.charAt(i), i), ans);
+            }
+
+            if (ans.get(ans.size() - 1) == 0) {
+                ans.remove(ans.size() - 1);
+            }
+            StringBuilder answer = new StringBuilder();
+            for (int i = ans.size() - 1; i >= 0; --i) {
+                answer.append(ans.get(i));
+            }
+            return answer.toString();
+        }
+    }
 }
