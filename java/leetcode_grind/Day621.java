843120b94323389be3231bb2e3bfbc9126367f40
54k <54k@bk.ru>
2024-07-30 23:12:07 +0200

    day 621

--- java/leetcode_grind/Day621.java
+++ java/leetcode_grind/Day621.java
@@ -1,5 +1,6 @@
 package leetcode_grind;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class Day621 {
@@ -60,6 +61,37 @@ public class Day621 {
             }
 
             return dp[n];
+        }
+    }
+
+    // https://leetcode.com/problems/valid-parentheses/description/
+    static class Solution4 {
+        HashMap<Character, Character> mappings;
+        {
+            mappings = new HashMap<>();
+            mappings.put(')', '(');
+            mappings.put('}', '{');
+            mappings.put(']', '[');
+        }
+
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if (mappings.containsKey(c)) {
+                    char topElement = stack.empty() ? '#' : stack.pop();
+
+                    if (topElement != mappings.get(c)) {
+                        return false;
+                    }
+                } else {
+                    stack.push(c);
+                }
+            }
+
+            return stack.isEmpty();
         }
     }
 }
