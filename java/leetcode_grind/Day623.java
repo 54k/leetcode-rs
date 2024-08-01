e599bd38cdc9ec73212c7738ab63d662ca9f75b6
54k <54k@bk.ru>
2024-08-01 15:42:59 +0200

    day 623

--- java/leetcode_grind/Day623.java
+++ java/leetcode_grind/Day623.java
@@ -20,4 +20,23 @@ public class Day623 {
             return seniorCount;
         }
     }
+
+    // https://leetcode.com/problems/trapping-rain-water/description/
+    static class Solution2 {
+        public int trap(int[] height) {
+            int ans = 0;
+            int size = height.length;
+            for (int i = 1; i < size - 1; i++) {
+                int left_max = 0, right_max = 0;
+                for (int j = i; j >= 0; j--) {
+                    left_max = Math.max(left_max, height[j]);
+                }
+                for (int j = i; j < size; j++) {
+                    right_max = Math.max(right_max, height[j]);
+                }
+                ans += Math.min(left_max, right_max) - height[i];
+            }
+            return ans;
+        }
+    }
 }
--- src/leetcode_grind/day_623.rs
+++ src/leetcode_grind/day_623.rs
@@ -8,3 +8,29 @@ pub fn count_seniors(details: Vec<String>) -> i32 {
     }
     ans
 }
+
+// https://leetcode.com/problems/trapping-rain-water/description/
+pub fn trap(height: Vec<i32>) -> i32 {
+    if height.len() == 0 {
+        return 0;
+    }
+    let mut ans = 0;
+    let size = height.len();
+    let mut left_max = vec![0; size];
+    let mut right_max = vec![0; size];
+
+    left_max[0] = height[0];
+    for i in 1..size {
+        left_max[i] = height[i].max(left_max[i - 1]);
+    }
+
+    right_max[size - 1] = height[size - 1];
+    for i in (0..size - 1).rev() {
+        right_max[i] = height[i].max(right_max[i + 1]);
+    }
+
+    for i in 1..size - 1 {
+        ans += left_max[i].min(right_max[i]) - height[i];
+    }
+    ans
+}
