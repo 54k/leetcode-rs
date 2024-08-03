49817c2dfe43900ecea1ca6da94b4eccad7addb6
54k <54k@bk.ru>
2024-08-03 13:11:06 +0200

    day 625

--- java/leetcode_grind/Day625.java
+++ java/leetcode_grind/Day625.java
@@ -1,6 +1,8 @@
 package leetcode_grind;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Day625 {
     // https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/description/?envType=daily-question&envId=2024-08-03
@@ -14,6 +16,51 @@ public class Day625 {
                 }
             }
             return true;
+        }
+    }
+
+    // https://leetcode.com/problems/longest-repeating-substring/description/?envType=weekly-question&envId=2024-08-01
+    static class Solution2 {
+        public int longestRepeatingSubstring(String s) {
+            Set<String> seenSubstrings = new HashSet<>();
+            int maxLength = s.length() - 1;
+            for (int start = 0; start <= s.length(); start++) {
+                int end = start;
+                if (end + maxLength > s.length()) {
+                    if (--maxLength == 0)
+                        break;
+                    start = -1;
+                    seenSubstrings.clear();
+                    continue;
+                }
+
+                String currentSubstring = s.substring(end, end + maxLength);
+                if (!seenSubstrings.add(currentSubstring)) {
+                    return maxLength;
+                }
+            }
+            return maxLength;
+        }
+    }
+
+    static class Solution3 {
+        public int longestRepeatingSubstring(String s) {
+            int length = s.length(), maxLength = 0;
+            Set<String> seenSubstrings = new HashSet<>();
+
+            for (int start = 0; start < length; start++) {
+                int end = start;
+                if (end + maxLength >= length) {
+                    return maxLength;
+                }
+                String currentSubstring = s.substring(end, end + maxLength + 1);
+                if (!seenSubstrings.add(currentSubstring)) {
+                    start = -1;
+                    seenSubstrings.clear();
+                    maxLength++;
+                }
+            }
+            return maxLength;
         }
     }
 }
--- src/leetcode_grind/day_625.rs
+++ src/leetcode_grind/day_625.rs
@@ -13,3 +13,27 @@ pub fn can_be_equal(target: Vec<i32>, arr: Vec<i32>) -> bool {
     }
     map.len() == 0
 }
+
+// https://leetcode.com/problems/longest-repeating-substring/description/?envType=weekly-question&envId=2024-08-01
+pub fn longest_repeating_substring(s: String) -> i32 {
+    let length = s.len();
+    let mut suffixes = vec![vec![]; length];
+
+    for i in 0..length {
+        suffixes[i] = s[i..].to_string().chars().collect::<Vec<_>>();
+    }
+
+    suffixes.sort();
+
+    let mut max_length = 0;
+    for i in 1..length {
+        let mut j = 0;
+        while j < suffixes[i].len().min(suffixes[i - 1].len())
+            && suffixes[i][j] == suffixes[i - 1][j]
+        {
+            j += 1;
+        }
+        max_length = max_length.max(j);
+    }
+    max_length as i32
+}
