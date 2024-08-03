31f38f13c9c49ea1c29209faa577aeeb0d550185
54k <54k@bk.ru>
2024-08-03 13:25:04 +0200

    day 625

--- java/leetcode_grind/Day625.java
+++ java/leetcode_grind/Day625.java
@@ -63,4 +63,32 @@ public class Day625 {
             return maxLength;
         }
     }
+
+    static class Solution4 {
+        public int longestRepeatingSubstring(String s) {
+            char[] characters = s.toCharArray();
+            int start = 1, end = characters.length - 1;
+
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if (hasRepeatingSubstring(characters, mid)) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            return end;
+        }
+
+        boolean hasRepeatingSubstring(char[] characters, int length) {
+            Set<String> seenSubstrings = new HashSet<>();
+            for (int i = 0; i <= characters.length - length; i++) {
+                String substring = new String(characters, i, length);
+                if (!seenSubstrings.add(substring)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
