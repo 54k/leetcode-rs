ea80e4b06ebbcc9b29c6b5c903355c0618204d84
54k <54k@bk.ru>
2024-08-03 21:00:32 +0200

    day 625

--- java/leetcode_grind/Day625.java
+++ java/leetcode_grind/Day625.java
@@ -1,7 +1,11 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Day625 {
@@ -141,6 +145,74 @@ public class Day625 {
             if (index >= s.length())
                 return 0;
             return s.charAt(index) - 'a' + 1;
+        }
+    }
+
+    // https://leetcode.com/problems/word-search-ii/description/
+    static class Solution6 {
+        static class TrieNode {
+            Map<Character, TrieNode> children = new HashMap<>();
+            String word = null;
+        }
+
+        char[][] _board = null;
+        ArrayList<String> _result = new ArrayList<String>();
+
+        public List<String> findWords(char[][] board, String[] words) {
+            TrieNode root = new TrieNode();
+            for (String word : words) {
+                TrieNode node = root;
+                for (Character letter : word.toCharArray()) {
+                    if (node.children.containsKey(letter)) {
+                        node = node.children.get(letter);
+                    } else {
+                        TrieNode newNode = new TrieNode();
+                        node.children.put(letter, newNode);
+                        node = newNode;
+                    }
+                }
+                node.word = word;
+            }
+
+            this._board = board;
+            for (int row = 0; row < board.length; ++row) {
+                for (int col = 0; col < board[row].length; ++col) {
+                    if (root.children.containsKey(board[row][col])) {
+                        backtracking(row, col, root);
+                    }
+                }
+            }
+            return this._result;
+        }
+
+        void backtracking(int row, int col, TrieNode parent) {
+            Character letter = this._board[row][col];
+            TrieNode currNode = parent.children.get(letter);
+
+            if (currNode.word != null) {
+                this._result.add(currNode.word);
+                currNode.word = null;
+            }
+
+            this._board[row][col] = '#';
+
+            int[] rowOffset = { -1, 0, 1, 0 };
+            int[] colOffset = { 0, 1, 0, -1 };
+            for (int i = 0; i < 4; i++) {
+                int newRow = row + rowOffset[i];
+                int newCol = col + colOffset[i];
+                if (newRow < 0 || newRow >= this._board.length || newCol < 0 || newCol >= this._board[0].length) {
+                    continue;
+                }
+                if (currNode.children.containsKey(this._board[newRow][newCol])) {
+                    backtracking(newRow, newCol, currNode);
+                }
+            }
+            this._board[row][col] = letter;
+
+            if (currNode.children.isEmpty()) {
+                parent.children.remove(letter);
+            }
         }
     }
 }
