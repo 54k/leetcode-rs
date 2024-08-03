d1dffb4e2a5141827078e0ff8f936503e860ada1
54k <54k@bk.ru>
2024-08-03 22:14:25 +0200

    day 625

--- java/leetcode_grind/Day625.java
+++ java/leetcode_grind/Day625.java
@@ -215,4 +215,61 @@ public class Day625 {
             }
         }
     }
+
+    // https://leetcode.com/problems/design-tic-tac-toe/description/
+    static class TicTacToe {
+
+        int[][] board;
+        int n;
+
+        public TicTacToe(int n) {
+            board = new int[n][n];
+            this.n = n;
+        }
+
+        public int move(int row, int col, int player) {
+            board[row][col] = player;
+            if ((checkRow(row, player)) || (checkColumn(col, player)) || (row == col && checkDiagonal(player))
+                    || (col == n - row - 1 && checkAntiDiagonal(player))) {
+                return player;
+            }
+            return 0;
+        }
+
+        boolean checkDiagonal(int player) {
+            for (int row = 0; row < n; row++) {
+                if (board[row][row] != player) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        boolean checkAntiDiagonal(int player) {
+            for (int row = 0; row < n; row++) {
+                if (board[row][n - row - 1] != player) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        boolean checkColumn(int col, int player) {
+            for (int row = 0; row < n; row++) {
+                if (board[row][col] != player) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        boolean checkRow(int row, int player) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] != player) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
