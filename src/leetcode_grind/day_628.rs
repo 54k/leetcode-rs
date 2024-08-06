fb2708e950337778ce3b21c8b4d7f6d76884d9fe
54k <54k@bk.ru>
2024-08-07 00:06:28 +0200

    day 628

--- src/leetcode_grind/day_628.rs
+++ src/leetcode_grind/day_628.rs
@@ -16,3 +16,31 @@ pub fn minimum_pushes(word: String) -> i32 {
     }
     ans as i32
 }
+
+// https://leetcode.com/problems/trapping-rain-water/description/
+pub fn trap(height: Vec<i32>) -> i32 {
+    let n = height.len();
+    let mut left = 0;
+    let mut right = n - 1;
+    let mut left_max = 0;
+    let mut right_max = 0;
+    let mut ans = 0;
+    while left < right {
+        if height[left] < height[right] {
+            if height[left] >= left_max {
+                left_max = height[left];
+            } else {
+                ans += left_max - height[left];
+            }
+            left += 1;
+        } else {
+            if height[right] >= right_max {
+                right_max = height[right].max(right_max);
+            } else {
+                ans += right_max - height[right];
+            }
+            right -= 1;
+        }
+    }
+    ans
+}
