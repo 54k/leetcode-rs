e82d8103e99c44d555bf7c020f40cb3285e72487
54k <54k@bk.ru>
2024-08-13 16:00:07 +0200

    day 635

--- java/leetcode_grind/Day635.java
+++ java/leetcode_grind/Day635.java
@@ -181,4 +181,52 @@ public class Day635 {
             return point[0] * point[0] + point[1] * point[1];
         }
     }
+
+    static class Solution6 {
+        public int[][] kClosest(int[][] points, int k) {
+            return quickSelect(points, k);
+        }
+
+        int[][] quickSelect(int[][] points, int k) {
+            int left = 0, right = points.length - 1;
+            int pivotIndex = points.length;
+            while (pivotIndex != k) {
+                pivotIndex = partition(points, left, right);
+                if (pivotIndex < k) {
+                    left = pivotIndex;
+                } else {
+                    right = pivotIndex - 1;
+                }
+            }
+            return Arrays.copyOf(points, k);
+        }
+
+        int partition(int[][] points, int left, int right) {
+            int[] pivot = choosePivot(points, left, right);
+            int pivotDist = squaredDistance(pivot);
+            while (left < right) {
+                if (squaredDistance(points[left]) >= pivotDist) {
+                    int[] temp = points[left];
+                    points[left] = points[right];
+                    points[right] = temp;
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+
+            if (squaredDistance(points[left]) < pivotDist) {
+                left++;
+            }
+            return left;
+        }
+
+        int[] choosePivot(int[][] points, int left, int right) {
+            return points[left + (right - left) / 2];
+        }
+
+        int squaredDistance(int[] point) {
+            return point[0] * point[0] + point[1] * point[1];
+        }
+    }
 }
