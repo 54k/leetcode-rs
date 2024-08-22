6cce140f79a742f8ba19f08a5192a836c5e3ce28
54k <54k@bk.ru>
2024-08-22 02:15:15 +0200

    day 644

---
+++ java/leetcode_grind/Day644.java
@@ -0,0 +1,12 @@
+package leetcode_grind;
+
+public class Day644 {
+    // https://leetcode.com/problems/number-complement/description/?envType=daily-question&envId=2024-08-22
+    static class Solution1 {
+        public int findComplement(int num) {
+            int n = (int) (Math.log(num) / Math.log(2)) + 1;
+            int bitmask = (1 << n) - 1;
+            return num ^ bitmask;
+        }
+    }
+}
