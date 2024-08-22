327c177556f2451e67abf8cd7ef9ac4bd9849f73
54k <54k@bk.ru>
2024-08-22 15:28:24 +0200

    day 644

--- java/leetcode_grind/Day644.java
+++ java/leetcode_grind/Day644.java
@@ -15,4 +15,36 @@ public class Day644 {
             return (Integer.highestOneBit(num) << 1) - num - 1;
         }
     }
+
+    // https://leetcode.com/problems/remove-boxes/description/
+    static class Solution3 {
+        public int removeBoxes(int[] boxes) {
+            int[][][] dp = new int[100][100][100];
+            return calculatePoints(boxes, dp, 0, boxes.length - 1, 0);
+        }
+
+        int calculatePoints(int[] boxes, int[][][] dp, int l, int r, int k) {
+            if (l > r)
+                return 0;
+
+            while (r > l && boxes[r] == boxes[r - 1]) {
+                r--;
+                k++;
+            }
+
+            if (dp[l][r][k] != 0) {
+                return dp[l][r][k];
+            }
+
+            dp[l][r][k] = calculatePoints(boxes, dp, l, r - 1, 0) + (k + 1) * (k + 1);
+            for (int i = l; i < r; i++) {
+                if (boxes[i] == boxes[r]) {
+                    dp[l][r][k] = Math.max(dp[l][r][k],
+                            calculatePoints(boxes, dp, l, i, k + 1) + calculatePoints(boxes, dp, i + 1, r - 1, 0));
+                }
+            }
+
+            return dp[l][r][k];
+        }
+    }
 }
