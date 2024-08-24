4caf950f2044d604d95689a3be996d30c6e9b88d
54k <54k@bk.ru>
2024-08-24 13:27:07 +0200

    day 646

--- java/leetcode_grind/Day646.java
+++ java/leetcode_grind/Day646.java
@@ -43,4 +43,60 @@ public class Day646 {
             return res;
         }
     }
+
+    static class Solution2 {
+        long convert(long num) {
+            String s = Long.toString(num);
+            int n = s.length();
+            int l = (n - 1) / 2, r = n / 2;
+            char[] sArray = s.toCharArray();
+            while (l >= 0) {
+                sArray[r++] = sArray[l--];
+            }
+            return Long.parseLong(new String(sArray));
+        }
+
+        long nextPalindrome(long num) {
+            long left = 0, right = num;
+            long ans = Long.MIN_VALUE;
+            while (left <= right) {
+                long mid = (right - left) / 2 + left;
+                long palin = convert(mid);
+
+                if (palin < num) {
+                    ans = palin;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return ans;
+        }
+
+        long previousPalindrome(long num) {
+            long left = num, right = (long) 1e18;
+            long ans = Long.MIN_VALUE;
+            while (left <= right) {
+                long mid = (right - left) / 2 + left;
+                long palin = convert(mid);
+                if (palin > num) {
+                    ans = palin;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return ans;
+        }
+
+        public String nearestPalindromic(String n) {
+            long num = Long.parseLong(n);
+            long a = nextPalindrome(num);
+            long b = previousPalindrome(num);
+            if (Math.abs(a - num) <= Math.abs(b - num)) {
+                return Long.toString(a);
+            }
+            return Long.toString(b);
+        }
+    }
 }
