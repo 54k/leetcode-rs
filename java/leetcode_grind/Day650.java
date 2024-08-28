6bb7876b31d81ce8be92cd6bdb6733d96b8398ab
54k <54k@bk.ru>
2024-08-28 15:13:57 +0200

    day 650

--- java/leetcode_grind/Day650.java
+++ java/leetcode_grind/Day650.java
@@ -1,6 +1,8 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Day650 {
@@ -241,6 +243,74 @@ public class Day650 {
             }
 
             return subIslandCounts;
+        }
+    }
+
+    // https://leetcode.com/problems/number-of-distinct-islands/description/
+    static class Solution4 {
+        List<List<int[]>> uniqueIslands = new ArrayList<>();
+        List<int[]> currentIsland = new ArrayList<>();
+        int[][] grid;
+        boolean[][] seen;
+
+        public int numDistinctIslands(int[][] grid) {
+            this.grid = grid;
+            this.seen = new boolean[grid.length][grid[0].length];
+            for (int row = 0; row < grid.length; row++) {
+                for (int col = 0; col < grid[0].length; col++) {
+                    dfs(row, col);
+                    if (currentIsland.isEmpty()) {
+                        continue;
+                    }
+                    int minCol = grid[0].length - 1;
+                    for (int i = 0; i < currentIsland.size(); i++) {
+                        minCol = Math.min(minCol, currentIsland.get(i)[1]);
+                    }
+                    for (int[] cell : currentIsland) {
+                        cell[0] -= row;
+                        cell[1] -= minCol;
+                    }
+                    if (currentIslandUnique()) {
+                        uniqueIslands.add(currentIsland);
+                    }
+                    currentIsland = new ArrayList<>();
+                }
+            }
+            return uniqueIslands.size();
+        }
+
+        void dfs(int row, int col) {
+            if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length)
+                return;
+            if (seen[row][col] || grid[row][col] == 0)
+                return;
+            seen[row][col] = true;
+            currentIsland.add(new int[] { row, col });
+            dfs(row + 1, col);
+            dfs(row - 1, col);
+            dfs(row, col + 1);
+            dfs(row, col - 1);
+        }
+
+        boolean currentIslandUnique() {
+            for (List<int[]> otherIsland : uniqueIslands) {
+                if (currentIsland.size() != otherIsland.size()) {
+                    continue;
+                }
+                if (equalIslands(currentIsland, otherIsland)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        boolean equalIslands(List<int[]> island1, List<int[]> island2) {
+            for (int i = 0; i < island1.size(); i++) {
+                if (island1.get(i)[0] != island2.get(i)[0] || island1.get(i)[1] != island2.get(i)[1]) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
