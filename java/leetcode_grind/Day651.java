d1ca02248df0723a291046ab9af5f0b2e90c0385
54k <54k@bk.ru>
2024-08-29 12:13:28 +0200

    day 651

--- java/leetcode_grind/Day651.java
+++ java/leetcode_grind/Day651.java
@@ -1,9 +1,14 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 public class Day651 {
     static class Pair<F, S> {
@@ -104,4 +109,141 @@ public class Day651 {
         }
     }
 
+    // https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/description/?envType=daily-question&envId=2024-08-29
+    static class Solution3 {
+        public int removeStones(int[][] stones) {
+            int n = stones.length;
+            List<Integer>[] adjacencyList = new List[n];
+            for (int i = 0; i < n; i++) {
+                adjacencyList[i] = new ArrayList<>();
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                        adjacencyList[i].add(j);
+                        adjacencyList[j].add(i);
+                    }
+                }
+            }
+
+            int numOfConnectedComponents = 0;
+            boolean[] visited = new boolean[n];
+
+            for (int i = 0; i < n; i++) {
+                if (!visited[i]) {
+                    depthFirstSearch(adjacencyList, visited, i);
+                    numOfConnectedComponents++;
+                }
+            }
+
+            return n - numOfConnectedComponents;
+        }
+
+        void depthFirstSearch(
+                List<Integer>[] adjacencyList,
+                boolean[] visited,
+                int stone) {
+            visited[stone] = true;
+
+            for (int neighbor : adjacencyList[stone]) {
+                if (!visited[neighbor]) {
+                    depthFirstSearch(adjacencyList, visited, neighbor);
+                }
+            }
+        }
+    }
+
+    static class Solution4 {
+        static class UnionFind {
+            int[] parent;
+            int count;
+
+            UnionFind(int n) {
+                parent = new int[n];
+                Arrays.fill(parent, -1);
+                count = n;
+            }
+
+            int find(int node) {
+                if (parent[node] == -1) {
+                    return node;
+                }
+                return parent[node] = find(parent[node]);
+            }
+
+            void union(int n1, int n2) {
+                int root1 = find(n1);
+                int root2 = find(n2);
+
+                if (root1 == root2) {
+                    return;
+                }
+
+                count--;
+                parent[root1] = root2;
+            }
+        }
+
+        public int removeStones(int[][] stones) {
+            int n = stones.length;
+            UnionFind uf = new UnionFind(n);
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                        uf.union(i, j);
+                    }
+                }
+            }
+            return n - uf.count;
+        }
+    }
+
+    static class Solution5 {
+        public int removeStones(int[][] stones) {
+            int n = stones.length;
+            UnionFind uf = new UnionFind(20002);
+            for (int i = 0; i < n; i++) {
+                uf.union(stones[i][0], stones[i][1] + 10001);
+            }
+            return n - uf.componentCount;
+        }
+
+        static class UnionFind {
+            int[] parent;
+            int componentCount;
+            Set<Integer> uniqueNodes;
+
+            UnionFind(int n) {
+                parent = new int[n];
+                Arrays.fill(parent, -1);
+                componentCount = 0;
+                uniqueNodes = new HashSet<>();
+            }
+
+            int find(int node) {
+                if (!uniqueNodes.contains(node)) {
+                    componentCount++;
+                    uniqueNodes.add(node);
+                }
+
+                if (parent[node] == -1) {
+                    return node;
+                }
+                return parent[node] = find(parent[node]);
+            }
+
+            void union(int node1, int node2) {
+                int root1 = find(node1);
+                int root2 = find(node2);
+
+                if (root1 == root2) {
+                    return;
+                }
+
+                parent[root1] = root2;
+                componentCount--;
+            }
+        }
+    }
 }
