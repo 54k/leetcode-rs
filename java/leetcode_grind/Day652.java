cd88ce3f924faf9224a71ff4a1fb14cedbabefc3
54k <54k@bk.ru>
2024-08-30 13:50:40 +0200

    day 652

--- java/leetcode_grind/Day652.java
+++ java/leetcode_grind/Day652.java
@@ -151,4 +151,44 @@ public class Day652 {
             return minDistance[destination];
         }
     }
+
+    // https://leetcode.com/problems/number-of-distinct-islands/description/
+    static class Solution3 {
+        int[][] grid;
+        boolean[][] visited;
+        StringBuffer currentIsland;
+
+        public int numDistinctIslands(int[][] grid) {
+            this.grid = grid;
+            this.visited = new boolean[grid.length][grid[0].length];
+            Set<String> islands = new HashSet<>();
+            for (int row = 0; row < grid.length; row++) {
+                for (int col = 0; col < grid[0].length; col++) {
+                    currentIsland = new StringBuffer();
+                    dfs(row, col, '0');
+                    if (currentIsland.length() == 0) {
+                        continue;
+                    }
+                    islands.add(currentIsland.toString());
+                }
+            }
+            return islands.size();
+        }
+
+        void dfs(int row, int col, char dir) {
+            if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) {
+                return;
+            }
+            if (visited[row][col] || grid[row][col] == 0) {
+                return;
+            }
+            visited[row][col] = true;
+            currentIsland.append(dir);
+            dfs(row + 1, col, 'D');
+            dfs(row - 1, col, 'U');
+            dfs(row, col + 1, 'R');
+            dfs(row, col - 1, 'L');
+            currentIsland.append('0');
+        }
+    }
 }
