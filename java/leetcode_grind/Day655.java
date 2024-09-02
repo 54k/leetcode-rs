a02731a299749428e4e83951a52020ab34f8906c
54k <54k@bk.ru>
2024-09-02 23:12:00 +0200

    day 655

--- java/leetcode_grind/Day655.java
+++ java/leetcode_grind/Day655.java
@@ -49,4 +49,54 @@ public class Day655 {
             return low;
         }
     }
+
+    // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+    static class Solution3 {
+        public int strStr(String haystack, String needle) {
+            int m = needle.length();
+            int n = haystack.length();
+            if (n < m)
+                return -1;
+
+            int[] longest_border = new int[m];
+            int prev = 0;
+            int i = 1;
+
+            while (i < m) {
+                if (needle.charAt(i) == needle.charAt(prev)) {
+                    prev += 1;
+                    longest_border[i] = prev;
+                    i += 1;
+                } else {
+                    if (prev == 0) {
+                        longest_border[i] = 0;
+                        i += 1;
+                    } else {
+                        prev = longest_border[prev - 1];
+                    }
+                }
+            }
+
+            int haystackPointer = 0;
+            int needlePointer = 0;
+
+            while (haystackPointer < n) {
+                if (haystack.charAt(haystackPointer) == needle.charAt(needlePointer)) {
+                    needlePointer += 1;
+                    haystackPointer += 1;
+                    if (needlePointer == m) {
+                        return haystackPointer - m;
+                    }
+                } else {
+                    if (needlePointer == 0) {
+                        haystackPointer += 1;
+                    } else {
+                        needlePointer = longest_border[needlePointer - 1];
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
 }
--- src/leetcode_grind/day_655.rs
+++ src/leetcode_grind/day_655.rs
@@ -17,3 +17,25 @@ pub fn chalk_replacer(chalk: Vec<i32>, mut k: i32) -> i32 {
     }
     0
 }
+
+// https://leetcode.com/problems/longest-happy-prefix/description/
+pub fn longest_prefix(s: String) -> String {
+    let mut maxi = 0;
+    let s = s.chars().collect::<Vec<_>>();
+    let mut p = vec![0; s.len()];
+    for i in 1..s.len() {
+        let mut j = p[i - 1];
+        while j > 0 && s[i] != s[j] {
+            j = p[j - 1];
+        }
+        if s[i] == s[j] {
+            j += 1;
+        }
+        p[i] = j;
+        if p[maxi] < p[i] {
+            maxi = i;
+        }
+    }
+
+    s[..p[s.len() - 1]].iter().collect()
+}
