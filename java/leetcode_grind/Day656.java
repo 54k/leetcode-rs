3763982866831907e6d1e5149d2a3e7cee557f13
54k <54k@bk.ru>
2024-09-03 02:12:03 +0200

    day 656

---
+++ java/leetcode_grind/Day656.java
@@ -0,0 +1,26 @@
+package leetcode_grind;
+
+public class Day656 {
+    // https://leetcode.com/problems/sum-of-digits-of-string-after-convert/description/?envType=daily-question&envId=2024-09-03
+    static class Solution1 {
+        public int getLucky(String s, int k) {
+            int currentNumber = 0;
+            for (char ch : s.toCharArray()) {
+                int position = ch - 'a' + 1;
+                while (position > 0) {
+                    currentNumber += position % 10;
+                    position /= 10;
+                }
+            }
+            for (int i = 1; i < k; i++) {
+                int digitSum = 0;
+                while (currentNumber > 0) {
+                    digitSum += currentNumber % 10;
+                    currentNumber /= 10;
+                }
+                currentNumber = digitSum;
+            }
+            return currentNumber;
+        }
+    }
+}
