96cadbeb450d37f59c4c45253aeb3325402654b8
54k <54k@bk.ru>
2024-09-07 17:12:26 +0200

    day 660

--- java/leetcode_grind/Day660.java
+++ java/leetcode_grind/Day660.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.*;
+
 public class Day660 {
     public class ListNode {
         int val;
@@ -42,6 +44,29 @@ public class Day660 {
         public boolean isSubPath(ListNode head, TreeNode root) {
             if (root == null)
                 return false;
+            boolean result = dfs(root, head);
+            result |= isSubPath(head, root.left);
+            result |= isSubPath(head, root.right);
+            return result;
+        }
+
+        boolean dfs(TreeNode node, ListNode head) {
+            if (head == null)
+                return true;
+            if (node == null)
+                return false;
+            boolean result = false;
+            if (node.val == head.val) {
+                result |= dfs(node.right, head.next) || dfs(node.left, head.next);
+            }
+            return result;
+        }
+    }
+
+    static class Solution2 {
+        public boolean isSubPath(ListNode head, TreeNode root) {
+            if (root == null)
+                return false;
             return checkPath(root, head);
         }
 
@@ -64,4 +89,105 @@ public class Day660 {
         }
     }
 
+    static class Solution3 {
+        public boolean isSubPath(ListNode head, TreeNode root) {
+            if (root == null)
+                return false;
+            Stack<TreeNode> nodes = new Stack<>();
+            nodes.push(root);
+
+            while (!nodes.isEmpty()) {
+                TreeNode node = nodes.pop();
+                if (isMatch(node, head)) {
+                    return true;
+                }
+                if (node.left != null) {
+                    nodes.push(node.left);
+                }
+                if (node.right != null) {
+                    nodes.push(node.right);
+                }
+            }
+            return false;
+        }
+
+        boolean isMatch(TreeNode node, ListNode lst) {
+            Stack<Map.Entry<TreeNode, ListNode>> s = new Stack<>();
+            s.push(new HashMap.SimpleEntry<>(node, lst));
+
+            while (!s.isEmpty()) {
+                Map.Entry<TreeNode, ListNode> entry = s.pop();
+                TreeNode currentNode = entry.getKey();
+                ListNode currentList = entry.getValue();
+
+                while (currentNode != null && currentList != null) {
+                    if (currentNode.val != currentList.val) {
+                        break;
+                    }
+
+                    currentList = currentList.next;
+
+                    if (currentList != null) {
+                        if (currentNode.left != null) {
+                            s.push(
+                                    new HashMap.SimpleEntry<>(
+                                            currentNode.left,
+                                            currentList));
+                        }
+                        if (currentNode.right != null) {
+                            s.push(
+                                    new HashMap.SimpleEntry<>(
+                                            currentNode.right,
+                                            currentList));
+                        }
+                        break;
+                    }
+                    if (currentList == null) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    static class Solution4 {
+        public boolean isSubPath(ListNode head, TreeNode root) {
+            List<Integer> pattern = new ArrayList<>();
+            List<Integer> prefixTable = new ArrayList<>();
+            pattern.add(head.val);
+            prefixTable.add(0);
+            int patternIndex = 0;
+            head = head.next;
+            while (head != null) {
+                while (patternIndex > 0 && head.val != pattern.get(patternIndex)) {
+                    patternIndex = prefixTable.get(patternIndex - 1);
+                }
+                patternIndex += head.val == pattern.get(patternIndex) ? 1 : 0;
+                pattern.add(head.val);
+                prefixTable.add(patternIndex);
+                head = head.next;
+            }
+            return searchInTree(root, 0, pattern, prefixTable);
+        }
+
+        boolean searchInTree(
+                TreeNode node,
+                int patternIndex,
+                List<Integer> pattern,
+                List<Integer> prefixTable) {
+            if (node == null)
+                return false;
+            while (patternIndex > 0 && node.val != pattern.get(patternIndex)) {
+                patternIndex = prefixTable.get(patternIndex - 1);
+            }
+            patternIndex += node.val == pattern.get(patternIndex) ? 1 : 0;
+
+            if (patternIndex == pattern.size())
+                return true;
+
+            return (searchInTree(node.left, patternIndex, pattern, prefixTable) ||
+                    searchInTree(node.right, patternIndex, pattern, prefixTable));
+        }
+    }
 }
