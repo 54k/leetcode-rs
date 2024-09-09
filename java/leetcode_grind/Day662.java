fd4f4df49029b504d98ade87b662050d427ba7f2
54k <54k@bk.ru>
2024-09-09 14:40:05 +0200

    day 662

--- java/leetcode_grind/Day662.java
+++ java/leetcode_grind/Day662.java
@@ -47,4 +47,130 @@ public class Day662 {
             return res;
         }
     }
+
+    // https://leetcode.com/problems/maximum-sum-of-subsequence-with-non-adjacent-elements/description/
+    static class Solution2 {
+        public int maximumSumSubsequence(int[] nums, int[][] queries) {
+            var MOD = 1_000_000_007;
+            var n = nums.length;
+            long totalSum = 0;
+
+            var dp_incl = new long[n]; // to track max sum till index i, when including index i
+            var dp_excl = new long[n]; // to track max sum till index i, when excluding index i
+
+            // fill dp initially
+            long incll = 0, excll = 0;
+            for (var i = 0; i < n; i++) {
+                long newExcl = Math.max(incll, excll);
+                incll = (excll + nums[i]) % MOD;
+                excll = newExcl;
+
+                dp_incl[i] = incll;
+                dp_excl[i] = excll;
+            }
+
+            // now, for each query, update the nums array
+            // and then start calculating incl/excl from the index that was changed
+            for (var query : queries) {
+                int pos = query[0];
+                int val = query[1];
+                nums[pos] = val;
+
+                long incl = 0, excl = 0;
+                if (pos != 0) {
+                    incl = dp_incl[pos - 1];
+                    excl = dp_excl[pos - 1];
+                }
+
+                for (var i = pos; i < n; i++) {
+                    long newExcl = Math.max(incl, excl);
+                    incl = (excl + nums[i]) % MOD;
+                    excl = newExcl;
+
+                    dp_incl[i] = incl;
+                    dp_excl[i] = excl;
+                }
+
+                long maxSum = Math.max(incl, excl);
+                totalSum = (totalSum + maxSum) % MOD;
+            }
+
+            return (int) totalSum;
+        }
+    }
+
+    static class Solution3 {
+        // each node in seg tree will have to maintain 4 values, so that we can apply
+        // house robber algo
+        // [0][0] : leftmost & rightmost in curr range is not selected
+        // [0][1] : leftmost in curr range is not selected, rightmost is selected
+        // [1][0] : leftmost in curr range is selected, rightmost is not selected
+        // [1][1] : leftmost & rightmost in curr range are both selected
+        public int maximumSumSubsequence(int[] nums, int[][] queries) {
+            var MOD = 1_000_000_007;
+            var segTree = new Node(nums, 0, nums.length - 1);
+            long res = 0;
+
+            for (var query : queries) {
+                segTree.update(query[0], query[1]);
+                res += segTree.getMaxSum();
+                res %= MOD;
+            }
+            return (int) res;
+        }
+
+        static class Node {
+            long inf = (long) 1e18;
+            Node l = null, r = null;
+            int lo, hi;
+            long[][] selected = new long[2][2];
+
+            public Node(int[] nums, int lo, int hi) {
+                this.lo = lo;
+                this.hi = hi;
+
+                if (lo < hi) {
+                    var mid = (lo + hi) / 2;
+                    l = new Node(nums, lo, mid);
+                    r = new Node(nums, mid + 1, hi);
+                    combine();
+                } else {
+                    selected[0][0] = 0;
+                    selected[0][1] = -inf;
+                    selected[1][0] = -inf;
+                    selected[1][1] = nums[lo]; // same as nums[hi]
+                }
+            }
+
+            void combine() {
+                selected[0][0] = Math.max(l.selected[0][0] + r.selected[0][0],
+                        Math.max(l.selected[0][1] + r.selected[0][0], l.selected[0][0] + r.selected[1][0]));
+                selected[0][1] = Math.max(l.selected[0][0] + r.selected[0][1],
+                        Math.max(l.selected[0][1] + r.selected[0][1], l.selected[0][0] + r.selected[1][1]));
+                selected[1][0] = Math.max(l.selected[1][0] + r.selected[0][0],
+                        Math.max(l.selected[1][1] + r.selected[0][0], l.selected[1][0] + r.selected[1][0]));
+                selected[1][1] = Math.max(l.selected[1][0] + r.selected[0][1],
+                        Math.max(l.selected[1][1] + r.selected[0][1], l.selected[1][0] + r.selected[1][1]));
+            }
+
+            public void update(int idx, long val) {
+                if (idx < lo || idx > hi)
+                    return;
+                if (lo == hi) {
+                    selected[0][0] = 0;
+                    selected[1][1] = val;
+                    return;
+                }
+                l.update(idx, val);
+                r.update(idx, val);
+                combine();
+            }
+
+            long getMaxSum() {
+                return Math.max(
+                        selected[0][0],
+                        Math.max(selected[0][1], Math.max(selected[1][0], selected[1][1])));
+            }
+        }
+    }
 }
