6974efe70810376ea8a367f7e3921d495e0d6d0a
54k <54k@bk.ru>
2024-09-20 11:01:12 +0200

    day 673

--- java/leetcode_grind/Day673.java
+++ java/leetcode_grind/Day673.java
@@ -33,4 +33,19 @@ public class Day673 {
         }
     }
 
+    static class Solution2 {
+        public String shortestPalindrome(String s) {
+            int length = s.length();
+            String reversedString = new StringBuilder(s).reverse().toString();
+            for (int i = 0; i < length; i++) {
+                if (s.substring(0, length - i).equals(reversedString.substring(i))) {
+                    return new StringBuilder(reversedString.substring(0, i))
+                            .append(s)
+                            .toString();
+                }
+            }
+            return "";
+        }
+    }
+
 }
