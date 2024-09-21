37e5784dd66bf18e9aa25d9fc613d1f3b0fae16c
54k <54k@bk.ru>
2024-09-21 09:51:37 +0200

    day 674

--- java/leetcode_grind/Day674.java
+++ java/leetcode_grind/Day674.java
@@ -24,4 +24,48 @@ public class Day674 {
             return res;
         }
     }
+
+    // https://leetcode.com/problems/shortest-palindrome/description/?envType=daily-question&envId=2024-09-20
+    static class Solution2 {
+        public String shortestPalindrome(String s) {
+            if (s == null || s.length() == 0) {
+                return s;
+            }
+
+            String modifiedString = preprocessString(s);
+            int[] palindromeRadiusArray = new int[modifiedString.length()];
+            int center = 0, rightBoundary = 0;
+            int maxPalindromeLength = 0;
+
+            for (int i = 1; i < modifiedString.length() - 1; i++) {
+                int mirrorIndex = 2 * center - i;
+                if (rightBoundary > i) {
+                    palindromeRadiusArray[i] = Math.min(rightBoundary - i, palindromeRadiusArray[mirrorIndex]);
+                }
+                while (modifiedString.charAt(i + 1 + palindromeRadiusArray[i]) == modifiedString
+                        .charAt(i - 1 - palindromeRadiusArray[i])) {
+                    palindromeRadiusArray[i]++;
+                }
+                if (i + palindromeRadiusArray[i] > rightBoundary) {
+                    center = i;
+                    rightBoundary = i + palindromeRadiusArray[i];
+                }
+
+                if (i - palindromeRadiusArray[i] == 1) {
+                    maxPalindromeLength = Math.max(maxPalindromeLength, palindromeRadiusArray[i]);
+                }
+            }
+
+            StringBuilder suffix = new StringBuilder(s.substring(maxPalindromeLength)).reverse();
+            return suffix.append(s).toString();
+        }
+
+        String preprocessString(String s) {
+            StringBuilder sb = new StringBuilder("^");
+            for (char c : s.toCharArray()) {
+                sb.append("#").append(c);
+            }
+            return sb.append("#$").toString();
+        }
+    }
 }
