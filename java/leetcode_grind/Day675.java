0b4ee5dbeacdf80e071037e59351670e32cca622
54k <54k@bk.ru>
2024-09-22 18:24:35 +0200

    day 675

--- java/leetcode_grind/Day675.java
+++ java/leetcode_grind/Day675.java
@@ -77,4 +77,30 @@ public class Day675 {
             }
         }
     }
+
+    // https://leetcode.com/problems/longest-word-in-dictionary/description/
+    static class Solution3 {
+        public String longestWord(String[] words) {
+            String ans = "";
+            Set<String> wordset = new HashSet<>();
+            for (String word : words) {
+                wordset.add(word);
+            }
+            for (String word : words) {
+                if (word.length() > ans.length() || word.length() == ans.length() && word.compareTo(ans) < 0) {
+                    boolean good = true;
+                    for (int k = 1; k < word.length(); ++k) {
+                        if (!wordset.contains(word.substring(0, k))) {
+                            good = false;
+                            break;
+                        }
+                    }
+                    if (good) {
+                        ans = word;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
 }
