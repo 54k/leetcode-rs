956ae119c889fb59f86ba0e6de75c8b0e68d91bd
54k <54k@bk.ru>
2024-09-22 19:33:29 +0200

    day 675

--- java/leetcode_grind/Day675.java
+++ java/leetcode_grind/Day675.java
@@ -103,4 +103,67 @@ public class Day675 {
             return ans;
         }
     }
+
+    // https://leetcode.com/problems/longest-word-in-dictionary/description/
+    static class Solution4 {
+        public String longestWord(String[] words) {
+            Trie trie = new Trie();
+            int index = 0;
+            for (String word : words) {
+                trie.insert(word, ++index);
+            }
+            trie.words = words;
+            return trie.dfs();
+        }
+
+        static class Node {
+            char c;
+            HashMap<Character, Node> children = new HashMap<>();
+            int end;
+
+            Node(char c) {
+                this.c = c;
+            }
+        }
+
+        static class Trie {
+            Node root;
+            String[] words;
+
+            Trie() {
+                root = new Node('0');
+            }
+
+            void insert(String word, int index) {
+                Node cur = root;
+                for (char c : word.toCharArray()) {
+                    cur.children.putIfAbsent(c, new Node(c));
+                    cur = cur.children.get(c);
+                }
+                cur.end = index;
+            }
+
+            String dfs() {
+                String ans = "";
+                Stack<Node> stack = new Stack<>();
+                stack.push(root);
+                while (!stack.empty()) {
+                    Node node = stack.pop();
+                    if (node.end > 0 || node == root) {
+                        if (node != root) {
+                            String word = words[node.end - 1];
+                            if (word.length() > ans.length()
+                                    || word.length() == ans.length() && word.compareTo(ans) < 0) {
+                                ans = word;
+                            }
+                        }
+                        for (Node nei : node.children.values()) {
+                            stack.push(nei);
+                        }
+                    }
+                }
+                return ans;
+            }
+        }
+    }
 }
