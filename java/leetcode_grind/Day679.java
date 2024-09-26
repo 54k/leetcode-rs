8e83d7503b1e18415fb09c0d55cdd5db7522d7dc
54k <54k@bk.ru>
2024-09-26 13:38:58 +0200

    day 679

--- java/leetcode_grind/Day679.java
+++ java/leetcode_grind/Day679.java
@@ -27,4 +27,22 @@ public class Day679 {
     // * boolean param_1 = obj.book(start,end);
     // */
 
+    // class MyCalendar {
+    //     TreeMap<Integer, Integer> calendar;
+
+    //     public MyCalendar() {
+    //         calendar = new TreeMap<>();
+    //     }
+
+    //     public boolean book(int start, int end) {
+    //         Integer prev = calendar.floorKey(start),
+    //                 next = calendar.ceilingKey(start);
+    //         if ((prev == null || calendar.get(prev) <= start) &&
+    //                 (next == null || end <= next)) {
+    //             calendar.put(start, end);
+    //             return true;
+    //         }
+    //         return false;
+    //     }
+    // }
 }
