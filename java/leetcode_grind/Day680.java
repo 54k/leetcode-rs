1fb04642865753470758cde52bf51432f5313637
54k <54k@bk.ru>
2024-09-27 11:16:21 +0200

    day 680

--- java/leetcode_grind/Day680.java
+++ java/leetcode_grind/Day680.java
@@ -122,4 +122,33 @@ public class Day680 {
             return vals.getOrDefault(1, 0);
         }
     }
+
+    static class MyCalendarThree3 {
+        TreeMap<Integer, Integer> starts;
+        int res;
+
+        public MyCalendarThree3() {
+            starts = new TreeMap<>();
+            starts.put(0, 0);
+            res = 0;
+        }
+
+        void split(int x) {
+            Integer prev = starts.floorKey(x);
+            Integer next = starts.ceilingKey(x);
+            if (next != null && next == x) {
+                return;
+            }
+            starts.put(x, starts.get(prev));
+        }
+
+        public int book(int start, int end) {
+            split(start);
+            split(end);
+            for (var interval : starts.subMap(start, true, end, false).entrySet()) {
+                res = Math.max(res, interval.setValue(interval.getValue() + 1) + 1);
+            }
+            return res;
+        }
+    }
 }
