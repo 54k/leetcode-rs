abe3d9af5b3cb3b90e0b28bf9cff1b1b1ad1bf6d
54k <54k@bk.ru>
2024-09-27 10:44:14 +0200

    day 680

--- java/leetcode_grind/Day680.java
+++ java/leetcode_grind/Day680.java
@@ -70,4 +70,25 @@ public class Day680 {
             return true;
         }
     }
+
+    // https://leetcode.com/problems/my-calendar-iii/submissions/1403756216/
+    static class MyCalendarThree1 {
+
+        Map<Integer, Integer> diff;
+
+        public MyCalendarThree1() {
+            diff = new TreeMap<>();
+        }
+
+        public int book(int start, int end) {
+            diff.put(start, diff.getOrDefault(start, 0) + 1);
+            diff.put(end, diff.getOrDefault(end, 0) - 1);
+            int res = 0, cur = 0;
+            for (int delta : diff.values()) {
+                cur += delta;
+                res = Math.max(res, cur);
+            }
+            return res;
+        }
+    }
 }
