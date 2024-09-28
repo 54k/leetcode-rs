ed2fd4ac35c3a32fc4a9735dcd865ff98d9222a5
54k <54k@bk.ru>
2024-09-28 18:05:13 +0200

    day 681

--- java/leetcode_grind/Day681.java
+++ java/leetcode_grind/Day681.java
@@ -1,5 +1,9 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Day681 {
 
     // https://leetcode.com/problems/design-circular-deque/description/?envType=daily-question&envId=2024-09-28
@@ -231,6 +235,52 @@ public class Day681 {
             while (i <= mid) {
                 nums[idx++] = helper[i++ - low];
             }
+        }
+    }
+
+    // https://leetcode.com/problems/count-of-smaller-numbers-after-self/description/
+    static class Solution2 {
+        public List<Integer> countSmaller(int[] nums) {
+            int offset = 10000;
+            int size = 2 * 10000 + 1;
+            int[] tree = new int[size * 2];
+            List<Integer> result = new ArrayList<Integer>();
+
+            for (int i = nums.length - 1; i >= 0; i--) {
+                int smaller_count = query(0, nums[i] + offset, tree, size);
+                result.add(smaller_count);
+                update(nums[i] + offset, 1, tree, size);
+            }
+            Collections.reverse(result);
+            return result;
+        }
+
+        void update(int index, int value, int[] tree, int size) {
+            index += size;
+            tree[index] += value;
+            while (index > 1) {
+                index /= 2;
+                tree[index] = tree[index * 2] + tree[index * 2 + 1];
+            }
+        }
+
+        int query(int left, int right, int[] tree, int size) {
+            int result = 0;
+            left += size;
+            right += size;
+            while (left < right) {
+                if (left % 2 == 1) {
+                    result += tree[left];
+                    left++;
+                }
+                left /= 2;
+                if (right % 2 == 1) {
+                    right--;
+                    result += tree[right];
+                }
+                right /= 2;
+            }
+            return result;
         }
     }
 }
