42f9761d63ef81e473e270c875edb8051eabd329
54k <54k@bk.ru>
2024-09-28 22:36:13 +0200

    day 681

--- java/leetcode_grind/Day681.java
+++ java/leetcode_grind/Day681.java
@@ -469,4 +469,36 @@ public class Day681 {
             return list.stream().mapToInt(Integer::intValue).toArray();
         }
     }
+
+    // https://leetcode.com/problems/find-the-occurrence-of-first-almost-equal-substring/description/
+    static class Solution7 {
+
+        int[] z_function(String s) {
+            int n = s.length(), l = 0, r = 0;
+            int[] z = new int[n];
+            for (int i = 1; i < n; i++) {
+                if (i <= r) {
+                    z[i] = Math.min(r - i + 1, z[i - l]);
+                }
+                while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i])) {
+                    ++z[i];
+                }
+                if (i + z[i] - 1 > r) {
+                    l = i;
+                    r = i + z[i] - 1;
+                }
+            }
+            return z;
+        }
+
+        public int minStartingIndex(String s, String pattern) {
+            int n = s.length(), m = pattern.length();
+            int[] z1 = z_function(pattern + s);
+            int[] z2 = z_function(new StringBuilder(s + pattern).reverse().toString());
+            for (int i = 0; i <= n - m; ++i)
+                if (z1[m + i] + 1 + z2[n - i] >= m)
+                    return i;
+            return -1;
+        }
+    }
 }
