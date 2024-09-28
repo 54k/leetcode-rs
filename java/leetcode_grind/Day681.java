989fb0a6730ef3b382bb6148da9d5e831ff58154
54k <54k@bk.ru>
2024-09-28 22:42:04 +0200

    day 681

--- java/leetcode_grind/Day681.java
+++ java/leetcode_grind/Day681.java
@@ -470,8 +470,30 @@ public class Day681 {
         }
     }
 
-    // https://leetcode.com/problems/find-the-occurrence-of-first-almost-equal-substring/description/
     static class Solution7 {
+        public int[] validSequence(String word1, String word2) {
+            int n = word1.length(), m = word2.length();
+            int[] last = new int[m];
+            Arrays.fill(last, -1);
+            for (int i = n - 1, j = m - 1; i >= 0; --i) {
+                if (j >= 0 && word1.charAt(i) == word2.charAt(j)) {
+                    last[j--] = i;
+                }
+            }
+            int j = 0, res[] = new int[m], skip = 0;
+            for (int i = 0; i < n && j < m; ++i) {
+                if (word1.charAt(i) == word2.charAt(j) || (skip == 0 && (j == m - 1 || i < last[j + 1]))) {
+                    res[j] = i;
+                    skip += (word1.charAt(i) != word2.charAt(j)) ? 1 : 0;
+                    j++;
+                }
+            }
+            return j == m ? res : new int[0];
+        }
+    }
+
+    // https://leetcode.com/problems/find-the-occurrence-of-first-almost-equal-substring/description/
+    static class Solution8 {
 
         int[] z_function(String s) {
             int n = s.length(), l = 0, r = 0;
