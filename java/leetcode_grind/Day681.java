e903b190a2e547086ced390d573d01e165f7369f
54k <54k@bk.ru>
2024-09-28 19:27:13 +0200

    day 681

--- java/leetcode_grind/Day681.java
+++ java/leetcode_grind/Day681.java
@@ -283,4 +283,93 @@ public class Day681 {
             return result;
         }
     }
+
+    static class Solution3 {
+        public List<Integer> countSmaller(int[] nums) {
+            int offset = 10000;
+            int size = 2 * 10000 + 2;
+            int[] tree = new int[size];
+            List<Integer> result = new ArrayList<Integer>();
+
+            for (int i = nums.length - 1; i >= 0; i--) {
+                int smaller_count = query(nums[i] + offset, tree);
+                result.add(smaller_count);
+                update(nums[i] + offset, 1, tree, size);
+            }
+            Collections.reverse(result);
+            return result;
+        }
+
+        void update(int index, int value, int[] tree, int size) {
+            index++;
+            while (index < size) {
+                tree[index] += value;
+                index += index & -index;
+            }
+        }
+
+        int query(int index, int[] tree) {
+            int result = 0;
+            while (index >= 1) {
+                result += tree[index];
+                index -= index & -index;
+            }
+            return result;
+        }
+    }
+
+    static class Solution4 {
+        public List<Integer> countSmaller(int[] nums) {
+            int n = nums.length;
+            int[] result = new int[n];
+            int[] indices = new int[n];
+            for (int i = 0; i < n; i++) {
+                indices[i] = i;
+            }
+            mergeSort(indices, 0, n, result, nums);
+            List<Integer> resultToReturn = new ArrayList<Integer>(n);
+            for (int i : result) {
+                resultToReturn.add(i);
+            }
+            return resultToReturn;
+        }
+
+        void mergeSort(int[] indices, int left, int right, int[] result, int[] nums) {
+            if (right - left <= 1) {
+                return;
+            }
+            int mid = (left + right) / 2;
+            mergeSort(indices, left, mid, result, nums);
+            mergeSort(indices, mid, right, result, nums);
+            merge(indices, left, right, mid, result, nums);
+        }
+
+        void merge(int[] indices, int left, int right, int mid, int[] result, int[] nums) {
+            int i = left;
+            int j = mid;
+            List<Integer> temp = new ArrayList<Integer>(right - left);
+            while (i < mid && j < right) {
+                if (nums[indices[i]] <= nums[indices[j]]) {
+                    result[indices[i]] += j - mid;
+                    temp.add(indices[i]);
+                    i++;
+                } else {
+                    temp.add(indices[j]);
+                    j++;
+                }
+            }
+            while (i < mid) {
+                result[indices[i]] += j - mid;
+                temp.add(indices[i]);
+                i++;
+            }
+            while (j < right) {
+                temp.add(indices[j]);
+                j++;
+            }
+            for (int k = left; k < right; k++) {
+                indices[k] = temp.get(k - left);
+            }
+        }
+    }
 }
