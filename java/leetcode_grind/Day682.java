60344f0307bc34d1673a9f77bb6f8c60348f7422
54k <54k@bk.ru>
2024-09-29 03:19:44 +0200

    day 682

--- java/leetcode_grind/Day682.java
+++ java/leetcode_grind/Day682.java
@@ -121,4 +121,67 @@ public class Day682 {
             nextNode.prev = prevNode;
         }
     }
+
+    // https://leetcode.com/problems/implement-trie-ii-prefix-tree/description/?envType=weekly-question&envId=2024-09-29
+    static class Trie {
+
+        class TrieNode {
+            TrieNode[] links = new TrieNode[26];
+            int wordsEndingHere = 0;
+            int wordsStartingHere = 0;
+        }
+
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char w : word.toCharArray()) {
+                int charIndex = w - 'a';
+                if (node.links[charIndex] == null) {
+                    node.links[charIndex] = new TrieNode();
+                }
+                node = node.links[charIndex];
+                node.wordsStartingHere++;
+            }
+            node.wordsEndingHere++;
+        }
+
+        public int countWordsEqualTo(String word) {
+            TrieNode node = root;
+            for (char w : word.toCharArray()) {
+                int charIndex = w - 'a';
+                if (node.links[charIndex] == null) {
+                    return 0;
+                }
+                node = node.links[charIndex];
+            }
+            return node.wordsEndingHere;
+        }
+
+        public int countWordsStartingWith(String prefix) {
+            TrieNode node = root;
+            for (char w : prefix.toCharArray()) {
+                int charIndex = w - 'a';
+                if (node.links[charIndex] == null) {
+                    return 0;
+                }
+                node = node.links[charIndex];
+            }
+            return node.wordsStartingHere;
+        }
+
+        public void erase(String word) {
+            TrieNode node = root;
+            for (char w : word.toCharArray()) {
+                int charIndex = w - 'a';
+                node = node.links[charIndex];
+                node.wordsStartingHere--;
+            }
+            node.wordsEndingHere--;
+        }
+    }
 }
