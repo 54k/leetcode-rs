b7119b9a479dc90e6bb24f52f96bb8011c174202
54k <54k@bk.ru>
2024-10-10 16:46:26 +0200

    day 694

--- java/leetcode_grind/Day694.java
+++ java/leetcode_grind/Day694.java
@@ -68,4 +68,41 @@ public class Day694 {
             return maxWidth;
         }
     }
+
+    // https://leetcode.com/problems/minimum-time-to-build-blocks/description/
+    static class Solution4 {
+        public int minBuildTime(int[] blocks, int split) {
+            Arrays.sort(blocks);
+            for (int i = 0; i < blocks.length / 2; i++) {
+                int temp = blocks[i];
+                blocks[i] = blocks[blocks.length - i - 1];
+                blocks[blocks.length - i - 1] = temp;
+            }
+
+            int[][] dp = new int[blocks.length][blocks.length + 1];
+            for (int[] row : dp) {
+                Arrays.fill(row, -1);
+            }
+            return solve(blocks, split, 0, 1, dp);
+        }
+
+        int solve(int[] blocks, int split, int b, int w, int[][] dp) {
+            if (b == blocks.length) {
+                return 0;
+            }
+            if (w == 0) {
+                return Integer.MAX_VALUE;
+            }
+            if (w >= blocks.length - b) {
+                return blocks[b];
+            }
+            if (dp[b][w] != -1) {
+                return dp[b][w];
+            }
+            int workHere = Math.max(blocks[b], solve(blocks, split, b + 1, w - 1, dp));
+            int splitHere = split + solve(blocks, split, b, Math.min(2 * w, blocks.length - b), dp);
+            dp[b][w] = Math.min(workHere, splitHere);
+            return dp[b][w];
+        }
+    }
 }
