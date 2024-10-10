9d2c79cb921ed949cdb505fb8cb33f3aef235a1b
54k <54k@bk.ru>
2024-10-10 03:39:45 +0200

    day 694

---
+++ java/leetcode_grind/Day694.java
@@ -0,0 +1,71 @@
+package leetcode_grind;
+
+import java.util.*;
+
+public class Day694 {
+    // https://leetcode.com/problems/maximum-width-ramp/description/?envType=daily-question&envId=2024-10-10
+    static class Solution1 {
+        public int maxWidthRamp(int[] nums) {
+            int n = nums.length;
+            Integer[] indices = new Integer[n];
+
+            for (int i = 0; i < n; i++) {
+                indices[i] = i;
+            }
+
+            Arrays.sort(indices, (i, j) -> nums[i] != nums[j] ? nums[i] - nums[j] : i - j);
+
+            int minIndex = n;
+            int maxWidth = 0;
+
+            for (int i : indices) {
+                maxWidth = Math.max(maxWidth, i - minIndex);
+                minIndex = Math.min(minIndex, i);
+            }
+            return maxWidth;
+        }
+
+    }
+
+    static class Solution2 {
+        public int maxWidthRamp(int[] nums) {
+            int n = nums.length;
+            int[] rightMax = new int[n];
+            rightMax[n - 1] = nums[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+            }
+            int left = 0, right = 0;
+            int maxWidth = 0;
+            while (right < n) {
+                while (left < right && nums[left] > rightMax[right]) {
+                    left++;
+                }
+                maxWidth = Math.max(maxWidth, right - left);
+                right++;
+            }
+            return maxWidth;
+        }
+    }
+
+    static class Solution3 {
+        public int maxWidthRamp(int[] nums) {
+            int n = nums.length;
+            Stack<Integer> indicesStack = new Stack<>();
+            for (int i = 0; i < n; i++) {
+                if (indicesStack.isEmpty() || nums[indicesStack.peek()] > nums[i]) {
+                    indicesStack.push(i);
+                }
+            }
+
+            int maxWidth = 0;
+            for (int j = n - 1; j >= 0; j--) {
+                while (!indicesStack.isEmpty() && nums[indicesStack.peek()] <= nums[j]) {
+                    maxWidth = Math.max(maxWidth, j - indicesStack.peek());
+                    indicesStack.pop();
+                }
+            }
+            return maxWidth;
+        }
+    }
+}
