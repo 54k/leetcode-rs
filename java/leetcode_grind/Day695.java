175a21f131deb11d31ed878ae8d6ec893ce6e200
54k <54k@bk.ru>
2024-10-11 14:27:54 +0200

    day 695

--- java/leetcode_grind/Day695.java
+++ java/leetcode_grind/Day695.java
@@ -152,5 +152,40 @@ public class Day695 {
         }
     }
 
-    
+    // https://leetcode.com/problems/the-number-of-the-smallest-unoccupied-chair/description/?envType=daily-question&envId=2024-10-11
+    static class Solution6 {
+        public int smallestChair(int[][] times, int targetFriend) {
+            List<int[]> events = new ArrayList<>();
+            for (int i = 0; i < times.length; i++) {
+                events.add(new int[] { times[i][0], i });
+                events.add(new int[] { times[i][1], ~i });
+                ;
+            }
+            Collections.sort(events, (a, b) -> a[0] - b[0]);
+            PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
+            PriorityQueue<int[]> occupiedChairs = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+            for (int i = 0; i < times.length; i++) {
+                availableChairs.add(i);
+            }
+            for (int[] event : events) {
+                int time = event[0];
+                int friendIndex = event[1];
+
+                while (!occupiedChairs.isEmpty() && occupiedChairs.peek()[0] <= time) {
+                    availableChairs.add(occupiedChairs.poll()[1]);
+                }
+
+                if (friendIndex >= 0) {
+                    int chair = availableChairs.poll();
+                    if (friendIndex == targetFriend) {
+                        return chair;
+                    }
+                    occupiedChairs.add(new int[] {
+                            times[friendIndex][1], chair
+                    });
+                }
+            }
+            return -1;
+        }
+    }
 }
