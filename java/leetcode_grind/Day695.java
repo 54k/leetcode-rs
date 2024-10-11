98fa379fbfe1b8ccc525aa42f848a5067b44a46b
54k <54k@bk.ru>
2024-10-11 14:43:15 +0200

    day 695

--- java/leetcode_grind/Day695.java
+++ java/leetcode_grind/Day695.java
@@ -188,4 +188,37 @@ public class Day695 {
             return -1;
         }
     }
+
+    static class Solution7 {
+        public int smallestChair(int[][] times, int targetFriend) {
+            int targetArrival = times[targetFriend][0];
+            Arrays.sort(times, (a, b) -> a[0] - b[0]);
+            int nextChair = 0;
+            PriorityQueue<int[]> leavingQueue = new PriorityQueue<>(
+                    (a, b) -> a[0] - b[0]);
+            TreeSet<Integer> availableChairs = new TreeSet<>();
+
+            for (int[] time : times) {
+                int arrival = time[0];
+                int leave = time[1];
+
+                while (!leavingQueue.isEmpty() && leavingQueue.peek()[0] <= arrival) {
+                    availableChairs.add(leavingQueue.poll()[1]);
+                }
+
+                int currentChair;
+                if (!availableChairs.isEmpty()) {
+                    currentChair = availableChairs.first();
+                    availableChairs.remove(currentChair);
+                } else {
+                    currentChair = nextChair++;
+                }
+
+                leavingQueue.offer(new int[] { leave, currentChair });
+                if (arrival == targetArrival)
+                    return currentChair;
+            }
+            return 0;
+        }
+    }
 }
