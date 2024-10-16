7ae4970712b4d80f5cc7d69af0c363245300daad
54k <54k@bk.ru>
2024-10-17 01:45:13 +0200

    day 700

--- java/leetcode_grind/Day700.java
+++ java/leetcode_grind/Day700.java
@@ -318,4 +318,96 @@ public class Day700 {
             }
         }
     }
+
+    static class Solution8 {
+        public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+            int[][] distance = new int[maze.length][maze[0].length];
+            boolean[][] visited = new boolean[maze.length][maze[0].length];
+            for (int[] row : distance) {
+                Arrays.fill(row, Integer.MAX_VALUE);
+            }
+            distance[start[0]][start[1]] = 0;
+            dijkstra(maze, distance, visited);
+            return distance[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1
+                    : distance[destination[0]][destination[1]];
+        }
+
+        int[] minDistance(int[][] distance, boolean[][] visited) {
+            int[] min = { -1, 1 };
+            int min_val = Integer.MAX_VALUE;
+            for (int i = 0; i < distance.length; i++) {
+                for (int j = 0; j < distance[0].length; j++) {
+                    if (!visited[i][j] && distance[i][j] < min_val) {
+                        min = new int[] { i, j };
+                        min_val = distance[i][j];
+                    }
+                }
+            }
+            return min;
+        }
+
+        void dijkstra(int[][] maze, int[][] distance, boolean[][] visited) {
+            int[][] dirs = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
+            while (true) {
+                int[] s = minDistance(distance, visited);
+                if (s[0] < 0) {
+                    break;
+                }
+                visited[s[0]][s[1]] = true;
+                for (int[] dir : dirs) {
+                    int x = s[0] + dir[0];
+                    int y = s[1] + dir[1];
+
+                    int count = 0;
+                    while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                        x += dir[0];
+                        y += dir[1];
+                        count++;
+                    }
+                    if (distance[s[0]][s[1]] + count < distance[x - dir[0]][y - dir[1]]) {
+                        distance[x - dir[0]][y - dir[1]] = distance[s[0]][s[1]] + count;
+                    }
+                }
+            }
+        }
+    }
+
+    static class Solution9 {
+        public int shortestDistance(int[][] maze, int[] start, int[] destination) {
+            int[][] distance = new int[maze.length][maze[0].length];
+            for (int[] row : distance) {
+                Arrays.fill(row, Integer.MAX_VALUE);
+            }
+            distance[start[0]][start[1]] = 0;
+            dijkstra(maze, start, distance);
+            return distance[destination[0]][destination[1]] == Integer.MAX_VALUE ? -1
+                    : distance[destination[0]][destination[1]];
+        }
+
+        void dijkstra(int[][] maze, int[] start, int[][] distance) {
+            int[][] dirs = { { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 } };
+            PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+            queue.offer(new int[] { start[0], start[1], 0 });
+            while (!queue.isEmpty()) {
+                int[] s = queue.poll();
+                if (distance[s[0]][s[1]] < s[2]) {
+                    continue;
+                }
+                for (int[] dir : dirs) {
+                    int x = s[0] + dir[0];
+                    int y = s[1] + dir[1];
+                    int count = 0;
+                    while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                        x += dir[0];
+                        y += dir[1];
+                        count++;
+                    }
+                    if (distance[s[0]][s[1]] + count < distance[x - dir[0]][y - dir[1]]) {
+                        distance[x - dir[0]][y - dir[1]] = distance[s[0]][s[1]] + count;
+                        queue.offer(new int[] { x - dir[0], y - dir[1], distance[x - dir[0]][y - dir[1]] });
+                    }
+                }
+            }
+        }
+    }
 }
