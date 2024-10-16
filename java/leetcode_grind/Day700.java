e9bd17605fb7e395642575624c10c6441a560235
54k <54k@bk.ru>
2024-10-16 15:05:40 +0200

    day 700

--- java/leetcode_grind/Day700.java
+++ java/leetcode_grind/Day700.java
@@ -214,4 +214,76 @@ public class Day700 {
             return neighbors;
         }
     }
+
+    // https://leetcode.com/problems/the-maze/description/
+    static class Solution5 {
+        boolean dfs(int m, int n, int[][] maze, int[] curr, int[] destination, boolean[][] visit) {
+            if (visit[curr[0]][curr[1]]) {
+                return false;
+            }
+            if (curr[0] == destination[0] && curr[1] == destination[1]) {
+                return true;
+            }
+
+            visit[curr[0]][curr[1]] = true;
+            int[] dirX = { 0, 1, 0, -1 };
+            int[] dirY = { -1, 0, 1, 0 };
+
+            for (int i = 0; i < 4; i++) {
+                int r = curr[0], c = curr[1];
+                while (r >= 0 && r < m && c >= 0 && c < n && maze[r][c] == 0) {
+                    r += dirX[i];
+                    c += dirY[i];
+                }
+                if (dfs(m, n, maze, new int[] { r - dirX[i], c - dirY[i] }, destination, visit)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+            int m = maze.length;
+            int n = maze[0].length;
+            boolean[][] visit = new boolean[m][n];
+            return dfs(m, n, maze, start, destination, visit);
+        }
+    }
+
+    static class Solution6 {
+        public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+            int m = maze.length;
+            int n = maze[0].length;
+
+            boolean[][] visit = new boolean[m][n];
+            int[] dirX = { 0, 1, 0, -1 };
+            int[] dirY = { -1, 0, 1, 0 };
+
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(start);
+            visit[start[0]][start[1]] = true;
+
+            while (!queue.isEmpty()) {
+                int[] curr = queue.poll();
+                if (curr[0] == destination[0] && curr[1] == destination[1]) {
+                    return true;
+                }
+                for (int i = 0; i < 4; i++) {
+                    int r = curr[0];
+                    int c = curr[1];
+                    while (r >= 0 && r < m && c >= 0 && c < n && maze[r][c] == 0) {
+                        r += dirX[i];
+                        c += dirY[i];
+                    }
+                    r -= dirX[i];
+                    c -= dirY[i];
+                    if (!visit[r][c]) {
+                        queue.offer(new int[] { r, c });
+                        visit[r][c] = true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
 }
