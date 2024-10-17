5d2cc4ca4442589caf29722d9ea34dc996a863cf
54k <54k@bk.ru>
2024-10-17 15:44:00 +0200

    day 701

--- java/leetcode_grind/Day701.java
+++ java/leetcode_grind/Day701.java
@@ -169,4 +169,35 @@ public class Day701 {
             return maxGroupSize;
         }
     }
+
+    // https://leetcode.com/problems/longest-cycle-in-a-graph/description/
+    static class Solution6 {
+        int answer = -1;
+
+        void dfs(int node, int[] edges, Map<Integer, Integer> dist, boolean[] visit) {
+            visit[node] = true;
+            int neighbor = edges[node];
+            if (neighbor != -1 && !visit[neighbor]) {
+                dist.put(neighbor, dist.get(node) + 1);
+                dfs(neighbor, edges, dist, visit);
+            } else if (neighbor != -1 && dist.containsKey(neighbor)) {
+                answer = Math.max(answer, dist.get(node) - dist.get(neighbor) + 1);
+            }
+        }
+
+        public int longestCycle(int[] edges) {
+            int n = edges.length;
+            boolean[] visit = new boolean[n];
+
+            for (int i = 0; i < n; i++) {
+                if (!visit[i]) {
+                    Map<Integer, Integer> dist = new HashMap<>();
+                    dist.put(i, 1);
+                    dfs(i, edges, dist, visit);
+                }
+            }
+
+            return answer;
+        }
+    }
 }
