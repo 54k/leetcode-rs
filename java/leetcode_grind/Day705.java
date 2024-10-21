fa5e19cee5375d6843af2ea182080d376f6edd76
54k <54k@bk.ru>
2024-10-22 00:58:49 +0200

    day 705

--- java/leetcode_grind/Day705.java
+++ java/leetcode_grind/Day705.java
@@ -51,4 +51,92 @@ public class Day705 {
             return ans;
         }
     }
+
+    // https://leetcode.com/problems/meeting-rooms-iii/description/
+    static class Solution3 {
+        public int mostBooked(int n, int[][] meetings) {
+            long[] roomAvailabilityTime = new long[n];
+            int[] meetingCount = new int[n];
+            Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+
+            for (int[] meeting : meetings) {
+                int start = meeting[0], end = meeting[1];
+                long minRoomAvailabilityTime = Long.MAX_VALUE;
+                int minAvailabilityTimeRoom = 0;
+                boolean foundUnusedRoom = false;
+
+                for (int i = 0; i < n; i++) {
+                    if (roomAvailabilityTime[i] <= start) {
+                        foundUnusedRoom = true;
+                        meetingCount[i]++;
+                        roomAvailabilityTime[i] = end;
+                        break;
+                    }
+
+                    if (minRoomAvailabilityTime > roomAvailabilityTime[i]) {
+                        minRoomAvailabilityTime = roomAvailabilityTime[i];
+                        minAvailabilityTimeRoom = i;
+                    }
+                }
+
+                if (!foundUnusedRoom) {
+                    roomAvailabilityTime[minAvailabilityTimeRoom] += end - start;
+                    meetingCount[minAvailabilityTimeRoom]++;
+                }
+            }
+
+            int maxMeetingCount = 0, maxMeetingCountRoom = 0;
+            for (int i = 0; i < n; i++) {
+                if (meetingCount[i] > maxMeetingCount) {
+                    maxMeetingCount = meetingCount[i];
+                    maxMeetingCountRoom = i;
+                }
+            }
+            return maxMeetingCountRoom;
+        }
+    }
+
+    static class Solution4 {
+        public int mostBooked(int n, int[][] meetings) {
+            var meetingCount = new int[n];
+            var usedRooms = new PriorityQueue<long[]>(
+                    (a, b) -> a[0] != b[0] ? Long.compare(a[0], b[0]) : Long.compare(a[1], b[1]));
+            var unusedRooms = new PriorityQueue<Integer>();
+
+            for (int i = 0; i < n; i++) {
+                unusedRooms.offer(i);
+            }
+
+            Arrays.sort(meetings, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+
+            for (int[] meeting : meetings) {
+                int start = meeting[0], end = meeting[1];
+
+                while (!usedRooms.isEmpty() && usedRooms.peek()[0] <= start) {
+                    int room = (int) usedRooms.poll()[1];
+                    unusedRooms.offer(room);
+                }
+
+                if (!unusedRooms.isEmpty()) {
+                    int room = unusedRooms.poll();
+                    usedRooms.offer(new long[] { end, room });
+                    meetingCount[room]++;
+                } else {
+                    long roomAvailabilityTime = usedRooms.peek()[0];
+                    int room = (int) usedRooms.poll()[1];
+                    usedRooms.offer(new long[] { roomAvailabilityTime + end - start, room });
+                    meetingCount[room]++;
+                }
+            }
+
+            int maxMeetingCount = 0, maxMeetingCountRoom = 0;
+            for (int i = 0; i < n; i++) {
+                if (meetingCount[i] > maxMeetingCount) {
+                    maxMeetingCount = meetingCount[i];
+                    maxMeetingCountRoom = i;
+                }
+            }
+            return maxMeetingCountRoom;
+        }
+    }
 }
