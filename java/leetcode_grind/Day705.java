e2fff86c5e11b2c75ce248a16418bdc30c364474
54k <54k@bk.ru>
2024-10-22 19:33:26 +0200

    day 705

--- java/leetcode_grind/Day705.java
+++ java/leetcode_grind/Day705.java
@@ -139,4 +139,233 @@ public class Day705 {
             return maxMeetingCountRoom;
         }
     }
+
+    // https://leetcode.com/problems/the-knights-tour/description/?envType=weekly-question&envId=2024-10-22
+    static class Solution5 {
+        public int[][] tourOfKnight(int m, int n, int r, int c) {
+            int[][] chessboard = new int[m][n];
+            chessboard[r][c] = -1;
+            solveKnightsTour(m, n, r, c, chessboard, 1);
+            chessboard[r][c] = 0;
+            return chessboard;
+        }
+
+        boolean solveKnightsTour(
+                int rows,
+                int cols,
+                int currentRow,
+                int currentCol,
+                int[][] chessboard,
+                int moveCount) {
+            if (moveCount == rows * cols) {
+                return true;
+            }
+
+            for (int newRow = 0; newRow < rows; newRow++) {
+                for (int newCol = 0; newCol < cols; newCol++) {
+                    if (!isValidMove(
+                            chessboard,
+                            currentRow,
+                            currentCol,
+                            newRow,
+                            newCol)) {
+                        continue;
+                    }
+
+                    chessboard[newRow][newCol] = moveCount;
+
+                    if (solveKnightsTour(rows, cols, newRow, newCol, chessboard, moveCount + 1)) {
+                        return true;
+                    }
+
+                    chessboard[newRow][newCol] = 0;
+                }
+            }
+            return false;
+        }
+
+        boolean isValidMove(
+                int[][] chessboard,
+                int fromRow,
+                int fromCol,
+                int toRow,
+                int toCol) {
+            return (toRow >= 0 &&
+                    toCol >= 0 &&
+                    toRow < chessboard.length &&
+                    toCol < chessboard[0].length &&
+                    Math.min(Math.abs(fromRow - toRow), Math.abs(fromCol - toCol)) == 1 &&
+                    Math.max(Math.abs(fromRow - toRow), Math.abs(fromCol - toCol)) == 2 &&
+                    chessboard[toRow][toCol] == 0);
+        }
+    }
+
+    static class Solution6 {
+
+        // Possible knight moves: (row, column) pairs
+        int[][] knightMoves = {
+                { -1, -2 },
+                { -2, -1 },
+                { -1, 2 },
+                { -2, 1 },
+                { 1, -2 },
+                { 2, -1 },
+                { 1, 2 },
+                { 2, 1 },
+        };
+
+        public int[][] tourOfKnight(int m, int n, int r, int c) {
+            int[][] chessboard = new int[m][n];
+            chessboard[r][c] = -1;
+
+            solveKnightsTour(m, n, r, c, chessboard, 1);
+
+            // Reset starting position to 0
+            chessboard[r][c] = 0;
+            return chessboard;
+        }
+
+        private boolean solveKnightsTour(
+                int rows,
+                int cols,
+                int currentRow,
+                int currentCol,
+                int[][] chessboard,
+                int moveCount) {
+            // Base case: if all cells are visited, we've found a solution
+            if (moveCount == rows * cols)
+                return true;
+
+            // Get and sort possible next moves based on Warnsdorff's rule
+            List<int[]> nextMoves = getNextMovesWarnsdorff(
+                    chessboard,
+                    currentRow,
+                    currentCol);
+
+            // Try each possible move
+            for (int[] move : nextMoves) {
+                int nextRow = currentRow + knightMoves[move[1]][0];
+                int nextCol = currentCol + knightMoves[move[1]][1];
+
+                if (!isValidMove(chessboard, nextRow, nextCol))
+                    continue;
+
+                chessboard[nextRow][nextCol] = moveCount;
+
+                // Recursively try to solve from this new position
+                if (solveKnightsTour(
+                        rows,
+                        cols,
+                        nextRow,
+                        nextCol,
+                        chessboard,
+                        moveCount + 1)) {
+                    return true;
+                }
+
+                // If the move doesn't lead to a solution, backtrack
+                chessboard[nextRow][nextCol] = 0;
+            }
+
+            return false; // No solution found from this position
+        }
+
+        // Implement Warnsdorff's rule: prefer moves with fewer onward moves
+        private List<int[]> getNextMovesWarnsdorff(
+                int[][] chessboard,
+                int row,
+                int col) {
+            List<int[]> nextMoves = new ArrayList<>();
+            for (int moveIndex = 0; moveIndex < 8; moveIndex++) {
+                int nextRow = row + knightMoves[moveIndex][0];
+                int nextCol = col + knightMoves[moveIndex][1];
+                int accessibilityScore = countAccessibleMoves(
+                        chessboard,
+                        nextRow,
+                        nextCol);
+                nextMoves.add(new int[] { accessibilityScore, moveIndex });
+            }
+            // Sort moves based on accessibility (fewer accessible squares first)
+            Collections.sort(nextMoves, (a, b) -> a[0] - b[0]);
+            return nextMoves;
+        }
+
+        private int countAccessibleMoves(int[][] chessboard, int row, int col) {
+            int count = 0;
+            for (int[] move : knightMoves) {
+                int nextRow = row + move[0];
+                int nextCol = col + move[1];
+                if (isValidMove(chessboard, nextRow, nextCol))
+                    count++;
+            }
+            return count;
+        }
+
+        private boolean isValidMove(int[][] chessboard, int row, int col) {
+            return (row >= 0 &&
+                    col >= 0 &&
+                    row < chessboard.length &&
+                    col < chessboard[0].length &&
+                    chessboard[row][col] == 0);
+        }
+    }
+
+    // https://leetcode.com/problems/find-peak-element/description/
+    static class Solution7 {
+        public int findPeakElement(int[] nums) {
+            return search(nums, 0, nums.length - 1);
+        }
+
+        int search(int[] nums, int l, int r) {
+            if (l == r)
+                return l;
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                return search(nums, l, mid);
+            return search(nums, mid + 1, r);
+        }
+    }
+
+    static class Solution8 {
+        public int findPeakElement(int[] nums) {
+            int l = 0, r = nums.length - 1;
+            while (l < r) {
+                int mid = (l + r) / 2;
+                if (nums[mid] > nums[mid + 1])
+                    r = mid;
+                else
+                    l = mid + 1;
+            }
+            return l;
+        }
+    }
+
+    // https://leetcode.com/problems/online-election/description/
+    static class TopVotedCandidate {
+        Map<Integer, Integer> m = new HashMap<>();
+        int[] time;
+
+        public TopVotedCandidate(int[] persons, int[] times) {
+            int n = persons.length, lead = -1;
+            Map<Integer, Integer> count = new HashMap<>();
+            time = times;
+            for (int i = 0; i < n; i++) {
+                count.put(persons[i], count.getOrDefault(persons[i], 0) + 1);
+                if (i == 0 || count.get(persons[i]) >= count.get(lead))
+                    lead = persons[i];
+                m.put(times[i], lead);
+            }
+        }
+
+        public int q(int t) {
+            int i = Arrays.binarySearch(time, t);
+            return i < 0 ? m.get(time[-i - 2]) : m.get(time[i]);
+        }
+    }
+
+    /**
+     * Your TopVotedCandidate object will be instantiated and called as such:
+     * TopVotedCandidate obj = new TopVotedCandidate(persons, times);
+     * int param_1 = obj.q(t);
+     */
 }
