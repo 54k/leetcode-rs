38e9faaae56ae52be7661038ee59e1afd9cfc201
54k <54k@bk.ru>
2024-10-29 21:26:59 +0400

    day 712

--- src/leetcode_grind/day_712.rs
+++ src/leetcode_grind/day_712.rs
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/maximum-number-of-moves-in-a-grid/description/?envType=daily-question&envId=2024-10-29
-pub fn max_moves(grid: Vec<Vec<i32>>) -> i32 {
+pub fn max_moves_i(grid: Vec<Vec<i32>>) -> i32 {
     let m = grid.len();
     let n = grid[0].len();
 
@@ -21,6 +21,39 @@ pub fn max_moves(grid: Vec<Vec<i32>>) -> i32 {
                 dp[i][j] = dp[i][j].max(dp[i + 1][j - 1] + 1);
             }
             max_moves = max_moves.max(dp[i][j] - 1);
+        }
+    }
+
+    max_moves
+}
+
+pub fn max_moves_ii(grid: Vec<Vec<i32>>) -> i32 {
+    let m = grid.len();
+    let n = grid[0].len();
+
+    let mut dp = vec![vec![0; 2]; m];
+    for i in 0..m {
+        dp[i][0] = 1;
+    }
+    let mut max_moves = 0;
+    for j in 1..n {
+        for i in 0..m {
+            if grid[i][j] > grid[i][j - 1] && dp[i][0] > 0 {
+                dp[i][1] = dp[i][1].max(dp[i][0] + 1);
+            }
+            if i >= 1 && grid[i][j] > grid[i - 1][j - 1] && dp[i - 1][0] > 0 {
+                dp[i][1] = dp[i][1].max(dp[i - 1][0] + 1);
+            }
+            if i + 1 < m && grid[i][j] > grid[i + 1][j - 1] && dp[i + 1][0] > 0 {
+                dp[i][1] = dp[i][1].max(dp[i + 1][0] + 1);
+            }
+
+            max_moves = max_moves.max(dp[i][1] - 1);
+        }
+
+        for k in 0..m {
+            dp[k][0] = dp[k][1];
+            dp[k][1] = 0;
         }
     }
 
