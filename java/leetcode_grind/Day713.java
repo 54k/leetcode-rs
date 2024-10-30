d41aaa17cda8a6203b0bc5164f3ea896831c6b70
54k <54k@bk.ru>
2024-10-30 19:24:34 +0400

    day 713

--- java/leetcode_grind/Day713.java
+++ java/leetcode_grind/Day713.java
@@ -39,4 +39,60 @@ public class Day713 {
             return minRemovals;
         }
     }
+
+    static class Solution2 {
+        List<Integer> getLongestIncreasingSubsequenceLength(List<Integer> v) {
+            List<Integer> lisLen = new ArrayList<>(Collections.nCopies(v.size(), 1));
+            List<Integer> lis = new ArrayList<>();
+            lis.add(v.get(0));
+
+            for (int i = 1; i < v.size(); i++) {
+                int index = lowerBound(lis, v.get(i));
+
+                if (index == lis.size()) {
+                    lis.add(v.get(i));
+                } else {
+                    lis.set(index, v.get(i));
+                }
+
+                lisLen.set(i, lis.size());
+            }
+            return lisLen;
+        }
+
+        int lowerBound(List<Integer> lis, int target) {
+            int left = 0, right = lis.size() - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (lis.get(mid) >= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+
+        public int minimumMountainRemovals(int[] nums) {
+            int N = nums.length;
+            List<Integer> numsList = new ArrayList<>();
+            for (int num : nums)
+                numsList.add(num);
+
+            List<Integer> lisLength = getLongestIncreasingSubsequenceLength(numsList);
+            Collections.reverse(numsList);
+            List<Integer> ldsLength = getLongestIncreasingSubsequenceLength(numsList);
+            Collections.reverse(ldsLength);
+
+            int minRemovals = Integer.MAX_VALUE;
+            for (int i = 0; i < N; i++) {
+                if (lisLength.get(i) > 1 && ldsLength.get(i) > 1) {
+                    minRemovals = Math.min(
+                            minRemovals,
+                            N - lisLength.get(i) - ldsLength.get(i) + 1);
+                }
+            }
+            return minRemovals;
+        }
+    }
 }
