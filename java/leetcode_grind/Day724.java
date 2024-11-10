bb2765b8061a457bb6ce77bd43cfe5e49e4b9383
54k <54k@bk.ru>
2024-11-10 12:17:41 +0700

    day 724

---
+++ java/leetcode_grind/Day724.java
@@ -0,0 +1,96 @@
+package leetcode_grind;
+
+public class Day724 {
+    // https://leetcode.com/problems/shortest-subarray-with-or-at-least-k-ii/description/?envType=daily-question&envId=2024-11-10
+    static class Solution1 {
+        boolean check(int[] nums, int targetSum, int windowSize) {
+            int arrayLength = nums.length;
+            int[] bitCounts = new int[32];
+            for (int right = 0; right < arrayLength; right++) {
+                updateBitCounts(bitCounts, nums[right], 1);
+                if (right >= windowSize) {
+                    updateBitCounts(bitCounts, nums[right - windowSize], -1);
+                }
+                if (right >= windowSize - 1 && convertBitCountsToNumber(bitCounts) >= targetSum) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        void updateBitCounts(int[] bitCounts, int number, int delta) {
+            for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
+                if (((number >> bitPosition) & 1) != 0) {
+                    bitCounts[bitPosition] += delta;
+                }
+            }
+        }
+
+        int convertBitCountsToNumber(int[] bitCounts) {
+            int number = 0;
+            for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
+                if (bitCounts[bitPosition] != 0) {
+                    number |= 1 << bitPosition;
+                }
+            }
+            return number;
+        }
+
+        public int minimumSubarrayLength(int[] nums, int k) {
+            int lo = 1, hi = nums.length;
+            int ans = -1;
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (check(nums, k, mid)) {
+                    ans = mid;
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+            return ans;
+        }
+    }
+
+    static class Solution2 {
+        public int minimumSubarrayLength(int[] nums, int k) {
+            int minLength = Integer.MAX_VALUE;
+            int windowStart = 0;
+            int windowEnd = 0;
+            int[] bitCounts = new int[32];
+
+            while (windowEnd < nums.length) {
+                updateBitCounts(bitCounts, nums[windowEnd], 1);
+
+                while (windowStart <= windowEnd && convertBitCountsToNumber(bitCounts) >= k) {
+                    minLength = Math.min(minLength, windowEnd - windowStart + 1);
+
+                    updateBitCounts(bitCounts, nums[windowStart], -1);
+                    windowStart++;
+                }
+
+                windowEnd++;
+            }
+
+            return minLength == Integer.MAX_VALUE ? -1 : minLength;
+        }
+
+        void updateBitCounts(int[] bitCounts, int number, int delta) {
+            for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
+                if (((number >> bitPosition) & 1) != 0) {
+                    bitCounts[bitPosition] += delta;
+                }
+            }
+        }
+
+        int convertBitCountsToNumber(int[] bitCounts) {
+            int result = 0;
+            for (int bitPosition = 0; bitPosition < 32; bitPosition++) {
+                if (bitCounts[bitPosition] != 0) {
+                    result |= 1 << bitPosition;
+                }
+            }
+            return result;
+        }
+    }
+}
