db664e3532740979234f9f00b4d402dcb23b3357
54k <54k@bk.ru>
2024-11-12 12:08:25 +0700

    day 726

--- java/leetcode_grind/Day726.java
+++ java/leetcode_grind/Day726.java
@@ -31,4 +31,29 @@ public class Day726 {
             return ans;
         }
     }
+
+    static class Solution2 {
+        public int[] maximumBeauty(int[][] items, int[] queries) {
+            int[] ans = new int[queries.length];
+            Arrays.sort(items, (a, b) -> a[0] - b[0]);
+            int[][] queriesWithIndices = new int[queries.length][2];
+            for (int i = 0; i < queries.length; i++) {
+                queriesWithIndices[i][0] = queries[i];
+                queriesWithIndices[i][1] = i;
+            }
+            Arrays.sort(queriesWithIndices, (a, b) -> a[0] - b[0]);
+            int itemIndex = 0, maxBeauty = 0;
+            for (int i = 0; i < queries.length; i++) {
+                int query = queriesWithIndices[i][0];
+                int originalIndex = queriesWithIndices[i][1];
+
+                while (itemIndex < items.length && items[itemIndex][0] <= query) {
+                    maxBeauty = Math.max(maxBeauty, items[itemIndex][1]);
+                    itemIndex++;
+                }
+                ans[originalIndex] = maxBeauty;
+            }
+            return ans;
+        }
+    }
 }
