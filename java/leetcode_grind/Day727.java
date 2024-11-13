ac9d317ac019d0106a9e4fdf65582cfadae68c1f
54k <54k@bk.ru>
2024-11-13 15:56:42 +0700

    day 727

---
+++ java/leetcode_grind/Day727.java
@@ -0,0 +1,53 @@
+package leetcode_grind;
+
+import java.util.*;
+
+public class Day727 {
+    // https://leetcode.com/problems/count-the-number-of-fair-pairs/description/?envType=daily-question&envId=2024-11-13
+    static class Solution1 {
+        long lower_bound(int[] nums, int low, int high, int element) {
+            while (low <= high) {
+                int mid = low + ((high - low) / 2);
+                if (nums[mid] >= element) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+
+        public long countFairPairs(int[] nums, int lower, int upper) {
+            Arrays.sort(nums);
+            long ans = 0;
+            for (int i = 0; i < nums.length; i++) {
+                long low = lower_bound(nums, i + 1, nums.length - 1, lower - nums[i]);
+                long high = lower_bound(nums, i + 1, nums.length - 1, upper - nums[i] + 1);
+                ans += 1 * (high - low);
+            }
+            return ans;
+        }
+    }
+
+    static class Solution2 {
+        long lower_bound(int[] nums, int value) {
+            int left = 0, right = nums.length - 1;
+            long result = 0;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum < value) {
+                    result += (right - left);
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            return result;
+        }
+
+        public long countFairPairs(int[] nums, int lower, int upper) {
+            Arrays.sort(nums);
+            return lower_bound(nums, upper + 1) - lower_bound(nums, lower);
+        }
+    }
+}
