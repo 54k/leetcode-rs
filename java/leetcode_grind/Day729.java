97bb20efc52e88be70723cae5ff4c57a9cf7b75f
54k <54k@bk.ru>
2024-11-15 19:03:47 +0700

    day 729

---
+++ java/leetcode_grind/Day729.java
@@ -0,0 +1,24 @@
+package leetcode_grind;
+
+public class Day729 {
+    // https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/description/?envType=daily-question&envId=2024-11-15
+    static class Solution {
+        public int findLengthOfShortestSubarray(int[] arr) {
+            int right = arr.length - 1;
+            while (right > 0 && arr[right] >= arr[right - 1]) {
+                right--;
+            }
+
+            int ans = right;
+            int left = 0;
+            while (left < right && (left == 0 || arr[left - 1] <= arr[left])) {
+                while (right < arr.length && arr[left] > arr[right]) {
+                    right++;
+                }
+                ans = Math.min(ans, right - left - 1);
+                left++;
+            }
+            return ans;
+        }
+    }
+}
