c75021584ac7eb3d80fc4ae6dd7769ae0c15984f
54k <54k@bk.ru>
2024-11-22 11:42:23 +0700

    day 735

---
+++ java/leetcode_grind/Day735.java
@@ -0,0 +1,64 @@
+package leetcode_grind;
+
+import java.util.*;
+
+public class Day735 {
+    // https://leetcode.com/problems/count-unguarded-cells-in-the-grid/description/?envType=daily-question&envId=2024-11-21
+    static class Solution1 {
+        static final int UNGUARDED = 0;
+        static final int GUARDED = 1;
+        static final int GUARD = 2;
+        static final int WALL = 3;
+
+        void markguarded(int row, int col, int[][] grid) {
+            // Traverse upwards
+            for (int r = row - 1; r >= 0; r--) {
+                if (grid[r][col] == WALL || grid[r][col] == GUARD)
+                    break;
+                grid[r][col] = GUARDED;
+            }
+            // Traverse downwards
+            for (int r = row + 1; r < grid.length; r++) {
+                if (grid[r][col] == WALL || grid[r][col] == GUARD)
+                    break;
+                grid[r][col] = GUARDED;
+            }
+            // Traverse leftwards
+            for (int c = col - 1; c >= 0; c--) {
+                if (grid[row][c] == WALL || grid[row][c] == GUARD)
+                    break;
+                grid[row][c] = GUARDED;
+            }
+            // Traverse rightwards
+            for (int c = col + 1; c < grid[0].length; c++) {
+                if (grid[row][c] == WALL || grid[row][c] == GUARD)
+                    break;
+                grid[row][c] = GUARDED;
+            }
+        }
+
+        public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+            int[][] grid = new int[m][n];
+            for (int[] guard : guards) {
+                grid[guard[0]][guard[1]] = GUARD;
+            }
+
+            for (int[] wall : walls) {
+                grid[wall[0]][wall[1]] = WALL;
+            }
+
+            for (int[] guard : guards) {
+                markguarded(guard[0], guard[1], grid);
+            }
+
+            int count = 0;
+            for (int[] row : grid) {
+                for (int cell : row) {
+                    if (cell == UNGUARDED)
+                        count++;
+                }
+            }
+            return count;
+        }
+    }
+}
