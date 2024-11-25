9d109729ec9add4d21ed687bfedb0c5e70a44833
54k <54k@bk.ru>
2024-11-25 10:33:59 +0700

    day 739

--- java/leetcode_grind/Day739.java
+++ java/leetcode_grind/Day739.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -111,6 +113,25 @@ public class Day739 {
             sb.setCharAt(i, str.charAt(j));
             sb.setCharAt(j, str.charAt(i));
             return sb.toString();
+        }
+    }
+
+    // https://leetcode.com/problems/two-city-scheduling/description/
+    static class Solution3 {
+        public int twoCitySchedCost(int[][] costs) {
+            Arrays.sort(costs, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return o1[0] - o1[1] - (o2[0] - o2[1]);
+                }
+            });
+
+            int total = 0;
+            int n = costs.length / 2;
+
+            for (int i = 0; i < n; i++)
+                total += costs[i][0] + costs[i + n][1];
+            return total;
         }
     }
 }
