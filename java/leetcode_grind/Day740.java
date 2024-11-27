ea3eb0e2d2df42f32b1b761615590146ec6a2981
54k <54k@bk.ru>
2024-11-27 11:15:07 +0700

    day 740

--- java/leetcode_grind/Day740.java
+++ java/leetcode_grind/Day740.java
@@ -113,4 +113,40 @@ public class Day740 {
         }
     }
 
+    static class Solution3 {
+        int findMinDistance(List<List<Integer>> adjList, int n) {
+            int[] dp = new int[n];
+            dp[n - 1] = 0;
+            for (int currentNode = n - 2; currentNode >= 0; currentNode--) {
+                int minDistance = n;
+                for (int neighbor : adjList.get(currentNode)) {
+                    minDistance = Math.min(minDistance, dp[neighbor] + 1);
+                }
+                dp[currentNode] = minDistance;
+            }
+            return dp[0];
+        }
+
+        public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+            List<Integer> answer = new ArrayList<>();
+            List<List<Integer>> adjList = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                adjList.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < n - 1; i++) {
+                adjList.get(i).add(i + 1);
+            }
+
+            for (int[] road : queries) {
+                int u = road[0];
+                int v = road[1];
+                adjList.get(u).add(v);
+                answer.add(findMinDistance(adjList, n));
+            }
+
+            return answer.stream().mapToInt(i -> i).toArray();
+        }
+    }
 }
