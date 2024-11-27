b7e5be59672edb84be9b5174666ef43ab4cf3d86
54k <54k@bk.ru>
2024-11-27 11:04:46 +0700

    day 740

--- java/leetcode_grind/Day740.java
+++ java/leetcode_grind/Day740.java
@@ -65,4 +65,52 @@ public class Day740 {
         }
     }
 
+    static class Solution2 {
+        int findMinDistance(
+                List<List<Integer>> adjList,
+                int n,
+                int currentNode,
+                int[] dp) {
+            if (currentNode == n - 1)
+                return 0;
+            if (dp[currentNode] != -1)
+                return dp[currentNode];
+
+            int minDistance = n;
+            for (int neighbor : adjList.get(currentNode)) {
+                minDistance = Math.min(minDistance, findMinDistance(adjList, n, neighbor, dp) + 1);
+            }
+            return dp[currentNode] = minDistance;
+        }
+
+        public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++) {
+                dp[i] = -1;
+            }
+            List<List<Integer>> adjList = new ArrayList<>(n);
+            for (int i = 0; i < n; i++) {
+                adjList.add(new ArrayList<>());
+            }
+            for (int i = 0; i < n - 1; i++) {
+                adjList.get(i).add(i + 1);
+            }
+            List<Integer> answer = new ArrayList<>();
+            for (int[] road : queries) {
+                int u = road[0];
+                int v = road[1];
+
+                adjList.get(u).add(v);
+
+                answer.add(findMinDistance(adjList, n, 0, dp));
+
+                for (int i = 0; i < n; i++) {
+                    dp[i] = -1;
+                }
+            }
+
+            return answer.stream().mapToInt(i -> i).toArray();
+        }
+    }
+
 }
