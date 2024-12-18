09717f64a2ca301a7be7928db3efbf1ee6841f23
54k <54k@bk.ru>
2024-12-18 23:56:31 +0700

    day 770

--- java/leetcode_grind/Day770.java
+++ java/leetcode_grind/Day770.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Stack;
+
 public class Day770 {
     // https://leetcode.com/problems/rabbits-in-forest/description/
     static class Solution1 {
@@ -13,6 +15,37 @@ public class Day770 {
                 ans += Math.floorMod(-count[k], k + 1) + count[k];
             }
             return ans;
+        }
+    }
+
+    // https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/description/
+    static class Solution2 {
+        public int[] finalPrices(int[] prices) {
+            int n = prices.length;
+            int[] result = prices.clone();
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (prices[j] <= prices[i]) {
+                        result[i] = prices[i] - prices[j];
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    static class Solution3 {
+        public int[] finalPrices(int[] prices) {
+            int[] result = prices.clone();
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < prices.length; i++) {
+                while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                    result[stack.pop()] -= prices[i];
+                }
+                stack.add(i);
+            }
+            return result;
         }
     }
 }
