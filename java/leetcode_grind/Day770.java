1d3cbbe131d47f51e47345655578344c9785e961
54k <54k@bk.ru>
2024-12-18 23:57:46 +0700

    day 770

--- java/leetcode_grind/Day770.java
+++ java/leetcode_grind/Day770.java
@@ -1,5 +1,6 @@
 package leetcode_grind;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Day770 {
@@ -46,6 +47,34 @@ public class Day770 {
                 stack.add(i);
             }
             return result;
+        }
+    }
+
+    // https://leetcode.com/problems/maximize-area-of-square-hole-in-grid/description/
+    static class Solution4 {
+        public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+            int x = find(hBars), y = find(vBars);
+            int res = Math.min(x, y) + 1;
+
+            return res * res;
+        }
+
+        private int find(int[] arr) {
+            Arrays.sort(arr);
+            int res = 1, i = 0, n = arr.length;
+
+            while (i < n) {
+                int count = 1;
+
+                while (i + 1 < n && arr[i] + 1 == arr[i + 1]) {
+                    i++;
+                    count++;
+                }
+                i++;
+                res = Math.max(res, count);
+            }
+
+            return res;
         }
     }
 }
