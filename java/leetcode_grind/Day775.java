9fe6fe5c424849a51ea8f198a9919998bb5f57ba
54k <54k@bk.ru>
2025-01-02 14:53:46 +0700

    day 775

---
+++ java/leetcode_grind/Day775.java
@@ -0,0 +1,67 @@
+package leetcode_grind;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Day775 {
+    // https://leetcode.com/problems/perform-string-shifts/description/?envType=weekly-question&envId=2025-01-01
+    static class Solution1 {
+        public int[] vowelStrings(String[] words, int[][] queries) {
+            int[] ans = new int[queries.length];
+            HashSet<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+            int[] prefixSum = new int[words.length];
+            int sum = 0;
+            for (int i = 0; i < words.length; i++) {
+                String currentWord = words[i];
+                if (vowels.contains(currentWord.charAt(0))
+                        && vowels.contains(currentWord.charAt(currentWord.length() - 1))) {
+                    sum++;
+                }
+                prefixSum[i] = sum;
+            }
+
+            for (int i = 0; i < queries.length; i++) {
+                int[] currentQuery = queries[i];
+                ans[i] = prefixSum[currentQuery[1]] - (currentQuery[0] == 0 ? 0 : prefixSum[currentQuery[0] - 1]);
+            }
+
+            return ans;
+        }
+    }
+
+    // https://leetcode.com/problems/jump-game-vii/description/
+    static class Solution2 {
+        public boolean canReach(String s, int minJump, int maxJump) {
+            int n = s.length(), pre = 0;
+            boolean[] dp = new boolean[n];
+            dp[0] = true;
+            for (int i = 1; i < n; ++i) {
+                if (i >= minJump && dp[i - minJump]) {
+                    pre++;
+                }
+                if (i > maxJump && dp[i - maxJump - 1]) {
+                    pre--;
+                }
+                dp[i] = pre > 0 && s.charAt(i) == '0';
+            }
+            return dp[n - 1];
+        }
+    }
+
+    // https://leetcode.com/problems/perform-string-shifts/description/?envType=weekly-question&envId=2025-01-01
+    static class Solution3 {
+        public String stringShift(String s, int[][] shift) {
+            int n = s.length();
+            for (int[] move : shift) {
+                int direction = move[0];
+                int amount = move[1] % n;
+                if (direction == 0) {
+                    s = s.substring(amount) + s.substring(0, amount);
+                } else {
+                    s = s.substring(n - amount) + s.substring(0, n - amount);
+                }
+            }
+            return s;
+        }
+    }
+}
