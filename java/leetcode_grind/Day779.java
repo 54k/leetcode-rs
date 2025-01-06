eef9566d2cfd3bef9e1fabc9859ed53e0596a273
54k <54k@bk.ru>
2025-01-06 18:31:52 +0700

    day 779

--- java/leetcode_grind/Day779.java
+++ java/leetcode_grind/Day779.java
@@ -1,5 +1,10 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Day779 {
     // https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/?envType=daily-question&envId=2025-01-06
     static class Solution1 {
@@ -15,4 +20,108 @@ public class Day779 {
             return answer;
         }
     }
+
+    // https://leetcode.com/problems/online-majority-element-in-subarray/description/?envType=problem-list-v2&envId=binary-indexed-tree
+    class MajorityChecker {
+        static class Node {
+            int val;
+            int freq;
+
+            Node(int v, int f) {
+                val = v;
+                freq = f;
+            }
+        }
+
+        int n;
+        Node[] tree;
+        Map<Integer, List<Integer>> idx = new HashMap<>();
+
+        public MajorityChecker(int[] arr) {
+            n = arr.length;
+            tree = new Node[n * 4];
+            build(0, 0, n, arr);
+        }
+
+        void build(int v, int l, int r, int[] arr) {
+            if (l + 1 >= r) {
+                tree[v] = new Node(arr[l], 1);
+                idx.computeIfAbsent(arr[l], $ -> new ArrayList<>()).add(l);
+                return;
+            }
+
+            int m = (l + r) / 2;
+            build(v * 2 + 1, l, m, arr);
+            build(v * 2 + 2, m, r, arr);
+            tree[v] = merge(tree[v * 2 + 1], tree[v * 2 + 2]);
+        }
+
+        public int query(int left, int right, int threshold) {
+            Node res = query(0, 0, n, left, right + 1);
+            if (res.freq == 0)
+                return -1;
+            var s = search_lb(idx.get(res.val), left);
+            var e = search_ub(idx.get(res.val), right);
+            if (e - s < threshold)
+                return -1;
+            return res.val;
+        }
+
+        int search_lb(List<Integer> arr, int t) {
+            int left = 0;
+            int right = arr.size();
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (arr.get(mid) < t) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+        int search_ub(List<Integer> arr, int t) {
+            int left = 0;
+            int right = arr.size();
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (arr.get(mid) <= t) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+        Node query(int v, int l, int r, int x, int y) {
+            if (l >= y || r <= x) {
+                return new Node(0, 0);
+            }
+            if (x <= l && r <= y) {
+                return tree[v];
+            }
+            int m = (l + r) / 2;
+            Node ln = query(v * 2 + 1, l, m, x, y);
+            Node rn = query(v * 2 + 2, m, r, x, y);
+            return merge(ln, rn);
+        }
+
+        Node merge(Node l, Node r) {
+            if (l.val == r.val) {
+                return new Node(r.val, l.freq + r.freq);
+            } else if (l.freq > r.freq) {
+                return new Node(l.val, l.freq - r.freq);
+            } else {
+                return new Node(r.val, r.freq - l.freq);
+            }
+        }
+    }
+
+    /**
+     * Your MajorityChecker object will be instantiated and called as such:
+     * MajorityChecker obj = new MajorityChecker(arr);
+     * int param_1 = obj.query(left,right,threshold);
+     */
 }
--- src/leetcode_grind/day_779.rs
+++ src/leetcode_grind/day_779.rs
@@ -22,3 +22,80 @@ pub fn min_operations(boxes: String) -> Vec<i32> {
     }
     answer
 }
+
+// https://leetcode.com/problems/online-majority-element-in-subarray/description/?envType=problem-list-v2&envId=binary-indexed-tree
+use std::collections::HashMap;
+use rand::Rng; // 0.8.5
+
+struct MajorityChecker {
+    idx: HashMap<i32, Vec<i32>>,
+    arr: Vec<i32>,
+}
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MajorityChecker {
+    fn new(arr: Vec<i32>) -> Self {
+        let mut idx = HashMap::new();
+        for (i, &v) in arr.iter().enumerate() {
+            idx.entry(v).or_insert(vec![]).push(i as i32);
+        }
+        MajorityChecker {
+            idx,
+            arr
+        }
+    }
+    
+    fn query(&self, left: i32, right: i32, threshold: i32) -> i32 {
+        for i in 0..20 {
+            let ri = rand::thread_rng().gen_range(left..=right);
+            let a = self.arr[ri as usize]; // random pick    
+            let l = self.lb(&self.idx[&a], left);
+            let r = self.ub(&self.idx[&a], right);
+            if r - l >= threshold {
+                return a;
+            }
+        }
+        return -1;
+    }
+
+    fn lb(&self, arr: &Vec<i32>, t: i32) -> i32 {
+        let mut left = 0;
+        let mut right = arr.len();
+
+        while left < right {
+            let mid = (left + right) / 2;
+
+            if arr[mid] < t {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        left as i32
+    }
+
+    fn ub(&self, arr: &Vec<i32>, t: i32) -> i32 {
+        let mut left = 0;
+        let mut right = arr.len();
+
+        while left < right {
+            let mid = (left + right) / 2;
+
+            if arr[mid] <= t {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        left as i32
+    }
+}
+
+/**
+ * Your MajorityChecker object will be instantiated and called as such:
+ * let obj = MajorityChecker::new(arr);
+ * let ret_1: i32 = obj.query(left, right, threshold);
+ */
