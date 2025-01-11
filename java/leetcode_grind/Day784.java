413ebeb19245a9a9acab0e4365108b0284a4a296
54k <54k@bk.ru>
2025-01-11 17:46:47 +0700

    day 784

--- java/leetcode_grind/Day784.java
+++ java/leetcode_grind/Day784.java
@@ -53,9 +53,32 @@ public class Day784 {
         }
     }
 
-    /**
-     * Your Solution object will be instantiated and called as such:
-     * Solution obj = new Solution(w);
-     * int param_1 = obj.pickIndex();
-     */
+    static class Solution3 {
+        private int[] prefixSums;
+        private int totalSum;
+
+        public Solution3(int[] w) {
+            this.prefixSums = new int[w.length];
+            int prefixSum = 0;
+            for (int i = 0; i < w.length; i++) {
+                prefixSum += w[i];
+                this.prefixSums[i] = prefixSum;
+            }
+            this.totalSum = prefixSum;
+        }
+
+        public int pickIndex() {
+            double target = this.totalSum * Math.random();
+            int low = 0, high = this.prefixSums.length;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                if (target > this.prefixSums[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+            return low;
+        }
+    }
 }
