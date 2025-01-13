6009cf431e2382389ffab0a74fc78ef7bcc7277e
54k <54k@bk.ru>
2025-01-13 14:30:06 +0700

    day 786

--- java/leetcode_grind/Day786.java
+++ java/leetcode_grind/Day786.java
@@ -1,10 +1,12 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -71,6 +73,7 @@ public class Day786 {
         }
     }
 
+    // https://leetcode.com/problems/random-point-in-non-overlapping-rectangles/description/
     /**
      * Your MovieRentingSystem object will be instantiated and called as such:
      * MovieRentingSystem obj = new MovieRentingSystem(n, entries);
@@ -80,4 +83,45 @@ public class Day786 {
      * List<List<Integer>> param_4 = obj.report();
      */
 
+    static class Solution2 {
+        int[][] rects;
+        List<Integer> psum = new ArrayList<>();
+        int tot = 0;
+        Random rand = new Random();
+
+        public Solution2(int[][] rects) {
+            this.rects = rects;
+            for (int[] x : rects) {
+                tot += (x[2] - x[0] + 1) * (x[3] - x[1] + 1);
+                psum.add(tot);
+            }
+        }
+
+        public int[] pick() {
+            int targ = rand.nextInt(tot);
+
+            int lo = 0;
+            int hi = rects.length - 1;
+
+            while (lo != hi) {
+                int mid = (lo + hi) / 2;
+                if (targ >= psum.get(mid))
+                    lo = mid + 1;
+                else
+                    hi = mid;
+            }
+
+            int[] x = rects[lo];
+            int width = x[2] - x[0] + 1;
+            int height = x[3] - x[1] + 1;
+            int base = psum.get(lo) - width * height;
+            return new int[] { x[0] + (targ - base) % width, x[1] + (targ - base) / width };
+        }
+    }
+
+    /**
+     * Your Solution object will be instantiated and called as such:
+     * Solution obj = new Solution(rects);
+     * int[] param_1 = obj.pick();
+     */
 }
