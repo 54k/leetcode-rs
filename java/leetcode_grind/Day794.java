548f6680a7d2bac9b414ad439aaa321db2a19825
54k <54k@bk.ru>
2025-01-21 15:18:18 +0700

    day 794

--- java/leetcode_grind/Day794.java
+++ java/leetcode_grind/Day794.java
@@ -55,4 +55,27 @@ public class Day794 {
             return earliestHour;
         }
     }
+
+    static class Solution3 {
+        public int bestClosingTime(String customers) {
+            int minPenalty = 0, curPenalty = 0;
+            int earliestHour = 0;
+
+            for (int i = 0; i < customers.length(); i++) {
+                char ch = customers.charAt(i);
+
+                if (ch == 'Y') {
+                    curPenalty--;
+                } else {
+                    curPenalty++;
+                }
+
+                if (curPenalty < minPenalty) {
+                    earliestHour = i + 1;
+                    minPenalty = curPenalty;
+                }
+            }
+            return earliestHour;
+        }
+    }
 }
