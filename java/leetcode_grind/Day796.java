c49b0605afa63bb01ef62c9139a0dc138f7c45aa
54k <54k@bk.ru>
2025-01-23 17:34:52 +0700

    day 796

--- java/leetcode_grind/Day796.java
+++ java/leetcode_grind/Day796.java
@@ -238,4 +238,45 @@ public class Day796 {
             return ans;
         }
     }
+
+    // https://leetcode.com/problems/minimum-number-of-flips-to-make-binary-grid-palindromic-ii/description/
+    static class Solution8 {
+        public int minFlips(int[][] A) {
+            int res = 0, one = 0, diff = 0, m = A.length, n = A[0].length;
+
+            // handle quadrants
+            for (int i = 0; i < m / 2; i++) {
+                for (int j = 0; j < n / 2; j++) {
+                    int v = A[i][j] + A[i][n - 1 - j] + A[m - 1 - i][j] + A[m - 1 - i][n - 1 - j];
+                    res += Math.min(v, 4 - v);
+                }
+            }
+            // handle middle column
+            if (n % 2 > 0) {
+                for (int i = 0; i < m / 2; i++) {
+                    diff += A[i][n / 2] ^ A[m - 1 - i][n / 2];
+                    one += A[i][n / 2] + A[m - 1 - i][n / 2];
+                }
+            }
+
+            // handle middle row
+            if (m % 2 > 0) {
+                for (int j = 0; j < n / 2; ++j) {
+                    diff += A[m / 2][j] ^ A[m / 2][n - 1 - j];
+                    one += A[m / 2][j] + A[m / 2][n - 1 - j];
+                }
+            }
+
+            // handle center point
+            if (n % 2 > 0 && m % 2 > 0) {
+                res += A[m / 2][n / 2];
+            }
+
+            // divisible by 4
+            if (diff == 0 && one % 4 > 0) {
+                res += 2;
+            }
+            return res + diff;
+        }
+    }
 }
