4e8618becc0b0990ef3bd7b772f93f68b4b5c54d
54k <54k@bk.ru>
2025-01-25 01:35:41 +0700

    day 797

--- java/leetcode_grind/Day797.java
+++ java/leetcode_grind/Day797.java
@@ -197,4 +197,28 @@ public class Day797 {
             return countAtLeastK == countLetters;
         }
     }
+
+    static class Solution7 {
+        public int longestSubstring(String s, int k) {
+            return longestSubstringUtil(s, 0, s.length(), k);
+        }
+
+        int longestSubstringUtil(String s, int start, int end, int k) {
+            if (end < k)
+                return 0;
+            int[] countMap = new int[26];
+            for (int i = start; i < end; i++) {
+                countMap[s.charAt(i) - 'a']++;
+            }
+            for (int mid = start; mid < end; mid++) {
+                if (countMap[s.charAt(mid) - 'a'] >= k)
+                    continue;
+                int midNext = mid + 1;
+                while (midNext < end && countMap[s.charAt(midNext) - 'a'] < k)
+                    midNext++;
+                return Math.max(longestSubstringUtil(s, start, mid, k), longestSubstringUtil(s, midNext, end, k));
+            }
+            return end - start;
+        }
+    }
 }
