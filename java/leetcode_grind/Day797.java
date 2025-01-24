c761aeb2e948dabfed0b4b9627c9738b25bfd26e
54k <54k@bk.ru>
2025-01-25 01:58:32 +0700

    day 797

--- java/leetcode_grind/Day797.java
+++ java/leetcode_grind/Day797.java
@@ -270,4 +270,52 @@ public class Day797 {
             return maxUnique;
         }
     }
+
+    // https://leetcode.com/problems/longest-subsequence-repeated-k-times/description/
+    static class Solution9 {
+        String res = "";
+        List<Character> list = new ArrayList<>();
+
+        public String longestSubsequenceRepeatedK(String s, int k) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray())
+                count[c - 'a']++;
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (count[c - 'a'] >= k)
+                    list.add(c);
+            }
+            dfs(s, "", k);
+            return res;
+        }
+
+        void dfs(String s, String sub, int k) {
+            if (sub.length() > 1 && !isSub(s, sub, k))
+                return;
+            if (sub.length() > res.length())
+                res = sub;
+            for (int i = list.size() - 1; i >= 0; i--) {
+                String next = sub + list.get(i);
+                dfs(s, next, k);
+            }
+        }
+
+        boolean isSub(String s, String sub, int k) {
+            int j = 0;
+            int repeat = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.length() - i < (k - repeat - 1) * sub.length())
+                    return false;
+                if (s.charAt(i) == sub.charAt(j)) {
+                    j++;
+                    j %= sub.length();
+                    if (j == 0) {
+                        repeat++;
+                        if (repeat == k)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
 }
