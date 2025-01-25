099114dc3e37ea17670b3f480c7dd0fc4d3ba728
54k <54k@bk.ru>
2025-01-25 19:20:12 +0700

    day 798

--- java/leetcode_grind/Day798.java
+++ java/leetcode_grind/Day798.java
@@ -1,8 +1,10 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Day798 {
     // https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements/description/?envType=daily-question&envId=2025-01-25
@@ -117,6 +119,78 @@ public class Day798 {
                 return countNodes(root);
             }
             return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
+        }
+    }
+
+    static class Solution3 {
+        TreeNode previous;
+
+        boolean isValidBST(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            if (!isValidBST(root.left)) {
+                return false;
+            }
+            if (previous != null && previous.val >= root.val) {
+                return false;
+            }
+            previous = root;
+            return isValidBST(root.right);
+        }
+
+        int countNodes(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+
+        public int largestBSTSubtree(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            previous = null;
+            if (isValidBST(root)) {
+                return countNodes(root);
+            }
+            return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
+        }
+    }
+
+    // https://leetcode.com/problems/shortest-cycle-in-a-graph/description/
+    static class Solution4 {
+        public int findShortestCycle(int n, int[][] edges) {
+            var adj = new HashMap<Integer, List<Integer>>();
+            for (var edge : edges) {
+                adj.computeIfAbsent(edge[0], $ -> new ArrayList<>()).add(edge[1]);
+                adj.computeIfAbsent(edge[1], $ -> new ArrayList<>()).add(edge[0]);
+            }
+
+            var cycle = n + 1;
+
+            for (int i = 0; i < n; i++) {
+                var dist = new int[n];
+                Arrays.fill(dist, -1);
+                dist[i] = 0;
+
+                var bfs = new LinkedList<Integer>();
+                bfs.add(i);
+                while (!bfs.isEmpty()) {
+                    var u = bfs.poll();
+                    for (var v : adj.getOrDefault(u, new ArrayList<>())) {
+                        if (dist[v] == -1) {
+                            dist[v] = dist[u] + 1;
+                            bfs.add(v);
+                        } else if (dist[v] >= dist[u]) {
+                            cycle = Math.min(cycle, dist[v] + dist[u] + 1);
+                        }
+                    }
+                }
+            }
+
+            return cycle > n ? -1 : cycle;
         }
     }
 }
