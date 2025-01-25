ad7782e26232cb54cd33150ec1805ccf23d70db9
54k <54k@bk.ru>
2025-01-25 13:52:41 +0700

    day 798

--- java/leetcode_grind/Day798.java
+++ java/leetcode_grind/Day798.java
@@ -45,4 +45,78 @@ public class Day798 {
         }
     }
 
+    // https://leetcode.com/problems/largest-bst-subtree/description/
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution2 {
+        boolean isValidBST(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+
+            int leftMax = findMax(root.left);
+            if (leftMax >= root.val) {
+                return false;
+            }
+
+            int rightMin = findMin(root.right);
+            if (rightMin <= root.val) {
+                return false;
+            }
+
+            if (isValidBST(root.left) && isValidBST(root.right)) {
+                return true;
+            }
+            return false;
+        }
+
+        int findMax(TreeNode root) {
+            if (root == null) {
+                return Integer.MIN_VALUE;
+            }
+            return Math.max(Math.max(root.val, findMax(root.left)), findMax(root.right));
+        }
+
+        int findMin(TreeNode root) {
+            if (root == null) {
+                return Integer.MAX_VALUE;
+            }
+            return Math.min(Math.min(root.val, findMin(root.left)), findMin(root.right));
+        }
+
+        int countNodes(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+
+        public int largestBSTSubtree(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            if (isValidBST(root)) {
+                return countNodes(root);
+            }
+            return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
+        }
+    }
 }
