570f8ee9a20666f01ae9fd62410ea550e86d6370
54k <54k@bk.ru>
2025-01-27 13:35:43 +0700

    day 800

--- java/leetcode_grind/Day800.java
+++ java/leetcode_grind/Day800.java
@@ -144,4 +144,42 @@ public class Day800 {
         }
     }
 
+    // https://leetcode.com/problems/binary-tree-pruning/description/
+    static public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution5 {
+        public TreeNode pruneTree(TreeNode root) {
+            return containsOne(root) ? root : null;
+        }
+
+        boolean containsOne(TreeNode node) {
+            if (node == null)
+                return false;
+            boolean leftContainsOne = containsOne(node.left);
+            boolean rightContainsOne = containsOne(node.right);
+            if (!leftContainsOne)
+                node.left = null;
+            if (!rightContainsOne)
+                node.right = null;
+            return node.val == 1 || leftContainsOne || rightContainsOne;
+        }
+    }
+
 }
