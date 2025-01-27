a6b705558720d384bb9df8950df91fa8a808e4c8
54k <54k@bk.ru>
2025-01-27 13:29:36 +0700

    day 800

--- java/leetcode_grind/Day800.java
+++ java/leetcode_grind/Day800.java
@@ -120,4 +120,28 @@ public class Day800 {
         }
     }
 
+    static class Solution4 {
+        public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+            boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
+            for (int[] edge : prerequisites) {
+                isPrerequisite[edge[0]][edge[1]] = true;
+            }
+
+            for (int intermediate = 0; intermediate < numCourses; intermediate++) {
+                for (int src = 0; src < numCourses; src++) {
+                    for (int target = 0; target < numCourses; target++) {
+                        isPrerequisite[src][target] = isPrerequisite[src][target] ||
+                                (isPrerequisite[src][intermediate] && isPrerequisite[intermediate][target]);
+                    }
+                }
+            }
+
+            List<Boolean> answer = new ArrayList<>();
+            for (int[] query : queries) {
+                answer.add(isPrerequisite[query[0]][query[1]]);
+            }
+            return answer;
+        }
+    }
+
 }
