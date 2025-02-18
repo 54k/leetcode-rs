7517c4a104838cf9fa04ba3427b77c93a2d8b9b8
54k <54k@bk.ru>
2025-02-18 12:08:19 +0700

    day 822

--- java/leetcode_grind/Day822.java
+++ java/leetcode_grind/Day822.java
@@ -67,4 +67,24 @@ public class Day822 {
         }
     }
 
+    static class Solution2 {
+        public String smallestNumber(String pattern) {
+            return String.valueOf(findSmallestNumber(pattern, 0, 0, 0));
+        }
+
+        int findSmallestNumber(String pattern, int currentPosition, int usedDigitsMask, int currentNum) {
+            if (currentPosition > pattern.length())
+                return currentNum;
+            int result = Integer.MAX_VALUE;
+            int lastDigit = currentNum % 10;
+            boolean shouldIncrement = currentPosition == 0 || pattern.charAt(currentPosition - 1) == 'I';
+            for (int currentDigit = 1; currentDigit <= 9; currentDigit++) {
+                if ((usedDigitsMask & (1 << currentDigit)) == 0 && currentDigit > lastDigit == shouldIncrement) {
+                    result = Math.min(result, findSmallestNumber(pattern, currentPosition + 1,
+                            usedDigitsMask | (1 << currentDigit), currentNum * 10 + currentDigit));
+                }
+            }
+            return result;
+        }
+    }
 }
