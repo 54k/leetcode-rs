8c6e7c67c477bbc7922a5219227a6addb0db0177
54k <54k@bk.ru>
2025-02-18 12:14:25 +0700

    day 822

--- java/leetcode_grind/Day822.java
+++ java/leetcode_grind/Day822.java
@@ -87,4 +87,28 @@ public class Day822 {
             return result;
         }
     }
+
+    static class Solution3 {
+        public String smallestNumber(String pattern) {
+            StringBuilder result = new StringBuilder();
+            buildSequence(0, 0, pattern.toCharArray(), result);
+            return result.reverse().toString();
+        }
+
+        int buildSequence(
+                int currentIndex,
+                int currentCount,
+                char[] patternArray,
+                StringBuilder result) {
+            if (currentIndex != patternArray.length) {
+                if (patternArray[currentIndex] == 'I') {
+                    buildSequence(currentIndex + 1, currentIndex + 1, patternArray, result);
+                } else {
+                    currentCount = buildSequence(currentIndex + 1, currentCount, patternArray, result);
+                }
+            }
+            result.append(currentCount + 1);
+            return currentCount + 1;
+        }
+    }
 }
