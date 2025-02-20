ea9ef79374467497b9544ccd132c47d2212cee3a
54k <54k@bk.ru>
2025-02-20 11:46:12 +0700

    day 824

--- java/leetcode_grind/Day824.java
+++ java/leetcode_grind/Day824.java
@@ -1,6 +1,7 @@
 package leetcode_grind;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class Day824 {
@@ -32,4 +33,46 @@ public class Day824 {
         }
     }
 
+    static class Solution2 {
+        public String findDifferentBinaryString(String[] nums) {
+            Set<Integer> integers = new HashSet<>();
+            for (String num : nums) {
+                integers.add(Integer.parseInt(num, 2));
+            }
+            int n = nums.length;
+            for (int num = 0; num <= n; num++) {
+                if (!integers.contains(num)) {
+                    String ans = Integer.toBinaryString(num);
+                    while (ans.length() < n) {
+                        ans = "0" + ans;
+                    }
+                    return ans;
+                }
+            }
+            return "";
+        }
+    }
+
+    static class Solution3 {
+        public String findDifferentBinaryString(String[] nums) {
+            Set<Integer> integers = new HashSet<>();
+            for (String num : nums) {
+                integers.add(Integer.parseInt(num, 2));
+            }
+
+            int ans = Integer.parseInt(nums[0], 2);
+            int n = nums.length;
+            Random rand = new Random();
+
+            while (integers.contains(ans)) {
+                ans = rand.nextInt((int) Math.pow(2, n));
+            }
+
+            String s = Integer.toBinaryString(ans);
+            while (s.length() < n) {
+                s = "0" + s;
+            }
+            return s;
+        }
+    }
 }
