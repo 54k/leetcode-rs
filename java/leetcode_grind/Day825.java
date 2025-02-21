d318b9530159eb3c73a7024852193b4ebdc05d99
54k <54k@bk.ru>
2025-02-21 11:08:22 +0700

    day 825

---
+++ java/leetcode_grind/Day825.java
@@ -0,0 +1,29 @@
+package leetcode_grind;
+
+import java.util.*;
+
+public class Day825 {
+    // https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/description/?envType=daily-question&envId=2025-02-21
+    // static class FindElements {
+    // HashSet<Integer> seen;
+
+    // public FindElements(TreeNode root) {
+    // seen = new HashSet<>();
+    // dfs(root, 0);
+    // }
+
+    // public boolean find(int target) {
+    // return seen.contains(target);
+    // }
+
+    // private void dfs(TreeNode currentNode, int currentValue) {
+    // if (currentNode == null)
+    // return;
+    // // visit current node by adding its value to seen
+    // seen.add(currentValue);
+    // dfs(currentNode.left, currentValue * 2 + 1);
+    // dfs(currentNode.right, currentValue * 2 + 2);
+    // }
+    // }
+
+}
