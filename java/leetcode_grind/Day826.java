82c2e4c993f135d0ca638335284289a83c9324e1
54k <54k@bk.ru>
2025-02-22 12:12:28 +0300

    day 826

--- java/leetcode_grind/Day826.java
+++ java/leetcode_grind/Day826.java
@@ -53,4 +53,44 @@ public class Day826 {
             return node;
         }
     }
+
+    static class Solution {
+        public TreeNode recoverFromPreorder(String traversal) {
+            Stack<TreeNode> stack = new Stack<>();
+            int index = 0;
+            while (index < traversal.length()) {
+                int depth = 0;
+                while (index < traversal.length() && traversal.charAt(index) == '-') {
+                    depth++;
+                    index++;
+                }
+
+                int value = 0;
+                while (index < traversal.length() && Character.isDigit(traversal.charAt(index))) {
+                    value = value * 10 + (traversal.charAt(index) - '0');
+                    index++;
+                }
+
+                TreeNode node = new TreeNode(value);
+                while (stack.size() > depth) {
+                    stack.pop();
+                }
+
+                if (!stack.isEmpty()) {
+                    if (stack.peek().left == null) {
+                        stack.peek().left = node;
+                    } else {
+                        stack.peek().right = node;
+                    }
+                }
+
+                stack.push(node);
+            }
+
+            while (stack.size() > 1) {
+                stack.pop();
+            }
+            return stack.peek();
+        }
+    }
 }
