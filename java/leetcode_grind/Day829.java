9b95fe62ee591223c26a06b91281abfb5ea6fdc8
54k <54k@bk.ru>
2025-02-25 16:58:59 +0300

    day 829

--- java/leetcode_grind/Day829.java
+++ java/leetcode_grind/Day829.java
@@ -1,5 +1,12 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
+
 public class Day829 {
     // https://leetcode.com/problems/number-of-sub-arrays-with-odd-sum/description/
     static class Solution1 {
@@ -53,6 +60,106 @@ public class Day829 {
                 count %= MOD;
             }
             return count;
+        }
+    }
+
+    static class Pair<F, S> {
+        F key;
+        S value;
+
+        public Pair(F k, S v) {
+            key = k;
+            value = v;
+        }
+
+        F getKey() {
+            return key;
+        }
+
+        S getValue() {
+            return value;
+        }
+    }
+
+    // https://leetcode.com/problems/find-servers-that-handled-most-number-of-requests/description/
+    static class Solution3 {
+
+        public List<Integer> busiestServers(int k, int[] arrival, int[] load) {
+            int[] count = new int[k];
+
+            TreeSet<Integer> free = new TreeSet<Integer>();
+            PriorityQueue<Pair<Integer, Integer>> busy = new PriorityQueue<>((a, b) -> a.getKey() - b.getKey());
+
+            for (int i = 0; i < k; i++) {
+                free.add(i);
+            }
+
+            for (int i = 0; i < arrival.length; i++) {
+                int start = arrival[i];
+                while (!busy.isEmpty() && busy.peek().getKey() <= start) {
+                    Pair<Integer, Integer> curr = busy.remove();
+                    int serverId = curr.getValue();
+                    free.add(serverId);
+                }
+
+                if (!free.isEmpty()) {
+                    Integer busyId = free.ceiling(i % k);
+                    if (busyId == null) {
+                        busyId = free.first();
+                    }
+                    free.remove(busyId);
+                    busy.add(new Pair<>(start + load[i], busyId));
+                    count[busyId]++;
+                }
+            }
+
+            int maxJob = Collections.max(Arrays.stream(count).boxed().toList());
+            List<Integer> answer = new ArrayList<>();
+            for (int i = 0; i < k; ++i) {
+                if (count[i] == maxJob) {
+                    answer.add(i);
+                }
+            }
+            return answer;
+        }
+    }
+
+    static class Solution4 {
+        public List<Integer> busiestServers(int k, int[] arrival, int[] load) {
+            int[] count = new int[k];
+            PriorityQueue<Integer> free = new PriorityQueue<>((a, b) -> a - b);
+            PriorityQueue<Pair<Integer, Integer>> busy = new PriorityQueue<>((a, b) -> a.getKey() - b.getKey());
+
+            for (int i = 0; i < k; i++) {
+                free.add(i);
+            }
+
+            for (int i = 0; i < arrival.length; i++) {
+                int start = arrival[i];
+
+                while (!busy.isEmpty() && busy.peek().getKey() <= start) {
+                    Pair<Integer, Integer> curr = busy.remove();
+                    int serverId = curr.getValue();
+                    int modifiedId = ((serverId - i) % k + k) % k + i;
+                    free.add(modifiedId);
+                }
+
+                if (!free.isEmpty()) {
+                    int busyId = free.peek() % k;
+                    free.remove();
+                    busy.add(new Pair<>(start + load[i], busyId));
+                    count[busyId]++;
+                }
+            }
+
+            int maxJob = Collections.max(Arrays.stream(count).boxed().collect(Collectors.toList()));
+            List<Integer> answer = new ArrayList<>();
+            for (int i = 0; i < k; i++) {
+                if (count[i] == maxJob) {
+                    answer.add(i);
+                }
+            }
+            return answer;
         }
     }
 }
