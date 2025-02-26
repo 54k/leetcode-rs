0d5b6afc6e18d904c9e7f1c85f03b18dba50f01a
54k <54k@bk.ru>
2025-02-26 22:36:32 +0300

    day 830

---
+++ java/leetcode_grind/Day830.java
@@ -0,0 +1,38 @@
+package leetcode_grind;
+
+public class Day830 {
+    // https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/description/
+    static class Solution1 {
+        public int maxAbsoluteSum(int[] nums) {
+            int minPrefixSum = Integer.MAX_VALUE, maxPrefixSum = Integer.MIN_VALUE;
+            int prefixSum = 0, maxAbsSum = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                prefixSum += nums[i];
+                minPrefixSum = Math.min(minPrefixSum, prefixSum);
+                maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
+
+                if (prefixSum >= 0) {
+                    maxAbsSum = Math.max(maxAbsSum, Math.max(prefixSum, prefixSum - minPrefixSum));
+                } else if (prefixSum <= 0) {
+                    maxAbsSum = Math.max(maxAbsSum, Math.max(Math.abs(prefixSum), Math.abs(prefixSum - maxPrefixSum)));
+                }
+            }
+
+            return maxAbsSum;
+        }
+    }
+
+    static class Solution2 {
+        public int maxAbsoluteSum(int[] nums) {
+            int minPrefixSum = 0, maxPrefixSum = 0;
+            int prefixSum = 0;
+            for (int i = 0; i < nums.length; i++) {
+                prefixSum += nums[i];
+                minPrefixSum = Math.min(minPrefixSum, prefixSum);
+                maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
+            }
+            return maxPrefixSum - minPrefixSum;
+        }
+    }
+}
