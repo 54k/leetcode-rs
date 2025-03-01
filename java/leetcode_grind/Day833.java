442ce2d523d9ae0daf5886456415356f4c4ff0c5
54k <54k@bk.ru>
2025-03-01 11:27:46 +0300

    day 833

--- java/leetcode_grind/Day833.java
+++ java/leetcode_grind/Day833.java
@@ -24,5 +24,28 @@ public class Day833 {
         }
     }
 
-    
+    static class Solution2 {
+        public int[] applyOperations(int[] nums) {
+            int n = nums.length;
+            int writeIndex = 0;
+
+            for (int index = 0; index < n; index++) {
+                if (index < n - 1 && nums[index] == nums[index + 1] && nums[index] != 0) {
+                    nums[index] *= 2;
+                    nums[index + 1] = 0;
+                }
+
+                if (nums[index] != 0) {
+                    if (index != writeIndex) {
+                        int temp = nums[index];
+                        nums[index] = nums[writeIndex];
+                        nums[writeIndex] = temp;
+                    }
+                    writeIndex++;
+                }
+            }
+
+            return nums;
+        }
+    }
 }
