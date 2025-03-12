3f9d0cefbcd1af3c276f03d6c702847b7a74b6ab
54k <54k@bk.ru>
2025-03-12 23:14:19 +0100

    day 844

--- java/leetcode_grind/Day844.java
+++ java/leetcode_grind/Day844.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Arrays;
+
 public class Day844 {
 
     // https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/description/?envType=daily-question&envId=2025-03-12
@@ -74,6 +76,80 @@ public class Day844 {
             int positiveCount = nums.length - upperBound(nums);
             int negativeCount = lowerBound(nums) + 1;
             return Math.max(positiveCount, negativeCount);
+        }
+    }
+
+    // https://leetcode.com/problems/candy/description/
+    static class Solution3 {
+        public int candy(int[] ratings) {
+            int[] candies = new int[ratings.length];
+            Arrays.fill(candies, 1);
+            boolean hasChanged = true;
+            while (hasChanged) {
+                hasChanged = false;
+                for (int i = 0; i < ratings.length; i++) {
+                    if (i != ratings.length - 1 && ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                        candies[i] = candies[i + 1] + 1;
+                        hasChanged = true;
+                    }
+
+                    if (i > 0 && ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
+                        candies[i] = candies[i - 1] + 1;
+                        hasChanged = true;
+                    }
+                }
+            }
+            int sum = 0;
+            for (int candy : candies) {
+                sum += candy;
+            }
+            return sum;
+        }
+    }
+
+    static class Solution4 {
+        public int candy(int[] ratings) {
+            int sum = 0;
+
+            int[] left2right = new int[ratings.length];
+            int[] right2left = new int[ratings.length];
+            Arrays.fill(left2right, 1);
+            Arrays.fill(right2left, 1);
+
+            for (int i = 1; i < ratings.length; i++) {
+                if (ratings[i] > ratings[i - 1]) {
+                    left2right[i] = left2right[i - 1] + 1;
+                }
+            }
+            for (int i = ratings.length - 2; i >= 0; i--) {
+                if (ratings[i] > ratings[i + 1]) {
+                    right2left[i] = right2left[i + 1] + 1;
+                }
+            }
+            for (int i = 0; i < ratings.length; i++) {
+                sum += Math.max(left2right[i], right2left[i]);
+            }
+            return sum;
+        }
+    }
+
+    static class Solution5 {
+        public int candy(int[] ratings) {
+            int[] candies = new int[ratings.length];
+            Arrays.fill(candies, 1);
+            for (int i = 1; i < ratings.length; i++) {
+                if (ratings[i] > ratings[i - 1]) {
+                    candies[i] = candies[i - 1] + 1;
+                }
+            }
+            int sum = candies[ratings.length - 1];
+            for (int i = ratings.length - 2; i >= 0; i--) {
+                if (ratings[i] > ratings[i + 1]) {
+                    candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+                }
+                sum += candies[i];
+            }
+            return sum;
         }
     }
 }
