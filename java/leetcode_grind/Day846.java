e1fc6a7ea4f7336998ad03db1e9aeab509820a52
54k <54k@bk.ru>
2025-03-14 15:13:45 +0100

    day 846

--- java/leetcode_grind/Day846.java
+++ java/leetcode_grind/Day846.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Arrays;
+
 public class Day846 {
     // https://leetcode.com/problems/maximum-candies-allocated-to-k-children/description/?envType=daily-question&envId=2025-03-14
     static class Solution1 {
@@ -29,4 +31,45 @@ public class Day846 {
             return lower;
         }
     }
+
+    // https://leetcode.com/problems/kth-ancestor-of-a-tree-node/description/
+    static class TreeAncestor {
+
+        int maxD;
+        int kthParent[][];
+
+        public TreeAncestor(int n, int[] parent) {
+            while ((1 << maxD) <= n) {
+                maxD++;
+            }
+
+            kthParent = new int[n][maxD];
+            for (int i = 0; i < n; i++) {
+                Arrays.fill(kthParent[i], -1);
+            }
+
+            for (int lg = 0; lg < maxD; lg++) {
+                for (int node = 0; node < n; node++) {
+                    if (lg == 0) {
+                        kthParent[node][lg] = parent[node];
+                    } else if (kthParent[node][lg - 1] != -1) {
+                        kthParent[node][lg] = kthParent[kthParent[node][lg - 1]][lg - 1];
+                    }
+                }
+            }
+        }
+
+        public int getKthAncestor(int node, int k) {
+            for (int lg = 0; lg < maxD; lg++) {
+                if (((1 << lg) & k) != 0) {
+                    node = kthParent[node][lg];
+                    if (node == -1) {
+                        break;
+                    }
+                }
+            }
+            return node;
+        }
+    }
+
 }
