58eb87508ae25c6f05802075ec9d6d33d14eb3b0
54k <54k@bk.ru>
2025-03-16 19:22:35 +0100

    day 848

---
+++ java/leetcode_grind/Day848.java
@@ -0,0 +1,88 @@
+package leetcode_grind;
+
+public class Day848 {
+    // https://leetcode.com/problems/minimum-time-to-repair-cars/description/?envType=daily-question&envId=2025-03-16
+    static class Solution1 {
+        public long repairCars(int[] ranks, int cars) {
+            int minRank = ranks[0], maxRank = ranks[0];
+            for (int rank : ranks) {
+                minRank = Math.min(minRank, rank);
+                maxRank = Math.max(maxRank, rank);
+            }
+
+            int[] freq = new int[maxRank + 1];
+            for (int rank : ranks) {
+                minRank = Math.min(minRank, rank);
+                freq[rank]++;
+            }
+
+            long low = 1, high = 1L * minRank * cars * cars;
+            while (low < high) {
+                long mid = (low + high) / 2;
+                long carsRepaired = 0;
+
+                for (int rank = 1; rank <= maxRank; rank++) {
+                    carsRepaired += freq[rank] * (long) Math.sqrt(mid / (long) rank);
+                }
+
+                if (carsRepaired >= cars) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+    }
+
+    static class Solution2 {
+        public long repairCars(int[] ranks, int cars) {
+            long low = 1, high = 1L * ranks[0] * cars * cars;
+            while (low < high) {
+                long mid = (low + high) / 2, carsRepaired = 0;
+                for (int rank : ranks) {
+                    carsRepaired += (long) (Math.sqrt((1.0 * mid) / rank));
+                }
+                if (carsRepaired < cars)
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
+            return low;
+        }
+    }
+
+    // https://leetcode.com/problems/nth-digit/description/
+    static class Solution4 {
+        public int findNthDigit(int n) {
+            // Шаг 1: Определяем, сколько цифр имеют числа в нужном диапазоне
+            long base = 9; // Количество чисел в текущей группе (9, 90, 900, ...)
+            int digits = 1; // Количество цифр в каждом числе текущей группы
+
+            // Пока n больше, чем количество цифр в текущем диапазоне, уменьшаем n
+            while (n - base * digits > 0) {
+                n -= base * digits; // Вычитаем цифры, уже учтенные в предыдущих диапазонах
+                base *= 10; // Переходим к следующему диапазону (1-9, 10-99, 100-999, ...)
+                digits++; // Увеличиваем количество цифр в числе (1 -> 2 -> 3 ...)
+            }
+
+            // Шаг 2: Определяем, в каком числе находится искомая цифра
+            int index = n % digits; // Порядковый номер цифры в числе
+            if (index == 0)
+                index = digits; // Если index == 0, значит цифра в конце числа
+
+            // Определяем первое число в текущем диапазоне (10, 100, 1000...)
+            long num = (long) Math.pow(10, digits - 1);
+
+            // Определяем само число, в котором находится искомая цифра
+            num += (index == digits) ? n / digits - 1 : n / digits;
+
+            // Шаг 3: Определяем нужную цифру в числе
+            for (int i = index; i < digits; i++) {
+                num /= 10; // Убираем последние цифры, пока не дойдем до нужной
+            }
+
+            return (int) (num % 10); // Возвращаем последнюю оставшуюся цифру
+        }
+    }
+}
