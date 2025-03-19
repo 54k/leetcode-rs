59c1dede127d1b47e1657ca938b3eb71dc6bed9e
54k <54k@bk.ru>
2025-03-19 11:04:29 +0100

    day 851

--- java/leetcode_grind/Day851.java
+++ java/leetcode_grind/Day851.java
@@ -1,5 +1,8 @@
 package leetcode_grind;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Day851 {
     // https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/description/?envType=daily-question&envId=2025-03-19
     static class Solution1 {
@@ -18,6 +21,30 @@ public class Day851 {
                 }
             }
             return ops;
+        }
+    }
+
+    // https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/description/?envType=daily-question&envId=2025-03-19
+    static class Solution2 {
+        public int minOperations(int[] nums) {
+            Deque<Integer> flipQueue = new ArrayDeque<>();
+            int count = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                while (!flipQueue.isEmpty() && i > flipQueue.peekFirst() + 2) {
+                    flipQueue.pollFirst();
+                }
+
+                if ((nums[i] + flipQueue.size()) % 2 == 0) {
+                    if (i + 2 >= nums.length) {
+                        return -1;
+                    }
+                    count++;
+                    flipQueue.offerLast(i);
+                }
+            }
+
+            return count;
         }
     }
 }
