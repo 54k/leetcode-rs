a950d9d8b9d57bb53b4300fd4e8e3480bb8365e2
54k <54k@bk.ru>
2025-03-20 12:28:06 +0100

    day 852

--- java/leetcode_grind/Day852.java
+++ java/leetcode_grind/Day852.java
@@ -1,5 +1,10 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Day852 {
     // https://leetcode.com/problems/minimum-cost-walk-in-weighted-graph/description/?envType=daily-question&envId=2025-03-20
     static class Solution1 {
@@ -68,5 +73,69 @@ public class Day852 {
         }
     }
 
-    
+    static class Solution2 {
+        public int[] minimumCost(int n, int[][] edges, int[][] query) {
+            List<List<int[]>> adjList = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                adjList.add(new ArrayList<>(2));
+            }
+
+            for (int[] edge : edges) {
+                adjList.get(edge[0]).add(new int[] { edge[1], edge[2] });
+                adjList.get(edge[1]).add(new int[] { edge[0], edge[2] });
+            }
+
+            boolean[] visited = new boolean[n];
+            int[] components = new int[n];
+            List<Integer> componentCost = new ArrayList<>();
+            int componentId = 0;
+            for (int node = 0; node < n; node++) {
+                if (!visited[node]) {
+                    componentCost.add(
+                            getComponentCost(
+                                    node, adjList, visited, components, componentId));
+                    componentId++;
+                }
+            }
+
+            int[] answer = new int[query.length];
+            for (int i = 0; i < query.length; i++) {
+                int start = query[i][0];
+                int end = query[i][1];
+
+                if (components[start] == components[end]) {
+                    answer[i] = componentCost.get(components[start]);
+                } else {
+                    answer[i] = -1;
+                }
+            }
+
+            return answer;
+        }
+
+        int getComponentCost(int source, List<List<int[]>> adjList, boolean[] visited, int[] components,
+                int componentId) {
+            Queue<Integer> nodesQueue = new LinkedList<>();
+            int componentCost = Integer.MAX_VALUE;
+            nodesQueue.offer(source);
+            visited[source] = true;
+
+            while (!nodesQueue.isEmpty()) {
+                int node = nodesQueue.poll();
+                components[node] = componentId;
+
+                for (int[] neighbor : adjList.get(node)) {
+                    int weight = neighbor[1];
+                    componentCost &= weight;
+
+                    if (visited[neighbor[0]])
+                        continue;
+
+                    visited[neighbor[0]] = true;
+                    nodesQueue.offer(neighbor[0]);
+                }
+            }
+            return componentCost;
+        }
+    }
 }
