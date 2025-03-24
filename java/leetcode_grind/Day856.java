6ba64f891c85db76d5e3c84a36e5b6e2d8de6ea3
54k <54k@bk.ru>
2025-03-24 11:53:39 +0300

    day 856

--- java/leetcode_grind/Day856.java
+++ java/leetcode_grind/Day856.java
@@ -1,5 +1,9 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -31,4 +35,28 @@ public class Day856 {
         }
     }
 
+    static class Solution2 {
+        public int[] rearrangeBarcodes(int[] barcodes) {
+            Map<Integer, Integer> cnt = new HashMap<>();
+            for (int i : barcodes)
+                cnt.put(i, cnt.getOrDefault(i, 0) + 1);
+
+            List<Map.Entry<Integer, Integer>> list = new ArrayList<>(cnt.entrySet());
+            Collections.sort(list, Map.Entry.<Integer, Integer>comparingByValue().reversed());
+
+            int l = barcodes.length, i = 0;
+            int[] res = new int[l];
+            for (Map.Entry<Integer, Integer> e : list) {
+                int time = e.getValue();
+                while (time-- > 0) {
+                    res[i] = e.getKey();
+                    i += 2;
+                    if (i >= barcodes.length)
+                        i = 1;
+                }
+            }
+
+            return res;
+        }
+    }
 }
