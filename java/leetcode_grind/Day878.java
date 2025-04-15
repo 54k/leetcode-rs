5dcfb07675e685d13797ecb713330f5311bb0aea
54k <54k@bk.ru>
2025-04-15 23:25:16 +0300

    day 878

---
+++ java/leetcode_grind/Day878.java
@@ -0,0 +1,54 @@
+package leetcode_grind;
+
+public class Day878 {
+    // https://leetcode.com/problems/count-good-triplets-in-an-array/description/?envType=daily-question&envId=2025-04-15
+    static class Solution1 {
+        static class FenwickTree {
+            int[] tree;
+
+            FenwickTree(int size) {
+                tree = new int[size + 1];
+            }
+
+            void update(int index, int delta) {
+                index++;
+                while (index < tree.length) {
+                    tree[index] += delta;
+                    index += index & -index;
+                }
+            }
+
+            int query(int index) {
+                index++;
+                int res = 0;
+                while (index > 0) {
+                    res += tree[index];
+                    index -= index & -index;
+                }
+                return res;
+            }
+        }
+
+        public long goodTriplets(int[] nums1, int[] nums2) {
+            int n = nums1.length;
+            int[] pos2 = new int[n], reversedIndexMapping = new int[n];
+            for (int i = 0; i < n; i++) {
+                pos2[nums2[i]] = i;
+            }
+            for (int i = 0; i < n; i++) {
+                reversedIndexMapping[pos2[nums1[i]]] = i;
+            }
+            FenwickTree tree = new FenwickTree(n);
+            long res = 0;
+            for (int value = 0; value < n; value++) {
+                int pos = reversedIndexMapping[value];
+                int left = tree.query(pos);
+                tree.update(pos, 1);
+                int right = (n - 1 - pos) - (value - left);
+                res += (long) left * right;
+            }
+            return res;
+        }
+    }
+
+}
