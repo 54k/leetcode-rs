552025cd596d6271f1b2b0489082bc2ed838811f
54k <54k@bk.ru>
2025-04-24 03:31:32 +0300

    day 887

---
+++ java/leetcode_grind/Day887.java
@@ -0,0 +1,39 @@
+package leetcode_grind;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Day887 {
+    static class Solution1 {
+        public int countCompleteSubarrays(int[] nums) {
+            int res = 0;
+            Map<Integer, Integer> cnt = new HashMap<>();
+            int n = nums.length;
+            int right = 0;
+            int distinct = new HashSet<>(Arrays.stream(nums).boxed().toList()).size();
+
+            for (int left = 0; left < n; left++) {
+                if (left > 0) {
+                    int remove = nums[left - 1];
+                    cnt.put(remove, cnt.get(remove) - 1);
+                    if (cnt.get(remove) == 0) {
+                        cnt.remove(remove);
+                    }
+                }
+
+                while (right < n && cnt.size() < distinct) {
+                    int add = nums[right];
+                    cnt.put(add, cnt.getOrDefault(add, 0) + 1);
+                    right++;
+                }
+
+                if (cnt.size() == distinct) {
+                    res += (n - right + 1);
+                }
+            }
+            return res;
+        }
+    }
+}
