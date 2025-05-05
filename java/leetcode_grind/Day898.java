35675f0acff8d177691cbfe17ec753cb99974ac3
54k <54k@bk.ru>
2025-05-05 02:51:13 +0200

    day 898

--- java/leetcode_grind/Day898.java
+++ java/leetcode_grind/Day898.java
@@ -44,4 +44,27 @@ public class Day898 {
             return (int) (f(n));
         }
     }
+
+    static class Solution2 {
+        public int numTilings(int n) {
+            int MOD = 1_000_000_007;        
+            if (n <= 2) {
+                return n;
+            }
+
+            long[] f = new long[n + 1];
+            long[] p = new long[n + 1];
+
+            f[1] = 1L;
+            f[2] = 2L;
+            p[2] = 1L;
+
+            for (int k = 3; k < n + 1; ++k) {
+                f[k] = (f[k - 1] + f[k - 2] + 2 * p[k - 1]) % MOD;
+                p[k] = (p[k - 1] + f[k - 2]) % MOD;
+            }
+
+            return (int) (f[n]);
+        }
+    }
 }
