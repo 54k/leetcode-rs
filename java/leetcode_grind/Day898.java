6f8eb82ffdfbf33977523da6ad59c8c8359894e1
54k <54k@bk.ru>
2025-05-05 03:10:36 +0200

    day 898

--- java/leetcode_grind/Day898.java
+++ java/leetcode_grind/Day898.java
@@ -67,4 +67,47 @@ public class Day898 {
             return (int) (f[n]);
         }
     }
+
+    // https://leetcode.com/problems/longest-valid-parentheses/description/
+    static class Solution3 {
+        public int longestValidParentheses(String s) {
+            int maxans = 0;        
+            int dp[] = new int[s.length()];
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == ')') {
+                    if (s.charAt(i - 1) == '(') {
+                        dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                    } else if (
+                        i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '('
+                    ) {
+                        dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i-1] - 2] : 0) + 2;
+                        
+                    }
+                    maxans = Math.max(maxans, dp[i]);
+                }
+            }
+            return maxans;
+        }
+    }
+
+    static class Solution4 {
+        public int longestValidParentheses(String s) {
+            int maxans = 0;        
+            Stack<Integer> stack = new Stack<>();
+            stack.push(-1);
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    stack.push(i);
+                } else {
+                    stack.pop();
+                    if (stack.empty()) {
+                        stack.push(i);
+                    } else {
+                        maxans = Math.max(maxans, i - stack.peek());
+                    }
+                }
+            }
+            return maxans;
+        }
+    }
 }
