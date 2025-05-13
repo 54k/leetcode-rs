c4aaf3e8c01ea4179bbda8523d2b47c4c3b22a9d
54k <54k@bk.ru>
2025-05-13 12:24:10 +0200

    day 906

---
+++ java/leetcode_grind/Day906.java
@@ -0,0 +1,29 @@
+package leetcode_grind;
+
+public class Day906 {
+    // https://leetcode.com/problems/total-characters-in-string-after-transformations-i/description/?envType=daily-question&envId=2025-05-13
+    static class Solution1 {
+        final int MOD = 1_000_000_007;
+
+        public int lengthAfterTransformations(String s, int t) {
+            int[] cnt = new int[26];
+            for (char ch : s.toCharArray()) {
+                ++cnt[ch - 'a'];
+            }
+            for (int round = 0; round < t; ++round) {
+                int[] nxt = new int[26];
+                nxt[0] = cnt[25];
+                nxt[1] = (cnt[25] + cnt[0]) % MOD;
+                for (int i = 2; i < 26; ++i) {
+                    nxt[i] = cnt[i - 1];
+                }
+                cnt = nxt;
+            }
+            int ans = 0;
+            for (int i = 0; i < 26; i++) {
+                ans = (ans + cnt[i]) % MOD;
+            }
+            return ans;
+        }
+    }
+}
