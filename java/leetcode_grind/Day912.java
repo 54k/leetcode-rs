3d209ff9ea97454f40e66da7a94676da6599cc18
54k <54k@bk.ru>
2025-05-19 21:49:04 +0200

    day 912

---
+++ java/leetcode_grind/Day912.java
@@ -0,0 +1,20 @@
+package leetcode_grind;
+
+import java.util.Arrays;
+
+public class Day912 {
+    static class Solution1 {
+        public String triangleType(int[] nums) {
+            Arrays.sort(nums);
+            if (nums[0] + nums[1] <= nums[2]) {
+                return "none";
+            } else if (nums[0] == nums[2]) {
+                return "equilateral";
+            } else if (nums[0] == nums[1] || nums[1] == nums[2]) {
+                return "isosceles";
+            } else {
+                return "scalene";
+            }
+        }
+    }
+}
