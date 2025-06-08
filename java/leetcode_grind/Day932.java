5e889ead736ffc304b9a629906784fb170181a26
54k <54k@bk.ru>
2025-06-08 12:31:31 +0300

    day 932

--- java/leetcode_grind/Day932.java
+++ java/leetcode_grind/Day932.java
@@ -26,5 +26,25 @@ public class Day932 {
         }
     }
 
-    
+    static class Solution2 {
+        public List<Integer> lexicalOrder(int n) {
+            List<Integer> lexicographicalNumbers = new ArrayList<>();
+            int currentNumber = 1;
+
+            for (int i = 0; i < n; i++) {
+                lexicographicalNumbers.add(currentNumber);
+
+                if (currentNumber * 10 <= n) {
+                    currentNumber *= 10;
+                } else {
+                    while (currentNumber % 10 == 9 || currentNumber >= n) {
+                        currentNumber /= 10;
+                    }
+                    currentNumber += 1;
+                }
+            }
+
+            return lexicographicalNumbers;
+        }
+    }
 }
