c1876c65a6a6cc9926846d820363f5aa4ed9e2b8
54k <54k@bk.ru>
2025-06-08 15:02:36 +0300

    day 932

--- java/leetcode_grind/Day932.java
+++ java/leetcode_grind/Day932.java
@@ -66,4 +66,45 @@ public class Day932 {
             return true;
         }
     }
+
+    static class Solution4 {
+        public boolean verifyPreorder(int[] preorder) {
+            int minLimit = Integer.MIN_VALUE;
+            int i = 0;
+            for (int num : preorder) {
+                while (i > 0 && preorder[i - 1] < num) {
+                    minLimit = preorder[i - 1];
+                    i--;
+                }
+                if (num <= minLimit) {
+                    return false;
+                }
+                preorder[i] = num;
+                i++;
+            }
+            return true;
+        }
+    }
+
+    static class Solution5 {
+        public boolean verifyPreorder(int[] preorder) {
+            int[] i = { 0 };
+            return helper(preorder, i, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+
+        boolean helper(int[] preorder, int[] i, int minLimit, int maxLimit) {
+            if (i[0] == preorder.length) {
+                return true;
+            }
+            int root = preorder[i[0]];
+            if (root <= minLimit || root >= maxLimit) {
+                return false;
+            }
+
+            i[0]++;
+            boolean left = helper(preorder, i, minLimit, root);
+            boolean right = helper(preorder, i, root, maxLimit);
+            return left || right;
+        }
+    }
 }
