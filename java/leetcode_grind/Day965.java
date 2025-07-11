9c73e19810c4e08716e7d42b9eaf69ab96ac169c
54k <54k@bk.ru>
2025-07-11 09:26:40 +0300

    day 965

--- java/leetcode_grind/Day965.java
+++ java/leetcode_grind/Day965.java
@@ -1,6 +1,7 @@
 package leetcode_grind;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Day965 {
     // https://leetcode.com/problems/meeting-rooms-iii/description/?envType=daily-question&envId=2025-07-11
@@ -33,6 +34,50 @@ public class Day965 {
                 if (!foundUnusedRoom) {
                     roomAvailabilityTime[minAvailableTimeRoom] += end - start;
                     meetingCount[minAvailableTimeRoom]++;
+                }
+            }
+
+            int maxMeetingCount = 0, maxMeetingCountRoom = 0;
+            for (int i = 0; i < n; i++) {
+                if (meetingCount[i] > maxMeetingCount) {
+                    maxMeetingCount = meetingCount[i];
+                    maxMeetingCountRoom = i;
+                }
+            }
+            return maxMeetingCountRoom;
+        }
+    }
+
+    static class Solution2 {
+        public int mostBooked(int n, int[][] meetings) {
+            var meetingCount = new int[n];
+
+            var usedRooms = new PriorityQueue<long[]>(
+                    (a, b) -> a[0] != b[0] ? Long.compare(a[0], b[0]) : Long.compare(a[1], b[1]));
+
+            var unusedRooms = new PriorityQueue<Integer>();
+            for (int i = 0; i < n; i++) {
+                unusedRooms.offer(i);
+            }
+
+            Arrays.sort(meetings, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+
+            for (int[] meeting : meetings) {
+                int start = meeting[0], end = meeting[1];
+                while (!usedRooms.isEmpty() && usedRooms.peek()[0] <= start) {
+                    int room = (int) usedRooms.poll()[1];
+                    unusedRooms.offer(room);
+                }
+
+                if (!unusedRooms.isEmpty()) {
+                    int room = unusedRooms.poll();
+                    usedRooms.offer(new long[] { end, room });
+                    meetingCount[room]++;
+                } else {
+                    long roomAvailibilityTime = usedRooms.peek()[0];
+                    int room = (int) usedRooms.poll()[1];
+                    usedRooms.offer(new long[] { roomAvailibilityTime + end - start, room });
+                    meetingCount[room]++;
                 }
             }
 
