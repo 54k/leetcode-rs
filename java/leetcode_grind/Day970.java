1597fb633576cecdce62df1f8e33dbd21146543f
54k <54k@bk.ru>
2025-07-16 12:20:06 +0300

    day 970

--- java/leetcode_grind/Day970.java
+++ java/leetcode_grind/Day970.java
@@ -1,5 +1,10 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Day970 {
     // https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i/description/?envType=daily-question&envId=2025-07-16
     static class Solution1 {
@@ -16,6 +21,146 @@ public class Day970 {
                 res = Math.max(res, cnt);
             }
             return res;
+        }
+    }
+
+    // https://leetcode.com/problems/word-squares/description/?envType=weekly-question&envId=2025-07-15
+    static class Solution2 {
+        int N = 0;
+        String[] words = null;
+        HashMap<String, List<String>> prefixHashTable = null;
+
+        public List<List<String>> wordSquares(String[] words) {
+            this.words = words;
+            this.N = words[0].length();
+
+            List<List<String>> results = new ArrayList<List<String>>();
+            this.buildPrefixHashTable(words);
+
+            for (String word : words) {
+                LinkedList<String> wordSquares = new LinkedList<String>();
+                wordSquares.addLast(word);
+                this.backtracking(1, wordSquares, results);
+            }
+
+            return results;
+        }
+
+        void backtracking(int step, LinkedList<String> wordSquares, List<List<String>> results) {
+            if (step == N) {
+                results.add((List<String>) wordSquares.clone());
+                return;
+            }
+
+            StringBuilder prefix = new StringBuilder();
+            for (String word : wordSquares) {
+                prefix.append(word.charAt(step));
+            }
+
+            for (String candidate : this.getWordsWithPrefix(prefix.toString())) {
+                wordSquares.addLast(candidate);
+                this.backtracking(step + 1, wordSquares, results);
+                wordSquares.removeLast();
+            }
+        }
+
+        void buildPrefixHashTable(String[] words) {
+            this.prefixHashTable = new HashMap<String, List<String>>();
+            for (String word : words) {
+                for (int i = 1; i < this.N; i++) {
+                    String prefix = word.substring(0, i);
+                    List<String> wordList = this.prefixHashTable.get(prefix);
+                    if (wordList == null) {
+                        wordList = new ArrayList<String>();
+                        wordList.add(word);
+                        this.prefixHashTable.put(prefix, wordList);
+                    } else {
+                        wordList.add(word);
+                    }
+                }
+            }
+        }
+
+        List<String> getWordsWithPrefix(String prefix) {
+            List<String> wordList = this.prefixHashTable.get(prefix);
+            return (wordList != null ? wordList : new ArrayList<String>());
+        }
+    }
+
+    static class Solution3 {
+        static class TrieNode {
+            HashMap<Character, TrieNode> children = new HashMap<>();
+            List<Integer> wordList = new ArrayList<>();
+        }
+
+        int N = 0;
+        String[] words = null;
+        TrieNode trie = null;
+
+        public List<List<String>> wordSquares(String[] words) {
+            this.words = words;
+            this.N = words[0].length();
+
+            List<List<String>> results = new ArrayList<List<String>>();
+            this.buildTrie(words);
+
+            for (String word : words) {
+                LinkedList<String> wordSquares = new LinkedList<String>();
+                wordSquares.addLast(word);
+                this.backtracking(1, wordSquares, results);
+            }
+
+            return results;
+        }
+
+        void backtracking(int step, LinkedList<String> wordSquares, List<List<String>> results) {
+            if (step == N) {
+                results.add((List<String>) wordSquares.clone());
+                return;
+            }
+
+            StringBuilder prefix = new StringBuilder();
+            for (String word : wordSquares) {
+                prefix.append(word.charAt(step));
+            }
+
+            for (Integer wordIndex : this.getWordsWithPrefix(prefix.toString())) {
+                wordSquares.addLast(this.words[wordIndex]);
+                this.backtracking(step + 1, wordSquares, results);
+                wordSquares.removeLast();
+            }
+        }
+
+        void buildTrie(String[] words) {
+            this.trie = new TrieNode();
+
+            for (int wordIndex = 0; wordIndex < words.length; ++wordIndex) {
+                String word = words[wordIndex];
+
+                TrieNode node = this.trie;
+                for (Character letter : word.toCharArray()) {
+                    if (node.children.containsKey(letter)) {
+                        node = node.children.get(letter);
+                    } else {
+                        TrieNode newNode = new TrieNode();
+                        node.children.put(letter, newNode);
+                        node = newNode;
+                    }
+                    node.wordList.add(wordIndex);
+                }
+            }
+        }
+
+        List<Integer> getWordsWithPrefix(String prefix) {
+            TrieNode node = this.trie;
+            for (Character letter : prefix.toCharArray()) {
+                if (node.children.containsKey(letter)) {
+                    node = node.children.get(letter);
+                } else {
+                    return new ArrayList<>();
+                }
+            }
+            return node.wordList;
         }
     }
 }
