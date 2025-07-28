136591fbdd8eb6e89563e96f276a9ee23a25b0b0
54k <54k@bk.ru>
2025-07-28 11:05:24 +0300

    day 982

--- java/leetcode_grind/Day982.java
+++ java/leetcode_grind/Day982.java
@@ -23,4 +23,74 @@ public class Day982 {
         }
     }
 
+    static class Solution2 {
+        public int countMaxOrSubsets(int[] nums) {
+            int n = nums.length;
+            int maxOrValue = 0;
+            for (int num : nums) {
+                maxOrValue |= num;
+            }
+
+            Integer[][] memo = new Integer[n][maxOrValue + 1];
+            return countSubsets(nums, 0, 0, maxOrValue, memo);
+        }
+
+        int countSubsets(int[] nums, int index, int currentOr, int targetOr, Integer[][] memo) {
+            if (index == nums.length) {
+                return (currentOr == targetOr) ? 1 : 0;
+            }
+
+            if (memo[index][currentOr] != null) {
+                return memo[index][currentOr];
+            }
+
+            int countWithout = countSubsets(nums, index + 1, currentOr, targetOr, memo);
+            int countWith = countSubsets(nums, index + 1, currentOr | nums[index], targetOr, memo);
+
+            return memo[index][currentOr] = countWith + countWithout;
+        }
+    }
+
+    static class Solution3 {
+        public int countMaxOrSubsets(int[] nums) {
+            int maxOrValue = 0;
+            for (int num : nums) {
+                maxOrValue |= num;
+            }
+
+            int totalSubsets = 1 << nums.length;
+            int subsetWithMaxOr = 0;
+
+            for (int subsetMask = 0; subsetMask < totalSubsets; subsetMask++) {
+                int currentOrValue = 0;
+                for (int i = 0; i < nums.length; i++) {
+                    if (((subsetMask >> i) & 1) == 1) {
+                        currentOrValue |= nums[i];
+                    }
+                }
+                if (currentOrValue == maxOrValue) {
+                    subsetWithMaxOr++;
+                }
+            }
+
+            return subsetWithMaxOr;
+        }
+    }
+
+    static class Solution4 {
+        public int countMaxOrSubsets(int[] nums) {
+            int max = 0;
+            int[] dp = new int[1 << 17];
+            dp[0] = 1;
+
+            for (int num : nums) {
+                for (int i = max; i >= 0; i--) {
+                    dp[i | num] += dp[i];
+                }
+                max |= num;
+            }
+
+            return dp[max];
+        }
+    }
 }
