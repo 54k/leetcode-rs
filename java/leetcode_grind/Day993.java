9b559f6f41ded157cdc9c207412fbd54b37b58be
54k <54k@bk.ru>
2025-08-08 14:35:54 +0300

    day 993

--- java/leetcode_grind/Day993.java
+++ java/leetcode_grind/Day993.java
@@ -1,5 +1,6 @@
 package leetcode_grind;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -101,6 +102,45 @@ public class Day993 {
                 }
             }
             return -1;
+        }
+    }
+
+    static class Solution5 {
+        public int smallestCommonElement(int[][] mat) {
+            int n = mat.length, m = mat[0].length;
+            for (int j = 0; j < m; j++) {
+                boolean found = true;
+                for (int i = 1; i < n && found; i++) {
+                    found = Arrays.binarySearch(mat[i], mat[0][j]) >= 0;
+                }
+                if (found) {
+                    return mat[0][j];
+                }
+            }
+            return -1;
+        }
+    }
+
+    static class Solution6 {
+        public int smallestCommonElement(int[][] mat) {
+            int n = mat.length, m = mat[0].length;
+            int pos[] = new int[n], cur_max = 0, cnt = 0;
+            while (true) {
+                for (int i = 0; i < n; i++) {
+                    while (pos[i] < m && mat[i][pos[i]] < cur_max) {
+                        ++pos[i];
+                    }
+                    if (pos[i] >= m) {
+                        return -1;
+                    }
+                    if (mat[i][pos[i]] != cur_max) {
+                        cnt = 1;
+                        cur_max = mat[i][pos[i]];
+                    } else if (++cnt == n) {
+                        return cur_max;
+                    }
+                }
+            }
         }
     }
 }
