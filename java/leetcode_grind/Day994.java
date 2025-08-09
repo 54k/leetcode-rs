0df87942e5cc1faf62f206c5ced422c81494e9a4
54k <54k@bk.ru>
2025-08-09 10:37:48 +0300

    day 994

---
+++ java/leetcode_grind/Day994.java
@@ -0,0 +1,12 @@
+package leetcode_grind;
+
+public class Day994 {
+    // https://leetcode.com/problems/power-of-two/description/?envType=daily-question&envId=2025-08-09
+    static class Solution1 {
+        public boolean isPowerOfTwo(int n) {
+            if (n <= 0)
+                return false;
+            return (n & (n - 1)) == 0;
+        }
+    }
+
