e8e31599664fa9afebc7f894fb4e3b06632aec46
54k <54k@bk.ru>
2025-08-14 18:04:50 +0300

    day 999

---
+++ java/leetcode_grind/Day999.java
@@ -0,0 +1,27 @@
+package leetcode_grind;
+
+public class Day999 {
+    // https://leetcode.com/problems/largest-3-same-digit-number-in-string/description/?envType=daily-question&envId=2025-08-14
+    static class Solution1 {
+        public String largestGoodInteger(String num) {
+            // Assign 'maxDigit' to the NUL character (smallest ASCII value character)
+            char maxDigit = '\0';
+
+            // Iterate on characters of the num string.
+            for (int index = 0; index <= num.length() - 3; ++index) {
+                // If 3 consecutive characters are the same,
+                // store the character in 'maxDigit' if it's bigger than what it already stores.
+                if (num.charAt(index) == num.charAt(index + 1) && num.charAt(index) == num.charAt(index + 2)) {
+                    maxDigit = (char) Math.max(maxDigit, num.charAt(index));
+                }
+            }
+
+            // If 'maxDigit' is NUL, return an empty string; otherwise, return a string of
+            // size 3 with 'maxDigit' characters.
+            return maxDigit == '\0' ? "" : new String(new char[] { maxDigit, maxDigit, maxDigit });
+        }
+    }
+
+    
+
+}
