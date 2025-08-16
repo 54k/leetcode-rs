3d122c97b97c6e6ab935fb73b142e2436c328552
54k <54k@bk.ru>
2025-08-16 16:42:47 +0200

    day 1001

--- java/leetcode_grind/Day1001.java
+++ java/leetcode_grind/Day1001.java
@@ -1,6 +1,7 @@
 package leetcode_grind;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -99,6 +100,68 @@ public class Day1001 {
                 res.add(s.pop());
             }
             return res;
+        }
+    }
+
+    static class Solution3 {
+        public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+            List<Integer> left_boundary = new LinkedList<>();
+            List<Integer> right_boundary = new LinkedList<>();
+            List<Integer> leaves = new LinkedList<>();
+            preorder(root, left_boundary, right_boundary, leaves, 0);
+            left_boundary.addAll(leaves);
+            left_boundary.addAll(right_boundary);
+            return left_boundary;
+        }
+
+        boolean isLeaf(TreeNode cur) {
+            return (cur.left == null && cur.right == null);
+        }
+
+        boolean isRightBoundary(int flag) {
+            return flag == 2;
+        }
+
+        boolean isLeftBoundary(int flag) {
+            return flag == 1;
+        }
+
+        boolean isRoot(int flag) {
+            return flag == 0;
+        }
+
+        int leftChildFlag(TreeNode cur, int flag) {
+            if (isLeftBoundary(flag) || isRoot(flag)) {
+                return 1;
+            } else if (isRightBoundary(flag) && cur.right == null) {
+                return 2;
+            }
+            return 3;
+        }
+
+        int rightChildFlag(TreeNode cur, int flag) {
+            if (isRightBoundary(flag) || isRoot(flag)) {
+                return 2;
+            } else if (isLeftBoundary(flag) && cur.left == null) {
+                return 1;
+            }
+            return 3;
+        }
+
+        void preorder(TreeNode cur, List<Integer> left_boundary, List<Integer> right_boundary, List<Integer> leaves,
+                int flag) {
+            if (cur == null) {
+                return;
+            }
+            if (isRightBoundary(flag)) {
+                right_boundary.add(0, cur.val);
+            } else if (isLeftBoundary(flag) || isRoot(flag)) {
+                left_boundary.add(cur.val);
+            } else if (isLeaf(cur)) {
+                leaves.add(cur.val);
+            }
+            preorder(cur.left, left_boundary, right_boundary, leaves, leftChildFlag(cur, flag));
+            preorder(cur.right, left_boundary, right_boundary, leaves, rightChildFlag(cur, flag));
         }
     }
 }
