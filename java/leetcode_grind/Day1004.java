5ef9a1f88e16c70d681bb1b311c67819bc9da2f1
54k <54k@bk.ru>
2025-08-19 12:49:23 +0200

    day 1004

--- java/leetcode_grind/Day1004.java
+++ java/leetcode_grind/Day1004.java
@@ -62,4 +62,101 @@ public class Day1004 {
             }
         }
     }
+
+    static class Solution3 {
+        public int removeStones(int[][] stones) {
+            int n = stones.length;
+            UnionFind uf = new UnionFind(n);
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                        uf.union(i, j);
+                    }
+                }
+            }
+
+            return n - uf.count;
+        }
+
+        static class UnionFind {
+            int[] parent;
+            int count;
+
+            UnionFind(int n) {
+                parent = new int[n];
+                Arrays.fill(parent, -1);
+                count = n;
+            }
+
+            int find(int node) {
+                if (parent[node] == -1) {
+                    return node;
+                }
+                return parent[node] = find(parent[node]);
+            }
+
+            void union(int n1, int n2) {
+                int root1 = find(n1);
+                int root2 = find(n2);
+
+                if (root1 == root2) {
+                    return;
+                }
+
+                count--;
+                parent[root1] = root2;
+            }
+        }
+    }
+
+    static class Solution4 {
+        public int removeStones(int[][] stones) {
+            int n = stones.length;
+            UnionFind uf = new UnionFind(20002);
+
+            for (int i = 0; i < n; i++) {
+                uf.union(stones[i][0], stones[i][1] + 10001);
+            }
+
+            return n - uf.count;
+        }
+
+        static class UnionFind {
+            int[] parent;
+            int count;
+            Set<Integer> uniqueNodes;
+
+            UnionFind(int n) {
+                parent = new int[n];
+                Arrays.fill(parent, -1);
+                count = 0;
+                uniqueNodes = new HashSet<>();
+            }
+
+            int find(int node) {
+                if (!uniqueNodes.contains(node)) {
+                    count++;
+                    uniqueNodes.add(node);
+                }
+
+                if (parent[node] == -1) {
+                    return node;
+                }
+                return parent[node] = find(parent[node]);
+            }
+
+            void union(int n1, int n2) {
+                int root1 = find(n1);
+                int root2 = find(n2);
+
+                if (root1 == root2) {
+                    return;
+                }
+
+                count--;
+                parent[root1] = root2;
+            }
+        }
+    }
 }
