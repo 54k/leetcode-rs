8c8362b9f3b6a681cd2d3fb5d810264b7c7f4a36
54k <54k@bk.ru>
2025-08-20 11:42:05 +0200

    day 1005

--- java/leetcode_grind/Day1005.java
+++ java/leetcode_grind/Day1005.java
@@ -79,4 +79,44 @@ public class Day1005 {
             return result;
         }
     }
+
+    // https://leetcode.com/problems/flip-equivalent-binary-trees/description/
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution4 {
+        public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+            if (root1 == null && root2 == null) {
+                return true;
+            }
+            if (root1 == null || root2 == null) {
+                return false;
+            }
+            if (root1.val != root2.val) {
+                return false;
+            }
+
+            boolean noSwap = flipEquiv(root1.left, root2.left) &&
+                    flipEquiv(root1.right, root2.right);
+            boolean swap = flipEquiv(root1.left, root2.right) &&
+                    flipEquiv(root1.right, root2.left);
+            return noSwap || swap;
+        }
+    }
 }
