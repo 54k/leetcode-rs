b62935802bc035b49f020ac06dc8c5173a6bbca1
54k <54k@bk.ru>
2025-08-20 12:18:12 +0200

    day 1005

--- java/leetcode_grind/Day1005.java
+++ java/leetcode_grind/Day1005.java
@@ -159,4 +159,45 @@ public class Day1005 {
             return true;
         }
     }
+
+    static class Solution6 {
+        void findCanonicalForm(TreeNode root) {
+            if (root == null)
+                return;
+            findCanonicalForm(root.left);
+            findCanonicalForm(root.right);
+
+            if (root.right == null)
+                return;
+
+            if (root.left == null) {
+                root.left = root.right;
+                root.right = null;
+                return;
+            }
+
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+            if (left.val > right.val) {
+                root.left = right;
+                root.right = left;
+            }
+        }
+
+        boolean areEquivalent(TreeNode root1, TreeNode root2) {
+            if (root1 == null && root2 == null)
+                return true;
+            if (root1 == null || root2 == null)
+                return false;
+            if (root1.val != root2.val)
+                return false;
+            return (areEquivalent(root1.left, root2.left) && areEquivalent(root1.right, root2.right));
+        }
+
+        public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+            findCanonicalForm(root1);
+            findCanonicalForm(root2);
+            return areEquivalent(root1, root2);
+        }
+    }
 }
