3d2d8228b5010e7caecc38ff4ca6d5f261a76d40
54k <54k@bk.ru>
2025-08-20 11:18:10 +0200

    day 1005

--- java/leetcode_grind/Day1005.java
+++ java/leetcode_grind/Day1005.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Arrays;
+
 public class Day1005 {
     // https://leetcode.com/problems/count-square-submatrices-with-all-ones/description/?envType=daily-question&envId=2025-08-20
     static class Solution1 {
@@ -18,6 +20,63 @@ public class Day1005 {
             }
 
             return ans;
+        }
+    }
+
+    static class Solution2 {
+        int solve(int i, int j, int[][] grid, int[][] dp) {
+            if (i >= grid.length || j >= grid[0].length) {
+                return 0;
+            }
+            if (grid[i][j] == 0) {
+                return 0;
+            }
+            if (dp[i][j] != -1) {
+                return dp[i][j];
+            }
+
+            int right = solve(i, j + 1, grid, dp);
+            int diagonal = solve(i + 1, j + 1, grid, dp);
+            int below = solve(i + 1, j, grid, dp);
+
+            return dp[i][j] = 1 + Math.min(right, Math.min(diagonal, below));
+        }
+
+        public int countSquares(int[][] grid) {
+            int ans = 0;
+            int[][] dp = new int[grid.length][grid[0].length];
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    Arrays.fill(dp[i], -1);
+                }
+            }
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    ans += solve(i, j, grid, dp);
+                }
+            }
+            return ans;
+        }
+    }
+
+    static class Solution3 {
+        public int countSquares(int[][] matrix) {
+            int row = matrix.length, col = matrix[0].length, result = 0, prev = 0;
+            int[] dp = new int[col + 1];
+
+            for (int i = 1; i <= row; i++) {
+                for (int j = 1; j <= col; j++) {
+                    if (matrix[i - 1][j - 1] == 1) {
+                        int temp = dp[j];
+                        dp[j] = 1 + Math.min(prev, Math.min(dp[j - 1], dp[j]));
+                        prev = temp;
+                        result += dp[j];
+                    } else {
+                        dp[j] = 0;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
