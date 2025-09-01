d8d18ac23db18b3684d488892d4dbc3991a7cc7e
54k <54k@bk.ru>
2025-09-01 12:51:16 +0200

    day 1017

--- java/leetcode_grind/Day1017.java
+++ java/leetcode_grind/Day1017.java
@@ -1,5 +1,6 @@
 package leetcode_grind;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class Day1017 {
@@ -42,4 +43,70 @@ public class Day1017 {
         }
     }
 
+    // https://leetcode.com/problems/campus-bikes-ii/description/?envType=weekly-question&envId=2025-09-01
+    static class Solution2 {
+        boolean visited[] = new boolean[10];
+        int smallestDistanceSum = Integer.MAX_VALUE;
+
+        int findDistance(int[] worker, int[] bike) {
+            return Math.abs(worker[0] - bike[0]) + Math.abs(worker[1] - bike[1]);
+        }
+
+        void minimumDistanceSum(int[][] workers, int workerIndex, int[][] bikes, int currDistanceSum) {
+            if (workerIndex >= workers.length) {
+                smallestDistanceSum = Math.min(smallestDistanceSum, currDistanceSum);
+                return;
+            }
+
+            if (currDistanceSum >= smallestDistanceSum) {
+                return;
+            }
+
+            for (int bikeIndex = 0; bikeIndex < bikes.length; bikeIndex++) {
+                if (!visited[bikeIndex]) {
+                    visited[bikeIndex] = true;
+                    minimumDistanceSum(workers, workerIndex + 1, bikes,
+                            currDistanceSum + findDistance(workers[workerIndex], bikes[bikeIndex]));
+                    visited[bikeIndex] = false;
+                }
+            }
+        }
+
+        public int assignBikes(int[][] workers, int[][] bikes) {
+            minimumDistanceSum(workers, 0, bikes, 0);
+            return smallestDistanceSum;
+        }
+    }
+
+    static class Solution3 {
+        int memo[] = new int[1024];
+
+        int findDistance(int[] worker, int[] bike) {
+            return Math.abs(worker[0] - bike[0]) + Math.abs(worker[1] - bike[1]);
+        }
+
+        int minimumDistanceSum(int[][] workers, int[][] bikes, int workerIndex, int mask) {
+            if (workerIndex >= workers.length) {
+                return 0;
+            }
+            if (memo[mask] != -1) {
+                return memo[mask];
+            }
+
+            int smallestDistanceSum = Integer.MAX_VALUE;
+            for (int bikeIndex = 0; bikeIndex < bikes.length; bikeIndex++) {
+                if ((mask & (1 << bikeIndex)) == 0) {
+                    smallestDistanceSum = Math.min(smallestDistanceSum,
+                            findDistance(workers[workerIndex], bikes[bikeIndex])
+                                    + minimumDistanceSum(workers, bikes, workerIndex + 1, mask | (1 << bikeIndex)));
+                }
+            }
+            return memo[mask] = smallestDistanceSum;
+        }
+
+        public int assignBikes(int[][] workers, int[][] bikes) {
+            Arrays.fill(memo, -1);
+            return minimumDistanceSum(workers, bikes, 0, 0);
+        }
+    }
 }
