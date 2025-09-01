d3aea0fbcfe9257df6a778fee37736f5ac3e6b70
54k <54k@bk.ru>
2025-09-01 13:01:51 +0200

    day 1017

--- java/leetcode_grind/Day1017.java
+++ java/leetcode_grind/Day1017.java
@@ -109,4 +109,53 @@ public class Day1017 {
             return minimumDistanceSum(workers, bikes, 0, 0);
         }
     }
+
+    static class Solution4 {
+        int memo[] = new int[1024];
+
+        int findDistance(int[] worker, int[] bike) {
+            return Math.abs(worker[0] - bike[0]) + Math.abs(worker[1] - bike[1]);
+        }
+
+        int countNumOfOnes(int mask) {
+            int count = 0;
+            while (mask != 0) {
+                mask &= (mask - 1);
+                count++;
+            }
+            return count;
+        }
+
+        int minimumDistanceSum(int[][] workers, int[][] bikes) {
+            int numOfBikes = bikes.length;
+            int numOfWorkers = workers.length;
+            int smallestDistanceSum = Integer.MAX_VALUE;
+
+            memo[0] = 0;
+
+            for (int mask = 0; mask < (1 << numOfBikes); mask++) {
+                int nextWorkerIndex = countNumOfOnes(mask);
+
+                if (nextWorkerIndex >= numOfWorkers) {
+                    smallestDistanceSum = Math.min(smallestDistanceSum, memo[mask]);
+                    continue;
+                }
+
+                for (int bikeIndex = 0; bikeIndex < numOfBikes; bikeIndex++) {
+                    if ((mask & (1 << bikeIndex)) == 0) {
+                        int newMask = (1 << bikeIndex) | mask;
+                        memo[newMask] = Math.min(memo[newMask],
+                                memo[mask] + findDistance(workers[nextWorkerIndex], bikes[bikeIndex]));
+                    }
+                }
+            }
+
+            return smallestDistanceSum;
+        }
+
+        public int assignBikes(int[][] workers, int[][] bikes) {
+            Arrays.fill(memo, Integer.MAX_VALUE);
+            return minimumDistanceSum(workers, bikes);
+        }
+    }
 }
