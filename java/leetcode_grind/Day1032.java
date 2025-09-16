a387e988059ebd3d7964e63e5030471e0138c7bb
54k <54k@bk.ru>
2025-09-16 11:03:42 +0200

    day 1032

--- java/leetcode_grind/Day1032.java
+++ java/leetcode_grind/Day1032.java
@@ -35,4 +35,47 @@ public class Day1032 {
         }
     }
 
+    // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+    static class Solution2 {
+        public int[] searchRange(int[] nums, int target) {
+            int firstOccurence = this.findBound(nums, target, true);
+
+            if (firstOccurence == -1) {
+                return new int[] { -1, -1 };
+            }
+
+            int lastOccurence = this.findBound(nums, target, false);
+            return new int[] { firstOccurence, lastOccurence };
+        }
+
+        int findBound(int[] nums, int target, boolean isFirst) {
+            int N = nums.length;
+            int begin = 0, end = N - 1;
+
+            while (begin <= end) {
+                int mid = (begin + end) / 2;
+
+                if (nums[mid] == target) {
+                    if (isFirst) {
+                        if (mid == begin || nums[mid - 1] != target) {
+                            return mid;
+                        }
+                        end = mid - 1;
+                    } else {
+                        if (mid == end || nums[mid + 1] != target) {
+                            return mid;
+                        }
+                        begin = mid + 1;
+                    }
+                } else if (nums[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    begin = mid + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+
 }
