373ce8b8ee9fe858c6690e9c5f4f87cde9041f76
54k <54k@bk.ru>
2025-09-21 11:19:19 +0200

    day 1037

--- java/leetcode_grind/Day1037.java
+++ java/leetcode_grind/Day1037.java
@@ -1,5 +1,6 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -58,4 +59,51 @@ public class Day1037 {
      * List<List<Integer>> param_4 = obj.report();
      */
 
+    // https://leetcode.com/problems/count-of-smaller-numbers-after-self/description/
+    static class Solution1 {
+        public List<Integer> countSmaller(int[] nums) {
+            int offset = 10000;
+            int size = 2 * 10000 + 1;
+            int[] tree = new int[size * 2];
+            List<Integer> result = new ArrayList<>();
+
+            for (int i = nums.length - 1; i >= 0; i--) {
+                int smallerCount = query(0, nums[i] + offset, tree, size);
+                result.add(smallerCount);
+                update(nums[i] + offset, 1, tree, size);
+            }
+            Collections.reverse(result);
+            return result;
+        }
+
+        void update(int index, int value, int[] tree, int size) {
+            index += size;
+            tree[index] += value;
+            while (index > 1) {
+                index /= 2;
+                tree[index] = tree[index * 2] + tree[index * 2 + 1];
+            }
+        }
+
+        int query(int left, int right, int[] tree, int size) {
+            int result = 0;
+            left += size;
+            right += size;
+
+            while (left < right) {
+                if (left % 2 == 1) {
+                    result += tree[left];
+                    left++;
+                }
+                left /= 2;
+
+                if (right % 2 == 1) {
+                    right--;
+                    result += tree[right];
+                }
+                right /= 2;
+            }
+            return result;
+        }
+    }
 }
