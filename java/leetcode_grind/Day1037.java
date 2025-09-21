c847617d52ba7417eb2d5e58eddea2835cae87f9
54k <54k@bk.ru>
2025-09-21 11:35:03 +0200

    day 1037

--- java/leetcode_grind/Day1037.java
+++ java/leetcode_grind/Day1037.java
@@ -140,4 +140,67 @@ public class Day1037 {
             return result;
         }
     }
+
+    static class Solution3 {
+        public List<Integer> countSmaller(int[] nums) {
+            int n = nums.length;
+            int[] result = new int[n];
+            int[] indices = new int[n]; // record the index. we are going to sort this array
+            for (int i = 0; i < n; i++) {
+                indices[i] = i;
+            }
+            // sort indices with their corresponding values in nums, i.e., nums[indices[i]]
+            mergeSort(indices, 0, n, result, nums);
+            // change int[] to List to return
+            List<Integer> resultToReturn = new ArrayList<Integer>(n);
+            for (int i : result) {
+                resultToReturn.add(i);
+            }
+            return resultToReturn;
+        }
+
+        private void mergeSort(int[] indices, int left, int right, int[] result, int[] nums) {
+            if (right - left <= 1) {
+                return;
+            }
+            int mid = (left + right) / 2;
+            mergeSort(indices, left, mid, result, nums);
+            mergeSort(indices, mid, right, result, nums);
+            merge(indices, left, right, mid, result, nums);
+        }
+
+        private void merge(int[] indices, int left, int right, int mid, int[] result, int[] nums) {
+            // merge [left, mid) and [mid, right)
+            int i = left; // current index for the left array
+            int j = mid; // current index for the right array
+            // use temp to temporarily store sorted array
+            List<Integer> temp = new ArrayList<Integer>(right - left);
+            while (i < mid && j < right) {
+                if (nums[indices[i]] <= nums[indices[j]]) {
+                    // j - mid numbers jump to the left side of indices[i]
+                    result[indices[i]] += j - mid;
+                    temp.add(indices[i]);
+                    i++;
+                } else {
+                    temp.add(indices[j]);
+                    j++;
+                }
+            }
+            // when one of the subarrays is empty
+            while (i < mid) {
+                // j - mid numbers jump to the left side of indices[i]
+                result[indices[i]] += j - mid;
+                temp.add(indices[i]);
+                i++;
+            }
+            while (j < right) {
+                temp.add(indices[j]);
+                j++;
+            }
+            // restore from temp
+            for (int k = left; k < right; k++) {
+                indices[k] = temp.get(k - left);
+            }
+        }
+    }
 }
