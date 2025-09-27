15dbca81340b8cea5ae98d54756466af86509078
54k <54k@bk.ru>
2025-09-27 17:27:01 +0200

    day 1043

--- java/leetcode_grind/Day1043.java
+++ java/leetcode_grind/Day1043.java
@@ -21,4 +21,25 @@ public class Day1043 {
         }
     }
 
+    // https://leetcode.com/problems/maximum-product-subarray/description/
+    static class Solution2 {
+        public int maxProduct(int[] nums) {
+            if (nums.length == 0)
+                return 0;
+
+            int max_so_far = nums[0];
+            int min_so_far = nums[0];
+            int result = max_so_far;
+
+            for (int i = 1; i < nums.length; i++) {
+                int curr = nums[i];
+                int temp_max = Math.max(curr, Math.max(max_so_far * curr, min_so_far * curr));
+                min_so_far = Math.min(curr, Math.min(max_so_far * curr, min_so_far * curr));
+                max_so_far = temp_max;
+                result = Math.max(max_so_far, result);
+            }
+
+            return result;
+        }
+    }
 }
