f7937dc3ffdaf2e645a265a0d6a058cad9448ed8
54k <54k@bk.ru>
2025-09-27 16:43:34 +0200

    day 1043

---
+++ java/leetcode_grind/Day1043.java
@@ -0,0 +1,24 @@
+package leetcode_grind;
+
+public class Day1043 {
+    // https://leetcode.com/problems/largest-triangle-area/description/?envType=daily-question&envId=2025-09-27
+    static class Solution1 {
+        public double largestTriangleArea(int[][] points) {
+            int N = points.length;
+            double ans = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; ++j) {
+                    for (int k = j + 1; k < N; ++k) {
+                        ans = Math.max(ans, area(points[i], points[j], points[k]));
+                    }
+                }
+            }
+            return ans;
+        }
+
+        double area(int[] P, int[] Q, int[] R) {
+            return 0.5 * Math.abs(P[0] * Q[1] + Q[0] * R[1] + R[0] * P[1] - P[1] * Q[0] - Q[1] * R[0] - R[1] * P[0]);
+        }
+    }
+
+}
