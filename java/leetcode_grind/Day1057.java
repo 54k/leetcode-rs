95323984be36407201c18ea2b6a7b7aee8806cb4
54k <54k@bk.ru>
2025-10-11 15:16:50 +0300

    day 1057

--- java/leetcode_grind/Day1057.java
+++ java/leetcode_grind/Day1057.java
@@ -38,4 +38,26 @@ public class Day1057 {
             return ans;
         }
     }
+
+    static class Solution2 {
+        public List<Integer> findClosestElements(int[] arr, int k, int x) {
+            int left = 0;
+            int right = arr.length - k;
+
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (x - arr[mid] > arr[mid + k] - x) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            List<Integer> result = new ArrayList<>();
+            for (int i = left; i < left + k; i++) {
+                result.add(arr[i]);
+            }
+            return result;
+        }
+    }
 }
