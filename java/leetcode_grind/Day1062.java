dfcb3a7f2ae4502141cead3c352902ce5124b190
54k <54k@bk.ru>
2025-10-16 13:08:24 +0300

    day 1062

---
+++ java/leetcode_grind/Day1062.java
@@ -0,0 +1,20 @@
+package leetcode_grind;
+
+public class Day1062 {
+    // https://leetcode.com/problems/smallest-missing-non-negative-integer-after-operations/description/?envType=daily-question&envId=2025-10-16
+    static class Solution1 {
+        public int findSmallestInteger(int[] nums, int value) {
+            int[] mp = new int[value];
+            for (int x : nums) {
+                int v = ((x % value) + value) % value;
+                mp[v]++;
+            }
+            int mex = 0;
+            while (mp[mex % value] > 0) {
+                mp[mex % value]--;
+                mex++;
+            }
+            return mex;
+        }
+    }
+}
