ea0eede46d8ac9718c32cfd8dfc1abcccce82418
54k <54k@bk.ru>
2025-10-20 12:16:16 +0300

    day 1066

--- java/leetcode_grind/Day1066.java
+++ java/leetcode_grind/Day1066.java
@@ -48,4 +48,30 @@ public class Day1066 {
             return Integer.toString(bulls) + "A" + Integer.toString(cows) + "B";
         }
     }
+
+    static class Solution3 {
+        public String getHint(String secret, String guess) {
+            Map<Character, Integer> h = new HashMap<>();
+
+            int bulls = 0, cows = 0;
+            int n = guess.length();
+            for (int idx = 0; idx < n; ++idx) {
+                char s = secret.charAt(idx);
+                char g = guess.charAt(idx);
+                if (s == g) {
+                    bulls++;
+                } else {
+                    if (h.getOrDefault(s, 0) < 0) {
+                        cows++;
+                    }
+                    if (h.getOrDefault(g, 0) > 0) {
+                        cows++;
+                    }
+                    h.put(s, h.getOrDefault(s, 0) + 1);
+                    h.put(g, h.getOrDefault(g, 0) - 1);
+                }
+            }
+            return Integer.toString(bulls) + "A" + Integer.toString(cows) + "B";
+        }
+    }
 }
