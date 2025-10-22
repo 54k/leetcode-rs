7d050ae8034492fd5d3d68379ed2880dfe779b00
54k <54k@bk.ru>
2025-10-22 16:09:48 +0300

    day 1068

--- java/leetcode_grind/Day1068.java
+++ java/leetcode_grind/Day1068.java
@@ -1,7 +1,9 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -110,8 +112,34 @@ public class Day1068 {
         }
     }
 
-    // Your SparseVector object will be instantiated and called as such:
-    // SparseVector v1 = new SparseVector(nums1);
-    // SparseVector v2 = new SparseVector(nums2);
-    // int ans = v1.dotProduct(v2);
+    static class SparseVector2 {
+        List<int[]> pairs;
+
+        SparseVector2(int[] nums) {
+            pairs = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    pairs.add(new int[] { i, nums[i] });
+                }
+            }
+        }
+
+        // Return the dotProduct of two sparse vectors
+        public int dotProduct(SparseVector2 vec) {
+            int result = 0, p = 0, q = 0;
+            while (p < pairs.size() && q < vec.pairs.size()) {
+                if (pairs.get(p)[0] == vec.pairs.get(q)[0]) {
+                    result += pairs.get(p)[1] * vec.pairs.get(q)[1];
+                    p++;
+                    q++;
+                } else if (pairs.get(p)[0] > vec.pairs.get(q)[0]) {
+                    q++;
+                } else {
+                    p++;
+                }
+            }
+            return result;
+        }
+    }
+
 }
