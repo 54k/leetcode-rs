f848821a04aa2872a752b3a5d70c22318d5a2edd
54k <54k@bk.ru>
2025-10-22 16:05:58 +0300

    day 1068

--- java/leetcode_grind/Day1068.java
+++ java/leetcode_grind/Day1068.java
@@ -84,4 +84,34 @@ public class Day1068 {
         }
     }
 
+    // https://leetcode.com/problems/dot-product-of-two-sparse-vectors/description/?envType=weekly-question&envId=2025-10-22
+    static class SparseVector1 {
+
+        Map<Integer, Integer> mapping;
+
+        SparseVector1(int[] nums) {
+            mapping = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    mapping.put(i, nums[i]);
+                }
+            }
+        }
+
+        // Return the dotProduct of two sparse vectors
+        public int dotProduct(SparseVector1 vec) {
+            int result = 0;
+            for (Integer i : this.mapping.keySet()) {
+                if (vec.mapping.containsKey(i)) {
+                    result += this.mapping.get(i) * vec.mapping.get(i);
+                }
+            }
+            return result;
+        }
+    }
+
+    // Your SparseVector object will be instantiated and called as such:
+    // SparseVector v1 = new SparseVector(nums1);
+    // SparseVector v2 = new SparseVector(nums2);
+    // int ans = v1.dotProduct(v2);
 }
