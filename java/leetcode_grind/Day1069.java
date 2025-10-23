ad7c238e71d97f41688f73af8f76ebf4ddca76a4
54k <54k@bk.ru>
2025-10-23 12:50:25 +0300

    day 1069

--- java/leetcode_grind/Day1069.java
+++ java/leetcode_grind/Day1069.java
@@ -1,5 +1,8 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day1069 {
     // https://leetcode.com/problems/check-if-digits-are-equal-in-string-after-operations-i/description/?envType=daily-question&envId=2025-10-23
     static class Solution1 {
@@ -16,4 +19,41 @@ public class Day1069 {
             return sArray[0] == sArray[1];
         }
     }
+
+    // https://leetcode.com/problems/my-calendar-ii/description/
+    static class MyCalendarTwo1 {
+        List<int[]> bookings;
+        List<int[]> overlapBookings;
+
+        boolean doesOverlap(int start1, int end1, int start2, int end2) {
+            return Math.max(start1, start2) < Math.min(end1, end2);
+        }
+
+        int[] getOverlapped(int start1, int end1, int start2, int end2) {
+            return new int[] { Math.max(start1, start2), Math.min(end1, end2) };
+        }
+
+        public MyCalendarTwo1() {
+            bookings = new ArrayList<>();
+            overlapBookings = new ArrayList<>();
+        }
+
+        public boolean book(int start, int end) {
+            for (int[] booking : overlapBookings) {
+                if (doesOverlap(booking[0], booking[1], start, end)) {
+                    return false;
+                }
+            }
+
+            for (int[] booking : bookings) {
+                if (doesOverlap(booking[0], booking[1], start, end)) {
+                    overlapBookings.add(getOverlapped(booking[0], booking[1], start, end));
+                }
+            }
+
+            bookings.add(new int[] { start, end });
+            return true;
+        }
+    }
+
 }
