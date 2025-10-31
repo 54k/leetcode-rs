cec1422b5bc57beaf8a657b8f9c62e7f73e042d7
54k <54k@bk.ru>
2025-10-31 18:12:24 +0300

    day 1077

--- java/leetcode_grind/Day1077.java
+++ java/leetcode_grind/Day1077.java
@@ -1,7 +1,13 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Day1077 {
@@ -123,15 +129,29 @@ public class Day1077 {
         }
     }
 
-    /**
-     * Your VideoSharingPlatform object will be instantiated and called as such:
-     * VideoSharingPlatform obj = new VideoSharingPlatform();
-     * int param_1 = obj.upload(video);
-     * obj.remove(videoId);
-     * String param_3 = obj.watch(videoId,startMinute,endMinute);
-     * obj.like(videoId);
-     * obj.dislike(videoId);
-     * int[] param_6 = obj.getLikesAndDislikes(videoId);
-     * int param_7 = obj.getViews(videoId);
-     */
+    // https://leetcode.com/problems/3sum/description/
+    static class Solution2 {
+        public List<List<Integer>> threeSum(int[] nums) {
+            Set<List<Integer>> res = new HashSet<>();
+            Set<Integer> dups = new HashSet<>();
+            Map<Integer, Integer> seen = new HashMap<>();
+            for (int i = 0; i < nums.length; ++i) {
+                if (dups.add(nums[i])) {
+                    for (int j = i + 1; j < nums.length; ++j) {
+                        int complement = -nums[i] - nums[j];
+                        if (seen.containsKey(complement) && seen.get(complement) == i) {
+                            List<Integer> triplet = Arrays.asList(
+                                    nums[i],
+                                    nums[j],
+                                    complement);
+                            Collections.sort(triplet);
+                            res.add(triplet);
+                        }
+                        seen.put(nums[j], i);
+                    }
+                }
+            }
+            return new ArrayList<>(res);
+        }
+    }
 }
