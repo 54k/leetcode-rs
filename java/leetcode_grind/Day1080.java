e7528a8f4218303c1d0e40595eeaeb8b343491e0
54k <54k@bk.ru>
2025-11-03 14:31:54 +0300

    day 1080

--- java/leetcode_grind/Day1080.java
+++ java/leetcode_grind/Day1080.java
@@ -22,4 +22,17 @@ public class Day1080 {
         }
     }
 
+    static class Solution2 {
+        public int minCost(String colors, int[] neededTime) {
+            int totalTime = 0, currMaxTime = 0;
+            for (int i = 0; i < colors.length(); i++) {
+                if (i > 0 && colors.charAt(i) != colors.charAt(i - 1)) {
+                    currMaxTime = 0;
+                }
+                totalTime += Math.min(currMaxTime, neededTime[i]);
+                currMaxTime = Math.max(currMaxTime, neededTime[i]);
+            }
+            return totalTime;
+        }
+    }
 }
