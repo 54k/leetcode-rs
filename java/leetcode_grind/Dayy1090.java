1de7ab717665f934d3ea1136b080466dce3e2786
54k <54k@bk.ru>
2025-11-13 15:41:30 +0300

    day 1090

---
+++ java/leetcode_grind/Dayy1090.java
@@ -0,0 +1,24 @@
+package leetcode_grind;
+
+public class Dayy1090 {
+    // https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/?envType=daily-question&envId=2025-11-13
+    static class Solution1 {
+        public int maxOperations(String s) {
+            int countOne = 0;
+            int ans = 0;
+            int i = 0;
+            while (i < s.length()) {
+                if (s.charAt(i) == '0') {
+                    while (i + 1 < s.length() && s.charAt(i + 1) == '0') {
+                        i++;
+                    }
+                    ans += countOne;
+                } else {
+                    countOne++;
+                }
+                i++;
+            }
+            return ans;
+        }
+    }
+}
