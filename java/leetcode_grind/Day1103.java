9258439e82fea2b6dd8a243baa74f363b88b4f5d
54k <54k@bk.ru>
2025-11-26 14:45:58 +0300

    day 1103

---
+++ java/leetcode_grind/Day1103.java
@@ -0,0 +1,32 @@
+package leetcode_grind;
+
+public class Day1103 {
+    // https://leetcode.com/problems/paths-in-matrix-whose-sum-is-divisible-by-k/description/?envType=daily-question&envId=2025-11-26
+    static class Solution1 {
+        static final int MOD = 1000_000_007;
+
+        public int numberOfPaths(int[][] grid, int k) {
+            int m = grid.length;
+            int n = grid[0].length;
+
+            long[][][] dp = new long[m + 1][n + 1][k];
+
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i == 1 && j == 1) {
+                        dp[i][j][grid[0][0] % k] = 1;
+                        continue;
+                    }
+
+                    int value = grid[i - 1][j - 1] % k;
+                    for (int r = 0; r < k; r++) {
+                        int prevMod = (r - value + k) % k;
+                        dp[i][j][r] = (dp[i - 1][j][prevMod] + dp[i][j - 1][prevMod]) % MOD;
+                    }
+                }
+            }
+
+            return (int) dp[m][n][0];
+        }
+    }
+}
