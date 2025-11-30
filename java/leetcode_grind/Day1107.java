2fa3acc3cb317a7c958772e8e39f2c763daf8fc1
54k <54k@bk.ru>
2025-11-30 16:19:00 +0300

    day 1107

---
+++ java/leetcode_grind/Day1107.java
@@ -0,0 +1,66 @@
+package leetcode_grind;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day1107 {
+    // https://leetcode.com/problems/make-sum-divisible-by-p/description/?envType=daily-question&envId=2025-11-30
+    static class Solution1 {
+        public int minSubarray(int[] nums, int p) {
+            int n = nums.length;
+            long totalSum = 0;
+            for (int num : nums) {
+                totalSum += num;
+            }
+
+            if (totalSum % p == 0)
+                return 0;
+
+            int minLen = n;
+            for (int start = 0; start < n; ++start) {
+                long subSum = 0;
+                for (int end = start; end < n; end++) {
+                    subSum += nums[end];
+                    long remainingSum = (totalSum - subSum) % p;
+                    if (remainingSum == 0) {
+                        minLen = Math.min(minLen, end - start + 1);
+                    }
+                }
+            }
+
+            return minLen == n ? -1 : minLen;
+        }
+    }
+
+    static class Solution2 {
+        public int minSubarray(int[] nums, int p) {
+            int n = nums.length;
+            int totalSum = 0;
+            for (int num : nums) {
+                totalSum = (totalSum + num) % p;
+            }
+
+            int target = totalSum % p;
+            if (target == 0) {
+                return 0;
+            }
+
+            Map<Integer, Integer> modMap = new HashMap<>();
+            modMap.put(0, -1);
+
+            int currentSum = 0;
+            int minLen = n;
+
+            for (int i = 0; i < n; i++) {
+                currentSum = (currentSum + nums[i]) % p;
+                int needed = (currentSum - target + p) % p;
+                if (modMap.containsKey(needed)) {
+                    minLen = Math.min(minLen, i - modMap.get(needed));
+                }
+                modMap.put(currentSum, i);
+            }
+            return minLen == n ? -1 : minLen;
+        }
+    }
+
+}
