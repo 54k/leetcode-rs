bcbf9f64f7b3b9d870f0f67df3dac7f53173c73c
54k <54k@bk.ru>
2025-12-01 11:36:01 +0300

    day 1108

--- java/leetcode_grind/Day1108.java
+++ java/leetcode_grind/Day1108.java
@@ -52,4 +52,53 @@ public class Day1108 {
             return left;
         }
     }
+
+    // https://leetcode.com/problems/two-sum-bsts/description/?envType=weekly-question&envId=2025-12-01
+    static public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution3 {
+        boolean binarySearch(TreeNode root2, int target2) {
+            if (root2 == null) {
+                return false;
+            }
+            if (root2.val == target2) {
+                return true;
+            } else if (root2.val > target2) {
+                return binarySearch(root2.left, target2);
+            } else {
+                return binarySearch(root2.right, target2);
+            }
+        }
+
+        boolean dfs(TreeNode root1, TreeNode root2, int target) {
+            if (root1 == null) {
+                return false;
+            }
+            if (binarySearch(root2, target - root1.val)) {
+                return true;
+            }
+            return dfs(root1.left, root2, target) || dfs(root1.right, root2, target);
+        }
+
+        public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+            return dfs(root1, root2, target);
+        }
+    }
 }
