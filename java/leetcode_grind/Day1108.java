892737c420efcb6c3a1380f9fd94a6507691161e
54k <54k@bk.ru>
2025-12-01 11:04:52 +0300

    day 1108

--- java/leetcode_grind/Day1108.java
+++ java/leetcode_grind/Day1108.java
@@ -26,4 +26,30 @@ public class Day1108 {
         }
     }
 
+    static class Solution2 {
+        public long maxRunTime(int n, int[] batteries) {
+            long sumPower = 0;
+            for (int power : batteries) {
+                sumPower += power;
+            }
+            long left = 1, right = sumPower / n;
+
+            while (left < right) {
+                long target = right - (right - left) / 2;
+                long extra = 0;
+
+                for (int power : batteries) {
+                    extra += Math.min(power, target);
+                }
+
+                if (extra >= (long) (n * target)) {
+                    left = target;
+                } else {
+                    right = target - 1;
+                }
+            }
+
+            return left;
+        }
+    }
 }
