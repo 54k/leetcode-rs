ad59098d53c68702d8e275b4c0f73b4770981853
54k <54k@bk.ru>
2025-12-08 15:05:10 +0300

    day 1115

--- java/leetcode_grind/Day1115.java
+++ java/leetcode_grind/Day1115.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Arrays;
+
 public class Day1115 {
     // https://leetcode.com/problems/count-square-sum-triples/description/?envType=daily-question&envId=2025-12-08
     static class Solution1 {
@@ -16,6 +18,29 @@ public class Day1115 {
                 }
             }
             return res;
+        }
+    }
+
+    // https://leetcode.com/problems/minimize-product-sum-of-two-arrays/description/?envType=weekly-question&envId=2025-12-08
+    static class Solution2 {
+        public int minProductSum(int[] nums1, int[] nums2) {
+            // Sort nums1 and nums2 in ascending order.
+            Arrays.sort(nums1);
+            Arrays.sort(nums2);
+
+            // Initialize sum as 0.
+            int ans = 0;
+            int n = nums2.length;
+
+            // Iterate over two sorted arrays and calculate the
+            // cumulative product sum.
+            for (int i = 0; i < n; ++i) {
+                // Since we also sort nums2 in ascending order,
+                // we need to iterate over nums2 in reverse order.
+                ans += nums1[i] * nums2[n - 1 - i];
+            }
+
+            return ans;
         }
     }
 }
