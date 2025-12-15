8b7eb75f68b9f036665a728b26fb3fbb2efbf360
54k <54k@bk.ru>
2025-12-15 15:44:19 +0300

    day 1122

--- java/leetcode_grind/Day1122.java
+++ java/leetcode_grind/Day1122.java
@@ -75,4 +75,29 @@ public class Day1122 {
             return length;
         }
     }
+
+    // https://leetcode.com/problems/number-of-ways-to-divide-a-long-corridor/description/?envType=daily-question&envId=2025-12-14
+    static class Solution4 {
+        public int numberOfWays(String corridor) {
+            final int MOD = 1_000_000_007;
+            int[][] count = new int[corridor.length() + 1][3];
+
+            count[corridor.length()][0] = 0;
+            count[corridor.length()][1] = 0;
+            count[corridor.length()][2] = 1;
+
+            for (int index = corridor.length() - 1; index >= 0; index--) {
+                if (corridor.charAt(index) == 'S') {
+                    count[index][0] = count[index + 1][1];
+                    count[index][1] = count[index + 1][2];
+                    count[index][2] = count[index + 1][1];
+                } else {
+                    count[index][0] = count[index + 1][0];
+                    count[index][1] = count[index + 1][1];
+                    count[index][2] = (count[index + 1][0] + count[index + 1][2]) % MOD;
+                }
+            }
+            return count[0][0];
+        }
+    }
 }
