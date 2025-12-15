934f2acffc9fd0ab8e58619b40324ea56f5b792d
54k <54k@bk.ru>
2025-12-15 15:30:31 +0300

    day 1122

--- java/leetcode_grind/Day1122.java
+++ java/leetcode_grind/Day1122.java
@@ -17,4 +17,37 @@ public class Day1122 {
             return ans;
         }
     }
+
+    // https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/description/?envType=weekly-question&envId=2025-12-15
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution {
+        public int longestConsecutive(TreeNode root) {
+            return dfs(root, null, 0);
+        }
+
+        int dfs(TreeNode p, TreeNode parent, int length) {
+            if (p == null)
+                return length;
+            length = (parent != null && p.val == parent.val + 1) ? length + 1 : 1;
+            return Math.max(length, Math.max(dfs(p.left, p, length), dfs(p.right, p, length)));
+        }
+    }
 }
