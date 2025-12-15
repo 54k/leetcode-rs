78e1a787ff2492bfe74c101f2870fd34e16cd8ea
54k <54k@bk.ru>
2025-12-15 15:33:45 +0300

    day 1122

--- java/leetcode_grind/Day1122.java
+++ java/leetcode_grind/Day1122.java
@@ -38,7 +38,7 @@ public class Day1122 {
         }
     }
 
-    static class Solution {
+    static class Solution2 {
         public int longestConsecutive(TreeNode root) {
             return dfs(root, null, 0);
         }
@@ -48,6 +48,31 @@ public class Day1122 {
                 return length;
             length = (parent != null && p.val == parent.val + 1) ? length + 1 : 1;
             return Math.max(length, Math.max(dfs(p.left, p, length), dfs(p.right, p, length)));
+        }
+    }
+
+    static class Solution3 {
+        int maxLength = 0;
+
+        public int longestConsecutive(TreeNode root) {
+            dfs(root);
+            return maxLength;
+        }
+
+        int dfs(TreeNode p) {
+            if (p == null)
+                return 0;
+            int L = dfs(p.left) + 1;
+            int R = dfs(p.right) + 1;
+            if (p.left != null && p.val + 1 != p.left.val) {
+                L = 1;
+            }
+            if (p.right != null && p.val + 1 != p.right.val) {
+                R = 1;
+            }
+            int length = Math.max(L, R);
+            maxLength = Math.max(maxLength, length);
+            return length;
         }
     }
 }
