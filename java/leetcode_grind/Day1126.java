cae0d9adbdfbe93bb58fa1544b6d28d684ce3068
54k <54k@bk.ru>
2025-12-19 14:38:09 +0300

    day 1126

--- java/leetcode_grind/Day1126.java
+++ java/leetcode_grind/Day1126.java
@@ -189,4 +189,64 @@ public class Day1126 {
             return ans;
         }
     }
+
+    // https://leetcode.com/problems/reachable-nodes-in-subdivided-graph/description/
+    static class Solution4 {
+        static class ANode {
+            int node, dist;
+
+            ANode(int n, int d) {
+                node = n;
+                dist = d;
+            }
+        }
+
+        public int reachableNodes(int[][] edges, int M, int N) {
+            Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+            for (int[] edge : edges) {
+                int u = edge[0], v = edge[1], w = edge[2];
+                graph.computeIfAbsent(u, x -> new HashMap<>()).put(v, w);
+                graph.computeIfAbsent(v, x -> new HashMap<>()).put(u, w);
+            }
+
+            PriorityQueue<ANode> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.dist, b.dist));
+            pq.offer(new ANode(0, 0));
+
+            Map<Integer, Integer> dist = new HashMap<>();
+            dist.put(0, 0);
+            Map<Integer, Integer> used = new HashMap<>();
+            int ans = 0;
+
+            while (!pq.isEmpty()) {
+                ANode anode = pq.poll();
+                int node = anode.node;
+                int d = anode.dist;
+
+                if (d > dist.getOrDefault(node, 0))
+                    continue;
+                ans++;
+
+                if (!graph.containsKey(node))
+                    continue;
+
+                for (int nei : graph.get(node).keySet()) {
+                    int weight = graph.get(node).get(nei);
+                    int v = Math.min(weight, M - d);
+                    used.put(N * node + nei, v);
+
+                    int d2 = d + weight + 1;
+                    if (d2 < dist.getOrDefault(nei, M + 1)) {
+                        pq.offer(new ANode(nei, d2));
+                        dist.put(nei, d2);
+                    }
+                }
+            }
+
+            for (int[] edge : edges) {
+                ans += Math.min(edge[2],
+                        used.getOrDefault(edge[0] * N + edge[1], 0) + used.getOrDefault(edge[1] * N + edge[0], 0));
+            }
+            return ans;
+        }
+    }
 }
