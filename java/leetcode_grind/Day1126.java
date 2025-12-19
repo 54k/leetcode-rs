f7984818ca051d5d24bb1b07c3ea47f867969080
54k <54k@bk.ru>
2025-12-19 14:03:08 +0300

    day 1126

--- java/leetcode_grind/Day1126.java
+++ java/leetcode_grind/Day1126.java
@@ -1,10 +1,16 @@
 package leetcode_grind;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Day1126 {
     // https://leetcode.com/problems/find-all-people-with-secret/description/?envType=daily-question&envId=2025-12-19
@@ -42,6 +48,60 @@ public class Day1126 {
             List<Integer> ans = new ArrayList<>();
             for (int i = 0; i < n; i++) {
                 if (visited[i]) {
+                    ans.add(i);
+                }
+            }
+            return ans;
+        }
+    }
+
+    static class Solution2 {
+        public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+            Arrays.sort(meetings, (a, b) -> a[2] - b[2]);
+            Map<Integer, List<int[]>> sameTimeMeetings = new TreeMap<>();
+            for (int[] meeting : meetings) {
+                int x = meeting[0], y = meeting[1], t = meeting[2];
+                sameTimeMeetings.computeIfAbsent(t, k -> new ArrayList<>()).add(new int[] { x, y });
+            }
+
+            boolean[] knowsSecret = new boolean[n];
+            knowsSecret[0] = true;
+            knowsSecret[firstPerson] = true;
+
+            for (int t : sameTimeMeetings.keySet()) {
+                Map<Integer, List<Integer>> meet = new HashMap<>();
+                for (int[] meeting : sameTimeMeetings.get(t)) {
+                    int x = meeting[0], y = meeting[1];
+                    meet.computeIfAbsent(x, k -> new ArrayList<>()).add(y);
+                    meet.computeIfAbsent(y, k -> new ArrayList<>()).add(x);
+                }
+
+                Set<Integer> start = new HashSet<>();
+                for (int[] meeting : sameTimeMeetings.get(t)) {
+                    int x = meeting[0], y = meeting[1];
+                    if (knowsSecret[x]) {
+                        start.add(x);
+                    }
+                    if (knowsSecret[y]) {
+                        start.add(y);
+                    }
+                }
+
+                Queue<Integer> q = new LinkedList<>(start);
+                while (!q.isEmpty()) {
+                    int person = q.poll();
+                    for (int nextPerson : meet.getOrDefault(person, new ArrayList<>())) {
+                        if (!knowsSecret[nextPerson]) {
+                            knowsSecret[nextPerson] = true;
+                            q.offer(nextPerson);
+                        }
+                    }
+                }
+            }
+
+            List<Integer> ans = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                if (knowsSecret[i]) {
                     ans.add(i);
                 }
             }
