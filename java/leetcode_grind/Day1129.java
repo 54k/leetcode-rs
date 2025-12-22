379652fa07cb41e676d61cfb80954d504533e25d
54k <54k@bk.ru>
2025-12-22 12:20:52 +0300

    day 1129

---
+++ java/leetcode_grind/Day1129.java
@@ -0,0 +1,32 @@
+package leetcode_grind;
+
+import java.util.Arrays;
+
+public class Day1129 {
+    // https://leetcode.com/problems/delete-columns-to-make-sorted-iii/description/?envType=daily-question&envId=2025-12-22
+    static class Solution1 {
+        public int minDeletionSize(String[] A) {
+            int W = A[0].length();
+            int[] dp = new int[W];
+            Arrays.fill(dp, 1);
+            for (int i = W - 2; i >= 0; i--) {
+                search: for (int j = i + 1; j < W; j++) {
+                    for (String row : A) {
+                        if (row.charAt(i) > row.charAt(j)) {
+                            continue search;
+                        }
+                    }
+
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+
+            int kept = 0;
+            for (int x : dp) {
+                kept = Math.max(kept, x);
+            }
+            return W - kept;
+        }
+    }
+
+}
