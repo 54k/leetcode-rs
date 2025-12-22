e336e943bb539711333495b7eca9a2bc73928454
54k <54k@bk.ru>
2025-12-22 12:50:30 +0300

    day 1129

--- java/leetcode_grind/Day1129.java
+++ java/leetcode_grind/Day1129.java
@@ -29,4 +29,59 @@ public class Day1129 {
         }
     }
 
+    // https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/description/?envType=weekly-question&envId=2025-12-22
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution2 {
+        int maxval = 0;
+
+        public int longestConsecutive(TreeNode root) {
+            longestPath(root);
+            return maxval;
+        }
+
+        int[] longestPath(TreeNode root) {
+            if (root == null) {
+                return new int[] { 0, 0 };
+            }
+
+            int inr = 1, dcr = 1;
+            if (root.left != null) {
+                int[] left = longestPath(root.left);
+                if (root.val == root.left.val + 1) {
+                    dcr = left[1] + 1;
+                } else if (root.val == root.left.val - 1) {
+                    inr = left[0] + 1;
+                }
+            }
+
+            if (root.right != null) {
+                int[] right = longestPath(root.right);
+                if (root.val == root.right.val + 1) {
+                    dcr = Math.max(dcr, right[1] + 1);
+                } else if (root.val == root.right.val - 1) {
+                    inr = Math.max(inr, right[0] + 1);
+                }
+            }
+            maxval = Math.max(maxval, dcr + inr - 1);
+            return new int[] { inr, dcr };
+        }
+    }
 }
