eb65f8c5f86a982ee95bd898a09d7e2f273ae236
54k <54k@bk.ru>
2025-12-26 17:43:19 +0300

    day 1133

--- java/leetcode_grind/Day1133.java
+++ java/leetcode_grind/Day1133.java
@@ -4,6 +4,37 @@ public class Day1133 {
     // https://leetcode.com/problems/minimum-penalty-for-a-shop/description/?envType=daily-question&envId=2025-12-26
     static class Solution1 {
         public int bestClosingTime(String customers) {
+            int curPenalty = 0;
+            for (int i = 0; i < customers.length(); i++) {
+                if (customers.charAt(i) == 'Y') {
+                    curPenalty++;
+                }
+            }
+
+            int minPenalty = curPenalty;
+            int earliestHour = 0;
+
+            for (int i = 0; i < customers.length(); i++) {
+                char ch = customers.charAt(i);
+
+                if (ch == 'Y') {
+                    curPenalty--;
+                } else {
+                    curPenalty++;
+                }
+
+                if (curPenalty < minPenalty) {
+                    earliestHour = i + 1;
+                    minPenalty = curPenalty;
+                }
+            }
+
+            return earliestHour;
+        }
+    }
+
+    static class Solution2 {
+        public int bestClosingTime(String customers) {
             int minPenalty = 0, curPenalty = 0;
             int earliestHour = 0;
 
