174868fdbaae84511bc82fccf8fa9c0a28acce29
54k <54k@bk.ru>
2026-01-02 12:43:26 +0300

    day 1140

--- java/leetcode_grind/Day1140.java
+++ java/leetcode_grind/Day1140.java
@@ -1,5 +1,10 @@
 package leetcode_grind;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class Day1140 {
     // https://leetcode.com/problems/n-repeated-element-in-size-2n-array/description/?envType=daily-question&envId=2026-01-02
     static class Solution1 {
@@ -14,4 +19,43 @@ public class Day1140 {
             throw null;
         }
     }
+
+    // https://leetcode.com/problems/design-a-leaderboard/description/?envType=weekly-question&envId=2026-01-01
+    static class Leaderboard1 {
+        Map<Integer, Integer> scores;
+
+        public Leaderboard1() {
+            scores = new HashMap<>();
+        }
+
+        public void addScore(int playerId, int score) {
+            if (!this.scores.containsKey(playerId)) {
+                this.scores.put(playerId, 0);
+            }
+            this.scores.put(playerId, this.scores.get(playerId) + score);
+        }
+
+        public int top(int K) {
+            PriorityQueue<Map.Entry<Integer, Integer>> heap = new PriorityQueue<>(
+                    (a, b) -> a.getValue() - b.getValue());
+            for (Map.Entry<Integer, Integer> entry : this.scores.entrySet()) {
+                heap.offer(entry);
+                if (heap.size() > K) {
+                    heap.poll();
+                }
+            }
+
+            int total = 0;
+            Iterator<Map.Entry<Integer, Integer>> value = heap.iterator();
+            while (value.hasNext()) {
+                total += value.next().getValue();
+            }
+            return total;
+        }
+
+        public void reset(int playerId) {
+            this.scores.put(playerId, 0);
+        }
+    }
+
 }
