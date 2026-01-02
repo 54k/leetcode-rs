118bde9239e6ced76a6c40553101588c90328a54
54k <54k@bk.ru>
2026-01-02 12:51:13 +0300

    day 1140

--- java/leetcode_grind/Day1140.java
+++ java/leetcode_grind/Day1140.java
@@ -1,9 +1,11 @@
 package leetcode_grind;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 public class Day1140 {
     // https://leetcode.com/problems/n-repeated-element-in-size-2n-array/description/?envType=daily-question&envId=2026-01-02
@@ -55,6 +57,71 @@ public class Day1140 {
 
         public void reset(int playerId) {
             this.scores.put(playerId, 0);
+        }
+    }
+
+    static class Leaderboard2 {
+
+        Map<Integer, Integer> scores;
+        TreeMap<Integer, Integer> sortedScores;
+
+        public Leaderboard2() {
+            scores = new HashMap<>();
+            sortedScores = new TreeMap<>(Collections.reverseOrder());
+        }
+
+        public void addScore(int playerId, int score) {
+            if (!scores.containsKey(playerId)) {
+                scores.put(playerId, score);
+                sortedScores.put(score, sortedScores.getOrDefault(score, 0) + 1);
+            } else {
+                int preScore = scores.get(playerId);
+                int playerCount = sortedScores.get(preScore);
+
+                if (playerCount == 1) {
+                    sortedScores.remove(preScore);
+                } else {
+                    sortedScores.put(preScore, playerCount - 1);
+                }
+
+                int newScore = preScore + score;
+                scores.put(playerId, newScore);
+                sortedScores.put(newScore, sortedScores.getOrDefault(newScore, 0) + 1);
+            }
+        }
+
+        public int top(int K) {
+            int count = 0;
+            int sum = 0;
+
+            for (Map.Entry<Integer, Integer> entry : sortedScores.entrySet()) {
+                int times = entry.getValue();
+                int key = entry.getKey();
+
+                for (int i = 0; i < times; i++) {
+                    sum += key;
+                    count++;
+
+                    if (count == K) {
+                        break;
+                    }
+                }
+
+                if (count == K) {
+                    break;
+                }
+            }
+
+            return sum;
+        }
+
+        public void reset(int playerId) {
+            int preScore = scores.get(playerId);
+            sortedScores.put(preScore, sortedScores.get(preScore) - 1);
+            if (sortedScores.get(preScore) == 0) {
+                sortedScores.remove(preScore);
+            }
+            scores.remove(playerId);
         }
     }
 
