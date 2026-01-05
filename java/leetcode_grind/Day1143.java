4ced023defe40a42c3e940baa00a7f2c32cbd5bc
54k <54k@bk.ru>
2026-01-05 14:19:26 +0300

    day 1143

---
+++ java/leetcode_grind/Day1143.java
@@ -0,0 +1,28 @@
+package leetcode_grind;
+
+public class Day1143 {
+    // https://leetcode.com/problems/maximum-matrix-sum/description/?envType=daily-question&envId=2026-01-05
+    static class Solution1 {
+        public long maxMatrixSum(int[][] matrix) {
+            long totalSum = 0;
+            int minAbsVal = Integer.MAX_VALUE;
+            int negativeCount = 0;
+
+            for (int[] row : matrix) {
+                for (int val : row) {
+                    totalSum += Math.abs(val);
+                    if (val < 0) {
+                        negativeCount++;
+                    }
+                    minAbsVal = Math.min(minAbsVal, Math.abs(val));
+                }
+            }
+
+            if (negativeCount % 2 != 0) {
+                totalSum -= 2 * minAbsVal;
+            }
+
+            return totalSum;
+        }
+    }
+}
