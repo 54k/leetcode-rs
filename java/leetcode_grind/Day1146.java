4c98ef484dfa9119ea19369a5414e43d5cf1111b
54k <54k@bk.ru>
2026-01-08 12:06:18 +0300

    day 1146

--- java/leetcode_grind/Day1146.java
+++ java/leetcode_grind/Day1146.java
@@ -48,6 +48,40 @@ public class Day1146 {
         }
     }
 
-    
+    static class Solution2 {
+        public int maxDotProduct(int[] nums1, int[] nums2) {
+            int firstMax = Integer.MIN_VALUE;
+            int secondMax = Integer.MIN_VALUE;
+            int firstMin = Integer.MAX_VALUE;
+            int secondMin = Integer.MAX_VALUE;
+
+            for (int num : nums1) {
+                firstMax = Math.max(firstMax, num);
+                firstMin = Math.min(firstMin, num);
+            }
+
+            for (int num : nums2) {
+                secondMax = Math.max(secondMax, num);
+                secondMin = Math.min(secondMin, num);
+            }
+
+            if (firstMax < 0 && secondMin > 0) {
+                return firstMax * secondMin;
+            }
+
+            if (firstMin > 0 && secondMax < 0) {
+                return firstMin * secondMax;
+            }
+
+            int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+            for (int i = nums1.length - 1; i >= 0; i--) {
+                for (int j = nums2.length - 1; j >= 0; j--) {
+                    int use = nums1[i] * nums2[j] + dp[i + 1][j + 1];
+                    dp[i][j] = Math.max(use, Math.max(dp[i + 1][j], dp[i][j + 1]));
+                }
+            }
+            return dp[0][0];
+        }
+    }
 
 }
