605c954085d32c21194d65689fcc9af90c6d064d
54k <54k@bk.ru>
2026-01-09 03:14:10 +0300

    day 1147

---
+++ java/leetcode_grind/Day1147.java
@@ -0,0 +1,93 @@
+package leetcode_grind;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day1147 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/description/?envType=daily-question&envId=2026-01-09
+    static class Solution1 {
+        Map<TreeNode, Integer> depth;
+        int max_depth;
+
+        public TreeNode subtreeWithAllDeepest(TreeNode root) {
+            depth = new HashMap<>();
+            depth.put(null, -1);
+            dfs(root, null);
+            max_depth = -1;
+            for (Integer d : depth.values()) {
+                max_depth = Math.max(max_depth, d);
+            }
+            return answer(root);
+        }
+
+        void dfs(TreeNode node, TreeNode parent) {
+            if (node != null) {
+                depth.put(node, depth.get(parent) + 1);
+                dfs(node.left, node);
+                dfs(node.right, node);
+            }
+        }
+
+        TreeNode answer(TreeNode node) {
+            if (node == null || depth.get(node) == max_depth) {
+                return node;
+            }
+            TreeNode L = answer(node.left);
+            TreeNode R = answer(node.right);
+
+            if (L != null && R != null)
+                return node;
+            if (L != null)
+                return L;
+            if (R != null)
+                return R;
+            return null;
+        }
+    }
+
+    static class Solution2 {
+        static class Result {
+            TreeNode node;
+            int dist;
+
+            Result(TreeNode n, int d) {
+                node = n;
+                dist = d;
+            }
+        }
+
+        Result dfs(TreeNode node) {
+            if (node == null)
+                return new Result(null, 0);
+            Result L = dfs(node.left), R = dfs(node.right);
+            if (L.dist > R.dist)
+                return new Result(L.node, L.dist + 1);
+            if (L.dist < R.dist)
+                return new Result(R.node, R.dist + 1);
+            return new Result(node, L.dist + 1);
+        }
+
+        public TreeNode subtreeWithAllDeepest(TreeNode root) {
+            return dfs(root).node;
+        }
+    }
+}
