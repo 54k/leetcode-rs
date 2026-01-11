664ace5bfe1de709fe5db523b0a60266d038df35
54k <54k@bk.ru>
2026-01-11 16:41:48 +0300

    day 1149

--- java/leetcode_grind/Day1149.java
+++ java/leetcode_grind/Day1149.java
@@ -1,5 +1,7 @@
 package leetcode_grind;
 
+import java.util.Stack;
+
 public class Day1149 {
     // https://leetcode.com/problems/maximal-rectangle/description/?envType=daily-question&envId=2026-01-11
     static class Solution1 {
@@ -28,4 +30,38 @@ public class Day1149 {
         }
     }
 
+    static class Solution2 {
+        int leetcode84(int[] heights) {
+            Stack<Integer> stack = new Stack<>();
+            stack.push(-1);
+            int maxarea = 0;
+            for (int i = 0; i < heights.length; i++) {
+                while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
+                    maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
+                }
+                stack.push(i);
+            }
+
+            while (stack.peek() != -1) {
+                maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+            }
+
+            return maxarea;
+        }
+
+        public int maximalRectangle(char[][] matrix) {
+            if (matrix.length == 0)
+                return 0;
+            int maxarea = 0;
+            int[] dp = new int[matrix[0].length];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    dp[j] = matrix[i][j] == '1' ? dp[j] + 1 : 0;
+                }
+
+                maxarea = Math.max(maxarea, leetcode84(dp));
+            }
+            return maxarea;
+        }
+    }
 }
