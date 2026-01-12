003e163322ac0a7a19be25468c0c901eb486ce87
54k <54k@bk.ru>
2026-01-12 10:31:23 +0300

    day 1150

---
+++ java/leetcode_grind/Day1150.java
@@ -0,0 +1,20 @@
+package leetcode_grind;
+
+public class Day1150 {
+    // https://leetcode.com/problems/minimum-time-visiting-all-points/description/?envType=daily-question&envId=2026-01-12
+    static class Solution1 {
+        public int minTimeToVisitAllPoints(int[][] points) {
+            int ans = 0;
+            for (int i = 0; i < points.length - 1; i++) {
+                int currX = points[i][0];
+                int currY = points[i][1];
+                int targetX = points[i + 1][0];
+                int targetY = points[i + 1][1];
+
+                ans += Math.max(Math.abs(targetX - currX), Math.abs(targetY - currY));
+            }
+            return ans;
+        }
+    }
+
+}
