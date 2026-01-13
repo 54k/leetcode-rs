ffb5d88e9994c3a7acb74d4e95e515d82aa9b76c
54k <54k@bk.ru>
2026-01-13 15:45:14 +0300

    day 1151

--- java/leetcode_grind/Day1151.java
+++ java/leetcode_grind/Day1151.java
@@ -1,5 +1,8 @@
 package leetcode_grind;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day1151 {
     // https://leetcode.com/problems/separate-squares-i/description/?envType=daily-question&envId=2026-01-13
     static class Solution1 {
@@ -43,4 +46,43 @@ public class Day1151 {
         }
     }
 
+    static class Solution2 {
+        public double separateSquares(int[][] squares) {
+            long totalArea = 0;
+            List<int[]> events = new ArrayList<>();
+
+            for (int[] sq : squares) {
+                int y = sq[1];
+                int l = sq[2];
+                totalArea += (long) l * l;
+                events.add(new int[] { y, l, 1 });
+                events.add(new int[] { y + l, l, -1 });
+            }
+
+            events.sort((a, b) -> Integer.compare(a[0], b[0]));
+
+            double coveredWidth = 0;
+            double curArea = 0;
+            double prevHeight = 0;
+
+            for (int[] event : events) {
+                int y = event[0];
+                int l = event[1];
+                int delta = event[2];
+
+                int diff = y - (int) prevHeight;
+                double area = coveredWidth * diff;
+
+                if (2L * (curArea + area) >= totalArea) {
+                    return (prevHeight + (totalArea - 2.0 * curArea) / (2.0 * coveredWidth));
+                }
+
+                coveredWidth += delta * l;
+                curArea += area;
+                prevHeight = y;
+            }
+
+            return 0.0;
+        }
+    }
 }
