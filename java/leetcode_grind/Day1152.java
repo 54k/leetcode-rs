45ec33ea332ff535aa8153ac12fa6495ce59575e
54k <54k@bk.ru>
2026-01-14 18:08:02 +0300

    day 1152

--- java/leetcode_grind/Day1152.java
+++ java/leetcode_grind/Day1152.java
@@ -2,6 +2,7 @@ package leetcode_grind;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -183,6 +184,52 @@ public class Day1152 {
                     }
                 }
             }
+            ans %= 1_000_000_007;
+            return (int) ans;
+        }
+    }
+
+    static class Solution3 {
+        public int rectangleArea(int[][] rectangles) {
+            int OPEN = 0, CLOSE = 1;
+            int[][] events = new int[rectangles.length * 2][];
+            int t = 0;
+            for (int[] rec : rectangles) {
+                events[t++] = new int[] { rec[1], OPEN, rec[0], rec[2] };
+                events[t++] = new int[] { rec[3], CLOSE, rec[0], rec[2] };
+            }
+            Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+
+            List<int[]> active = new ArrayList<>();
+            int cur_y = events[0][0];
+            long ans = 0;
+            for (int[] event : events) {
+                int y = event[0], typ = event[1], x1 = event[2], x2 = event[3];
+                long query = 0;
+                int cur = -1;
+                for (int[] xs : active) {
+                    cur = Math.max(cur, xs[0]);
+                    query += Math.max(xs[1] - cur, 0);
+                    cur = Math.max(cur, xs[1]);
+                }
+
+                ans += query * (y - cur_y);
+
+                if (typ == OPEN) {
+                    active.add(new int[] { x1, x2 });
+                    Collections.sort(active, (a, b) -> Integer.compare(a[0], b[0]));
+                } else {
+                    for (int i = 0; i < active.size(); i++) {
+                        if (active.get(i)[0] == x1 && active.get(i)[1] == x2) {
+                            active.remove(i);
+                            break;
+                        }
+                    }
+                }
+
+                cur_y = y;
+            }
+
             ans %= 1_000_000_007;
             return (int) ans;
         }
