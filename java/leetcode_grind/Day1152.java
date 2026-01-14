74a08f89f69f00c1cd0d13c15bd3d8c83ea85da2
54k <54k@bk.ru>
2026-01-14 17:56:23 +0300

    day 1152

--- java/leetcode_grind/Day1152.java
+++ java/leetcode_grind/Day1152.java
@@ -1,7 +1,11 @@
 package leetcode_grind;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -130,6 +134,57 @@ public class Day1152 {
                 }
             }
             return result;
+        }
+    }
+
+    // https://leetcode.com/problems/rectangle-area-ii/description/
+    static class Solution2 {
+        public int rectangleArea(int[][] rectangles) {
+            int N = rectangles.length;
+            Set<Integer> Xvals = new HashSet<>();
+            Set<Integer> Yvals = new HashSet<>();
+
+            for (int[] rec : rectangles) {
+                Xvals.add(rec[0]);
+                Xvals.add(rec[2]);
+                Yvals.add(rec[1]);
+                Yvals.add(rec[3]);
+            }
+
+            Integer[] imapx = Xvals.toArray(new Integer[0]);
+            Arrays.sort(imapx);
+            Integer[] imapy = Yvals.toArray(new Integer[0]);
+            Arrays.sort(imapy);
+
+            Map<Integer, Integer> mapx = new HashMap<>();
+            Map<Integer, Integer> mapy = new HashMap<>();
+
+            for (int i = 0; i < imapx.length; i++) {
+                mapx.put(imapx[i], i);
+            }
+            for (int i = 0; i < imapy.length; i++) {
+                mapy.put(imapy[i], i);
+            }
+
+            boolean[][] grid = new boolean[imapx.length][imapy.length];
+            for (int[] rec : rectangles) {
+                for (int x = mapx.get(rec[0]); x < mapx.get(rec[2]); ++x) {
+                    for (int y = mapy.get(rec[1]); y < mapy.get(rec[3]); ++y) {
+                        grid[x][y] = true;
+                    }
+                }
+            }
+
+            long ans = 0;
+            for (int x = 0; x < grid.length; x++) {
+                for (int y = 0; y < grid[0].length; y++) {
+                    if (grid[x][y]) {
+                        ans += (long) (imapx[x + 1] - imapx[x]) * (imapy[y + 1] - imapy[y]);
+                    }
+                }
+            }
+            ans %= 1_000_000_007;
+            return (int) ans;
         }
     }
 
